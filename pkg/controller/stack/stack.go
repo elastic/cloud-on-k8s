09121a760a0a2555c4140f362e28ad109c3e0327
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-11-02 17:47:35 +0100

    Remove Elasticsearch reference from comment

--- pkg/controller/stack/stack.go
+++ pkg/controller/stack/stack.go
@@ -1,6 +1,6 @@
 package stack
 
-// StackID returns the Elasticsearch cluster id
+// StackID returns the qualified identifier for this stack deployment
 // based on the given namespace and stack name, following
 // the convention: <namespace>-<stack name>
 func StackID(namespace string, stackName string) string {
