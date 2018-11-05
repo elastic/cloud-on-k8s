a6f3e977780d7e167d65b81f83e88cb8076399c5
sebgl <contact.sebgl@gmail.com>
2018-11-05 14:30:59 +0100

    Setup emptyDir for logs and data storage
    This associates an emptyDir to all ES pods, to store data and logs.
    Quota is not considered yet.

---
+++ pkg/controller/stack/elasticsearch/volume.go
@@ -0,0 +1,71 @@
+package elasticsearch
+
+import (
+	"path/filepath"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+// Default values for the volume name and paths
+const (
+	defaultVolumeName = "volume"
+	defaultMountPath  = "/volume"
+	defaultDataSubDir = "data"
+	defaultLogsSubDir = "logs"
+)
+
+// Volume is a storage unit for an ES node,
+// to be mounted into the pod
+type Volume interface {
+	Volume() corev1.Volume
+	VolumeMount() corev1.VolumeMount
+	DataPath() string
+	LogsPath() string
+}
+
+// EmptyDirVolume used to store ES data on the node main disk
+// Its lifecycle is bound to the pod lifecycle on the node.
+type EmptyDirVolume struct {
+	name       string
+	mountPath  string
+	dataSubDir string
+	logsSubDir string
+}
+
+// NewDefaultEmptyDirVolume creates an EmptyDirVolume with default values
+func NewDefaultEmptyDirVolume() Volume {
+	return EmptyDirVolume{
+		name:       defaultVolumeName,
+		mountPath:  defaultMountPath,
+		dataSubDir: defaultDataSubDir,
+		logsSubDir: defaultLogsSubDir,
+	}
+}
+
+// Volume returns the associated k8s volume
+func (v EmptyDirVolume) Volume() corev1.Volume {
+	return corev1.Volume{
+		Name: v.name,
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{},
+		},
+	}
+}
+
+// VolumeMount returns the associated k8s volume mount
+func (v EmptyDirVolume) VolumeMount() corev1.VolumeMount {
+	return corev1.VolumeMount{
+		MountPath: v.mountPath,
+		Name:      v.name,
+	}
+}
+
+// DataPath returns the absolute path to the directory storing ES data
+func (v EmptyDirVolume) DataPath() string {
+	return filepath.Join(v.mountPath, v.dataSubDir)
+}
+
+// LogsPath returns the absolute path to the directory storing ES logs
+func (v EmptyDirVolume) LogsPath() string {
+	return filepath.Join(v.mountPath, v.logsSubDir)
+}
