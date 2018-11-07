b7c18e6d33f79afaa14542048cc3767658a51ea1
Marc Lopez Rubio <marc5.12@outlook.com>
2018-11-07 15:11:11 +0000

    dev: Add GKE as a possible development target (#75)
    This change adds the ability to develop and run the operator to use a
    GKE cluster. Defaults to use `europe-west3` as the region and deploys a
    Kubernetes node per each zone (3 by default).
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ hack/gcp-k8s-cluster.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script takes responsibility to create a GCP GKE cluster. It has all
+# of the necessary default settings so that no positional arguments have to
+# be specified.
+
+GCLOUD_PROJECT=${1:-elastic-cloud-dev}
+GKE_CLUSTER_NAME=${2:-${USER}-dev-cluster}
+GKE_CLUSTER_REGION=${3:-europe-west3}
+GKE_ADMIN_USERNAME=${4:-admin}
+GKE_CLUSTER_VERSION=${5:-1.11.2-gke.15}
+GKE_MACHINE_TYPE=${6:-n1-highmem-4}
+GKE_LOCAL_SSD_COUNT=${7:-1}
+GKE_NODE_COUNT_PER_ZONE=${8:-1}
+GKE_GCP_SCOPES='https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append'
+
+
+if gcloud beta container clusters --project "${GCLOUD_PROJECT}" describe --region "${GKE_CLUSTER_REGION}" "${GKE_CLUSTER_NAME}" > /dev/null 2>&1; then
+    echo "-> GKE cluster is running."
+    exit 0
+fi
+
+# Don't use the _stable_ API to be able to use beta features
+gcloud config set container/use_v1_api_client false
+
+# Create cluster
+echo "-> Creating GKE cluster..."
+gcloud beta container --project ${GCLOUD_PROJECT} clusters create ${GKE_CLUSTER_NAME} \
+    --region "${GKE_CLUSTER_REGION}" --username "${GKE_ADMIN_USERNAME}" --cluster-version "${GKE_CLUSTER_VERSION}" \
+    --machine-type "${GKE_MACHINE_TYPE}" --image-type "COS" --disk-type "pd-ssd" --disk-size "30" \
+    --local-ssd-count "${GKE_LOCAL_SSD_COUNT}" --scopes "${GKE_GCP_SCOPES}" --num-nodes "${GKE_NODE_COUNT_PER_ZONE}" \
+    --enable-cloud-logging --enable-cloud-monitoring --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+    --no-enable-autoupgrade --no-enable-autorepair --network "projects/elastic-cloud-dev/global/networks/default" \
+    --subnetwork "projects/elastic-cloud-dev/regions/europe-west3/subnetworks/default"
