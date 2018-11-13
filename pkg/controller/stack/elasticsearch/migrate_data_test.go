b562be3f36b31b855af0aac357bf6ca460ca3ac8
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-11-13 13:00:47 +0100

    Fix allocation filter timestamp fix

--- pkg/controller/stack/elasticsearch/migrate_data_test.go
+++ pkg/controller/stack/elasticsearch/migrate_data_test.go
@@ -1,7 +1,10 @@
 package elasticsearch
 
 import (
+	"context"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/elastic/stack-operators/pkg/controller/stack/elasticsearch/client"
 	"github.com/stretchr/testify/assert"
@@ -47,4 +50,52 @@ func TestCopyIsInitializing(t *testing.T) {
 		client.Shard{Index: "index-1", Shard: 0, State: client.INITIALIZING, Node: "B"},
 	}
 	assert.True(t, nodeIsMigratingData("A", shards), "shard copy exists but is still initializing")
+}
+
+type mockClient struct {
+	t    *testing.T
+	seen []string
+}
+
+func (m *mockClient) ExcludeFromShardAllocation(context context.Context, nodes string) error {
+	m.seen = append(m.seen, nodes)
+	return nil
+}
+
+func (m *mockClient) getAndReset() []string {
+	v := m.seen
+	m.seen = []string{}
+	return v
+}
+
+func TestMigrateData(t *testing.T) {
+	now := time.Now()
+	tests := []struct {
+		name  string
+		input []string
+		want  string
+	}{
+		{
+			name:  "no indices to migrate",
+			input: []string{},
+			want:  "none_excluded",
+		},
+		{
+			name:  "one index to migrate",
+			input: []string{"test-index"},
+			want:  "test-index," + strconv.FormatInt(now.Unix(), 10),
+		},
+		{
+			name:  "multiple indices to migrate",
+			input: []string{"test-index1", "test-index2"},
+			want:  "test-index1,test-index2," + strconv.FormatInt(now.Unix(), 10),
+		},
+
+	}
+
+	for _, tt := range tests {
+		client := &mockClient{t: t,}
+		setAllocationExcludes(client, tt.input, now)
+		assert.Contains(t, client.getAndReset(), tt.want)
+	}
 }
