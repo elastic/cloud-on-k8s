7e2ba7789eb14846ecd1f883452be62f42375de2
sebgl <contact.sebgl@gmail.com>
2018-11-21 16:11:44 +0100

    Create PV provisioner

---
+++ local-volume/cmd/provisioner/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/elastic/localvolume/pkg/provisioner"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Short: "Run the local volume provisioner",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := provisioner.Start()
+		if err != nil {
+			log.Error(err)
+		}
+	},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
