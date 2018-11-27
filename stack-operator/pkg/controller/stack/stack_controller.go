ec2a25d6f7b56f0e70924e1086f7a2f71a9d74b8
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-11-27 20:25:52 +0100

    Revert "Be defensive about preDelete func"
    This reverts commit da925227d73bb472b1c6ccb6d3e4b78436ee4fd3.

--- stack-operator/pkg/controller/stack/stack_controller.go
+++ stack-operator/pkg/controller/stack/stack_controller.go
@@ -566,10 +566,8 @@ func (r *ReconcileStack) DeleteElasticsearchPod(
 			return state, err
 		}
 
-		if preDelete != nil {
-			if err := preDelete(); err != nil {
-				return state, err
-			}
+		if err := preDelete(); err != nil {
+			return state, err
 		}
 		if err := r.Delete(context.TODO(), &pvc); err != nil && !apierrors.IsNotFound(err) {
 			return state, err
