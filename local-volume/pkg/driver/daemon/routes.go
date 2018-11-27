279552cefc696cd6f57d5a05bc2ea3d0906e36a2
sebgl <contact.sebgl@gmail.com>
2018-11-27 10:30:29 +0100

    Add a comment regarding updating PV before mounting volume

--- local-volume/pkg/driver/daemon/routes.go
+++ local-volume/pkg/driver/daemon/routes.go
@@ -44,19 +44,28 @@ func (s *Server) MountHandler() func(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// Start by updating PV with the current node name.
+		// The reason why we do this before mounting the volume is error handling.
+		// If we do it the other way around (mount first, then update PV), then what
+		// should we do if we cannot update the PV (eg. APIServer cannot be reached,
+		// or resource disappeared)?
+		// Moreover, the pod was assigned to this node and this won't change.
 		pvName := pathutil.ExtractPVCID(params.TargetDir)
 		log.Infof("Updating PV %s with affinity for node %s", pvName, s.nodeName)
 		if err := s.k8sClient.UpdatePVNodeAffinity(pvName, s.nodeName); err != nil {
 			log.WithError(err).Error("Cannot update Persistent Volume node affinity")
 			err500(w, err)
+			return
 		}
 
+		// Then, mount the volume
 		log.Info("Mounting volume to the host")
 		resp := s.driver.Mount(params)
 		log.Infof("%+v", resp)
 
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			err500(w, err)
+			return
 		}
 	}
 }
