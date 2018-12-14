1fd7159c7299850fe2d7afe9d845fbd8900b0fd6
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-12-14 17:28:39 +0100

    Add generic reconciler

---
+++ stack-operator/pkg/controller/common/reconciler/reconciler.go
@@ -0,0 +1,74 @@
+package reconciler
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+var (
+	log = logf.Log.WithName("generic-reconciler")
+)
+
+type Reconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Owner  metav1.Object
+}
+
+// ReconcileObj reconciles a given runtime object using the factory, diffing and modifying functions given.
+func (r Reconciler) ReconcileObj(
+	obj runtime.Object,
+	new func() runtime.Object,
+	diff func(expected, found *runtime.Object) bool,
+	mod func(expected, found *runtime.Object) runtime.Object,
+) error {
+	meta, ok := obj.(metav1.Object)
+	if !ok {
+		return errors.Errorf("%v is not a metadata Object", obj)
+	}
+	namespace := meta.GetNamespace()
+	name := meta.GetName()
+	kinds, unversioned, err := r.Scheme.ObjectKinds(obj)
+	kind := "unknown"
+	if !unversioned && err == nil {
+		kind = kinds[0].Kind
+	}
+	if err := controllerutil.SetControllerReference(r.Owner, meta, r.Scheme); err != nil {
+		return err
+	}
+	// Check if already exists
+	expected := obj
+	found := new()
+	err = r.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, found)
+	if err != nil && apierrors.IsNotFound(err) {
+		// Create if needed
+		log.Info(fmt.Sprintf("Creating %s %s/%s", kind, namespace, name))
+
+		err = r.Create(context.TODO(), expected)
+		if err != nil {
+			return err
+		}
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	// Update if needed
+	if !diff(&expected, &found) {
+		log.Info(fmt.Sprintf("Updating %s %s/%s ", kind, namespace, name))
+		err := r.Update(context.TODO(), mod(&expected, &found))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- stack-operator/pkg/controller/common/service_control.go
+++ stack-operator/pkg/controller/common/service_control.go
@@ -1,17 +1,15 @@
 package common
 
 import (
-	"context"
 	"reflect"
+
+	"github.com/elastic/stack-operators/stack-operator/pkg/controller/common/reconciler"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
@@ -24,54 +22,44 @@ func ReconcileService(
 	service *corev1.Service,
 	owner v1.Object,
 ) (reconcile.Result, error) {
-	if err := controllerutil.SetControllerReference(owner, service, scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-	// Check if already exists
-	expected := service
-	found := &corev1.Service{}
-	err := c.Get(context.TODO(), types.NamespacedName{Name: expected.Name, Namespace: expected.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		// Create if needed
-		log.Info(Concat("Creating service ", expected.Namespace, "/", expected.Name))
 
-		err = c.Create(context.TODO(), expected)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-		return reconcile.Result{}, nil
-	} else if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// ClusterIP might not exist in the expected service,
-	// but might have been set after creation by k8s on the actual resource.
-	// In such case, we want to use these values for comparison.
-	if expected.Spec.ClusterIP == "" {
-		expected.Spec.ClusterIP = found.Spec.ClusterIP
-	}
-	// same for the target port and node port
-	if len(expected.Spec.Ports) == len(found.Spec.Ports) {
-		for i := range expected.Spec.Ports {
-			if expected.Spec.Ports[i].TargetPort.IntValue() == 0 {
-				expected.Spec.Ports[i].TargetPort = found.Spec.Ports[i].TargetPort
+	return reconcile.Result{}, reconciler.Reconciler{
+		Client: c,
+		Scheme: scheme,
+		Owner:  owner,
+	}.ReconcileObj(
+		service,
+		func() runtime.Object {
+			return &corev1.Service{}
+		},
+		func(exp, fnd *runtime.Object) bool {
+			expected := (*exp).(*corev1.Service)
+			found := (*fnd).(*corev1.Service)
+			// ClusterIP might not exist in the expected service,
+			// but might have been set after creation by k8s on the actual resource.
+			// In such case, we want to use these values for comparison.
+			if expected.Spec.ClusterIP == "" {
+				expected.Spec.ClusterIP = found.Spec.ClusterIP
 			}
-			if expected.Spec.Ports[i].NodePort == 0 {
-				expected.Spec.Ports[i].NodePort = found.Spec.Ports[i].NodePort
+			// same for the target port and node port
+			if len(expected.Spec.Ports) == len(found.Spec.Ports) {
+				for i := range expected.Spec.Ports {
+					if expected.Spec.Ports[i].TargetPort.IntValue() == 0 {
+						expected.Spec.Ports[i].TargetPort = found.Spec.Ports[i].TargetPort
+					}
+					if expected.Spec.Ports[i].NodePort == 0 {
+						expected.Spec.Ports[i].NodePort = found.Spec.Ports[i].NodePort
+					}
+				}
 			}
-		}
-	}
+			return reflect.DeepEqual(expected.Spec, found.Spec)
+		},
+		func(exp, fnd *runtime.Object) runtime.Object {
+			expected := (*exp).(*corev1.Service)
+			found := (*fnd).(*corev1.Service)
+			found.Spec = expected.Spec // only update spec, keep the rest
+			return found
+		},
+	)
 
-	// Update if needed
-	if !reflect.DeepEqual(expected.Spec, found.Spec) {
-		log.Info(
-			Concat("Updating service ", expected.Namespace, "/", expected.Name),
-		)
-		found.Spec = expected.Spec // only update spec, keep the rest
-		err := c.Update(context.TODO(), found)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-	}
-	return reconcile.Result{}, nil
 }
