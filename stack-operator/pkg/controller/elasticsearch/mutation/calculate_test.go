f52e590ff6d1e88fc48392e43962c9380ab3f686
sebgl <contact.sebgl@gmail.com>
2018-12-14 14:21:06 +0100

    Move CalculateChanges to its own file

---
+++ stack-operator/pkg/controller/elasticsearch/mutation/calculate_test.go
@@ -0,0 +1,82 @@
+package mutation
+
+import (
+	"testing"
+
+	"github.com/elastic/stack-operators/stack-operator/pkg/controller/elasticsearch/support"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestCalculateChanges(t *testing.T) {
+	var taintedPod = defaultPod
+	taintedPod.Annotations = map[string]string{TaintedAnnotationName: "true"}
+	type args struct {
+		expected []support.PodSpecContext
+		state    support.ResourcesState
+	}
+	tests := []struct {
+		name string
+		args args
+		want Changes
+	}{
+		{
+			name: "no changes",
+			args: args{
+				expected: []support.PodSpecContext{defaultPodSpecCtx, defaultPodSpecCtx},
+				state:    support.ResourcesState{CurrentPods: []corev1.Pod{defaultPod, defaultPod}},
+			},
+			want: Changes{ToKeep: []corev1.Pod{defaultPod, defaultPod}},
+		},
+		{
+			name: "2 new pods",
+			args: args{
+				expected: []support.PodSpecContext{defaultPodSpecCtx, defaultPodSpecCtx, defaultPodSpecCtx, defaultPodSpecCtx},
+				state:    support.ResourcesState{CurrentPods: []corev1.Pod{defaultPod, defaultPod}},
+			},
+			want: Changes{
+				ToKeep: []corev1.Pod{defaultPod, defaultPod},
+				ToAdd:  []PodToAdd{{PodSpecCtx: defaultPodSpecCtx}, {PodSpecCtx: defaultPodSpecCtx}},
+			},
+		},
+		{
+			name: "2 less pods",
+			args: args{
+				expected: []support.PodSpecContext{},
+				state:    support.ResourcesState{CurrentPods: []corev1.Pod{defaultPod, defaultPod}},
+			},
+			want: Changes{ToDelete: []corev1.Pod{defaultPod, defaultPod}},
+		},
+		{
+			name: "1 pod replaced",
+			args: args{
+				expected: []support.PodSpecContext{defaultPodSpecCtx, ESPodSpecContext("another-image", defaultCPULimit)},
+				state:    support.ResourcesState{CurrentPods: []corev1.Pod{defaultPod, defaultPod}},
+			},
+			want: Changes{
+				ToKeep:   []corev1.Pod{defaultPod},
+				ToDelete: []corev1.Pod{defaultPod},
+				ToAdd:    []PodToAdd{{PodSpecCtx: ESPodSpecContext("another-image", defaultCPULimit)}},
+			},
+		},
+		{
+			name: "1 pod replaced on pod tainted",
+			args: args{
+				expected: []support.PodSpecContext{defaultPodSpecCtx, defaultPodSpecCtx},
+				state:    support.ResourcesState{CurrentPods: []corev1.Pod{taintedPod, defaultPod}},
+			},
+			want: Changes{ToKeep: []corev1.Pod{defaultPod}, ToDelete: []corev1.Pod{defaultPod}, ToAdd: []PodToAdd{PodToAdd{PodSpecCtx: defaultPodSpecCtx}}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CalculateChanges(tt.args.expected, tt.args.state, func(ctx support.PodSpecContext) (corev1.Pod, error) {
+				return corev1.Pod{}, nil // TODO: fix
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, len(tt.want.ToKeep), len(got.ToKeep))
+			assert.Equal(t, len(tt.want.ToAdd), len(got.ToAdd))
+			assert.Equal(t, len(tt.want.ToDelete), len(got.ToDelete))
+		})
+	}
+}
--- stack-operator/pkg/controller/elasticsearch/mutation/changes.go
+++ stack-operator/pkg/controller/elasticsearch/mutation/changes.go
@@ -1,8 +1,6 @@
 package mutation
 
 import (
-	"sort"
-
 	"github.com/elastic/stack-operators/stack-operator/pkg/apis/elasticsearch/v1alpha1"
 	"github.com/elastic/stack-operators/stack-operator/pkg/controller/elasticsearch/support"
 	corev1 "k8s.io/api/core/v1"
@@ -136,96 +134,4 @@ func partitionPodsBySelector(selector labels.Selector, pods []corev1.Pod) ([]cor
 		}
 	}
 	return matchingPods, remainingPods
-}
-
-// CalculateChanges returns Changes to perform by comparing actual pods to expected pods spec
-func CalculateChanges(expectedPodSpecCtxs []support.PodSpecContext, state support.ResourcesState, podBuilder PodBuilder) (Changes, error) {
-	// work on copies of the arrays, on which we can safely remove elements
-	expectedCopy := make([]support.PodSpecContext, len(expectedPodSpecCtxs))
-	copy(expectedCopy, expectedPodSpecCtxs)
-	actualCopy := make([]corev1.Pod, len(state.CurrentPods))
-	copy(actualCopy, state.CurrentPods)
-
-	return mutableCalculateChanges(expectedCopy, actualCopy, state, podBuilder)
-}
-
-func mutableCalculateChanges(
-	expectedPodSpecCtxs []support.PodSpecContext,
-	actualPods []corev1.Pod,
-	state support.ResourcesState,
-	podBuilder PodBuilder,
-) (Changes, error) {
-	changes := EmptyChanges()
-
-	for _, expectedPodSpecCtx := range expectedPodSpecCtxs {
-		comparisonResult, err := getAndRemoveMatchingPod(expectedPodSpecCtx, actualPods, state)
-		if err != nil {
-			return changes, err
-		}
-		if comparisonResult.IsMatch {
-			// matching pod already exists, keep it
-			changes.ToKeep = append(changes.ToKeep, comparisonResult.MatchingPod)
-			// one less pod to compare with
-			actualPods = comparisonResult.RemainingPods
-		} else {
-			// no matching pod, a new one should be added
-			pod, err := podBuilder(expectedPodSpecCtx)
-			if err != nil {
-				return changes, err
-			}
-			changes.ToAdd = append(changes.ToAdd, PodToAdd{
-				Pod:             pod,
-				PodSpecCtx:      expectedPodSpecCtx,
-				MismatchReasons: comparisonResult.MismatchReasonsPerPod,
-			})
-		}
-	}
-	// remaining actual pods should be removed
-	changes.ToDelete = actualPods
-
-	// sort changes for idempotent processing
-	sort.SliceStable(changes.ToKeep, sortPodByCreationTimestampAsc(changes.ToKeep))
-	sort.SliceStable(changes.ToDelete, sortPodByCreationTimestampAsc(changes.ToDelete))
-
-	return changes, nil
-}
-
-type PodComparisonResult struct {
-	IsMatch               bool
-	MatchingPod           corev1.Pod
-	MismatchReasonsPerPod map[string][]string
-	RemainingPods         []corev1.Pod
-}
-
-func getAndRemoveMatchingPod(podSpecCtx support.PodSpecContext, pods []corev1.Pod, state support.ResourcesState) (PodComparisonResult, error) {
-	mismatchReasonsPerPod := map[string][]string{}
-
-	for i, pod := range pods {
-		if IsTainted(pod) {
-			mismatchReasonsPerPod[pod.Name] = []string{TaintedReason}
-			continue
-		}
-
-		isMatch, mismatchReasons, err := podMatchesSpec(pod, podSpecCtx, state)
-		if err != nil {
-			return PodComparisonResult{}, err
-		}
-		if isMatch {
-			// matching pod found
-			// remove it from the remaining pods
-			return PodComparisonResult{
-				IsMatch:               true,
-				MatchingPod:           pod,
-				MismatchReasonsPerPod: mismatchReasonsPerPod,
-				RemainingPods:         append(pods[:i], pods[i+1:]...),
-			}, nil
-		}
-		mismatchReasonsPerPod[pod.Name] = mismatchReasons
-	}
-	// no matching pod found
-	return PodComparisonResult{
-		IsMatch:               false,
-		MismatchReasonsPerPod: mismatchReasonsPerPod,
-		RemainingPods:         pods,
-	}, nil
 }
