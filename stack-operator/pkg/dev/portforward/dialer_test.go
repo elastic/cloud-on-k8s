f59ac7c7abb7fa2e8e5ab1677d615a2d09d369da
Njal Karevoll <njal@karevoll.no>
2018-12-17 13:53:00 +0100

    simplify case with one test during dialing

--- stack-operator/pkg/dev/portforward/dialer_test.go
+++ stack-operator/pkg/dev/portforward/dialer_test.go
@@ -2,11 +2,11 @@ package portforward
 
 import (
 	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
 	"net"
-	"reflect"
-	"testing"
-
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
 )
 
 type stubForwarder struct {
@@ -31,52 +31,20 @@ func (f *stubForwarder) DialContext(ctx context.Context) (net.Conn, error) {
 }
 
 func TestForwardingDialer_DialContext(t *testing.T) {
-	type args struct {
-		ctx     context.Context
-		network string
-		addr    string
-	}
-	tests := []struct {
-		name            string
-		tweaks          func(dialer *ForwardingDialer)
-		args            args
-		want            net.Conn
-		wantErr         bool
-		extraAssertions func(t *testing.T, dialer *ForwardingDialer)
-	}{
-		{
-			name: "sample",
-			tweaks: func(dialer *ForwardingDialer) {
-				dialer.forwarderFactory = DialerForwarderFactoryFunc(
-					func(_ client.Client, network, addr string) (Forwarder, error) {
-						return &stubForwarder{
-							network: network, addr: addr,
-						}, nil
-					},
-				)
-			},
+	customError := errors.New("DialContext test error")
+
+	d := NewForwardingDialer()
+	d.forwarderFactory = DialerForwarderFactoryFunc(
+		func(_ client.Client, network, addr string) (Forwarder, error) {
+			return &stubForwarder{
+				network: network, addr: addr,
+				onDialContext: func(ctx context.Context) (net.Conn, error) {
+					return nil, customError
+				},
+			}, nil
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := NewForwardingDialer()
+	)
 
-			if tt.tweaks != nil {
-				tt.tweaks(d)
-			}
-
-			got, err := d.DialContext(tt.args.ctx, tt.args.network, tt.args.addr)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ForwardingDialer.DialContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ForwardingDialer.DialContext() = %v, want %v", got, tt.want)
-			}
-
-			if tt.extraAssertions != nil {
-				tt.extraAssertions(t, d)
-			}
-		})
-	}
+	_, err := d.DialContext(context.TODO(), "tcp", "localhost:8080")
+	assert.Equal(t, customError, err)
 }
