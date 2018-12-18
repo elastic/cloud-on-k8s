b4039360e91c010fac88d510d40dbe3cf392bb8b
Peter Brachwitz <peter.brachwitz@elastic.co>
2018-12-18 17:02:26 +0100

    Reinstate custom differ for config map

--- stack-operator/pkg/controller/elasticsearch/configmap_control.go
+++ stack-operator/pkg/controller/elasticsearch/configmap_control.go
@@ -1,6 +1,8 @@
 package elasticsearch
 
 import (
+	"reflect"
+
 	"github.com/elastic/stack-operators/stack-operator/pkg/controller/common/reconciler"
 
 	"github.com/elastic/stack-operators/stack-operator/pkg/apis/elasticsearch/v1alpha1"
@@ -15,6 +17,9 @@ func (r *ReconcileElasticsearch) ReconcileConfigMap(es v1alpha1.ElasticsearchClu
 			Scheme: r.scheme,
 			Owner:  &es,
 			Object: &expected,
+			Differ: func(expected, found *corev1.Secret) bool {
+				return !reflect.DeepEqual(expected.Data, found.Data)
+			},
 			Modifier: func(expected, found *corev1.ConfigMap) {
 				found.Data = expected.Data
 			},
