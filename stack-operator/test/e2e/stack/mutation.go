3f87ca13f4459bb3ca687f20ebda1764663804a5
sebgl <contact.sebgl@gmail.com>
2018-12-19 18:12:37 +0100

    Continously check for cluster health during mutation

--- stack-operator/test/e2e/stack/mutation.go
+++ stack-operator/test/e2e/stack/mutation.go
@@ -1,12 +1,23 @@
 package stack
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/elastic/stack-operators/stack-operator/pkg/apis/deployments/v1alpha1"
+	estype "github.com/elastic/stack-operators/stack-operator/pkg/apis/elasticsearch/v1alpha1"
+	esclient "github.com/elastic/stack-operators/stack-operator/pkg/controller/elasticsearch/client"
 	"github.com/elastic/stack-operators/stack-operator/test/e2e/helpers"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	continousHealthCheckInterval = 5 * time.Second
+	continousHealthCheckTimeout  = 10 * time.Second
 )
 
 // MutationTestSteps tests topology changes on the given stack
@@ -17,10 +28,19 @@ func MutationTestSteps(stack v1alpha1.Stack, k *helpers.K8sHelper) []helpers.Tes
 
 	var clusterIDBeforeMutation string
 
-	// TODO: continuous ES requests to check cluster health is always at least yellow
+	var continuousHealthChecks *ContinousHealthCheck
 
 	return helpers.TestStepList{}.
 		WithSteps(
+			helpers.TestStep{
+				Name: "Start querying ES cluster health while mutation is going on",
+				Test: func(t *testing.T) {
+					var err error
+					continuousHealthChecks, err = NewContinousHealthCheck(stack, k)
+					require.NoError(t, err)
+					continuousHealthChecks.Start()
+				},
+			},
 			helpers.TestStep{
 				Name: "Retrieve cluster ID before mutation for comparison purpose",
 				Test: helpers.Eventually(func() error {
@@ -62,5 +82,82 @@ func MutationTestSteps(stack v1alpha1.Stack, k *helpers.K8sHelper) []helpers.Tes
 					require.Equal(t, clusterIDBeforeMutation, clusterIDAfterMutation)
 				},
 			},
+			helpers.TestStep{
+				Name: "Cluster health should not have been red during mutation process",
+				Test: func(t *testing.T) {
+					continuousHealthChecks.Stop()
+					assert.NotZero(t, continuousHealthChecks.SuccessCount)
+					assert.Equal(t, 0, continuousHealthChecks.FailureCount)
+					assert.Empty(t, continuousHealthChecks.Failures)
+				},
+			},
 		)
+}
+
+// ContinuousHealthCheckFailure represents a healthchechk failure
+type ContinuousHealthCheckFailure struct {
+	err       error
+	timestamp time.Time
+}
+
+// ContinousHealthCheck continously runs health checks against Elasticsearch
+// during the whole mutation process
+type ContinousHealthCheck struct {
+	SuccessCount int
+	FailureCount int
+	Failures     []ContinuousHealthCheckFailure
+	stopChan     chan struct{}
+	esClient     *esclient.Client
+}
+
+// NewContinousHealthCheck sets up a ContinousHealthCheck struct
+func NewContinousHealthCheck(stack v1alpha1.Stack, k *helpers.K8sHelper) (*ContinousHealthCheck, error) {
+	esClient, err := helpers.NewElasticsearchClient(stack, k)
+	if err != nil {
+		return nil, err
+	}
+	return &ContinousHealthCheck{
+		stopChan: make(chan struct{}),
+		esClient: esClient,
+	}, nil
+}
+
+// AppendErr sets the given error as a failure
+func (hc *ContinousHealthCheck) AppendErr(err error) {
+	hc.Failures = append(hc.Failures, ContinuousHealthCheckFailure{
+		err:       err,
+		timestamp: time.Now(),
+	})
+	hc.FailureCount++
+}
+
+// Start runs health checks in a goroutine, until stopped
+func (hc *ContinousHealthCheck) Start() {
+	go func() {
+		ticker := time.NewTicker(continousHealthCheckInterval)
+		for {
+			select {
+			case <-hc.stopChan:
+				return
+			case <-ticker.C:
+				ctx, cancel := context.WithTimeout(context.Background(), continousHealthCheckTimeout)
+				defer cancel()
+				health, err := hc.esClient.GetClusterHealth(ctx)
+				if err != nil {
+					hc.AppendErr(err)
+					continue
+				}
+				if estype.ElasticsearchHealth(health.Status) == estype.ElasticsearchRedHealth {
+					hc.AppendErr(errors.New("Cluster health red"))
+					continue
+				}
+				hc.SuccessCount++
+			}
+		}
+	}()
+}
+
+// Stop the health checks goroutine
+func (hc *ContinousHealthCheck) Stop() {
+	hc.stopChan <- struct{}{}
 }
