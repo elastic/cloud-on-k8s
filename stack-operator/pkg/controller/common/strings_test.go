04243f8a54b3e2e5868fc408b4539d681bc1aba4
sebgl <contact.sebgl@gmail.com>
2019-01-17 16:03:09 +0100

    Add a method to remove occurrences of a string in a sliceOD

--- stack-operator/pkg/controller/common/strings_test.go
+++ stack-operator/pkg/controller/common/strings_test.go
@@ -3,6 +3,8 @@ package common
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -37,6 +39,48 @@ func TestStringInSlice(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := StringInSlice(tt.args.str, tt.args.list)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestRemoveStringInSlice(t *testing.T) {
+	type args struct {
+		s     string
+		slice []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "removes string from slice",
+			args: args{
+				s:     "b",
+				slice: []string{"a", "b", "c"},
+			},
+			want: []string{"a", "c"},
+		},
+		{
+			name: "removes string from slice multiple occurrences",
+			args: args{
+				s:     "b",
+				slice: []string{"a", "b", "c", "b"},
+			},
+			want: []string{"a", "c"},
+		},
+		{
+			name: "noop when string not found",
+			args: args{
+				s:     "d",
+				slice: []string{"a", "b", "c"},
+			},
+			want: []string{"a", "b", "c"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.ElementsMatch(t, tt.want, RemoveStringInSlice(tt.args.s, tt.args.slice))
 		})
 	}
 }
