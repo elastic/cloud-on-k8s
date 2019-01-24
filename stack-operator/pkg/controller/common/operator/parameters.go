fad9a70ca5cb25fdede52161599d4a17654d62a6
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-01-24 09:42:22 +0100

    Fix formatting

--- stack-operator/pkg/controller/common/operator/parameters.go
+++ stack-operator/pkg/controller/common/operator/parameters.go
@@ -2,7 +2,7 @@ package operator
 
 import "github.com/elastic/stack-operators/stack-operator/pkg/utils/net"
 
-//Parameters contain parameters to create new operators.
+// Parameters contain parameters to create new operators.
 type Parameters struct {
 	// OperatorImage is the operator docker image. The operator needs to be aware of its image to use it in sidecars.
 	OperatorImage string
