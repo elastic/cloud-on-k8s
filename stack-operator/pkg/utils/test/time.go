9e8a20a9cbd916fa4a1863fa4272924cb98b0e6b
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-02-04 11:30:58 +0100

    Godoc

--- stack-operator/pkg/utils/test/time.go
+++ stack-operator/pkg/utils/test/time.go
@@ -5,7 +5,8 @@ import (
 	"time"
 )
 
-// Time is a convenience function to create a 0 hour 0 minute time value from a string with layourt 2006-01-02. Panics!
+// Time is a convenience function to create a 0 hour 0 minute time value from a string with layourt 2006-01-02.
+// Panics! Convenience function for testing purposes only.
 func Time(dateStr string) time.Time {
 	layout := "2006-01-02"
 	parsed, err := time.Parse(layout, dateStr)
@@ -15,11 +16,13 @@ func Time(dateStr string) time.Time {
 	return parsed
 }
 
-// Millis calculates milliseconds from the given date string with layout 2006-01-02. Panics! For testing purposes only.
+// Millis calculates milliseconds from the given date string with layout 2006-01-02.
+// Panics! Convenience function for testing purposes only.
 func Millis(dateStr string) int64 {
 	return ToMillis(Time(dateStr))
 }
 
+// ToMillis returns t in milliseconds.
 func ToMillis(t time.Time) int64 {
 	return t.UnixNano() / int64(time.Millisecond)
 }
