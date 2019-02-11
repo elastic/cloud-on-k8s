7581a60464e34ea7a06931497fbbe01ac24cb840
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-02-11 17:38:11 +0100

    Godoc III

--- operators/pkg/controller/elasticsearch/keystore/keystore.go
+++ operators/pkg/controller/elasticsearch/keystore/keystore.go
@@ -6,7 +6,7 @@ package keystore
 
 const (
 	managedSecretSuffix = "-keystore"
-	// SecretMountPath is the mount path for keystore secrets in init container.
+	// SecretMountPath is the mount path for keystore secrets in the init container.
 	SecretMountPath = "/keystore-secrets"
 	// SecretVolumeName is the name of the volume where the keystore secret is referenced.
 	SecretVolumeName = "keystore"
