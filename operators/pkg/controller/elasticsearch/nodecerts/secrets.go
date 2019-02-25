02b59913ffbf4802e3d1517aff879cc10f5befbe
sebgl <contact.sebgl@gmail.com>
2019-02-25 18:03:43 +0100

    Extract some functions from secrets.go into csr.go

--- operators/pkg/controller/elasticsearch/nodecerts/secrets.go
+++ operators/pkg/controller/elasticsearch/nodecerts/secrets.go
@@ -7,17 +7,12 @@ package nodecerts
 import (
 	"bytes"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
-	"io"
-	"net"
 	"time"
 
 	"github.com/elastic/k8s-operators/operators/pkg/controller/common/certificates"
-	"github.com/elastic/k8s-operators/operators/pkg/controller/elasticsearch/initcontainer"
 	"github.com/elastic/k8s-operators/operators/pkg/utils/k8s"
-	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -108,35 +103,6 @@ func EnsureNodeCertificateSecretExists(
 
 	// TODO: in the future we should consider reconciling the existing secret as well instead of leaving it untouched.
 	return &secret, nil
-}
-
-// maybeRequestCSR requests the pod for a new CSR if required, or returns nil.
-func maybeRequestCSR(pod corev1.Pod, csrClient certificates.CSRClient, lastCSRUpdate string) ([]byte, error) {
-	// If the CSR secret was updated very recently, chances are we already issued a new certificate
-	// which has not yet been propagated to the pod (it can take more than 1 minute).
-	// In such case, there is no need to request the same CSR again and again at each reconciliation.
-	lastUpdate, err := time.Parse(time.RFC3339, lastCSRUpdate)
-	if err != nil {
-		log.V(1).Info("lastCSRUpdate time cannot be parsed, probably because not set yet. Ignoring.", "pod", pod.Name)
-	} else {
-		delay := time.Now().Sub(lastUpdate)
-		if delay > 0 && delay < CSRRequestDelay {
-			log.V(1).Info("CSR was already updated recently, let's wait before requesting a new one", "pod", pod.Name)
-			return nil, nil
-		}
-	}
-	// Check status of the pod's cert-initializer init container: if running, it's waiting for
-	// a valid certificate to be issued, hence we should request a new CSR.
-	for _, c := range pod.Status.InitContainerStatuses {
-		if c.Name == initcontainer.CertInitializerContainerName && c.State.Running != nil {
-			newCSR, err := csrClient.RetrieveCSR(pod)
-			if err != nil && err != io.EOF { // EOF is ok, just the cert-initializer shutting down
-				return nil, err
-			}
-			return newCSR, nil
-		}
-	}
-	return nil, nil
 }
 
 // ReconcileNodeCertificateSecret ensures that the node certificate secret has the available and correct Data keys
@@ -272,104 +238,4 @@ func shouldIssueNewCertificate(secret corev1.Secret, ca *certificates.Ca, pod co
 	// TODO: verify expected SANs in certificate, otherwise we wont actually reconcile such changes
 
 	return false
-}
-
-// CreateValidatedCertificateTemplate validates a CSR and creates a certificate template.
-func CreateValidatedCertificateTemplate(
-	pod corev1.Pod,
-	clusterName, namespace string,
-	svcs []corev1.Service,
-	csr *x509.CertificateRequest,
-) (*certificates.ValidatedCertificateTemplate, error) {
-	podIp := net.ParseIP(pod.Status.PodIP)
-	if podIp == nil {
-		return nil, fmt.Errorf("pod currently has no valid IP, found: [%s]", pod.Status.PodIP)
-	}
-
-	commonName := buildCertificateCommonName(pod, clusterName, namespace)
-	commonNameUTF8OtherName := &certificates.UTF8StringValuedOtherName{
-		OID:   certificates.CommonNameObjectIdentifier,
-		Value: commonName,
-	}
-	commonNameOtherName, err := commonNameUTF8OtherName.ToOtherName()
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to create othername")
-	}
-
-	// because we're using the ES-customized subject alternative-names extension, we have to handle all the general
-	// names here instead of using x509.Certificate.DNSNames, .IPAddresses etc.
-	generalNames := []certificates.GeneralName{
-		{OtherName: *commonNameOtherName},
-		{DNSName: commonName},
-		{DNSName: pod.Name},
-		{IPAddress: maybeIPTo4(podIp)},
-		{IPAddress: net.ParseIP("127.0.0.1").To4()},
-	}
-
-	if svcs != nil {
-		for _, svc := range svcs {
-			if ip := net.ParseIP(svc.Spec.ClusterIP); ip != nil {
-				generalNames = append(generalNames,
-					certificates.GeneralName{IPAddress: maybeIPTo4(ip)},
-				)
-			}
-
-			generalNames = append(generalNames,
-				certificates.GeneralName{DNSName: svc.Name},
-				certificates.GeneralName{DNSName: getServiceFullyQualifiedHostname(svc)},
-			)
-		}
-	}
-
-	generalNamesBytes, err := certificates.MarshalToSubjectAlternativeNamesData(generalNames)
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO: csr signature is not checked, common name not verified
-	// TODO: add services dns entries / ip addresses to cert?
-
-	certificateTemplate := certificates.ValidatedCertificateTemplate(x509.Certificate{
-		Subject: pkix.Name{
-			CommonName:         commonName,
-			OrganizationalUnit: []string{clusterName},
-		},
-
-		ExtraExtensions: []pkix.Extension{
-			{Id: certificates.SubjectAlternativeNamesObjectIdentifier, Value: generalNamesBytes},
-		},
-		NotBefore: time.Now().Add(-10 * time.Minute),
-		NotAfter:  time.Now().Add(365 * 24 * time.Hour),
-
-		PublicKeyAlgorithm: csr.PublicKeyAlgorithm,
-		PublicKey:          csr.PublicKey,
-
-		Signature:          csr.Signature,
-		SignatureAlgorithm: csr.SignatureAlgorithm,
-
-		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-	})
-
-	return &certificateTemplate, nil
-}
-
-// buildCertificateCommonName returns the CN (and ES othername) entry for a given pod within a stack
-// this needs to be kept in sync with the usage of trust_restrictions (see elasticsearch.TrustConfig)
-func buildCertificateCommonName(pod corev1.Pod, clusterName, namespace string) string {
-	return fmt.Sprintf("%s.node.%s.%s.es.cluster.local", pod.Name, clusterName, namespace)
-}
-
-// getServiceFullyQualifiedHostname returns the fully qualified DNS name for a service
-func getServiceFullyQualifiedHostname(svc corev1.Service) string {
-	// TODO: cluster.local suffix should be configurable
-	return fmt.Sprintf("%s.%s.svc.cluster.local", svc.Name, svc.Namespace)
-}
-
-// maybeIPTo4 attempts to convert the provided net.IP to a 4-byte representation if possible, otherwise does nothing.
-func maybeIPTo4(ipAddress net.IP) net.IP {
-	if ip := ipAddress.To4(); ip != nil {
-		return ip
-	}
-	return ipAddress
 }
--- operators/pkg/controller/elasticsearch/nodecerts/secrets_test.go
+++ operators/pkg/controller/elasticsearch/nodecerts/secrets_test.go
@@ -9,10 +9,8 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"net"
 	"reflect"
 	"testing"
-	"time"
 
 	"github.com/elastic/k8s-operators/operators/pkg/controller/common/certificates"
 	"github.com/elastic/k8s-operators/operators/pkg/controller/elasticsearch/initcontainer"
@@ -133,42 +131,6 @@ func init() {
 		panic("Failed to create cert data:" + err.Error())
 	}
 	pemCert = certificates.EncodePEMCert(certData, testCa.Cert.Raw)
-}
-
-func Test_createValidatedCertificateTemplate(t *testing.T) {
-	validatedCert, err := CreateValidatedCertificateTemplate(testPod, "test-es-name", "test-namespace", []corev1.Service{testSvc}, testCSR)
-	require.NoError(t, err)
-
-	// roundtrip the certificate
-	certRT, err := roundTripSerialize(validatedCert)
-	require.NoError(t, err)
-	require.NotNil(t, certRT, "roundtripped certificate should not be nil")
-
-	// regular dns names and ip addresses should be present in the cert
-	assert.Contains(t, certRT.DNSNames, testPod.Name)
-	assert.Contains(t, certRT.IPAddresses, net.ParseIP(testIP).To4())
-	assert.Contains(t, certRT.IPAddresses, net.ParseIP("127.0.0.1").To4())
-
-	// service ip and hosts should be present in the cert
-	assert.Contains(t, certRT.IPAddresses, net.ParseIP(testSvc.Spec.ClusterIP).To4())
-	assert.Contains(t, certRT.DNSNames, testSvc.Name)
-	assert.Contains(t, certRT.DNSNames, getServiceFullyQualifiedHostname(testSvc))
-
-	// es specific othernames is a bit more difficult to get to, but should be present:
-	otherNames, err := certificates.ParseSANGeneralNamesOtherNamesOnly(certRT)
-	require.NoError(t, err)
-
-	// we expect this name to be used for both the common name as well as the es othername
-	cn := "test-pod-name.node.test-es-name.test-namespace.es.cluster.local"
-
-	otherName, err := (&certificates.UTF8StringValuedOtherName{
-		OID:   certificates.CommonNameObjectIdentifier,
-		Value: cn,
-	}).ToOtherName()
-	require.NoError(t, err)
-
-	assert.Equal(t, certRT.Subject.CommonName, cn)
-	assert.Contains(t, otherNames, certificates.GeneralName{OtherName: *otherName})
 }
 
 // roundTripSerialize does a serialization round-trip of the certificate in order to make sure any extra extensions
@@ -304,65 +266,6 @@ func Test_shouldIssueNewCertificate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := shouldIssueNewCertificate(tt.args.secret, testCa, tt.args.pod); got != tt.want {
 				t.Errorf("shouldIssueNewCertificate() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_maybeRequestCSR(t *testing.T) {
-	tests := []struct {
-		name          string
-		lastCSRUpdate string
-		pod           corev1.Pod
-		want          []byte
-		wantErr       bool
-	}{
-		{
-			name:          "last request was made a day ago, should request a new CSR",
-			lastCSRUpdate: time.Now().Add(-24 * time.Hour).Format(time.RFC3339),
-			pod:           podWithRunningCertInitializer,
-			want:          fakeCSRClient.csr,
-		},
-		{
-			name:          "last request was made very recently, should not request a new CSR",
-			lastCSRUpdate: time.Now().Add(-5 * time.Second).Format(time.RFC3339),
-			pod:           podWithRunningCertInitializer,
-			want:          nil,
-		},
-		{
-			name:          "last request time isn't set, should request a new CSR",
-			lastCSRUpdate: "",
-			pod:           podWithRunningCertInitializer,
-			want:          fakeCSRClient.csr,
-		},
-		{
-			name:          "last request time has the wrong format, request a new CSR",
-			lastCSRUpdate: "yolo",
-			pod:           podWithRunningCertInitializer,
-			want:          fakeCSRClient.csr,
-		},
-		{
-			name:          "last request time is in the future, should request a new CSR",
-			lastCSRUpdate: time.Now().Add(1 * time.Hour).Format(time.RFC3339),
-			pod:           podWithRunningCertInitializer,
-			want:          fakeCSRClient.csr,
-		},
-		{
-			name:          "cert-initializer is terminated, should not request a CSR",
-			lastCSRUpdate: time.Now().Add(1 * time.Hour).Format(time.RFC3339),
-			pod:           podWithTerminatedCertInitializer,
-			want:          nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := maybeRequestCSR(tt.pod, fakeCSRClient, tt.lastCSRUpdate)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("maybeRequestCSR() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("maybeRequestCSR() = %v, want %v", got, tt.want)
 			}
 		})
 	}
