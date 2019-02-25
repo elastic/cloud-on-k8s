6d5d28247bf5c75cec762ac73a5fd6595176349d
sebgl <contact.sebgl@gmail.com>
2019-02-25 18:08:31 +0100

    Move reconciliation-related funcs from secrets.go to reconcile.go

--- operators/pkg/controller/elasticsearch/nodecerts/reconcile.go
+++ operators/pkg/controller/elasticsearch/nodecerts/reconcile.go
@@ -5,6 +5,9 @@
 package nodecerts
 
 import (
+	"bytes"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"time"
@@ -84,7 +87,7 @@ func ReconcileNodeCertificateSecrets(
 
 		switch certificateType {
 		case LabelNodeCertificateTypeElasticsearchAll:
-			if res, err := ReconcileNodeCertificateSecret(
+			if res, err := doReconcile(
 				c, secret, pod, csrClient, es.Name, es.Namespace, services, ca, additionalCAs,
 			); err != nil {
 				return res, err
@@ -118,4 +121,139 @@ func findNodeCertificateSecrets(
 	}
 
 	return nodeCertificateSecrets.Items, nil
+}
+
+// doReconcile ensures that the node certificate secret has the available and correct Data keys
+// provided.
+func doReconcile(
+	c k8s.Client,
+	secret corev1.Secret,
+	pod corev1.Pod,
+	csrClient certificates.CSRClient,
+	clusterName, namespace string,
+	svcs []corev1.Service,
+	ca *certificates.Ca,
+	additionalTrustedCAsPemEncoded [][]byte,
+) (reconcile.Result, error) {
+	// a placeholder secret may have nil entries, create them if needed
+	if secret.Data == nil {
+		secret.Data = make(map[string][]byte)
+	}
+	if secret.Annotations == nil {
+		secret.Annotations = make(map[string]string)
+	}
+
+	csr := secret.Data[CSRFileName]                              // may be nil
+	lastCSRUpdate := secret.Annotations[LastCSRUpdateAnnotation] // may be empty
+
+	// check if the existing cert is correct
+	issueNewCertificate := shouldIssueNewCertificate(secret, ca, pod)
+
+	// if needed, replace the CSR by a fresh one
+	newCSR, err := maybeRequestCSR(pod, csrClient, lastCSRUpdate)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	if len(newCSR) > 0 && !bytes.Equal(csr, newCSR) {
+		// pod issued a new CSR, probably generated from a new private key
+		// we should issue a new cert for this CSR
+		csr = newCSR
+		issueNewCertificate = true
+		lastCSRUpdate = time.Now().Format(time.RFC3339)
+	}
+
+	if len(csr) == 0 {
+		// no csr yet, let's requeue until cert-initializer is available
+		return reconcile.Result{}, nil
+	}
+
+	if !issueNewCertificate {
+		// nothing to do, we're all set
+		return reconcile.Result{}, nil
+	}
+
+	log.Info(
+		"Issuing new certificate",
+		"secret", secret.Name,
+		"clusterName", clusterName,
+		"namespace", namespace,
+	)
+
+	// create a cert from the csr
+	parsedCSR, err := x509.ParseCertificateRequest(csr)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	validatedCertificateTemplate, err := CreateValidatedCertificateTemplate(pod, clusterName, namespace, svcs, parsedCSR)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	// sign the certificate
+	certData, err := ca.CreateCertificate(*validatedCertificateTemplate)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// store CA cert, CSR and signed certificate in a secret mounted into the pod
+	secret.Data[certificates.CAFileName] = certificates.EncodePEMCert(ca.Cert.Raw)
+	for _, caPemBytes := range additionalTrustedCAsPemEncoded {
+		secret.Data[certificates.CAFileName] = append(secret.Data[certificates.CAFileName], caPemBytes...)
+	}
+	secret.Data[CSRFileName] = csr
+	secret.Data[CertFileName] = certificates.EncodePEMCert(certData, ca.Cert.Raw)
+	// store last CSR update in the pod annotations
+	secret.Annotations[LastCSRUpdateAnnotation] = lastCSRUpdate
+
+	log.Info("Updating node certificate secret", "secret", secret.Name)
+	if err := c.Update(&secret); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// shouldIssueNewCertificate returns true if we should issue a new certificate.
+// Reasons for reissuing a certificate:
+// - no certificate yet
+// - certificate has the wrong format
+// - certificate is invalid or expired
+// - certificate SAN and IP does not match pod SAN and IP
+func shouldIssueNewCertificate(secret corev1.Secret, ca *certificates.Ca, pod corev1.Pod) bool {
+	certData, ok := secret.Data[CertFileName]
+	if !ok {
+		// certificate is missing
+		return true
+	}
+
+	block, _ := pem.Decode(certData)
+	if block == nil {
+		log.Info("Invalid certificate data found, issuing new certificate", "secret", secret.Name)
+		return true
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Info("Invalid certificate found as first block, issuing new certificate", "secret", secret.Name)
+		return true
+	}
+
+	pool := x509.NewCertPool()
+	pool.AddCert(ca.Cert)
+	verifyOpts := x509.VerifyOptions{
+		DNSName:       pod.Name,
+		Roots:         pool,
+		Intermediates: pool,
+	}
+	if _, err := cert.Verify(verifyOpts); err != nil {
+		log.Info(
+			fmt.Sprintf("Certificate was not valid, should issue new: %s", err),
+			"subject", cert.Subject,
+			"issuer", cert.Issuer,
+			"current_ca_subject", ca.Cert.Subject,
+		)
+		return true
+	}
+
+	// TODO: verify expected SANs in certificate, otherwise we wont actually reconcile such changes
+
+	return false
 }
--- operators/pkg/controller/elasticsearch/nodecerts/secrets_test.go
+++ operators/pkg/controller/elasticsearch/nodecerts/secrets_test.go
@@ -9,14 +9,12 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"reflect"
 	"testing"
 
 	"github.com/elastic/k8s-operators/operators/pkg/controller/common/certificates"
 	"github.com/elastic/k8s-operators/operators/pkg/controller/elasticsearch/initcontainer"
 	"github.com/elastic/k8s-operators/operators/pkg/utils/k8s"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -203,157 +201,6 @@ func TestEnsureNodeCertificateSecretExists(t *testing.T) {
 				return
 			}
 			tt.want(t, got)
-		})
-	}
-}
-
-func Test_shouldIssueNewCertificate(t *testing.T) {
-	type args struct {
-		secret corev1.Secret
-		pod    corev1.Pod
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "missing cert in secret",
-			args: args{
-				secret: corev1.Secret{},
-				pod:    testPod,
-			},
-			want: true,
-		},
-		{
-			name: "invalid cert data",
-			args: args{
-				secret: corev1.Secret{
-					Data: map[string][]byte{
-						CertFileName: []byte("invalid"),
-					},
-				},
-				pod: testPod,
-			},
-			want: true,
-		},
-		{
-			name: "pod name mismatch",
-			args: args{
-				secret: corev1.Secret{
-					Data: map[string][]byte{
-						CertFileName: pemCert,
-					},
-				},
-				pod: corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "different"}},
-			},
-			want: true,
-		},
-		{
-			name: "valid cert",
-			args: args{
-				secret: corev1.Secret{
-					Data: map[string][]byte{
-						CertFileName: pemCert,
-					},
-				},
-				pod: testPod,
-			},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := shouldIssueNewCertificate(tt.args.secret, testCa, tt.args.pod); got != tt.want {
-				t.Errorf("shouldIssueNewCertificate() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestReconcileNodeCertificateSecret(t *testing.T) {
-	objMeta := metav1.ObjectMeta{
-		Namespace: "namespace",
-		Name:      "secret",
-	}
-	tests := []struct {
-		name              string
-		secret            corev1.Secret
-		pod               corev1.Pod
-		wantSecretUpdated bool
-	}{
-		{
-			name:              "no cert generated yet: issue one",
-			secret:            corev1.Secret{ObjectMeta: objMeta},
-			pod:               podWithRunningCertInitializer,
-			wantSecretUpdated: true,
-		},
-		{
-			name:              "no cert generated, but pod has no IP yet: requeue",
-			secret:            corev1.Secret{ObjectMeta: objMeta},
-			pod:               corev1.Pod{},
-			wantSecretUpdated: false,
-		},
-		{
-			name:              "no cert generated, but cert-initializer not running: requeue",
-			secret:            corev1.Secret{ObjectMeta: objMeta},
-			pod:               podWithTerminatedCertInitializer,
-			wantSecretUpdated: false,
-		},
-		{
-			name: "a cert already exists, is valid, and cert-initializer is not running: requeue",
-			secret: corev1.Secret{
-				ObjectMeta: objMeta,
-				Data: map[string][]byte{
-					CertFileName: pemCert,
-				},
-			},
-			pod:               podWithTerminatedCertInitializer,
-			wantSecretUpdated: false,
-		},
-		{
-			name: "a cert already exists, is valid, and cert-initializer is running to serve a new CSR: issue cert",
-			secret: corev1.Secret{
-				ObjectMeta: objMeta,
-				Data: map[string][]byte{
-					CertFileName: pemCert,
-				},
-			},
-			pod:               podWithRunningCertInitializer,
-			wantSecretUpdated: true,
-		},
-		{
-			name: "a cert already exists, is valid, and cert-initializer is running to serve the same CSR: requeue",
-			secret: corev1.Secret{
-				ObjectMeta: objMeta,
-				Data: map[string][]byte{
-					CertFileName: pemCert,
-					CSRFileName:  testCSRBytes,
-				},
-			},
-			pod:               podWithRunningCertInitializer,
-			wantSecretUpdated: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			fakeClient := k8s.WrapClient(fake.NewFakeClient(&tt.secret))
-
-			_, err := ReconcileNodeCertificateSecret(fakeClient, tt.secret, tt.pod, fakeCSRClient, "cluster", "namespace", []corev1.Service{testSvc}, testCa, nil)
-			require.NoError(t, err)
-
-			var updatedSecret corev1.Secret
-			err = fakeClient.Get(k8s.ExtractNamespacedName(&objMeta), &updatedSecret)
-			require.NoError(t, err)
-
-			isUpdated := !reflect.DeepEqual(tt.secret, updatedSecret)
-			require.Equal(t, tt.wantSecretUpdated, isUpdated)
-			if tt.wantSecretUpdated {
-				assert.NotEmpty(t, updatedSecret.Annotations[LastCSRUpdateAnnotation])
-				assert.NotEmpty(t, updatedSecret.Data[certificates.CAFileName])
-				assert.NotEmpty(t, updatedSecret.Data[CSRFileName])
-				assert.NotEmpty(t, updatedSecret.Data[CertFileName])
-			}
 		})
 	}
 }
