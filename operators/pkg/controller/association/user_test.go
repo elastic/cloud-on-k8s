3404fa07871003a603ee5a9207692a7187d3f5c0
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-02-25 14:49:10 +0100

    Test user creation in association controller

---
+++ operators/pkg/controller/association/user_test.go
@@ -0,0 +1,160 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package association
+
+import (
+	"testing"
+
+	assoctype "github.com/elastic/k8s-operators/operators/pkg/apis/associations/v1alpha1"
+	estype "github.com/elastic/k8s-operators/operators/pkg/apis/elasticsearch/v1alpha1"
+	"github.com/elastic/k8s-operators/operators/pkg/utils/k8s"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func Test_reconcileEsUser(t *testing.T) {
+	sc := scheme.Scheme
+	if err := assoctype.SchemeBuilder.AddToScheme(sc); err != nil {
+		assert.Fail(t, "failed to add assoc types")
+	}
+	if err := estype.SchemeBuilder.AddToScheme(sc); err != nil {
+		assert.Fail(t, "faild to add Es types")
+	}
+
+	const expectedKey = "foo-elastic-internal-kibana"
+	testAssociation := assoctype.KibanaElasticsearchAssociation{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: assoctype.KibanaElasticsearchAssociationSpec{
+			Elasticsearch: assoctype.ObjectSelector{
+				Name:      "es",
+				Namespace: "default",
+			},
+			Kibana: assoctype.ObjectSelector{
+				Name:      "kibana",
+				Namespace: "default",
+			},
+		},
+	}
+
+	type args struct {
+		initialObjects []runtime.Object
+		assoc          assoctype.KibanaElasticsearchAssociation
+	}
+	tests := []struct {
+		name          string
+		args          args
+		wantErr       bool
+		postCondition func(client k8s.Client)
+	}{
+		{
+			name: "Happy path: should create a secret and a user CRD",
+			args: args{
+				initialObjects: nil,
+				assoc:          testAssociation,
+			},
+			postCondition: func(c k8s.Client) {
+				key := types.NamespacedName{
+					Name:      expectedKey,
+					Namespace: "default",
+				}
+				assert.NoError(t, c.Get(key, &estype.User{}))
+				assert.NoError(t, c.Get(key, &corev1.Secret{}))
+			},
+			wantErr: false,
+		},
+		{
+			name: "Existing secret but different namespace: create new",
+			args: args{
+				initialObjects: []runtime.Object{&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      expectedKey,
+						Namespace: "other",
+					}}},
+				assoc: testAssociation,
+			},
+			wantErr: false,
+			postCondition: func(c k8s.Client) {
+				list := corev1.SecretList{}
+				assert.NoError(t, c.List(&client.ListOptions{}, &list))
+				assert.Equal(t, 2, len(list.Items))
+				for _, s := range list.Items {
+					assert.Equal(t, expectedKey, s.Name)
+				}
+			},
+		},
+		{
+			name: "Reconcile updates existing resources",
+			args: args{
+				initialObjects: []runtime.Object{&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      expectedKey,
+						Namespace: "default",
+					},
+				}},
+				assoc: testAssociation,
+			},
+			wantErr: false,
+			postCondition: func(c k8s.Client) {
+				var s corev1.Secret
+				assert.NoError(t, c.Get(types.NamespacedName{Name: expectedKey, Namespace: "default"}, &s))
+				password, ok := s.Data[InternalKibanaServerUserName]
+				assert.True(t, ok)
+				assert.NotEmpty(t, password)
+			},
+		},
+		{
+			name: "Reconcile is namespace aware",
+			args: args{
+				assoc: assoctype.KibanaElasticsearchAssociation{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "ns-1",
+					},
+					Spec: assoctype.KibanaElasticsearchAssociationSpec{
+						Elasticsearch: assoctype.ObjectSelector{
+							Name:      "es",
+							Namespace: "ns-1",
+						},
+						Kibana: assoctype.ObjectSelector{
+							Name:      "kb",
+							Namespace: "ns-2",
+						},
+					},
+				},
+			},
+			wantErr: false,
+			postCondition: func(c k8s.Client) {
+				// user CR should be in ES namespace
+				assert.NoError(t, c.Get(types.NamespacedName{
+					Namespace: "ns-1",
+					Name:      expectedKey,
+				}, &estype.User{}))
+				// secret should be in Kibana namespace
+				assert.NoError(t, c.Get(types.NamespacedName{
+					Namespace: "ns-2",
+					Name:      expectedKey,
+				}, &corev1.Secret{}))
+			},
+		},
+	}
+	for _, tt := range tests {
+		c := k8s.WrapClient(fake.NewFakeClient(tt.args.initialObjects...))
+		t.Run(tt.name, func(t *testing.T) {
+			if err := reconcileEsUser(c, sc, tt.args.assoc); (err != nil) != tt.wantErr {
+				t.Errorf("reconcileEsUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			tt.postCondition(c)
+		})
+	}
+}
