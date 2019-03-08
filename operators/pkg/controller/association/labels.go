8c03d5876fb5e764524ff8a8aca9f9be6eb414c1
Thibault Richard <thb.krkr@gmail.com>
2019-03-08 17:18:12 +0100

    Association label renaming

--- operators/pkg/controller/association/labels.go
+++ operators/pkg/controller/association/labels.go
@@ -10,10 +10,12 @@ import (
 
 const (
 	// AssociationLabelName marks resources created by this controller for easier retrieval.
-	AssociationLabelName = "associations.k8s.elastic.co/name"
+	AssociationLabelName = "association.k8s.elastic.co/name"
 )
 
 // NewResourceSelector selects resources labeled as related to the named association.
 func NewResourceSelector(name string) labels.Selector {
-	return labels.Set(map[string]string{AssociationLabelName: name}).AsSelector()
+	return labels.Set(map[string]string{
+		AssociationLabelName: name,
+	}).AsSelector()
 }
