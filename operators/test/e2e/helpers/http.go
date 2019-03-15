97ba11b29a4f412dc47529613766cb2928085b52
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-03-15 12:48:02 +0100

    Add Kibana login e2e test

---
+++ operators/test/e2e/helpers/http.go
@@ -0,0 +1,22 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package helpers
+
+import (
+	"net/http"
+
+	"github.com/elastic/k8s-operators/operators/pkg/dev/portforward"
+)
+
+// NewHTTPClient creates a new HTTP client that is aware of any port forwarding configuration.
+func NewHTTPClient() http.Client {
+	client := http.Client{}
+	if *autoPortForward {
+		client.Transport = &http.Transport{
+			DialContext: portforward.NewForwardingDialer().DialContext,
+		}
+	}
+	return client
+}
