be3a166818693c98bb30cd6409aa48ffb660bc6d
Thibault Richard <thb.krkr@gmail.com>
2019-03-20 12:29:15 +0100

    Inject process manager

--- operators/pkg/controller/elasticsearch/initcontainer/inject_pm.go
+++ operators/pkg/controller/elasticsearch/initcontainer/inject_pm.go
@@ -6,17 +6,19 @@ package initcontainer
 
 import corev1 "k8s.io/api/core/v1"
 
+const script = `
+	#!/usr/bin/env bash -eu
+	cp process-manager $ES_BIN
+`
+
 func NewInjectProcessManagerInitContainer(imageName string, sharedVolume SharedVolume) (corev1.Container, error) {
 	container := corev1.Container{
 		Image: imageName,
 		Env: []corev1.EnvVar{
 			{Name: "ES_BIN", Value: sharedVolume.InitContainerMountPath},
 		},
-		Name: "inject-process-manager",
-		Command: []string{"bash", "-c", `
-			#!/usr/bin/env bash -eu
-			cp process-manager $ES_BIN
-    `},
+		Name:         "inject-process-manager",
+		Command:      []string{"bash", "-c", script},
 		VolumeMounts: []corev1.VolumeMount{sharedVolume.InitContainerVolumeMount()},
 	}
 	return container, nil
