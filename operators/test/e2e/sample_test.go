715bbb7cc1df8106711e3ebd93918bee1f9b53da
sebgl <contact.sebgl@gmail.com>
2019-03-26 14:20:09 +0100

    Rename kibana sample file to association

--- operators/test/e2e/sample_test.go
+++ operators/test/e2e/sample_test.go
@@ -20,7 +20,7 @@ import (
 // Re-use the sample stack for e2e tests.
 // This is a way to make sure both the sample and the e2e tests are always up-to-date.
 // Path is relative to the e2e directory.
-const sampleStackFile = "../../config/samples/es_kibana_sample.yaml"
+const sampleStackFile = "../../config/samples/kibana/association.yaml"
 
 func readSampleStack() stack.Builder {
 	// build stack from yaml sample
