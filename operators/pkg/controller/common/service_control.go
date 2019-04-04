17d639632e543ed74edc80758f816540ea40a24c
Thibault Richard <thb.krkr@gmail.com>
2019-04-04 15:05:58 +0200

    Extract the function and unit test

--- operators/pkg/controller/common/service_control.go
+++ operators/pkg/controller/common/service_control.go
@@ -34,31 +34,33 @@ func ReconcileService(
 		Expected:   expected,
 		Reconciled: reconciled,
 		NeedsUpdate: func() bool {
-
-			// ClusterIP might not exist in the expected service,
-			// but might have been set after creation by k8s on the actual resource.
-			// In such case, we want to use these values for comparison.
-			if expected.Spec.ClusterIP == "" {
-				expected.Spec.ClusterIP = reconciled.Spec.ClusterIP
-			}
-			// same for the target port and node port
-			if len(expected.Spec.Ports) == len(reconciled.Spec.Ports) {
-				for i := range expected.Spec.Ports {
-					if expected.Spec.Ports[i].TargetPort.IntValue() == 0 {
-						expected.Spec.Ports[i].TargetPort = reconciled.Spec.Ports[i].TargetPort
-					}
-					// check if NodePort makes sense for this service type
-					if hasNodePort(expected.Spec.Type) && expected.Spec.Ports[i].NodePort == 0 {
-						expected.Spec.Ports[i].NodePort = reconciled.Spec.Ports[i].NodePort
-					}
-				}
-			}
-			return !reflect.DeepEqual(expected.Spec, reconciled.Spec)
+			return needsUpdate(expected, reconciled)
 		},
 		UpdateReconciled: func() {
 			reconciled.Spec = expected.Spec // only update spec, keep the rest
 		},
 	})
 	return reconcile.Result{}, err
+}
 
+func needsUpdate(expected *corev1.Service, reconciled *corev1.Service) bool {
+	// ClusterIP might not exist in the expected service,
+	// but might have been set after creation by k8s on the actual resource.
+	// In such case, we want to use these values for comparison.
+	if expected.Spec.ClusterIP == "" {
+		expected.Spec.ClusterIP = reconciled.Spec.ClusterIP
+	}
+	// same for the target port and node port
+	if len(expected.Spec.Ports) == len(reconciled.Spec.Ports) {
+		for i := range expected.Spec.Ports {
+			if expected.Spec.Ports[i].TargetPort.IntValue() == 0 {
+				expected.Spec.Ports[i].TargetPort = reconciled.Spec.Ports[i].TargetPort
+			}
+			// check if NodePort makes sense for this service type
+			if hasNodePort(expected.Spec.Type) && expected.Spec.Ports[i].NodePort == 0 {
+				expected.Spec.Ports[i].NodePort = reconciled.Spec.Ports[i].NodePort
+			}
+		}
+	}
+	return !reflect.DeepEqual(expected.Spec, reconciled.Spec)
 }
---
+++ operators/pkg/controller/common/service_control_test.go
@@ -0,0 +1,141 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package common
+
+import (
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"reflect"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestNeedsUpdate(t *testing.T) {
+	type args struct {
+		expected   corev1.Service
+		reconciled corev1.Service
+	}
+	tests := []struct {
+		name string
+		args args
+		want corev1.Service
+	}{
+		{
+			name: "Reconciled clusterIP is used if expected clusterIP is empty",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP, ClusterIP: "",
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP, ClusterIP: "1.2.3.4",
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Type: corev1.ServiceTypeClusterIP, ClusterIP: "1.2.3.4",
+			}},
+		},
+		{
+			name: "Reconciled clusterIP is not used if expected clusterIP is set",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP, ClusterIP: "0.0.0.0",
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP, ClusterIP: "1.2.3.4",
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Type: corev1.ServiceTypeClusterIP, ClusterIP: "0.0.0.0",
+			}},
+		},
+		{
+			name: "Reconciled target ports are used",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{{Port: int32(9200)}},
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{{Port: int32(9200), TargetPort: intstr.FromInt(9200)}},
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{{Port: int32(9200), TargetPort: intstr.FromInt(9200)}},
+			}},
+		},
+		{
+			name: "Reconciled target ports are not used if there is not the same number of ports",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{{Port: int32(9200)}, {Port: int32(9300)}},
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{{Port: int32(9200), TargetPort: intstr.FromInt(9200)}},
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{{Port: int32(9200)}, {Port: int32(9300)}},
+			}},
+		},
+		{
+			name: "Reconciled node ports are used",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeLoadBalancer,
+					Ports: []corev1.ServicePort{{Port: int32(9200)}},
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeLoadBalancer,
+					Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33433)}},
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Type:  corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33433)}},
+			}},
+		},
+		{
+			name: "Reconciled node ports are not used if defined in the expected service",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33111)}},
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeLoadBalancer,
+					Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33222)}},
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Type:  corev1.ServiceTypeNodePort,
+				Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33111)}},
+			}},
+		},
+		{
+			name: "Reconciled node ports are not used if it makes no sense depending the service type",
+			args: args{
+				expected: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{{Port: int32(9200)}},
+				}},
+				reconciled: corev1.Service{Spec: corev1.ServiceSpec{
+					Type:  corev1.ServiceTypeLoadBalancer,
+					Ports: []corev1.ServicePort{{Port: int32(9200), NodePort: int32(33433)}},
+				}},
+			},
+			want: corev1.Service{Spec: corev1.ServiceSpec{
+				Type:  corev1.ServiceTypeClusterIP,
+				Ports: []corev1.ServicePort{{Port: int32(9200)}},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_ = needsUpdate(&tt.args.expected, &tt.args.reconciled)
+			if !reflect.DeepEqual(tt.args.expected, tt.want) {
+				t.Errorf("needsUpdate(expected, reconcilied); expected is %v, want %v", tt.args.expected, tt.want)
+			}
+		})
+	}
+}
