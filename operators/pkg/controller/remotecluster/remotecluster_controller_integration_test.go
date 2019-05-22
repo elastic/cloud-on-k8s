755cec6c51b3e81d1caf8aa90cf506e264643c03
sebgl <contact.sebgl@gmail.com>
2019-05-22 12:26:45 +0200

    Improve remote cluster integration test
    Rely on require instead of assert to fail early, don't bother with
    garbage collecting resources, pass the operator namespace to the
    initTrial helper function, remove checkReconcileCalled usage.

--- operators/pkg/controller/remotecluster/remotecluster_controller_integration_test.go
+++ operators/pkg/controller/remotecluster/remotecluster_controller_integration_test.go
@@ -9,35 +9,30 @@ package remotecluster
 import (
 	"errors"
 	"fmt"
+	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
-	"github.com/elastic/cloud-on-k8s/operators/pkg/utils/k8s"
-	"github.com/elastic/cloud-on-k8s/operators/pkg/utils/test"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
+	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/common/operator"
+	"github.com/elastic/cloud-on-k8s/operators/pkg/utils/test"
 )
 
 var (
-	c                     k8s.Client
 	key                   = types.NamespacedName{Name: "remotecluster-sample-1-2", Namespace: "default"}
 	trustRelationship1Key = types.NamespacedName{Name: "rc-remotecluster-sample-1-2", Namespace: "default"}
 	trustRelationship2Key = types.NamespacedName{Name: "rcr-remotecluster-sample-1-2-default", Namespace: "default"}
 )
 
-var expectedRequest = reconcile.Request{NamespacedName: key}
-
-const timeout = time.Second * 5
 func TestMain(m *testing.M) {
 	test.RunWithK8s(m, filepath.Join("..", "..", "..", "config", "crds"))
 }
 
 func TestReconcile(t *testing.T) {
+	operatorNamespace := "operator-namespace"
 
 	// start the test manager & controller
 	c, stop := test.StartTestController(t, Add, operator.Parameters{
@@ -52,39 +47,43 @@ func TestReconcile(t *testing.T) {
 	)
 
 	ca1 := newCASecret("default", "trust-one-es-ca", ca1)
-	assert.NoError(t, c.Create(ca1))
+	require.NoError(t, c.Create(ca1))
 	ca2 := newCASecret("default", "trust-two-es-ca", ca2)
-	assert.NoError(t, c.Create(ca2))
+	require.NoError(t, c.Create(ca2))
 
-	// Create the RemoteCluster object and expect the Reconcile and Deployment to be created
-	err = c.Create(instance)
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		return
-	}
-	assert.NoError(t, err)
-	defer c.Delete(instance)
-	test.CheckReconcileCalled(t, requests, expectedRequest)
+	// Create the RemoteCluster object
+	require.NoError(t, c.Create(instance))
 
-	trustRelationship1 := &v1alpha1.TrustRelationship{}
-	// commercial features disabled
-	assert.Error(t, c.Get(trustRelationship1Key, trustRelationship1))
-	test.StartTrial(t, c)
+	// commercial features disabled: no trust relationship should be created
+	test.RetryUntilSuccess(t, func() error {
+		// status should be updated accordingly
+		var remoteCluster v1alpha1.RemoteCluster
+		if err := c.Get(key, &remoteCluster); err != nil {
+			return err
+		}
+		if remoteCluster.Status.Phase != v1alpha1.RemoteClusterFeatureDisabled {
+			return errors.New("remote cluster status phase not set to disabled yet")
+		}
+		// there should be no trust relationship created
+		trustRelationship1 := &v1alpha1.TrustRelationship{}
+		require.True(t, apierrors.IsNotFound(c.Get(trustRelationship1Key, trustRelationship1)))
+		return nil
+	})
 
-	// looks like we need four rounds to do the actual reconciling
-	test.CheckReconcileCalledIn(t, requests, expectedRequest, 4, 4)
+	// start a trial license to enable the feature
+	test.StartTrial(t, c, operatorNamespace)
+
 	// expect the creation of the first TrustRelationship
+	trustRelationship1 := &v1alpha1.TrustRelationship{}
 	test.RetryUntilSuccess(t, func() error {
 		err := c.Get(trustRelationship1Key, trustRelationship1)
 		if err != nil {
 			return err
 		}
-		switch {
-		case len(trustRelationship1.Spec.CaCert) == 0:
-			return errors.New("Not reconciled yet")
-		default:
-			return nil
+		if len(trustRelationship1.Spec.CaCert) == 0 {
+			return errors.New("no ca cert reconciled yet")
 		}
+		return nil
 	})
 
 	// expect the creation of the second TrustRelationship
@@ -117,8 +116,6 @@ func TestReconcile(t *testing.T) {
 	test.DeleteIfExists(t, c, ca1)
 
 	// Ensure association goes back to pending if one of the CA is deleted.
-	test.CheckReconcileCalled(t, requests, expectedRequest)
-	test.CheckReconcileCalled(t, requests, expectedRequest)
 	test.RetryUntilSuccess(t, func() error {
 		fetched := v1alpha1.RemoteCluster{}
 		err := c.Get(key, &fetched)
@@ -130,6 +127,4 @@ func TestReconcile(t *testing.T) {
 		}
 		return nil
 	})
-
-	test.DeleteIfExists(t, c, instance)
 }
