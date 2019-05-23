29520782f3ba224b118294dd53d9bfbec7993009
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-05-23 14:53:20 +0200

    Run smoke e2e test during PR build (#903)
    * Add smoke test to run in PR build
    
    * Change Jenkins pipeline to run smoke e2e test
    
    * Update Makefile
    
    * Update Jenkinsfile
    
    * Disable CI job fail after first fail in parallel execution
    
    If set, then it will abort all other parallel tasks after first fail. If PR will introduce fails in both unit/integration and e2e tests, then it's a chance that we will need to run job twice to get both failures

---
+++ operators/test/e2e/smoke_test.go
@@ -0,0 +1,36 @@
+package e2e
+
+import (
+	"bufio"
+	"os"
+	"testing"
+
+	"github.com/elastic/cloud-on-k8s/operators/test/e2e/apm"
+	"github.com/elastic/cloud-on-k8s/operators/test/e2e/helpers"
+	"github.com/elastic/cloud-on-k8s/operators/test/e2e/stack"
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
+
+func TestSmoke(t *testing.T) {
+	var sampleStack stack.Builder
+	var sampleApm apm.Builder
+
+	yamlFile, err := os.Open(sampleEsApmFile)
+	helpers.ExitOnErr(err)
+
+	decoder := yaml.NewYAMLToJSONDecoder(bufio.NewReader(yamlFile))
+	helpers.ExitOnErr(decoder.Decode(&sampleStack.Elasticsearch))
+	helpers.ExitOnErr(decoder.Decode(&sampleApm.ApmServer))
+	helpers.ExitOnErr(decoder.Decode(&sampleStack.Kibana))
+
+	namespacedSampleStack := sampleStack.WithNamespace(helpers.DefaultNamespace)
+	namespacedSampleApm := sampleApm.WithNamespace(helpers.DefaultNamespace)
+
+	k := helpers.NewK8sClientOrFatal()
+	helpers.TestStepList{}.
+		WithSteps(stack.InitTestSteps(namespacedSampleStack, k)...).
+		WithSteps(apm.InitTestSteps(namespacedSampleApm, k)...).
+		WithSteps(stack.CreationTestSteps(namespacedSampleStack, k)...).
+		WithSteps(apm.CreationTestSteps(namespacedSampleApm, namespacedSampleStack.Elasticsearch, k)...).
+		RunSequential(t)
+}
