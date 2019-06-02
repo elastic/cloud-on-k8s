8c13e3b143eecdfedb70c6a9de2465eaedab338e
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-06-02 20:47:51 +0200

    Add test for license parsing

--- operators/pkg/controller/common/license/parser_test.go
+++ operators/pkg/controller/common/license/parser_test.go
@@ -5,3 +5,87 @@
  */
 
 package license
+
+import (
+	"io/ioutil"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseEnterpriseLicenses(t *testing.T) {
+	good, err := ioutil.ReadFile("testdata/test-license.json")
+	require.NoError(t, err)
+	bad, err := ioutil.ReadFile("testdata/test-error.json")
+	require.NoError(t, err)
+
+	type args struct {
+		raw map[string][]byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []SourceEnterpriseLicense
+		wantErr bool
+	}{
+		{
+			name: "single license",
+			args: args{
+				raw: map[string][]byte{
+					"_": good,
+				},
+			},
+			want: []SourceEnterpriseLicense{
+				expectedLicenseSpec,
+			},
+			wantErr: false,
+		},
+		{
+			name: "multiple licenses",
+			args: args{
+				raw: map[string][]byte{
+					"1": good,
+					"2": good,
+				},
+			},
+			want: []SourceEnterpriseLicense{
+				expectedLicenseSpec,
+				expectedLicenseSpec,
+			},
+			wantErr: false,
+		},
+		{
+			name: "malformed license",
+			args: args{
+				raw: map[string][]byte{
+					"_": bad,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "mixed good/bad: all or nothing",
+			args: args{
+				raw: map[string][]byte{
+					"1": good,
+					"2": bad,
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseEnterpriseLicenses(tt.args.raw)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseEnterpriseLicenses() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseEnterpriseLicenses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
