a492ad0fc3a555713375752b889adf794062504a
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-06-14 19:37:07 +0200

    Fixes issue where explicit non-pvc volume in template was ignored

--- operators/test/e2e/stack/builder.go
+++ operators/test/e2e/stack/builder.go
@@ -9,6 +9,7 @@ import (
 	"github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
 	estype "github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
 	kbtype "github.com/elastic/cloud-on-k8s/operators/pkg/apis/kibana/v1alpha1"
+	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/elasticsearch/volume"
 	"github.com/elastic/cloud-on-k8s/operators/test/e2e/params"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -131,6 +132,20 @@ func (b Builder) withESTopologyElement(topologyElement estype.NodeSpec) Builder 
 func (b Builder) WithESSecureSettings(secretName string) Builder {
 	b.Elasticsearch.Spec.SecureSettings = &commonv1alpha1.SecretRef{
 		SecretName: secretName,
+	}
+	return b
+}
+
+func (b Builder) WithEmptyDirVolumes() Builder {
+	for i := range b.Elasticsearch.Spec.Nodes {
+		b.Elasticsearch.Spec.Nodes[i].PodTemplate.Spec.Volumes = []corev1.Volume{
+			{
+				Name: volume.ElasticsearchDataVolumeName,
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				},
+			},
+		}
 	}
 	return b
 }
---
+++ operators/test/e2e/volume_test.go
@@ -0,0 +1,47 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package e2e
+
+import (
+	"testing"
+
+	"github.com/elastic/cloud-on-k8s/operators/test/e2e/helpers"
+	"github.com/elastic/cloud-on-k8s/operators/test/e2e/stack"
+)
+
+func TestVolumeDefaultPVC(t *testing.T) {
+	k := helpers.NewK8sClientOrFatal()
+
+	// create a single node cluster
+	initStack := stack.NewStackBuilder("test-es-default-pvc").
+		WithESMasterNodes(1, stack.DefaultResources)
+
+	helpers.TestStepList{}.
+		WithSteps(stack.InitTestSteps(initStack, k)...).
+		WithSteps(stack.CreationTestSteps(initStack, k)...).
+		WithSteps(stack.ESClusterChecks(initStack.Elasticsearch, k)...).
+		WithSteps(stack.CheckDefaultPVC(initStack.Elasticsearch, k)).
+		WithSteps(stack.DeletionTestSteps(initStack, k)...).
+		RunSequential(t)
+
+}
+
+func TestVolumeEmptyDir(t *testing.T) {
+	k := helpers.NewK8sClientOrFatal()
+
+	// create a single node cluster
+	initStack := stack.NewStackBuilder("test-es-explicit-empty-dir").
+		WithESMasterNodes(1, stack.DefaultResources).WithEmptyDirVolumes()
+
+	helpers.TestStepList{}.
+		WithSteps(stack.InitTestSteps(initStack, k)...).
+		WithSteps(stack.CreationTestSteps(initStack, k)...).
+		WithSteps(stack.ESClusterChecks(initStack.Elasticsearch, k)...).
+		WithSteps(stack.CheckEmptyDir(initStack.Elasticsearch, k)).
+		WithSteps(stack.DeletionTestSteps(initStack, k)...).
+		RunSequential(t)
+}
