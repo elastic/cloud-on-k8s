33a195defaf3faf08ed4015ce05ad2b296f4a508
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-06-14 16:05:25 +0200

    Shortcut license provisioning with cluster mutation

--- operators/test/e2e/license_test.go
+++ operators/test/e2e/license_test.go
@@ -28,21 +28,31 @@ func TestEnterpriseLicenseSingle(t *testing.T) {
 	require.NoError(t, err)
 
 	// create a single node cluster
-	s := stack.NewStackBuilder("test-es-license-provisioning").
+	initStack := stack.NewStackBuilder("test-es-license-provisioning").
 		WithESMasterDataNodes(1, stack.DefaultResources)
 
-	testContext := stack.NewLicenseTestContext(k, s.Elasticsearch)
+	mutated := initStack.
+		WithNoESTopology().
+		WithESMasterDataNodes(1, stack.DefaultResources)
+
+	testContext := stack.NewLicenseTestContext(k, initStack.Elasticsearch)
 
 	helpers.TestStepList{}.
-		WithSteps(stack.InitTestSteps(s, k)...).
-		WithSteps(stack.CreationTestSteps(s, k)...).
+		WithSteps(stack.InitTestSteps(initStack, k)...).
+		// make sure no left over license is still around
+		WithSteps(testContext.DeleteEnterpriseLicenseSecret()).
+		WithSteps(stack.CreationTestSteps(initStack, k)...).
+		WithSteps(testContext.Init()).
 		WithSteps(
-			testContext.WrapSteps(
-				testContext.CheckElasticsearchLicense(license.ElasticsearchLicenseTypeBasic),
-				testContext.CreateEnterpriseLicenseSecret(licenseBytes),
-				// enterprise license can contain all kinds of cluster licenses so we are a bit lenient here and expect either gold or platinum
-				testContext.CheckElasticsearchLicense(license.ElasticsearchLicenseTypeGold, license.ElasticsearchLicenseTypePlatinum),
-			)...).
+			testContext.CheckElasticsearchLicense(license.ElasticsearchLicenseTypeBasic),
+			testContext.CreateEnterpriseLicenseSecret(licenseBytes)).
+		// Mutation shortcuts the license provisioning check...
+		WithSteps(stack.MutationTestSteps(mutated, k)...).
+		// enterprise license can contain all kinds of cluster licenses so we are a bit lenient here and expect either gold or platinum
+		WithSteps(testContext.CheckElasticsearchLicense(
+			license.ElasticsearchLicenseTypeGold,
+			license.ElasticsearchLicenseTypePlatinum,
+		)).
 		RunSequential(t)
 
 }
