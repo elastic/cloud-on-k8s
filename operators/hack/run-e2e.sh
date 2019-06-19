55ee2b65ef3aa73c47228df7c0ed0c53bf55ea22
Michael Morello <michael.morello@elastic.co>
2019-06-19 10:23:25 +0200

    add some debug

--- operators/hack/run-e2e.sh
+++ operators/hack/run-e2e.sh
@@ -9,7 +9,7 @@
 # Usage: ./hack/run-e2e.sh <e2e_docker_image_name> <go_tests_matcher>
 #
 
-set -eu
+set -eux
 
 IMG="$1" # Docker image name
 TESTS_MATCH="$2" # Expression to match go test names (can be "")
