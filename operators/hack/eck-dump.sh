31a8e5570a2b07d65f046b532c6bb1b40d29e840
Thibault Richard <thb.krkr@gmail.com>
2019-06-20 11:31:33 +0200

    Utility to dump info to diagnose problems

---
+++ operators/hack/eck-dump.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+#
+# Support diagnostics utility for Elastic Cloud in Kubernetes (ECK)
+#
+set -eu
+
+help() {
+  echo 'Usage: ./eck-dump.sh [OPTIONS]
+
+Dumps Elastic Cloud in Kubernetes (ECK) info out suitable for debugging and diagnosing problems.
+
+By default, dumps everything to stdout. You can optionally specify a directory with --output-directory.
+By default only dumps things in the namespaces "elastic-system", "elastic-namespace-operators" and the 
+current, but you can switch to different namespaces with the --operator-namespaces and --elastic-namespaces
+flags.
+
+Options:
+  -N, --operator-namespaces     Namespace(s) in which operator(s) are running in (list separated by comma)
+  -n, --resources-namespaces    Namespace(s) in which resources are managed      (list separated by comma)
+  -d, --output-directory        Path to output dump files (default: .)
+  -v, --verbose                 Verbose mode
+
+Dependencies:
+  kubectl'
+  exit
+}
+
+OPERATOR_NS=elastic-system,elastic-namespace-operators
+ELASTIC_NS="default"
+OUTPUT_DIR=""
+VERBOSE=0
+
+parse_args() {
+  while :; do
+    local flag=${1:-""} value=${2:-""}
+    case $flag in
+    -h|--help)
+      help
+    ;;
+    -N|--operator-namespaces)
+      OPERATOR_NS=${value:-OPERATOR_NS}
+    ;;
+    -n|--elastic-namespaces)
+      ELASTIC_NS=${value:-${$(current_namespace):-ELASTIC_NS}}
+    ;;
+    -d|--output-directory)
+      OUTPUT_DIR=${value:-${OUTPUT_DIR:-"."}}
+    ;;
+    -v|--verbose)
+      VERBOSE=1
+    ;;
+    esac
+    shift || break
+  done
+}
+
+main() {
+  parse_args $@
+
+  IFS=, # use comma as field separator for iterations
+  
+  # start by checking the namespaces exist
+  local all_ns="$OPERATOR_NS,$ELASTIC_NS"
+  for ns in $all_ns; do
+    check_namespace $ns
+  done
+
+  # get global info
+  kubectl version   -o json | to_stdin_or_file version.json
+  kubectl get nodes -o json | to_stdin_or_file nodes.json
+
+  # get info from the namespaces in which operators are running in 
+  for ns in $OPERATOR_NS; do
+    get_resources $ns statefulsets
+    get_resources $ns pods
+    get_resources $ns services
+    get_resources $ns events
+    get_logs $ns
+  done
+
+  # get info from the namespaces in which resources are managed 
+  for ns in $ELASTIC_NS; do
+    get_resources $ns replicasets
+    get_resources $ns deployments
+    get_resources $ns pods
+    get_resources $ns services
+    get_resources $ns events
+    
+    local types="kibana,elasticsearch,apmserver"
+    for t in $types; do
+      get_resources $ns $t
+      get_logs $ns common.k8s.elastic.co/type=$t
+    done
+  done
+
+  if [[ "$OUTPUT_DIR" != "" ]]; then
+    echo "ECK info dumped to $OUTPUT_DIR"
+  fi
+}
+
+# get_resources lists resources in a speficied namespace in JSON output format
+get_resources() {
+  local ns=$1 resources=${2}
+  kubectl get -n $ns $resources -o json | to_stdin_or_file $ns/$resources.json
+}
+
+# get_logs retrieves logs for all pods in a specified namespace
+get_logs() {
+  local ns=$1 label=${2:-""} # optional label selector
+  while read name; do
+  (
+    echo "==== START logs for pod $ns/$name ===="
+    kubectl -n $ns logs $name --all-containers 
+    echo "==== END logs for pod $ns/$name ===="
+  ) | to_stdin_or_file $ns/$name/logs.txt
+  done \
+  < <(list_pods_names $ns $label)
+}
+
+# list_pods_names lists the names of the pods in a specified namespace
+list_pods_names() {
+  local ns=$1 label=${2:-}
+  [[ "$label" != "" ]] && label="-l $label"
+  kubectl get pods -n $ns $label --no-headers=true -o name 
+}
+
+# to_stdin_or_file redirects stdin to a file if OUTPUT_DIR is defined, else to stdout
+to_stdin_or_file() {
+  local filepath=$1
+  if [[ "$VERBOSE" == "1" ]]; then
+    >&2 echo "$OUTPUT_DIR/$filepath"
+  fi
+  if [[ $OUTPUT_DIR != "" ]]; then
+    mkdir -p $(dirname $OUTPUT_DIR/$filepath)
+    cat /dev/stdin > $OUTPUT_DIR/$filepath 
+  else
+    cat /dev/stdin
+  fi
+}
+
+# check_namespace fails and exits the program if the namespace does not exist
+check_namespace() {
+  local ns="$1"
+  kubectl get namespace $1 >/dev/null
+}
+
+# current_namespace returns the current namespace, empty if there is none
+current_namespace() {
+  kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null
+}
+
+main "$@"
