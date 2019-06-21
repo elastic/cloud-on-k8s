f098dbb46d0d50d80990972da2d6b46172763a9d
Thibault Richard <thb.krkr@gmail.com>
2019-06-21 10:52:14 +0200

    Fix license header

--- operators/pkg/controller/common/certificates/x509_othername.go
+++ operators/pkg/controller/common/certificates/x509_othername.go
@@ -2,21 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// ELASTICSEARCH CONFIDENTIAL
-// __________________
-//
-//  Copyright Elasticsearch B.V. All rights reserved.
-//
-// NOTICE:  All information contained herein is, and remains
-// the property of Elasticsearch B.V. and its suppliers, if any.
-// The intellectual and technical concepts contained herein
-// are proprietary to Elasticsearch B.V. and its suppliers and
-// may be covered by U.S. and Foreign Patents, patents in
-// process, and are protected by trade secret or copyright
-// law.  Dissemination of this information or reproduction of
-// this material is strictly forbidden unless prior written
-// permission is obtained from Elasticsearch B.V.
-
 package certificates
 
 import (
