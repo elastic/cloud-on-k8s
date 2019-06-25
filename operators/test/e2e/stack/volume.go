c620d77da746c04729fed29945da4573b2c047c4
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-06-25 20:58:00 +0200

    refactor default storage class check

--- operators/test/e2e/stack/volume.go
+++ operators/test/e2e/stack/volume.go
@@ -9,13 +9,16 @@ package stack
 import (
 	"testing"
 
+	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/common"
 	"github.com/elastic/cloud-on-k8s/operators/test/e2e/helpers"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/storage/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+const IsDefaultStorageClassAnnotation common.TrueFalseLabel = "storageclass.kubernetes.io/is-default-class"
+const BetaIsDefaultStorageClassAnnotation common.TrueFalseLabel = "storageclass.beta.kubernetes.io/is-default-class"
 
 func CreateStorageClass(storageClass v1.StorageClass, k *helpers.K8sHelper) helpers.TestStep {
 	return helpers.TestStep{
@@ -36,26 +39,11 @@ func DefaultStorageClass(k *helpers.K8sHelper) (*v1.StorageClass, error) {
 		return nil, err
 	}
 	for _, sc := range scs.Items {
-		if isDefaultAnnotation(sc.ObjectMeta) {
+		if IsDefaultStorageClassAnnotation.HasValue(true, sc.Annotations) ||
+			BetaIsDefaultStorageClassAnnotation.HasValue(true, sc.Annotations) {
 			return &sc, nil
 		}
 
 	}
 	return nil, errors.New("no default storage class found")
-}
-
-const IsDefaultStorageClassAnnotation = "storageclass.kubernetes.io/is-default-class"
-const BetaIsDefaultStorageClassAnnotation = "storageclass.beta.kubernetes.io/is-default-class"
-
-// IsDefaultAnnotation returns a boolean if
-// the annotation is set
-func isDefaultAnnotation(obj metav1.ObjectMeta) bool {
-	if obj.Annotations[IsDefaultStorageClassAnnotation] == "true" {
-		return true
-	}
-	if obj.Annotations[BetaIsDefaultStorageClassAnnotation] == "true" {
-		return true
-	}
-
-	return false
 }
