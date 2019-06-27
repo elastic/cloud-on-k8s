c2c60a63199a4f870e4c6cdd83636a53825dc32b
Michael Morello <michael.morello@elastic.co>
2019-06-27 07:59:13 +0200

    fix test

--- operators/pkg/controller/apmserver/config/config_test.go
+++ operators/pkg/controller/apmserver/config/config_test.go
@@ -64,7 +64,7 @@ func Test_getCredentials(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotUsername, gotPassword, err := getCredentials(tt.args.c, tt.args.as)
+			gotUsername, gotPassword, err := elasticsearchAuthSettings(tt.args.c, tt.args.as)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("getCredentials() error = %v, wantErr %v", err, tt.wantErr)
 				return
