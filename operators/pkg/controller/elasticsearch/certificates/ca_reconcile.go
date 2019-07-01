5d237cbadf42a41e60e8a1d485d83af06774b339
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-07-01 10:29:50 +0200

    Remove RemoteCluster and TrustRelationship (#1164)
    * Remove RemoteCluster and TrustRelationship
    
    * Remove remote cluster status and RBAC

--- operators/pkg/controller/elasticsearch/certificates/ca_reconcile.go
+++ operators/pkg/controller/elasticsearch/certificates/ca_reconcile.go
@@ -81,11 +81,6 @@ func Reconcile(
 		return nil, results.WithError(err)
 	}
 
-	trustRelationships, err := transport.LoadTrustRelationships(c, es.Name, es.Namespace)
-	if err != nil {
-		return nil, results.WithError(err)
-	}
-
 	transportCA, err := certificates.ReconcileCAForOwner(
 		c,
 		scheme,
@@ -115,7 +110,6 @@ func Reconcile(
 		transportCA,
 		es,
 		services,
-		trustRelationships,
 		certRotation,
 	)
 	if results.WithResult(result).WithError(err).HasError() {
