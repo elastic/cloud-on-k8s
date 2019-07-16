573323d840394de6c6dbf2e7ee35167cac1e581e
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-07-16 11:25:14 +0200

    fix license header

--- operators/test/e2e/test/version.go
+++ operators/test/e2e/test/version.go
@@ -2,12 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 package test
 
 import (
