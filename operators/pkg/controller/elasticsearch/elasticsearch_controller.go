218997ac722916d0fa5c41096cd34e092a8889ca
Jon Sabo <jon@sabolicio.us>
2019-07-18 19:04:13 -0500

    WIP

--- operators/pkg/controller/elasticsearch/elasticsearch_controller.go
+++ operators/pkg/controller/elasticsearch/elasticsearch_controller.go
@@ -8,6 +8,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"k8s.io/apimachinery/pkg/selection"
+
 	elasticsearchv1alpha1 "github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
 	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/common"
 	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/common/annotation"
@@ -26,10 +28,14 @@ import (
 	"github.com/elastic/cloud-on-k8s/operators/pkg/controller/elasticsearch/validation"
 	"github.com/elastic/cloud-on-k8s/operators/pkg/utils/k8s"
 	semver "github.com/hashicorp/go-version"
+	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -191,7 +197,14 @@ func (r *ReconcileElasticsearch) Reconcile(request reconcile.Request) (reconcile
 		log.Info("Object is paused. Skipping reconciliation", "namespace", es.Namespace, "es_name", es.Name, "iteration", currentIteration)
 		return common.PauseRequeue, nil
 	}
-
+	compat, err := r.reconcileCompatibility(&es)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	if !compat {
+		// this resource is not able to be reconciled by this version of the controller, so we will skip it and not requeue
+		return reconcile.Result{}, nil
+	}
 	err = annotation.UpdateControllerVersion(r.Client, &es, r.OperatorInfo.BuildInfo.Version)
 	if err != nil {
 		return reconcile.Result{}, err
@@ -285,30 +298,77 @@ func (r *ReconcileElasticsearch) finalizersFor(
 	}
 }
 
-// At the beginning of each reconciliation, retrieve the current resource version from annotations
-// if the annotation is set, use it (eg. return 0.9)
-// if the annotation isn't set, 2 possibilities:
-// a. the resource was just created and does not have the annotation yet
-// b. the resource was created with ECK 0.8
-// To detect if we are in case b, list pods and secrets labeled with the ES cluster name (or APM or Kibana): if there are some, then the resource was created with ECK 0.8 (return 0.8). If there are none (length 0), return noVersion. Listing pods and secrets from the cache should be a very cheap operation.
-// Check compatibility of the resource version with the operator version. Each ECK version comes with its own comparison logic (but in most cases, it should probably just check the annotated resource version is in the backward compatibility range of the operator version).
-// If not compatible, log and return. Don't reconcile.
-// If compatible:
-// Make sure the resource is annotated with the current operator version. If it isn't, annotate it and requeue. What we want to avoid here is moving on with creating pods/secrets while the resource isn't yet annotated in the cache (we would end up in the 0.8 scenario if newly created pods and secrets appear in the cache before the annotated resource appears).
-// Move on to normal reconciliation
+// reconcileCompatibility determines if this controller is compatible with a given resource by examining the controller version annotation
+// controller versions 0.9.0+ cannot reconcile resources created with earlier controllers, so this lets our controller skip those resources until they can be manually recreated
+// if an object does not have an annotation, it will determine if it is a new object or if it has been previously reconciled by an older controller version, as this annotation
+// was not applied by earlier controller versions. it will update the object's annotations indicating it is incompatible if so
+func (r *ReconcileElasticsearch) reconcileCompatibility(es *elasticsearchv1alpha1.Elasticsearch) (bool, error) {
+	annExists := es.Annotations != nil && es.Annotations[annotation.ControllerVersionAnnotation] != ""
 
-func (r *ReconcileElasticsearch) checkCompatibility(es *elasticsearchv1alpha1.Elasticsearch) (bool, error) {
-	var (
-		compatible bool
-		err        error
-	)
-	if es.Annotations == nil {
-		// skip to
-		return compatible, err
+	// if the annotation does not exist, it might indicate it was reconciled by an older controller version that did not add the version annotation,
+	// in which case it is incompatible with the current controller, or it is a brand new resource that has not been reconciled by any controller yet
+	if !annExists {
+		exist, err := r.checkExistingResources(es)
+		if err != nil {
+			return false, err
+		}
+		if exist {
+			log.Info("Resource was previously reconciled by incompatible controller version and missing annotation, adding annotation", "controller_version", r.OperatorInfo.BuildInfo.Version, "namespace", es.Namespace, "es_name", es.Name)
+			err = annotation.UpdateControllerVersion(r.Client, es, "0.8.0-UNKNOWN")
+			return false, err
+		}
+		// no annotation exists and there are no existing resources, so this has not previously been reconciled
+		err = annotation.UpdateControllerVersion(r.Client, es, r.OperatorInfo.BuildInfo.Version)
+		return true, err
 	}
-	currentVersion, err := semver.NewVersion(es.Annotations["test"])
-	minVersion, err := semver.NewVersion("0.9.0")
-	operatorVersion, err := semver.NewVersion(r.OperatorInfo.BuildInfo.Version)
 
-	return compatible, err
+	// if we have an annotation we need to check if it is compatible
+	currentVersion, err := semver.NewVersion(es.Annotations[annotation.ControllerVersionAnnotation])
+	if err != nil {
+		return false, errors.Wrap(err, "Error parsing current version on resource")
+	}
+	minVersion, err := semver.NewVersion("0.9.0-ALPHA")
+	if err != nil {
+		return false, errors.Wrap(err, "Error parsing minimum compatible version")
+	}
+	ctrlVersion, err := semver.NewVersion(r.OperatorInfo.BuildInfo.Version)
+	if err != nil {
+		return false, errors.Wrap(err, "Error parsing controller version")
+	}
+
+	// if the current version is gte the minimum version then they are compatible
+	if currentVersion.GreaterThanOrEqual(minVersion) {
+		log.V(1).Info("Current controller version on resource is compatible with running controller version", "controller_version", ctrlVersion.String(),
+			"resource_controller_version", currentVersion.String(), "namespace", es.Namespace, "es_name", es.Name)
+		return true, nil
+	}
+
+	log.Info("Resource was created with older version of operator, will not take action", "controller_version", ctrlVersion.String(),
+		"resource_controller_version", currentVersion.String(), "namespace", es.Namespace, "es_name", es.Name)
+	return false, nil
+}
+
+// checkExistingResources returns a bool indicating if there are existing resources created for a given resource
+func (r *ReconcileElasticsearch) checkExistingResources(es *elasticsearchv1alpha1.Elasticsearch) (bool, error) {
+	// if there's no controller version annotation on the ES instance, then we need to see maybe the CR has been reconciled by an older, incompatible controller version
+	selector := labels.NewSelector()
+	req, err := labels.NewRequirement(label.ClusterNameLabelName, selection.Equals, []string{es.Name})
+	if err != nil {
+		return false, err
+	}
+	selector.Add(*req)
+	opts := ctrlclient.ListOptions{
+		LabelSelector: selector,
+		Namespace:     es.Namespace,
+	}
+	pods := v1.PodList{}
+	err = r.Client.List(&opts, &pods)
+	if err != nil {
+		return false, err
+	}
+	// if we listed any pods successfully, then we know this cluster was reconciled by an old version since any CRs reconciled by a 0.9.0+ operator would have a label
+	if len(pods.Items) != 0 {
+		return true, err
+	}
+	return false, nil
 }
