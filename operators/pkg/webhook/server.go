5ded918a9fd26a24863b3e5c5bdc7bc0369f480b
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-07-22 23:25:24 +0200

    Workaround controller-runtime webhook upsert bug

--- operators/pkg/webhook/server.go
+++ operators/pkg/webhook/server.go
@@ -5,11 +5,14 @@
 package webhook
 
 import (
+	"context"
+
 	"github.com/elastic/cloud-on-k8s/operators/pkg/apis/elasticsearch/v1alpha1"
 	"github.com/elastic/cloud-on-k8s/operators/pkg/webhook/elasticsearch"
 	"github.com/elastic/cloud-on-k8s/operators/pkg/webhook/license"
 	admission "k8s.io/api/admissionregistration/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -51,6 +54,17 @@ func RegisterValidations(mgr manager.Manager, params Parameters) error {
 	}
 
 	disabled := !params.AutoInstall
+	if params.AutoInstall {
+		// nasty side effect in register function
+		webhookSvc := corev1.Service{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      params.Bootstrap.Service.Name,
+				Namespace: params.Bootstrap.Service.Namespace,
+			},
+		}
+		// best effort deletion attempt to handle incompatible services from previous versions
+		_ = mgr.GetClient().Delete(context.Background(), &webhookSvc)
+	}
 	svr, err := webhook.NewServer(admissionServerName, mgr, webhook.ServerOptions{
 		Port:                          serverPort,
 		CertDir:                       "/tmp/cert",
