5ebb90e3448320b48e1da174f2dacd6df069b303
Michael Morello <michael.morello@elastic.co>
2019-08-19 12:11:17 +0200

    Add missing header

--- operators/hack/kind/kind.sh
+++ operators/hack/kind/kind.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+
 ##################################################################################
 # Utility script to:                                                             #
 # 1. Setup new Kind cluster                                                      #
