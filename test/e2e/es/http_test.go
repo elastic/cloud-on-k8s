ec7c3fedc91aadd0ad97778c07d2b139e57d9998
Peter Brachwitz <peter.brachwitz@elastic.co>
2019-08-23 16:43:37 +0200

    add e2e test

---
+++ test/e2e/es/http_test.go
@@ -0,0 +1,23 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package es
+
+import (
+	"testing"
+
+	"github.com/elastic/cloud-on-k8s/test/e2e/test"
+	"github.com/elastic/cloud-on-k8s/test/e2e/test/elasticsearch"
+)
+
+// TestHTTPWithoutTLS tests a manual override of the default persistent storage with emptyDir.
+func TestHTTPWithoutTLS(t *testing.T) {
+	b := elasticsearch.NewBuilder("test-es-http").
+		WithESMasterDataNodes(1, elasticsearch.DefaultResources).
+		WithTLSDisabled()
+
+	// volume type will be checked in creation steps
+	test.Sequence(nil, test.EmptySteps, b).
+		RunSequential(t)
+}
