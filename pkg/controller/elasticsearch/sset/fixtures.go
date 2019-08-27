cf8aea21d36315498ca6385ea648e74e5d2bd9b7
sebgl <contact.sebgl@gmail.com>
2019-08-27 18:08:18 +0200

    Remove a bit that belongs to another PR

--- pkg/controller/elasticsearch/sset/fixtures.go
+++ pkg/controller/elasticsearch/sset/fixtures.go
@@ -5,11 +5,11 @@
 package sset
 
 import (
-	"github.com/elastic/cloud-on-k8s/pkg/controller/common/hash"
-	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/label"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/label"
 )
 
 type TestSset struct {
@@ -30,7 +30,7 @@ func (t TestSset) Build() appsv1.StatefulSet {
 	}
 	label.NodeTypesMasterLabelName.Set(t.Master, labels)
 	label.NodeTypesDataLabelName.Set(t.Data, labels)
-	statefulSet := appsv1.StatefulSet{
+	return appsv1.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: t.Name,
 		},
@@ -50,13 +50,6 @@ func (t TestSset) Build() appsv1.StatefulSet {
 		},
 		Status: t.Status,
 	}
-	statefulSet.Labels = hash.SetTemplateHashLabel(statefulSet.Labels, statefulSet.Spec)
-	return statefulSet
-}
-
-func (t TestSset) BuildPtr() *appsv1.StatefulSet {
-	built := t.Build()
-	return &built
 }
 
 type TestPod struct {
