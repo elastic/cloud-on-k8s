2bfc656f1659aff0d3c69ed358068be2ec0bc04e
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-09-02 08:23:27 +0200

    Update kind.sh

--- hack/kind/kind.sh
+++ hack/kind/kind.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License;
