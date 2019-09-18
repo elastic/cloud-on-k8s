8fd15189de49ddabecd85bb9faacd34e52ca0f7b
Charith Ellawala <charith.ellawala@elastic.co>
2019-09-18 15:24:52 +0100

    Restore update script

---
+++ hack/update-samples-stack-version.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+
+#
+# Update the Elastic stack version in samples 
+# Usage: ./hack/update-samples-stack-version.sh <version>
+#
+
+set -eu
+
+# Elastic stack version
+VERSION="$1"
+
+# Directories containing version references to replace
+: "${DIRS:="config/samples"}"
+
+# For all yaml files in the directory trees, replace the existing version with sed.
+# We use the "-i.bak" trick to be compatible with both Linux and OSX.
+# We are replacing occurrences of:
+# - version: 1.2.3<EOL>
+# - version: "1.2.3"<EOL>
+LC_CTYPE=C LANG=C find ${DIRS} -type f -iname \*.yaml \
+    -exec sed -i.bak -E "s|version: \"?[0-9]\.[0-9]\.[0-9]\"?$|version: $VERSION|g" "{}" \; \
+    -exec rm "{}.bak" \;
