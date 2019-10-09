2f53fc82dd09a7518b0ec733ab8bdb56279be859
Michael Morello <michael.morello@elastic.co>
2019-10-09 11:09:34 +0200

    Add namespace and es name

--- pkg/controller/elasticsearch/elasticsearch_controller.go
+++ pkg/controller/elasticsearch/elasticsearch_controller.go
@@ -251,6 +251,8 @@ func (r *ReconcileElasticsearch) internalReconcile(
 	if len(violations) > 0 {
 		log.Error(
 			fmt.Errorf("manifest validation failed"), "Elasticsearch manifest validation failed",
+			"namespace", es.Namespace,
+			"es_name", es.Name,
 			"violations", violations,
 		)
 		reconcileState.UpdateElasticsearchInvalid(violations)
