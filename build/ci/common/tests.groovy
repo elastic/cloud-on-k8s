42914141bc89e0601962b84ab50e3ebebca04b89
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-10-15 12:03:17 +0200

    Add list of failed tests for Slack notification for jobs with only one e2e test run

---
+++ build/ci/common/tests.groovy
@@ -0,0 +1,16 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// Common test related code for Jenkins pipelines
+
+def getListOfFailedTests() {
+    def failed = currentBuild.rawBuild.getAction(hudson.tasks.test.AbstractTestResultAction.class)?.getResult()?.getFailedTests()
+    def result = []
+    failed.each { ft ->
+        result.add(String.format("%s\r\n%s", ft.getDisplayName(), ft.getErrorStackTrace()))
+    }
+    return result
+}
+
+return this
