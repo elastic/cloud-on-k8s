c07b666d0ac6e73d5a6fba5bb4c875c017a3c029
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-10-17 12:27:23 +0200

    Add failed e2e tests to Slack notifications (#1998)
    * Temprorarily add XML with test output for test purposes
    
    * Add list of failed tests for Slack notification for jobs with only one e2e test run
    
    * Temp change for test, need to revert later
    
    * Add failed tests to Slack notification to job with parallel e2e tests
    
    * No need to provide additional info for tests running in parallel
    
    * Update tests.groovy
    
    * Filter failed tests
    
    * Temp set for local testing
    
    * Move Slack message generation to common
    
    * Revert "Temprorarily add XML with test output for test purposes"
    
    This reverts commit 836248f0ffd43b17bfe373c8efad884ea5c622be.
    
    * Return back proper channel for notifications
    
    * Cleanup

---
+++ build/ci/common/tests.groovy
@@ -0,0 +1,32 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// Common test related code for Jenkins pipelines
+
+def getListOfFailedTests() {
+    def failed = currentBuild.rawBuild.getAction(hudson.tasks.test.AbstractTestResultAction.class)?.getResult()?.getFailedTests()
+    def result = []
+    failed.each { ft ->
+        result.add(String.format("%s\r\n%s", ft.getDisplayName(), ft.getErrorStackTrace()))
+    }
+    return result
+}
+
+def generateSlackMessage(baseMsg, URL, failedTests) {
+    def sb = new StringBuilder()
+    sb.append(baseMsg)
+    sb.append("\r\n")
+    sb.append(URL)
+    if (failedTests.size > 0) {
+        sb.append("\r\n")
+        sb.append("List of failed tests:")
+        failedTests.each { ft ->
+            sb.append("\r\n")
+            sb.append(ft)
+        }
+    }
+    return sb.toString()
+}
+
+return this
