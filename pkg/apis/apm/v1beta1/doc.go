e570fd2a77a999f1868e15f236615257343756ba
Charith Ellawala <charith.ellawala@elastic.co>
2019-10-22 16:35:33 +0100

    Address CR comments

--- pkg/apis/apm/v1beta1/doc.go
+++ pkg/apis/apm/v1beta1/doc.go
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// Package v1beta1 contains API schema definitions for managing APM server resources.
+// Package v1beta1 contains API schema definitions for managing APM Server resources.
 // +kubebuilder:object:generate=true
 // +groupName=apm.k8s.elastic.co
 package v1beta1
--- pkg/apis/common/v1beta1/common.go
+++ pkg/apis/common/v1beta1/common.go
@@ -71,7 +71,7 @@ type TLSOptions struct {
 	// Certificate is a reference to a Kubernetes secret that contains the certificate and private key for enabling TLS.
 	// The referenced secret should contain the following:
 	//
-	// - `ca.crt`: The certificate authority (optional)
+	// - `ca.crt`: The certificate authority (optional).
 	// - `tls.crt`: The certificate (or a chain).
 	// - `tls.key`: The private key to the first certificate in the certificate chain.
 	Certificate SecretRef `json:"certificate,omitempty"`
