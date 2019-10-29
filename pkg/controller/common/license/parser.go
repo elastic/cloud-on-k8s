0848e8476bb78e7f44660e6fde66f89e4bb592af
David Kowalski <david.kowalski@elastic.co>
2019-10-29 07:14:16 +0100

    Use any file in license secret data

--- pkg/controller/common/license/parser.go
+++ pkg/controller/common/license/parser.go
@@ -12,9 +12,30 @@ import (
 
 func ParseEnterpriseLicense(raw map[string][]byte) (EnterpriseLicense, error) {
 	var license EnterpriseLicense
-	err := json.Unmarshal(raw[FileName], &license)
+
+	bytes, err := FetchLicenseData(raw)
 	if err != nil {
-		return EnterpriseLicense{}, errors.Wrapf(err, "license cannot be unmarshalled")
+		return license, err
 	}
+
+	err = json.Unmarshal(bytes, &license)
+	if err != nil {
+		return license, errors.Wrapf(err, "license cannot be unmarshalled")
+	}
+
 	return license, nil
+}
+
+func FetchLicenseData(raw map[string][]byte) ([]byte, error) {
+	if len(raw) != 1 {
+		return nil, errors.New("Elasticsearch license secret needs to contain exactly one file with any name")
+	}
+
+	var result []byte
+	// will only loop once due to the check above
+	for _, bytes := range raw {
+		result = bytes
+	}
+
+	return result, nil
 }
