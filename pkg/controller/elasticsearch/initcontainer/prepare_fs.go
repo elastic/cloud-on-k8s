f2026a0f93bca254befe57038f8a8afef0f3142d
Sander Mathijssen <smathijssen@binck.nl>
2019-11-28 16:10:57 +0100

    Added limits to initpod

--- pkg/controller/elasticsearch/initcontainer/prepare_fs.go
+++ pkg/controller/elasticsearch/initcontainer/prepare_fs.go
@@ -15,6 +15,7 @@ import (
 	esvolume "github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/volume"
 	"github.com/elastic/cloud-on-k8s/pkg/utils/stringsutil"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 const (
@@ -77,6 +78,17 @@ var (
 			},
 		},
 	}
+	//defaultResources are the default request and limits for the init container.
+	defaultResources = corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceMemory: resource.MustParse("500Mi"),
+			corev1.ResourceCPU:    resource.MustParse("500m"),
+		},
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceMemory: resource.MustParse("1Gi"),
+			corev1.ResourceCPU:    resource.MustParse("1"),
+		},
+	}
 )
 
 // NewPrepareFSInitContainer creates an init container to handle things such as:
@@ -117,6 +129,7 @@ func NewPrepareFSInitContainer(
 			esvolume.DefaultDataVolumeMount,
 			esvolume.DefaultLogsVolumeMount,
 		),
+		Resources: defaultResources,
 	}
 
 	return container, nil
