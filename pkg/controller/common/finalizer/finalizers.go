272a3cd0147665b1b4b817a692d6992b3a5a7e7b
Michael Morello <michael.morello@elastic.co>
2019-12-03 16:50:49 +0100

    only remove Elastic finalizers

--- pkg/controller/common/finalizer/finalizers.go
+++ pkg/controller/common/finalizer/finalizers.go
@@ -5,10 +5,14 @@
 package finalizer
 
 import (
+	"regexp"
+
 	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 )
+
+const finalizersRegExp = `^finalizer\.(.*)\.k8s.elastic.co\/(.*)$`
 
 // RemoveAll removes all existing Finalizers on an Object
 func RemoveAll(c k8s.Client, obj runtime.Object) error {
@@ -19,6 +23,25 @@ func RemoveAll(c k8s.Client, obj runtime.Object) error {
 	if len(accessor.GetFinalizers()) == 0 {
 		return nil
 	}
-	accessor.SetFinalizers([]string{})
+	filterFinalizers, err := filterFinalizers(accessor.GetFinalizers())
+	if err != nil {
+		return err
+	}
+	accessor.SetFinalizers(filterFinalizers)
 	return c.Update(obj)
+}
+
+// filterFinalizers removes Elastic finalizers
+func filterFinalizers(finalizers []string) ([]string, error) {
+	filteredFinalizers := make([]string, 0)
+	r, err := regexp.Compile(finalizersRegExp)
+	if err != nil {
+		return filteredFinalizers, err
+	}
+	for _, finalizer := range finalizers {
+		if !r.MatchString(finalizer) {
+			filteredFinalizers = append(filteredFinalizers, finalizer)
+		}
+	}
+	return filteredFinalizers, nil
 }
