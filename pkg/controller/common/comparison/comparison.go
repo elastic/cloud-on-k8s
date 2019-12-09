07ea14457bd87fd7949c16e5348b09f9c53d9ff1
Anya Sabo <anya@sabolicio.us>
2019-12-09 17:23:54 -0600

    Update reconciler

--- pkg/controller/common/comparison/comparison.go
+++ pkg/controller/common/comparison/comparison.go
@@ -4,12 +4,13 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // Equal compares two objects ignoring the TypeMeta and ResourceVersion. Often used for tests ensuring that we receive structs that match what we expect without
 // runtime-specific information
 // Does it make sense to consume metav1.Object or runtime.Object?
-func Equal(a, b metav1.Object) bool {
+func Equal(a, b runtime.Object) bool {
 	typemeta := cmpopts.IgnoreTypes(metav1.TypeMeta{})
 	rv := cmpopts.IgnoreFields(metav1.ObjectMeta{}, "ResourceVersion")
 	return cmp.Equal(a, b, typemeta, rv)
@@ -18,7 +19,7 @@ func Equal(a, b metav1.Object) bool {
 // Diff compares two objects ignoring the TypeMeta and ResourceVersion. Often used for tests ensuring that we receive structs that match what we expect without
 // runtime-specific information
 // Does it make sense to consume metav1.Object or runtime.Object?
-func Diff(a, b metav1.Object) string {
+func Diff(a, b runtime.Object) string {
 	typemeta := cmpopts.IgnoreTypes(metav1.TypeMeta{})
 	rv := cmpopts.IgnoreFields(metav1.ObjectMeta{}, "ResourceVersion")
 	return cmp.Diff(a, b, typemeta, rv)
--- pkg/controller/common/comparison/comparison_test.go
+++ pkg/controller/common/comparison/comparison_test.go
@@ -1,18 +1,20 @@
 package comparison
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestEqual(t *testing.T) {
 	tt := []struct {
 		name     string
-		a        metav1.Object
-		b        metav1.Object
+		a        runtime.Object
+		b        runtime.Object
 		expected bool
 	}{
 		{
