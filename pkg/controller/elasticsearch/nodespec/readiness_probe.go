654a03d5c6ee720def5872b1ffcb5843dc93870f
Michael Morello <michael.morello@elastic.co>
2019-12-16 15:57:47 +0100

    Redirect errors to stderr

--- pkg/controller/elasticsearch/nodespec/readiness_probe.go
+++ pkg/controller/elasticsearch/nodespec/readiness_probe.go
@@ -32,7 +32,7 @@ const ReadinessProbeScript = `#!/usr/bin/env bash
 # fail should be called as a last resort to help the user to understand why the probe failed
 function fail {
   timestamp=$(date --iso-8601=seconds)
-  echo "{\"timestamp\": \"${timestamp}\", \"message\": \"readiness probe failed\", "$1"}" | tee /proc/1/fd/1
+  echo "{\"timestamp\": \"${timestamp}\", \"message\": \"readiness probe failed\", "$1"}" | tee /proc/1/fd/2
   exit 1
 }
 
