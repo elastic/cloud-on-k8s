8c8f241104c682b42ee9bc83915595b59851a1f5
Sebastien Guilloux <contact.sebgl@gmail.com>
2020-01-08 11:03:52 +0100

    Backport 30sec preStop timeout & associated docs (#2369)
    * Document PreStop hook and it's env vars (#2274)
    
    * Document PreStop hook and it's env vars
    
    * Add missing token
    
    * Update docs/elasticsearch-specification.asciidoc
    
    Co-Authored-By: alaudazzi <46651782+alaudazzi@users.noreply.github.com>
    
    * Update docs/elasticsearch-specification.asciidoc
    
    Co-Authored-By: Anya Sabo <1638148+anyasabo@users.noreply.github.com>
    
    * Update docs/elasticsearch-specification.asciidoc
    
    Co-Authored-By: Anya Sabo <1638148+anyasabo@users.noreply.github.com>
    
    * Fix asciidoc tag
    
    Co-authored-by: alaudazzi <46651782+alaudazzi@users.noreply.github.com>
    Co-authored-by: Anya Sabo <1638148+anyasabo@users.noreply.github.com>
    
    * Wait 30sec in the preStop hook to account for kube-proxy refresh (#2360)
    
    * Wait 30sec in the preStop hook to account for kube-proxy refresh
    
    Kube-proxy defaults to a 30sec iptables-sync-period (overridden to 10sec
    on GKE).
    
    For that reason, when an Elasticsearch Pod is gracefully shutdown, some
    clients in the k8s cluster might still target the Pod IP, because the
    local kube-proxy iptables (or ipvs) rules are not updated yet.
    
    Let's increase our initial 1 sec additional wait time to 30 sec so we
    account for those changes. It has the side-effect of slowing down
    Pods deletion (hence rolling upgrades) by 30sec per Pod, but should help
    keeping Elasticsearch available during rolling upgrades.
    
    This commit also increases the Elasticsearch container gracePeriod to
    account for the increased wait time. I changed it from 120 to 180
    seconds (technically could have been 150, but 180 rounds up to 3
    minutes).
    
    * Fix references in the doc
    
    * Use the default 30sec ADDITIONAL_WAIT_SECONDS in E2E test
    
    * Add a prefix to the preStop env variables
    
    This makes the variable names more explicit when overridden by the user
    in the ES spec.
    
    * Improve comments and docs with kube-proxy details
    
    * Wording: dont guarantee no downtime, but minimise unavailability
    
    Co-authored-by: david-kow <50632861+david-kow@users.noreply.github.com>
    Co-authored-by: alaudazzi <46651782+alaudazzi@users.noreply.github.com>
    Co-authored-by: Anya Sabo <1638148+anyasabo@users.noreply.github.com>

--- pkg/controller/elasticsearch/nodespec/lifecycle_hook.go
+++ pkg/controller/elasticsearch/nodespec/lifecycle_hook.go
@@ -23,31 +23,34 @@ const PreStopHookScript = `#!/usr/bin/env bash
 
 set -eux
 
-# This script will wait for up to $MAX_WAIT_SECONDS for $POD_IP to disappear from DNS record,
-# then it will wait additional $ADDITIONAL_WAIT_SECONDS and exit. This slows down the process shutdown
+# This script will wait for up to $PRE_STOP_MAX_WAIT_SECONDS for $POD_IP to disappear from DNS record,
+# then it will wait additional $PRE_STOP_ADDITIONAL_WAIT_SECONDS and exit. This slows down the process shutdown
 # and allows to make changes to the pool gracefully, without blackholing traffic when DNS
 # contains IP that is already inactive. Assumes $HEADLESS_SERVICE_NAME and $POD_IP env variables are defined.
 
-# max time to wait for pods IP to disappear from DNS. As this runs in parallel to grace period
-# (defaulting to 30s) after which process is SIGKILLed, it should be set to allow enough time
-# for the process to gracefully terminate.
-MAX_WAIT_SECONDS=${MAX_WAIT_SECONDS:=20}
+# Max time to wait for pods IP to disappear from DNS.
+# As this runs in parallel to grace period after which process is SIGKILLed,
+# it should be set to allow enough time for the process to gracefully terminate.
+PRE_STOP_MAX_WAIT_SECONDS=${PRE_STOP_MAX_WAIT_SECONDS:=20}
 
-# additional wait, allows queries to successfully use IP from DNS from before pod termination
-# this gives a little bit more time for clients that resolved DNS just before DNS record
-# was updated.
-ADDITIONAL_WAIT_SECONDS=${ADDITIONAL_WAIT_SECONDS:=1}
+# Additional wait before shutting down Elasticsearch.
+# It allows kube-proxy to refresh its rules and remove the terminating Pod IP.
+# Kube-proxy refresh period defaults to every 30 seconds, but the operation itself can take much longer if
+# using iptables with a lot of services, in which case the default 30sec might not be enough.
+# Also gives some additional bonus time to in-flight requests to terminate, and new requests to still
+# target the Pod IP before Elasticsearch stops.
+PRE_STOP_ADDITIONAL_WAIT_SECONDS=${PRE_STOP_ADDITIONAL_WAIT_SECONDS:=30}
 
 START_TIME=$(date +%s)
 while true; do
    ELAPSED_TIME=$(($(date +%s) - $START_TIME))
 
-   if [ $ELAPSED_TIME -gt $MAX_WAIT_SECONDS ]; then
+   if [ $ELAPSED_TIME -gt $PRE_STOP_MAX_WAIT_SECONDS ]; then
       exit 1
    fi
 
    if ! getent hosts $HEADLESS_SERVICE_NAME | grep $POD_IP; then
-      sleep $ADDITIONAL_WAIT_SECONDS
+      sleep $PRE_STOP_ADDITIONAL_WAIT_SECONDS
       exit 0
    fi
 
