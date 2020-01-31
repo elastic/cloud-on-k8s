01437745f4bf2bae5539adb5535859289ff852b0
David Kowalski <50632861+david-kow@users.noreply.github.com>
2020-01-31 10:42:24 +0100

    Add config validation for unknown fields (#2433)
    * Add es config validation for unknown fields
    
    * Make validation generic, add UT
    
    * Added additional check to prevent locking users from updates
    
    * Document how to bypass this validation
    
    * Revert "Added additional check to prevent locking users from updates"
    
    This reverts commit 205c8f9c8eb57249728ed48d11de2b2ca06c8702.
    
    * Improve docs and error message
    
    * Fix PR comments
    
    * Update docs/troubleshooting.asciidoc
    
    Co-Authored-By: Peter Brachwitz <peter.brachwitz@gmail.com>
    
    * Update error message in docs
    
    * Fix PR comment
    
    Co-authored-by: Peter Brachwitz <peter.brachwitz@gmail.com>

---
+++ pkg/apis/common/v1/validations.go
@@ -0,0 +1,40 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// NoUnknownFields checks whether the last applied config annotation contains json with unknown fields.
+func NoUnknownFields(dest interface{}, meta metav1.ObjectMeta) field.ErrorList {
+	var errs field.ErrorList
+	if cfg, ok := meta.Annotations[v1.LastAppliedConfigAnnotation]; ok {
+		d := json.NewDecoder(strings.NewReader(cfg))
+		d.DisallowUnknownFields()
+		if err := d.Decode(&dest); err != nil {
+			errString := err.Error()
+			unknownPrefix := "json: unknown field "
+			if strings.HasPrefix(errString, unknownPrefix) {
+				fld := strings.TrimPrefix(errString, unknownPrefix)
+				if len(fld) >= 2 {
+					fld = fld[1 : len(fld)-1] // removes quotes from fld
+				}
+				err := field.Invalid(
+					field.NewPath(fld),
+					fld,
+					fmt.Sprintf("%s field found in the %s annotation is unknown", fld, v1.LastAppliedConfigAnnotation))
+				errs = append(errs, err)
+			}
+		}
+	}
+	return errs
+}
