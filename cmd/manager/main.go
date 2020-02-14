elastic/cloud-on-k8s

commit 3819530fd62f700a9f56a0a71e4ef13d61ef3d5f

Remove operator roles (#2530)
* remove the concept of operator roles as flags
* remove the global operator manifests 
* keep/modify the namespace operator manifests to allow deployments restricted to one or more namespaces
* allow the parameterization of the operator name to allow multiple (namespaced) operators to be deployed in a single namespace
* make the operator logs target work for both variants
* run the e2e tests with a all-in-one operator
* add a dedicated webhook toggle flag
* disable the webhook by default in the namespaced variant, enable it in the all-in-one variant
* Remove duplicated word

Co-authored-by: Michael Morello <michael.morello@gmail.com>

cmd/manager/main.go
@@ -123,6 +123,11 @@ func init() {
 		false,
 		"Enable APM tracing in the operator. Endpoint, token etc are to be configured via environment variables. See https://www.elastic.co/guide/en/apm/agent/go/1.x/configuration.html")
 	Cmd.Flags().Bool(
+		operator.EnableWebhookFlag,
+		false,
+		"Enables a validating webhook server in the operator process.",
+	)
+	Cmd.Flags().Bool(
 		operator.ManageWebhookCertsFlag,
 		true,
 		"Enables automatic certificates management for the webhook. The Secret and the ValidatingWebhookConfiguration must be created before running the operator",
@@ -141,11 +146,6 @@ func init() {
 		operator.OperatorNamespaceFlag,
 		"",
 		"K8s namespace the operator runs in",
-	)
-	Cmd.Flags().StringSlice(
-		operator.OperatorRolesFlag,
-		[]string{operator.All},
-		"Roles this operator should assume (either namespace, global, webhook or all)",
 	)
 	Cmd.Flags().String(
 		operator.WebhookCertDirFlag,
@@ -268,14 +268,6 @@ func execute() {
 	caCertValidity, caCertRotateBefore := ValidateCertExpirationFlags(operator.CACertValidityFlag, operator.CACertRotateBeforeFlag)
 	certValidity, certRotateBefore := ValidateCertExpirationFlags(operator.CertValidityFlag, operator.CertRotateBeforeFlag)
 
-	// Setup all Controllers
-	roles := viper.GetStringSlice(operator.OperatorRolesFlag)
-	err = operator.ValidateRoles(roles)
-	if err != nil {
-		log.Error(err, "invalid roles specified")
-		os.Exit(1)
-	}
-
 	// Setup a client to set the operator uuid config map
 	clientset, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
@@ -283,12 +275,12 @@ func execute() {
 		os.Exit(1)
 	}
 
-	operatorInfo, err := about.GetOperatorInfo(clientset, operatorNamespace, roles)
+	operatorInfo, err := about.GetOperatorInfo(clientset, operatorNamespace)
 	if err != nil {
 		log.Error(err, "unable to get operator info")
 		os.Exit(1)
 	}
-	log.Info("Setting up controllers", "roles", roles)
+	log.Info("Setting up controllers")
 	var tracer *apm.Tracer
 	if viper.GetBool(operator.EnableTracingFlag) {
 		tracer = tracing.NewTracer("elastic-operator")
@@ -308,7 +300,7 @@ func execute() {
 		Tracer: tracer,
 	}
 
-	if operator.HasRole(operator.WebhookServer, roles) {
+	if viper.GetBool(operator.EnableWebhookFlag) {
 		setupWebhook(mgr, params.CertRotation, clientset)
 	}
 
@@ -321,48 +313,46 @@ func execute() {
 		accessReviewer = rbac.NewPermissiveAccessReviewer()
 	}
 
-	if operator.HasRole(operator.NamespaceOperator, roles) {
-		if err = apmserver.Add(mgr, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "ApmServer")
-			os.Exit(1)
-		}
-		if err = elasticsearch.Add(mgr, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "Elasticsearch")
-			os.Exit(1)
-		}
-		if err = kibana.Add(mgr, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "Kibana")
-			os.Exit(1)
-		}
-		if err = asesassn.Add(mgr, accessReviewer, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "ApmServerElasticsearchAssociation")
-			os.Exit(1)
-		}
-		if err = kbassn.Add(mgr, accessReviewer, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "KibanaAssociation")
-			os.Exit(1)
-		}
-
-		// Garbage collect any orphaned user Secrets leftover from deleted resources while the operator was not running.
-		garbageCollectUsers(cfg, managedNamespaces)
+	if err = apmserver.Add(mgr, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "ApmServer")
+		os.Exit(1)
 	}
-	if operator.HasRole(operator.GlobalOperator, roles) {
-		if err = license.Add(mgr, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "License")
-			os.Exit(1)
-		}
-		if err = licensetrial.Add(mgr, params); err != nil {
-			log.Error(err, "unable to create controller", "controller", "LicenseTrial")
-			os.Exit(1)
-		}
-
-		go func() {
-			time.Sleep(10 * time.Second)         // wait some arbitrary time for the manager to start
-			mgr.GetCache().WaitForCacheSync(nil) // wait until k8s client cache is initialized
-			r := licensing.NewResourceReporter(mgr.GetClient())
-			r.Start(operatorNamespace, licensing.ResourceReporterFrequency)
-		}()
+	if err = elasticsearch.Add(mgr, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "Elasticsearch")
+		os.Exit(1)
 	}
+	if err = kibana.Add(mgr, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "Kibana")
+		os.Exit(1)
+	}
+	if err = asesassn.Add(mgr, accessReviewer, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "ApmServerElasticsearchAssociation")
+		os.Exit(1)
+	}
+	if err = kbassn.Add(mgr, accessReviewer, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "KibanaAssociation")
+		os.Exit(1)
+	}
+
+	if err = license.Add(mgr, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "License")
+		os.Exit(1)
+	}
+	if err = licensetrial.Add(mgr, params); err != nil {
+		log.Error(err, "unable to create controller", "controller", "LicenseTrial")
+		os.Exit(1)
+	}
+
+	// Garbage collect any orphaned user Secrets leftover from deleted resources while the operator was not running.
+	garbageCollectUsers(cfg, managedNamespaces)
+
+	go func() {
+		time.Sleep(10 * time.Second)         // wait some arbitrary time for the manager to start
+		mgr.GetCache().WaitForCacheSync(nil) // wait until k8s client cache is initialized
+		r := licensing.NewResourceReporter(mgr.GetClient())
+		r.Start(operatorNamespace, licensing.ResourceReporterFrequency)
+	}()
+
 	log.Info("Starting the manager", "uuid", operatorInfo.OperatorUUID,
 		"namespace", operatorNamespace, "version", operatorInfo.BuildInfo.Version,
 		"build_hash", operatorInfo.BuildInfo.Hash, "build_date", operatorInfo.BuildInfo.Date,

Makefile
@@ -66,11 +66,11 @@ SKIP_DOCKER_COMMAND ?= false
 
 ## -- Namespaces
 
-# namespace in which the global operator is deployed (see config/global-operator)
-GLOBAL_OPERATOR_NAMESPACE ?= elastic-system
-# namespace in which the namespace operator is deployed (see config/namespace-operator)
-NAMESPACE_OPERATOR_NAMESPACE ?= elastic-namespace-operators
-# comma separated list of namespaces in which the namespace operator should watch resources
+# namespace in which the operator is deployed (see config/operator)
+OPERATOR_NAMESPACE ?= elastic-system
+# name of the operator statefulset and related resources
+OPERATOR_NAME ?= elastic-operator
+# comma separated list of namespaces in which the operator should watch resources
 MANAGED_NAMESPACES ?=
 
 ## -- Security
@@ -150,13 +150,13 @@ install-crds: generate-crds
 run: install-crds go-run
 
 go-run:
-	# Run the operator locally with role All, with debug logs, operator image set to latest and operator namespace for a global operator
+	# Run the operator locally with debug logs and operator image set to latest
 	AUTO_PORT_FORWARD=true \
 		go run \
 			-ldflags "$(GO_LDFLAGS)" \
 			-tags "$(GO_TAGS)" \
 			./cmd/main.go manager \
-				--development --operator-roles=global,namespace \
+				--development \
 				--log-verbosity=$(LOG_VERBOSITY) \
 				--ca-cert-validity=10h --ca-cert-rotate-before=1h \
 				--operator-namespace=default \
@@ -169,7 +169,6 @@ go-debug:
 		-- \
 		manager \
 		--development \
-		--operator-roles=global,namespace \
 		--log-verbosity=$(LOG_VERBOSITY) \
 		--ca-cert-validity=10h \
 		--ca-cert-rotate-before=1h \
@@ -190,16 +189,14 @@ ifndef GCLOUD_PROJECT
 endif
 endif
 
-# Deploy both the global and namespace operators against the current k8s cluster
-deploy: check-gke install-crds build-operator-image apply-operators
+# Deploy the operator against the current k8s cluster
+deploy: check-gke install-crds build-operator-image apply-operator
 
-apply-operators:
+apply-operator:
 	OPERATOR_IMAGE=$(OPERATOR_IMAGE) \
-	NAMESPACE=$(GLOBAL_OPERATOR_NAMESPACE) \
-		$(MAKE) --no-print-directory -sC config/operator generate-global | kubectl apply -f -
-	OPERATOR_IMAGE=$(OPERATOR_IMAGE) \
-	NAMESPACE=$(NAMESPACE_OPERATOR_NAMESPACE) \
-	MANAGED_NAMESPACE=$(MANAGED_NAMESPACE) \
+	OPERATOR_NAME=$(OPERATOR_NAME) \
+	NAMESPACE=$(OPERATOR_NAMESPACE) \
+	MANAGED_NAMESPACES=$(MANAGED_NAMESPACES) \
 		$(MAKE) --no-print-directory -sC config/operator generate-namespace | kubectl apply -f -
 
 apply-psp:
@@ -211,7 +208,8 @@ ALL_IN_ONE_OUTPUT_FILE=config/all-in-one.yaml
 generate-all-in-one:
 	cp -f $(ALL_CRDS) $(ALL_IN_ONE_OUTPUT_FILE)
 	OPERATOR_IMAGE=$(OPERATOR_IMAGE) \
-		NAMESPACE=$(GLOBAL_OPERATOR_NAMESPACE) \
+		OPERATOR_NAME=$(OPERATOR_NAME) \
+		NAMESPACE=$(OPERATOR_NAMESPACE) \
 		$(MAKE) --no-print-directory -sC config/operator generate-all-in-one >> $(ALL_IN_ONE_OUTPUT_FILE)
 
 # Deploy an all in one operator against the current k8s cluster
@@ -219,11 +217,8 @@ deploy-all-in-one: GO_TAGS ?= release
 deploy-all-in-one: docker-build docker-push
 	kubectl apply -f $(ALL_IN_ONE_OUTPUT_FILE)
 
-logs-namespace-operator:
-	@ kubectl --namespace=$(NAMESPACE_OPERATOR_NAMESPACE) logs -f statefulset.apps/elastic-namespace-operator
-
-logs-global-operator:
-	@ kubectl --namespace=$(GLOBAL_OPERATOR_NAMESPACE) logs -f statefulset.apps/elastic-global-operator
+logs-operator:
+	@ kubectl --namespace=$(OPERATOR_NAMESPACE) logs -f statefulset.apps/$(OPERATOR_NAME)
 
 samples:
 	@ echo "-> Pushing samples to Kubernetes cluster..."
@@ -242,7 +237,7 @@ cluster-bootstrap: install-crds
 
 clean-k8s-cluster:
 	kubectl delete --ignore-not-found=true  ValidatingWebhookConfiguration validating-webhook-configuration
-	for ns in $(NAMESPACE_OPERATOR_NAMESPACE) $(GLOBAL_OPERATOR_NAMESPACE) $(MANAGED_NAMESPACE); do \
+	for ns in $(OPERATOR_NAMESPACE) $(MANAGED_NAMESPACES); do \
 		echo "Deleting resources in $$ns"; \
 		kubectl delete statefulsets -n $$ns --all; \
 		kubectl delete deployments -n $$ns --all; \
@@ -474,7 +469,7 @@ kind-with-operator-%: kind-node-variable-check docker-build
 	./hack/kind/kind.sh \
 		--load-images $(OPERATOR_IMAGE) \
 		--nodes "${*}" \
-		make install-crds apply-operators
+		make install-crds apply-operator
 
 ## Run all the e2e tests in a Kind cluster
 set-kind-e2e-image:

config/e2e/managed_namespaces.yaml
@@ -1,5 +1,5 @@
 {{- $testRun := .TestRun -}}
-{{- range .NamespaceOperator.ManagedNamespaces }}
+{{- range .Operator.ManagedNamespaces }}
 ---
 apiVersion: v1
 kind: Namespace

config/e2e/namespace_operator.yaml
@@ -1,224 +1,0 @@
-{{- $testRun := .TestRun -}}
-{{- $operatorImage := .OperatorImage -}}
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: elastic-operator
-  labels:
-    test-run: {{ .TestRun }}
-rules:
-- apiGroups:
-  - ""
-  resources:
-  - configmaps
-  verbs:
-  - get
-  - list
-  - create
-  - delete
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: elastic-namespace-operator
-  labels:
-    test-run: {{ .TestRun }}
-rules:
-- apiGroups:
-  - ""
-  resources:
-  - pods
-  - endpoints
-  - events
-  - persistentvolumeclaims
-  - secrets
-  - services
-  - configmaps
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - apps
-  resources:
-  - deployments
-  - statefulsets
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - policy
-  resources:
-  - poddisruptionbudgets
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - elasticsearch.k8s.elastic.co
-  resources:
-  - elasticsearches
-  - elasticsearches/status
-  - elasticsearches/finalizers
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - kibana.k8s.elastic.co
-  resources:
-  - kibanas
-  - kibanas/status
-  - kibanas/finalizers
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - associations.k8s.elastic.co
-  resources:
-  - apmserverelasticsearchassociations
-  - apmserverelasticsearchassociations/status
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - apm.k8s.elastic.co
-  resources:
-  - apmservers
-  verbs:
-  - get
-  - list
-  - watch
-  - create
-  - update
-  - patch
-  - delete
-- apiGroups:
-  - apm.k8s.elastic.co
-  resources:
-  - apmservers/status
-  - apmservers/finalizers
-  verbs:
-  - get
-  - update
-  - patch
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: {{ .NamespaceOperator.Name }}
-  namespace: {{ .NamespaceOperator.Namespace }}
-  labels:
-    test-run: {{ $testRun }}
----
-# allow operator to manage resources in its namespace
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: {{ .NamespaceOperator.Name }}
-  namespace: {{ .NamespaceOperator.Namespace }}
-  labels:
-    test-run: {{ $testRun }}
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: elastic-operator
-subjects:
-- kind: ServiceAccount
-  name: {{ .NamespaceOperator.Name }}
-  namespace: {{ .NamespaceOperator.Namespace }}
-
-{{- $nsOperator := .NamespaceOperator -}}
-{{ range $nsOperator.ManagedNamespaces }}
----
-# allow operator to manage resources in the managed namespace
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: {{ $nsOperator.Name }}
-  namespace: {{ . }}
-  labels:
-    test-run: {{ $testRun }}
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: elastic-namespace-operator
-subjects:
-- kind: ServiceAccount
-  name: {{ $nsOperator.Name }}
-  namespace: {{ $nsOperator.Namespace }}
-{{- end }}
-
----
-apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  name: {{ .NamespaceOperator.Name }}
-  namespace: {{ .NamespaceOperator.Namespace }}
-  labels:
-    control-plane: {{ .NamespaceOperator.Name }}
-    test-run: {{ $testRun }}
-spec:
-  selector:
-    matchLabels:
-      control-plane: {{ .NamespaceOperator.Name }}
-  serviceName: {{ .NamespaceOperator.Name }}
-  template:
-    metadata:
-      annotations:
-        # Rename the fields "error" to "error.message" and "source" to "event.source"
-        # This is to avoid a conflict with the ECS "error" and "source" documents.
-        "co.elastic.logs/raw": "[{\"type\":\"container\",\"json.keys_under_root\":true,\"paths\":[\"/var/log/containers/*${data.kubernetes.container.id}.log\"],\"processors\":[{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"error\",\"to\":\"_error\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"_error\",\"to\":\"error.message\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"source\",\"to\":\"_source\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"_source\",\"to\":\"event.source\"}]}}]}]"
-      labels:
-        control-plane: {{ .NamespaceOperator.Name }}
-        test-run: {{ $testRun }}
-    spec:
-      serviceAccountName: {{ .NamespaceOperator.Name }}
-      containers:
-      - image: {{ $operatorImage }}
-        imagePullPolicy: IfNotPresent
-        name: manager
-        args: ["manager", "--namespaces", "{{ .NamespaceOperator.ManagedNamespaces | join "," }}", "--operator-roles", "namespace", "--log-verbosity=1"]
-        env:
-          - name: OPERATOR_NAMESPACE
-            valueFrom:
-              fieldRef:
-                fieldPath: metadata.namespace
-          - name: OPERATOR_IMAGE
-            value: {{ $operatorImage }}
-        resources:
-          limits:
-            cpu: 1
-            memory: 100Mi
-          requests:
-            cpu: 100m
-            memory: 20Mi
-      terminationGracePeriodSeconds: 10

config/e2e/operator.yaml
@@ -2,18 +2,16 @@
 apiVersion: v1
 kind: ServiceAccount
 metadata:
-  name: {{ .GlobalOperator.Name }}
-  namespace: {{ .GlobalOperator.Namespace }}
+  name: {{ .Operator.Name }}
+  namespace: {{ .Operator.Namespace }}
   labels:
     test-run: {{ .TestRun }}
 ---
-# The global operator has cluster-wide permissions on all required resources.
-# Same resources as the namespace operator, except for the addition of:
-# - enterpriselicenses
+# The operator has cluster-wide permissions on all required resources.
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
-  name: elastic-global-operator
+  name: elastic-operator
   labels:
     test-run: {{ .TestRun }}
 rules:
@@ -40,6 +38,18 @@ rules:
   resources:
   - deployments
   - statefulsets
+  verbs:
+  - get
+  - list
+  - watch
+  - create
+  - update
+  - patch
+  - delete
+- apiGroups:
+  - policy
+  resources:
+  - poddisruptionbudgets
   verbs:
   - get
   - list
@@ -122,39 +132,39 @@ rules:
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
-  name: elastic-global-operator
+  name: elastic-operator
   labels:
     test-run: {{ .TestRun }}
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
-  name: elastic-global-operator
+  name: elastic-operator
 subjects:
 - kind: ServiceAccount
-  name: {{ .GlobalOperator.Name }}
-  namespace: {{ .GlobalOperator.Namespace }}
+  name: {{ .Operator.Name }}
+  namespace: {{ .Operator.Namespace }}
 ---
 apiVersion: v1
 kind: Secret
 metadata:
   name: webhook-server-secret
-  namespace: {{ .GlobalOperator.Namespace }}
+  namespace: {{ .Operator.Namespace }}
   labels:
     test-run: {{ .TestRun }}
 ---
 apiVersion: apps/v1
 kind: StatefulSet
 metadata:
-  name: {{ .GlobalOperator.Name }}
-  namespace: {{ .GlobalOperator.Namespace }}
+  name: {{ .Operator.Name }}
+  namespace: {{ .Operator.Namespace }}
   labels:
-    control-plane: {{ .GlobalOperator.Name }}
+    control-plane: {{ .Operator.Name }}
     test-run: {{ .TestRun }}
 spec:
   selector:
     matchLabels:
-      control-plane: {{ .GlobalOperator.Name }}
-  serviceName: {{ .GlobalOperator.Name }}
+      control-plane: {{ .Operator.Name }}
+  serviceName: {{ .Operator.Name }}
   template:
     metadata:
       annotations:
@@ -162,15 +172,15 @@ spec:
         # This is to avoid a conflict with the ECS "error" and "source" documents.
         "co.elastic.logs/raw": "[{\"type\":\"container\",\"json.keys_under_root\":true,\"paths\":[\"/var/log/containers/*${data.kubernetes.container.id}.log\"],\"processors\":[{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"error\",\"to\":\"_error\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"_error\",\"to\":\"error.message\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"source\",\"to\":\"_source\"}]}},{\"convert\":{\"mode\":\"rename\",\"ignore_missing\":true,\"fields\":[{\"from\":\"_source\",\"to\":\"event.source\"}]}}]}]"
       labels:
-        control-plane: {{ .GlobalOperator.Name }}
+        control-plane: {{ .Operator.Name }}
         test-run: {{ .TestRun }}
     spec:
-      serviceAccountName: {{ .GlobalOperator.Name }}
+      serviceAccountName: {{ .Operator.Name }}
       containers:
       - image: {{ .OperatorImage }}
         imagePullPolicy: IfNotPresent
         name: manager
-        args: ["manager", "--operator-roles", "global,webhook", "--log-verbosity=1"]
+        args: ["manager", "--namespaces", "{{ .Operator.ManagedNamespaces | join "," }}", "--enable-webhook", "--log-verbosity=1"]
         env:
           - name: OPERATOR_NAMESPACE
             valueFrom:
@@ -179,7 +189,7 @@ spec:
           - name: WEBHOOK_SECRET
             value: webhook-server-secret
           - name: WEBHOOK_PODS_LABEL
-            value: {{ .GlobalOperator.Name }}
+            value: {{ .Operator.Name }}
           - name: OPERATOR_IMAGE
             value: {{ .OperatorImage }}
         resources:
@@ -208,13 +218,13 @@ apiVersion: admissionregistration.k8s.io/v1beta1
 kind: ValidatingWebhookConfiguration
 metadata:
   name: elastic-webhook.k8s.elastic.co
-  namespace: {{ .GlobalOperator.Namespace }}
+  namespace: {{ .Operator.Namespace }}
 webhooks:
   - clientConfig:
       caBundle: Cg==
       service:
         name: elastic-webhook-server
-        namespace: {{ .GlobalOperator.Namespace }}
+        namespace: {{ .Operator.Namespace }}
         # this is the path controller-runtime automatically generates
         path: /validate-elasticsearch-k8s-elastic-co-v1beta1-elasticsearch
     failurePolicy: {{ if .IgnoreWebhookFailures }}Ignore{{ else }}Fail{{ end }}
@@ -233,7 +243,7 @@ webhooks:
       caBundle: Cg==
       service:
         name: elastic-webhook-server
-        namespace: {{ .GlobalOperator.Namespace }}
+        namespace: {{ .Operator.Namespace }}
         # this is the path controller-runtime automatically generates
         path: /validate-elasticsearch-k8s-elastic-co-v1-elasticsearch
     failurePolicy: {{ if .IgnoreWebhookFailures }}Ignore{{ else }}Fail{{ end }}
@@ -253,10 +263,10 @@ apiVersion: v1
 kind: Service
 metadata:
   name: elastic-webhook-server
-  namespace: {{ .GlobalOperator.Namespace }}
+  namespace: {{ .Operator.Namespace }}
 spec:
   ports:
     - port: 443
       targetPort: 9443
   selector:
-    control-plane: {{ .GlobalOperator.Name }}
+    control-plane: {{ .Operator.Name }}

config/e2e/operator_namespaces.yaml
@@ -3,13 +3,6 @@
 apiVersion: v1
 kind: Namespace
 metadata:
-  name: {{ .GlobalOperator.Namespace }}
-  labels:
-    test-run: {{ $testRun }}
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: {{ .NamespaceOperator.Namespace }}
+  name: {{ .Operator.Namespace }}
   labels:
     test-run: {{ $testRun }}

config/e2e/rbac.yaml
@@ -57,11 +57,8 @@ subjects:
     name: system:serviceaccounts
     namespace: kube-system
   - kind: ServiceAccount
-    name: {{ .GlobalOperator.Name }}
-    namespace: {{ .GlobalOperator.Namespace }}
-  - kind: ServiceAccount
-    name: {{ .NamespaceOperator.Name }}
-    namespace: {{ .NamespaceOperator.Namespace }}
+    name: {{ .Operator.Name }}
+    namespace: {{ .Operator.Namespace }}
 ---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole

config/operator/Makefile
@@ -14,6 +14,9 @@ endif
 ifndef OPERATOR_IMAGE
 $(error OPERATOR_IMAGE not set)
 endif
+ifndef OPERATOR_NAME
+$(error OPERATOR_NAME not set)
+endif
 
 generate-all-in-one:
 	@ for yaml in $$(LC_COLLATE=C ls all-in-one/*yaml); do \
@@ -22,15 +25,7 @@ generate-all-in-one:
 	sed \
 		-e "/# /d" \
 		-e "s|<OPERATOR_IMAGE>|$$OPERATOR_IMAGE|g" \
-		-e "s|<NAMESPACE>|$$NAMESPACE|g"
-
-generate-global:
-	@ for yaml in $$(LC_COLLATE=C ls global/*yaml); do \
-		printf "\n---\n" && cat $$yaml ; \
-	done | \
-	sed \
-		-e "/# /d" \
-		-e "s|<OPERATOR_IMAGE>|$$OPERATOR_IMAGE|g" \
+		-e "s|<OPERATOR_NAME>|$$OPERATOR_NAME|g" \
 		-e "s|<NAMESPACE>|$$NAMESPACE|g"
 
 generate-namespace:
@@ -43,6 +38,7 @@ endif
 	sed \
 		-e "/# /d" \
 		-e "s|<OPERATOR_IMAGE>|$$OPERATOR_IMAGE|g" \
+		-e "s|<OPERATOR_NAME>|$$OPERATOR_NAME|g" \
 		-e "s|<NAMESPACE>|$$NAMESPACE|g" \
 		-e "s|<MANAGED_NAMESPACE_LIST>|$$MANAGED_NAMESPACES|g"
 
@@ -51,6 +47,7 @@ endif
 		printf "\n---\n" && sed \
 		-e "/# /d" \
 		-e "s|<NAMESPACE>|$$NAMESPACE|g" \
+		-e "s|<OPERATOR_NAME>|$$OPERATOR_NAME|g" \
 		-e "s|<MANAGED_NAMESPACE>|$$managed_ns|g" \
 		namespace/managed_ns_role_binding.template.yaml ;\
 	  done)

commit f63396e905a2fbaa5f5746fc9655044bddf400f5

Always include operator namespace in cached k8s client namespaces (#2566)
The trial license e2e test was failing since #2530 because we are now running the e2e with a operator restricted to the managed namespaces via --namespaces, but we did not include the operator namespace itself in the list of namespaces.

As the list of namespaces controls which resources will be cached by the k8s client the operator was not able to access the license related secrets in the operator namespace and trial mode could not be activated.

This commit makes sure the operator namespace is always included in the client cache even when running in namespace restricted mode.

cmd/manager/main.go
@@ -239,15 +239,16 @@ func execute() {
 
 	// configure the manager cache based on the number of managed namespaces
 	managedNamespaces := viper.GetStringSlice(operator.NamespacesFlag)
-	switch len(managedNamespaces) {
-	case 0:
+	switch {
+	case len(managedNamespaces) == 0:
 		log.Info("Operator configured to manage all namespaces")
-	case 1:
-		log.Info("Operator configured to manage a single namespace", "namespace", managedNamespaces[0])
+	case len(managedNamespaces) == 1 && managedNamespaces[0] == operatorNamespace:
+		log.Info("Operator configured to manage a single namespace", "namespace", managedNamespaces[0], "operator_namespace", operatorNamespace)
 		opts.Namespace = managedNamespaces[0]
 	default:
-		log.Info("Operator configured to manage multiple namespaces", "namespaces", managedNamespaces)
-		opts.NewCache = cache.MultiNamespacedCacheBuilder(managedNamespaces)
+		log.Info("Operator configured to manage multiple namespaces", "namespaces", managedNamespaces, "operator_namespace", operatorNamespace)
+		// always include the operator namespace into the manager cache so that we can work with operator-internal resources in there
+		opts.NewCache = cache.MultiNamespacedCacheBuilder(append(managedNamespaces, operatorNamespace))
 	}
 
 	// only expose prometheus metrics if provided a non-zero port