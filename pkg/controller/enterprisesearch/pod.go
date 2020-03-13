a1bd8301d82da484611085e7a52e16b948860346
Sebastien Guilloux <contact.sebgl@gmail.com>
2020-03-13 10:47:44 +0100

    Add the EnterpriseSearch CRD and controller (#2688)
    Introduce the EnterpriseSearch CRD and controller in ECK.
    
    It seems to be working fine so far but has not been extensively tested.
    To be used, it requires the image field to be set with a valid Docker image.
    
    This is obviously work in progress, since:
    
    parts of the controller should be refactored to share common code with APMServer and Kibana
    most of the association controller should be refactored to share common code with APMServer and Kibana
    there are 0 unit tests (mostly because I wanted to go fast in the prototype phase, and because many unit tests should be written along with the refactoring above)
    there are 0 E2E tests
    it is missing a few features (podDisruptionBudget, secret config refs, version upgrade handling, etc.)
    
    ---
    
    Built from many commits:
    
    * Add Enterprise Search CRD
    
    * Add the Enterprise Search controller - minimal deployment ok
    
    * Add a working minimal Enterprise Search sample
    
    * Reconcile a configuration secret instead of env vars (#2)
    
    This is cleaner, more similar to what we do for other resources, and
    avoids leaking secrets in Pod environment variables.
    
    Note that Pods do not get automatically rotated when config changes so
    far.
    
    * Setup the EnterpriseSearch - Elasticsearch association controller
    
    This is heavily based on the APMServer association controller.
    Would largely benefit from some common refactoring in the future.
    
    * Patch the EnterpriseSearch CRD to fix the subresources
    
    I don't know why this isn't there in the first place.
    I plan to investigate in the future, but for now this is required to
    perform status updates.
    
    * Label Pods with a hash of the config for auto rotation (#4)
    
    We want Pods to be rotated with the new config.
    
    * Reuse the JAVA_OPTS const (#5)
    
    * Setup a readiness probe for smooth deployment rotation (#6)
    
    Let's query /swiftype-app-version which seems to be the default out
    there.
    The initial bootstrap is pretty slow so I set an InitialDelaySeconds of
    60sec.
    
    * Generate the default enterprise_search user in a secret (#7)
    
    I *think* this user can end up being useful if Elasticsearch is down
    hence the native realm cannot be used.
    
    * Strip out private Docker image from default sample (#8)
    
    * Fix EnterpriseSearch CRD generation (#9)
    
    This fixes CRD generation following a rebase on master.
    
    * Use self-signed TLS certificates by default (#10)
    
    Generate self-signed certificates for Enterprise Search and enable TLS
    encryption by default. This can be disabled in the EnterpriseSearch
    spec, and user-provided certificates can be used instead.
    
    * Switch http/https protocol in readiness probe and config (#11)
    
    Depending on the TLS spec, let's use the correct protocol: http or
    https.
    
    * Inject a hash of TLS certs for pod rotation (#12)
    
    Automatically rotate the Pod if:
    - the ent search config changes
    - the ent search TLS certs change
    - the referenced Elasticsearch certs change
    
    This is done through hashing them all and using that hash as a label in
    the podTemplate.
    
    * Cleanup the code files (#13)
    
    * Minor cosmetic housekeeping
    
    * goimports files
    
    * Add missing license headers
    
    * Fix linter warnings
    
    * Add api docs
    
    * Struct comments improvements
    
    * Regenerate CRDs and api docs
    
    * Fixes from PR review
    
    * Rely on the global scheme.Scheme
    
    * Setup RBAC for Enterprise Search

---
+++ pkg/controller/enterprisesearch/pod.go
@@ -0,0 +1,108 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package enterprisesearch
+
+import (
+	"fmt"
+	"path/filepath"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	entsv1beta1 "github.com/elastic/cloud-on-k8s/pkg/apis/enterprisesearch/v1beta1"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/certificates/http"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/container"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/defaults"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/volume"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/enterprisesearch/name"
+)
+
+const (
+	HTTPPort            = 3002
+	DefaultJavaOpts     = "-Xms3500m -Xmx3500m"
+	ConfigHashLabelName = "enterprisesearch.k8s.elastic.co/config-hash"
+)
+
+var (
+	DefaultMemoryLimits = resource.MustParse("4Gi")
+	DefaultResources    = corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceMemory: DefaultMemoryLimits,
+		},
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceMemory: DefaultMemoryLimits,
+		},
+	}
+	DefaultEnv = []corev1.EnvVar{
+		{Name: "JAVA_OPTS", Value: DefaultJavaOpts},
+		{Name: "ENT_SEARCH_CONFIG_PATH", Value: filepath.Join(ConfigMountPath, ConfigFilename)},
+	}
+)
+
+func readinessProbe(ents entsv1beta1.EnterpriseSearch) corev1.Probe {
+	return corev1.Probe{
+		FailureThreshold:    3,
+		InitialDelaySeconds: 60, // initial startup is pretty slow
+		PeriodSeconds:       10,
+		SuccessThreshold:    1,
+		TimeoutSeconds:      5,
+		Handler: corev1.Handler{
+			Exec: &corev1.ExecAction{
+				Command: []string{"bash", "-c",
+					fmt.Sprintf(
+						`curl -o /dev/null -w "%%{http_code}" %s://127.0.0.1:%d/swiftype-app-version -k -s`,
+						ents.Spec.HTTP.Protocol(),
+						HTTPPort,
+					),
+				},
+			},
+		},
+	}
+}
+
+func newPodSpec(ents entsv1beta1.EnterpriseSearch, configHash string) corev1.PodTemplateSpec {
+	cfgVolume := ConfigSecretVolume(ents)
+
+	builder := defaults.NewPodTemplateBuilder(
+		ents.Spec.PodTemplate, entsv1beta1.EnterpriseSearchContainerName).
+		WithResources(DefaultResources).
+		WithDockerImage(ents.Spec.Image, container.ImageRepository(container.EnterpriseSearchImage, ents.Spec.Version)).
+		WithPorts([]corev1.ContainerPort{
+			{Name: ents.Spec.HTTP.Protocol(), ContainerPort: int32(HTTPPort), Protocol: corev1.ProtocolTCP},
+		}).
+		WithReadinessProbe(readinessProbe(ents)).
+		WithVolumes(cfgVolume.Volume()).
+		WithVolumeMounts(cfgVolume.VolumeMount()).
+		WithEnv(append(DefaultEnv, DefaultUserEnvVar(ents))...).
+		// ensure the Pod gets rotated on config change
+		WithLabels(map[string]string{ConfigHashLabelName: configHash})
+
+	builder = withESCertsVolume(builder, ents)
+	builder = withHTTPCertsVolume(builder, ents)
+
+	return builder.PodTemplate
+}
+
+func withESCertsVolume(builder *defaults.PodTemplateBuilder, ents entsv1beta1.EnterpriseSearch) *defaults.PodTemplateBuilder {
+	if !ents.AssociationConf().CAIsConfigured() {
+		return builder
+	}
+	vol := volume.NewSecretVolumeWithMountPath(
+		ents.AssociationConf().GetCASecretName(),
+		"es-certs",
+		ESCertsPath,
+	)
+	return builder.
+		WithVolumes(vol.Volume()).
+		WithVolumeMounts(vol.VolumeMount())
+}
+
+func withHTTPCertsVolume(builder *defaults.PodTemplateBuilder, ents entsv1beta1.EnterpriseSearch) *defaults.PodTemplateBuilder {
+	if !ents.Spec.HTTP.TLS.Enabled() {
+		return builder
+	}
+	vol := http.HTTPCertSecretVolume(name.EntSearchNamer, ents.Name)
+	return builder.WithVolumes(vol.Volume()).WithVolumeMounts(vol.VolumeMount())
+}
