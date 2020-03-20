elastic/cloud-on-k8s

commit 174e09b26b4feeed88104a75c5f56d82e0807b7f

Homogenize secrets reconciliation with a helper function (#2707)
We reconcile secrets in too many different ways, especially regarding labels and annotations comparison.

This fixes it by creating a single common reconciler.ReconcileSecret() function, to be reused everywhere.

Behaviour:

keep existing annotations and labels if they are not expected (may have been defaulted or manually set by the user)
replace expected labels and annotations if their values mismatch
require secret data to be strictly equal


* Add a reusable ReconcileSecret function

* Rely on the common ReconcileSecret function where possible

* Refactor apmserver secret reconciliation to rely on the common function

* Refactor association user secret reconciliation to rely on the common function

* Rename ApmServerSecret to TokenSecret

* Fix typo

pkg/controller/elasticsearch/certificates/transport/reconcile.go
@@ -56,12 +56,12 @@ func ReconcileTransportCertificatesSecrets(
 		}
 
 		if err := ensureTransportCertificatesSecretContentsForPod(
-			es, secret, pod, ca, rotationParams,
+			es, &secret, pod, ca, rotationParams,
 		); err != nil {
 			return results.WithError(err)
 		}
 		certCommonName := buildCertificateCommonName(pod, es.Name, es.Namespace)
-		cert := extractTransportCert(*secret, pod, certCommonName)
+		cert := extractTransportCert(secret, pod, certCommonName)
 		if cert == nil {
 			return results.WithError(errors.New("No certificate found for pod"))
 		}
@@ -104,7 +104,7 @@ func ReconcileTransportCertificatesSecrets(
 	}
 
 	if !reflect.DeepEqual(secret, currentTransportCertificatesSecret) {
-		if err := c.Update(secret); err != nil {
+		if err := c.Update(&secret); err != nil {
 			return results.WithError(err)
 		}
 		for _, pod := range pods.Items {
@@ -120,56 +120,24 @@ func ReconcileTransportCertificatesSecrets(
 func ensureTransportCertificatesSecretExists(
 	c k8s.Client,
 	es esv1.Elasticsearch,
-) (*corev1.Secret, error) {
+) (corev1.Secret, error) {
 	expected := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: es.Namespace,
 			Name:      esv1.TransportCertificatesSecret(es.Name),
-
 			Labels: map[string]string{
 				// a label showing which es these certificates belongs to
 				label.ClusterNameLabelName: es.Name,
 			},
 		},
 	}
-
-	// reconcile the secret resource
-	var reconciled corev1.Secret
-	if err := reconciler.ReconcileResource(reconciler.Params{
-		Client:     c,
-		Owner:      &es,
-		Expected:   &expected,
-		Reconciled: &reconciled,
-		NeedsUpdate: func() bool {
-			// we only care about labels, not contents at this point, and we can allow additional labels
-			if reconciled.Labels == nil {
-				return true
-			}
-
-			for k, v := range expected.Labels {
-				if rv, ok := reconciled.Labels[k]; !ok || rv != v {
-					return true
-				}
-			}
-			return false
-		},
-		UpdateReconciled: func() {
-			if reconciled.Labels == nil {
-				reconciled.Labels = expected.Labels
-			} else {
-				for k, v := range expected.Labels {
-					reconciled.Labels[k] = v
-				}
-			}
-		},
-	}); err != nil {
-		return nil, err
+	reconciled, err := reconciler.ReconcileSecret(c, expected, &es)
+	if err != nil {
+		return corev1.Secret{}, err
 	}
-
 	// a placeholder secret may have nil entries, create them if needed
 	if reconciled.Data == nil {
 		reconciled.Data = make(map[string][]byte)
 	}
-
-	return &reconciled, nil
+	return reconciled, nil
 }

pkg/controller/elasticsearch/certificates/transport/reconcile_test.go
@@ -41,7 +41,7 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    func(*testing.T, *corev1.Secret)
+		want    func(*testing.T, corev1.Secret)
 		wantErr bool
 	}{
 		{
@@ -50,12 +50,12 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				c:     k8s.WrappedFakeClient(),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret *corev1.Secret) {
+			want: func(t *testing.T, secret corev1.Secret) {
 				// owner references are set upon creation, so ignore for comparison
 				expected := defaultSecretWith(func(s *corev1.Secret) {
 					s.OwnerReferences = secret.OwnerReferences
 				})
-				comparison.AssertEqual(t, expected, secret)
+				comparison.AssertEqual(t, expected, &secret)
 			},
 		},
 		{
@@ -66,11 +66,11 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				})),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret *corev1.Secret) {
+			want: func(t *testing.T, secret corev1.Secret) {
 				// UID should be kept the same
 				comparison.AssertEqual(t, defaultSecretWith(func(secret *corev1.Secret) {
 					secret.ObjectMeta.UID = types.UID("42")
-				}), secret)
+				}), &secret)
 			},
 		},
 		{
@@ -81,10 +81,10 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				})),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret *corev1.Secret) {
+			want: func(t *testing.T, secret corev1.Secret) {
 				comparison.AssertEqual(t, defaultSecretWith(func(secret *corev1.Secret) {
 					secret.ObjectMeta.Labels["foo"] = "bar"
-				}), secret)
+				}), &secret)
 			},
 		},
 	}

pkg/controller/apmserver/config/reconcile.go
@@ -5,8 +5,6 @@
 package config
 
 import (
-	"reflect"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -24,21 +22,20 @@ var log = logf.Log.WithName("apmserver-config")
 
 // Reconcile reconciles the configuration of the APM server: it first creates the configuration from the APM
 // specification and then reconcile the underlying secret.
-func Reconcile(client k8s.Client, as *apmv1.ApmServer) (*corev1.Secret, error) {
-
+func Reconcile(client k8s.Client, as *apmv1.ApmServer) (corev1.Secret, error) {
 	// Create a new configuration from the APM object spec.
 	cfg, err := NewConfigFromSpec(client, as)
 	if err != nil {
-		return nil, err
+		return corev1.Secret{}, err
 	}
 
 	cfgBytes, err := cfg.Render()
 	if err != nil {
-		return nil, err
+		return corev1.Secret{}, err
 	}
 
 	// Reconcile the configuration in a secret
-	expectedConfigSecret := &corev1.Secret{
+	expectedConfigSecret := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: as.Namespace,
 			Name:      name.Config(as.Name),
@@ -48,33 +45,5 @@ func Reconcile(client k8s.Client, as *apmv1.ApmServer) (*corev1.Secret, error) {
 			ApmCfgSecretKey: cfgBytes,
 		},
 	}
-
-	reconciledConfigSecret := &corev1.Secret{}
-	if err := reconciler.ReconcileResource(
-		reconciler.Params{
-			Client: client,
-
-			Owner:      as,
-			Expected:   expectedConfigSecret,
-			Reconciled: reconciledConfigSecret,
-
-			NeedsUpdate: func() bool {
-				return !reflect.DeepEqual(reconciledConfigSecret.Data, expectedConfigSecret.Data) ||
-					!reflect.DeepEqual(reconciledConfigSecret.Labels, expectedConfigSecret.Labels)
-			},
-			UpdateReconciled: func() {
-				reconciledConfigSecret.Labels = expectedConfigSecret.Labels
-				reconciledConfigSecret.Data = expectedConfigSecret.Data
-			},
-			PreCreate: func() {
-				log.Info("Creating config secret", "name", expectedConfigSecret.Name)
-			},
-			PreUpdate: func() {
-				log.Info("Updating config secret", "name", expectedConfigSecret.Name)
-			},
-		},
-	); err != nil {
-		return nil, err
-	}
-	return reconciledConfigSecret, nil
+	return reconciler.ReconcileSecret(client, expectedConfigSecret, as)
 }

pkg/controller/elasticsearch/certificates/remoteca/reconcile.go
@@ -6,8 +6,11 @@ package remoteca
 
 import (
 	"bytes"
-	"reflect"
 	"sort"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/certificates"
@@ -15,10 +18,6 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/label"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/remotecluster/remoteca"
 	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
-	"github.com/elastic/cloud-on-k8s/pkg/utils/maps"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // Reconcile fetches the list of remote certificate authorities and concatenates them into a single Secret
@@ -58,19 +57,6 @@ func Reconcile(
 			certificates.CAFileName: bytes.Join(remoteCertificateAuthorities, nil),
 		},
 	}
-
-	var reconciled v1.Secret
-	return reconciler.ReconcileResource(reconciler.Params{
-		Client:     c,
-		Owner:      &es,
-		Expected:   &expected,
-		Reconciled: &reconciled,
-		NeedsUpdate: func() bool {
-			return !maps.IsSubset(expected.Labels, reconciled.Labels) || !reflect.DeepEqual(expected.Data, reconciled.Data)
-		},
-		UpdateReconciled: func() {
-			reconciled.Labels = maps.Merge(reconciled.Labels, expected.Labels)
-			reconciled.Data = expected.Data
-		},
-	})
+	_, err := reconciler.ReconcileSecret(c, expected, &es)
+	return err
 }

pkg/controller/apmserver/apmserver_controller.go
@@ -270,8 +270,10 @@ func (r *ReconcileApmServer) onDelete(obj types.NamespacedName) {
 	r.dynamicWatches.Secrets.RemoveHandlerForKey(keystore.SecureSettingsWatchName(obj))
 }
 
-func (r *ReconcileApmServer) reconcileApmServerSecret(as *apmv1.ApmServer) (*corev1.Secret, error) {
-	expectedApmServerSecret := &corev1.Secret{
+// reconcileApmServerToken reconciles a Secret containing the APM Server token.
+// It reuses the existing token if possible.
+func reconcileApmServerToken(c k8s.Client, as *apmv1.ApmServer) (corev1.Secret, error) {
+	expectedApmServerSecret := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: as.Namespace,
 			Name:      apmname.SecretToken(as.Name),
@@ -281,48 +283,17 @@ func (r *ReconcileApmServer) reconcileApmServerSecret(as *apmv1.ApmServer) (*cor
 			SecretTokenKey: []byte(rand.String(24)),
 		},
 	}
-	reconciledApmServerSecret := &corev1.Secret{}
-	return reconciledApmServerSecret, reconciler.ReconcileResource(
-		reconciler.Params{
-			Client: r.Client,
+	// reuse the secret token if it already exists
+	var existingSecret corev1.Secret
+	err := c.Get(k8s.ExtractNamespacedName(&expectedApmServerSecret), &existingSecret)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return corev1.Secret{}, err
+	}
+	if token, exists := existingSecret.Data[SecretTokenKey]; exists {
+		expectedApmServerSecret.Data[SecretTokenKey] = token
+	}
 
-			Owner:      as,
-			Expected:   expectedApmServerSecret,
-			Reconciled: reconciledApmServerSecret,
-
-			NeedsUpdate: func() bool {
-				if !reflect.DeepEqual(reconciledApmServerSecret.Labels, expectedApmServerSecret.Labels) {
-					return true
-				}
-
-				if reconciledApmServerSecret.Data == nil {
-					return true
-				}
-
-				// re-use the secret token key if it exists
-				existingSecretTokenKey, hasExistingSecretTokenKey := reconciledApmServerSecret.Data[SecretTokenKey]
-				if hasExistingSecretTokenKey {
-					expectedApmServerSecret.Data[SecretTokenKey] = existingSecretTokenKey
-				}
-
-				if !reflect.DeepEqual(reconciledApmServerSecret.Data, expectedApmServerSecret.Data) {
-					return true
-				}
-
-				return false
-			},
-			UpdateReconciled: func() {
-				reconciledApmServerSecret.Labels = expectedApmServerSecret.Labels
-				reconciledApmServerSecret.Data = expectedApmServerSecret.Data
-			},
-			PreCreate: func() {
-				log.Info("Creating apm server secret", "namespace", expectedApmServerSecret.Namespace, "secret_name", expectedApmServerSecret.Name, "as_name", as.Name)
-			},
-			PreUpdate: func() {
-				log.Info("Updating apm server secret", "namespace", expectedApmServerSecret.Namespace, "secret_name", expectedApmServerSecret.Name, "as_name", as.Name)
-			},
-		},
-	)
+	return reconciler.ReconcileSecret(c, expectedApmServerSecret, as)
 }
 
 func (r *ReconcileApmServer) deploymentParams(
@@ -421,7 +392,7 @@ func (r *ReconcileApmServer) reconcileApmServerDeployment(
 	span, _ := apm.StartSpan(ctx, "reconcile_deployment", tracing.SpanTypeApp)
 	defer span.End()
 
-	reconciledApmServerSecret, err := r.reconcileApmServerSecret(as)
+	tokenSecret, err := reconcileApmServerToken(r.Client, as)
 	if err != nil {
 		return state, err
 	}
@@ -447,8 +418,8 @@ func (r *ReconcileApmServer) reconcileApmServerDeployment(
 
 		PodTemplate: as.Spec.PodTemplate,
 
-		ApmServerSecret: *reconciledApmServerSecret,
-		ConfigSecret:    *reconciledConfigSecret,
+		TokenSecret:  tokenSecret,
+		ConfigSecret: reconciledConfigSecret,
 
 		keystoreResources: keystoreResources,
 	}
@@ -462,7 +433,7 @@ func (r *ReconcileApmServer) reconcileApmServerDeployment(
 	if err != nil {
 		return state, err
 	}
-	state.UpdateApmServerState(result, *reconciledApmServerSecret)
+	state.UpdateApmServerState(result, tokenSecret)
 	return state, nil
 }
 

pkg/controller/apmserver/apmserver_controller_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	apmv1 "github.com/elastic/cloud-on-k8s/pkg/apis/apm/v1"
+	apmname "github.com/elastic/cloud-on-k8s/pkg/controller/apmserver/name"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/certificates"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/certificates/http"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/defaults"
@@ -200,7 +201,7 @@ func TestReconcileApmServer_deploymentParams(t *testing.T) {
 	}
 	defaultPodSpecParams := PodSpecParams{
 		Version: "1.0",
-		ApmServerSecret: corev1.Secret{
+		TokenSecret: corev1.Secret{
 			ObjectMeta: v1.ObjectMeta{
 				Name: "test-apm-server-apm-token",
 			},
@@ -410,6 +411,47 @@ func TestReconcileApmServer_doReconcile(t *testing.T) {
 			require.Equal(t, got.Requeue, tt.wantRequeue)
 			// We just check that the requeue is not zero
 			require.True(t, got.RequeueAfter > 0)
+		})
+	}
+}
+
+func Test_reconcileApmServerToken(t *testing.T) {
+	apm := &apmv1.ApmServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "ns",
+			Name:      "apm",
+		},
+	}
+	tests := []struct {
+		name       string
+		c          k8s.Client
+		reuseToken []byte
+	}{
+		{
+			name: "no secret exists: create one",
+			c:    k8s.WrappedFakeClient(),
+		},
+		{
+			name: "reuse token if it already exists",
+			c: k8s.WrappedFakeClient(&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "ns",
+					Name:      apmname.SecretToken(apm.Name),
+				},
+				Data: map[string][]byte{
+					SecretTokenKey: []byte("existing"),
+				},
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := reconcileApmServerToken(tt.c, apm)
+			require.NoError(t, err)
+			require.NotEmpty(t, got.Data[SecretTokenKey])
+			if tt.reuseToken != nil {
+				require.Equal(t, tt.reuseToken, got.Data[SecretTokenKey])
+			}
 		})
 	}
 }

pkg/controller/apmserver/pod.go
@@ -78,8 +78,8 @@ type PodSpecParams struct {
 
 	PodTemplate corev1.PodTemplateSpec
 
-	ApmServerSecret corev1.Secret
-	ConfigSecret    corev1.Secret
+	TokenSecret  corev1.Secret
+	ConfigSecret corev1.Secret
 
 	keystoreResources *keystore.Resources
 }
@@ -95,7 +95,7 @@ func newPodSpec(as *apmv1.ApmServer, p PodSpecParams) corev1.PodTemplateSpec {
 		Name: "SECRET_TOKEN",
 		ValueFrom: &corev1.EnvVarSource{
 			SecretKeyRef: &corev1.SecretKeySelector{
-				LocalObjectReference: corev1.LocalObjectReference{Name: p.ApmServerSecret.Name},
+				LocalObjectReference: corev1.LocalObjectReference{Name: p.TokenSecret.Name},
 				Key:                  SecretTokenKey,
 			},
 		},

pkg/controller/apmserver/pod_test.go
@@ -51,9 +51,9 @@ func TestNewPodSpec(t *testing.T) {
 						Name:      "config-secret",
 					},
 				},
-				ApmServerSecret: corev1.Secret{
+				TokenSecret: corev1.Secret{
 					ObjectMeta: metav1.ObjectMeta{
-						Name: "apm-secret",
+						Name: "token-secret",
 					},
 				},
 			},
@@ -84,7 +84,7 @@ func TestNewPodSpec(t *testing.T) {
 									Name: "SECRET_TOKEN",
 									ValueFrom: &corev1.EnvVarSource{
 										SecretKeyRef: &corev1.SecretKeySelector{
-											LocalObjectReference: corev1.LocalObjectReference{Name: "apm-secret"},
+											LocalObjectReference: corev1.LocalObjectReference{Name: "token-secret"},
 											Key:                  SecretTokenKey,
 										},
 									},

commit bf5bc233590441c85eb260ec06664b37d62a4e63

Fix transport certificates reconciliation
Commit 174e09b26b4feeed88104a75c5f56d82e0807b7f introduced a regression
by using the generic secret reconciler when we first create the
transport certs secret.

This secret reconciliation is rather special, since it ensures the
secret exists (before Pods are created), but does not care about its
data (since we don't have Pods IPs yet).
If an existing secret has some data already, we must make sure we don't
clear it. Which is what the generic reconciler would do.

This commit basically restores the code as it existed before the
refactoring in 174e09b26b4feeed88104a75c5f56d82e0807b7f, and adds a unit
test to catch the regression.

pkg/controller/elasticsearch/certificates/transport/reconcile.go
@@ -18,6 +18,8 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/reconciler"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/label"
 	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/maps"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -56,12 +58,12 @@ func ReconcileTransportCertificatesSecrets(
 		}
 
 		if err := ensureTransportCertificatesSecretContentsForPod(
-			es, &secret, pod, ca, rotationParams,
+			es, secret, pod, ca, rotationParams,
 		); err != nil {
 			return results.WithError(err)
 		}
 		certCommonName := buildCertificateCommonName(pod, es.Name, es.Namespace)
-		cert := extractTransportCert(secret, pod, certCommonName)
+		cert := extractTransportCert(*secret, pod, certCommonName)
 		if cert == nil {
 			return results.WithError(errors.New("No certificate found for pod"))
 		}
@@ -104,7 +106,7 @@ func ReconcileTransportCertificatesSecrets(
 	}
 
 	if !reflect.DeepEqual(secret, currentTransportCertificatesSecret) {
-		if err := c.Update(&secret); err != nil {
+		if err := c.Update(secret); err != nil {
 			return results.WithError(err)
 		}
 		for _, pod := range pods.Items {
@@ -120,7 +122,7 @@ func ReconcileTransportCertificatesSecrets(
 func ensureTransportCertificatesSecretExists(
 	c k8s.Client,
 	es esv1.Elasticsearch,
-) (corev1.Secret, error) {
+) (*corev1.Secret, error) {
 	expected := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: es.Namespace,
@@ -131,13 +133,31 @@ func ensureTransportCertificatesSecretExists(
 			},
 		},
 	}
-	reconciled, err := reconciler.ReconcileSecret(c, expected, &es)
-	if err != nil {
-		return corev1.Secret{}, err
+	// reconcile the secret resource:
+	// - create it if it doesn't exist
+	// - update labels & annotations if they don't match
+	// - do not touch the existing data as it probably already contains certificates - it will be reconciled later on
+	var reconciled corev1.Secret
+	if err := reconciler.ReconcileResource(reconciler.Params{
+		Client:     c,
+		Owner:      &es,
+		Expected:   &expected,
+		Reconciled: &reconciled,
+		NeedsUpdate: func() bool {
+			return !maps.IsSubset(expected.Labels, reconciled.Labels) ||
+				!maps.IsSubset(expected.Annotations, reconciled.Annotations)
+		},
+		UpdateReconciled: func() {
+			reconciled.Labels = maps.Merge(reconciled.Labels, expected.Labels)
+			reconciled.Annotations = maps.Merge(reconciled.Annotations, expected.Annotations)
+		},
+	}); err != nil {
+		return nil, err
 	}
 	// a placeholder secret may have nil entries, create them if needed
 	if reconciled.Data == nil {
 		reconciled.Data = make(map[string][]byte)
 	}
-	return reconciled, nil
+
+	return &reconciled, nil
 }

pkg/controller/elasticsearch/certificates/transport/reconcile_test.go
@@ -41,7 +41,7 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    func(*testing.T, corev1.Secret)
+		want    func(*testing.T, *corev1.Secret)
 		wantErr bool
 	}{
 		{
@@ -50,12 +50,12 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				c:     k8s.WrappedFakeClient(),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret corev1.Secret) {
+			want: func(t *testing.T, secret *corev1.Secret) {
 				// owner references are set upon creation, so ignore for comparison
 				expected := defaultSecretWith(func(s *corev1.Secret) {
 					s.OwnerReferences = secret.OwnerReferences
 				})
-				comparison.AssertEqual(t, expected, &secret)
+				comparison.AssertEqual(t, expected, secret)
 			},
 		},
 		{
@@ -66,11 +66,32 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				})),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret corev1.Secret) {
+			want: func(t *testing.T, secret *corev1.Secret) {
 				// UID should be kept the same
 				comparison.AssertEqual(t, defaultSecretWith(func(secret *corev1.Secret) {
 					secret.ObjectMeta.UID = types.UID("42")
-				}), &secret)
+				}), secret)
+			},
+		},
+		{
+			name: "should not modify the secret data if already exists",
+			args: args{
+				c: k8s.WrappedFakeClient(defaultSecretWith(func(secret *corev1.Secret) {
+					secret.ObjectMeta.UID = types.UID("42")
+					secret.Data = map[string][]byte{
+						"existing": []byte("data"),
+					}
+				})),
+				owner: testES,
+			},
+			want: func(t *testing.T, secret *corev1.Secret) {
+				// UID and data should be kept
+				comparison.AssertEqual(t, defaultSecretWith(func(secret *corev1.Secret) {
+					secret.ObjectMeta.UID = types.UID("42")
+					secret.Data = map[string][]byte{
+						"existing": []byte("data"),
+					}
+				}), secret)
 			},
 		},
 		{
@@ -81,10 +102,10 @@ func Test_ensureTransportCertificateSecretExists(t *testing.T) {
 				})),
 				owner: testES,
 			},
-			want: func(t *testing.T, secret corev1.Secret) {
+			want: func(t *testing.T, secret *corev1.Secret) {
 				comparison.AssertEqual(t, defaultSecretWith(func(secret *corev1.Secret) {
 					secret.ObjectMeta.Labels["foo"] = "bar"
-				}), &secret)
+				}), secret)
 			},
 		},
 	}