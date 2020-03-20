8a3c2f0822fa2d13f3e53120045c36f5d34a2846
Peter Brachwitz <peter.brachwitz@elastic.co>
2020-03-20 18:49:03 +0100

    Switch to /api/status for Kibana e2e tests (#2730)
    Instead of checking for the presence of a string in the login page make an authenticated request to the Kibana status API. 
    
    This should be slightly more resilient against changes between different stack versions.
    
    The expected behaviour is as follows:
     -  api/status will respond with 503 until the server is ready and able to talk to Elasticsearch and run migrations
     - If Kibana loses communication with ES and the status.allowAnonymous is not set to true then you will get 401 from Kibana on this endpoint
     - If Kibana loses communication with ES and the status.allowAnonymous is set to true then you will get 200 from Kibana on this endpoint with a status.overall.state property set to red 
    
             
    In practice I have seen it simply timing out when Elasticsearch is not available.

--- test/e2e/test/kibana/http_client.go
+++ test/e2e/test/kibana/http_client.go
@@ -33,16 +33,17 @@ func NewKibanaClient(kb kbv1.Kibana, k *test.K8sClient) (*http.Client, error) {
 }
 
 // DoKibanaReq executes an HTTP request against a Kibana instance.
-func DoKibanaReq(k *test.K8sClient, b Builder, method string, uri string, body []byte) ([]byte, error) {
-	password, err := k.GetElasticPassword(b.Kibana.Spec.ElasticsearchRef.Namespace, b.Kibana.Spec.ElasticsearchRef.Name)
+func DoKibanaReq(k *test.K8sClient, kb kbv1.Kibana, method string, uri string, body []byte) ([]byte, error) {
+	password, err := k.GetElasticPassword(kb.Spec.ElasticsearchRef.Namespace, kb.Spec.ElasticsearchRef.Name)
 	if err != nil {
 		return nil, errors.Wrap(err, "while getting elastic password")
 	}
 	scheme := "http"
-	if b.Kibana.Spec.HTTP.TLS.Enabled() {
+	if kb.Spec.HTTP.TLS.Enabled() {
 		scheme = "https"
 	}
-	u, err := url.Parse(fmt.Sprintf("%s://%s.%s:5601", scheme, kbname.HTTPService(b.Kibana.Name), b.Kibana.Namespace))
+	// add .svc suffix so that requests work when using the port-forwarder during local test runs
+	u, err := url.Parse(fmt.Sprintf("%s://%s.%s.svc:5601", scheme, kbname.HTTPService(kb.Name), kb.Namespace))
 	if err != nil {
 		return nil, errors.Wrap(err, "while parsing url")
 	}
@@ -56,8 +57,8 @@ func DoKibanaReq(k *test.K8sClient, b Builder, method string, uri string, body [
 	req.SetBasicAuth("elastic", password)
 	req.Header.Set("Content-Type", "application/json")
 	// send the kbn-version header expected by the Kibana server to protect against xsrf attacks
-	req.Header.Set("kbn-version", b.Kibana.Spec.Version)
-	client, err := NewKibanaClient(b.Kibana, k)
+	req.Header.Set("kbn-version", kb.Spec.Version)
+	client, err := NewKibanaClient(kb, k)
 	if err != nil {
 		return nil, errors.Wrap(err, "while creating kibana client")
 	}
