368a8ded0025fc6a4ebb3e440a0654931a3e7eac
Peter Brachwitz <peter.brachwitz@elastic.co>
2020-03-30 14:19:07 +0200

    inline and log on warn level

--- pkg/controller/common/unmanaged.go
+++ pkg/controller/common/unmanaged.go
@@ -27,24 +27,19 @@ var (
 
 // IsUnmanaged checks if a given resource is currently unmanaged.
 func IsUnmanaged(meta metav1.ObjectMeta) bool {
-	return getBoolFromAnnotation(meta.Annotations)
-}
-
-// Extract the desired state from the map that contains annotations.
-func getBoolFromAnnotation(annotations map[string]string) bool {
-	if annotations == nil {
+	if meta.Annotations == nil {
 		return false
 	}
 
 	parse := func(ann string, defaultValue bool) (bool, bool) {
-		stateAsString, exists := annotations[ann]
+		stateAsString, exists := meta.Annotations[ann]
 		if !exists {
 			return defaultValue, false
 		}
 
 		expectedState, err := strconv.ParseBool(stateAsString)
 		if err != nil {
-			log.Error(err, fmt.Sprintf("Cannot parse %s=%s as a bool, defaulting to %t", ann, stateAsString, defaultValue))
+			log.Error(err, fmt.Sprintf("Cannot parse %s=%s as a bool, defaulting to %t", ann, stateAsString, defaultValue), "namespace", meta.Namespace, "name", meta.Name)
 			return defaultValue, true
 		}
 		return expectedState, true
@@ -53,11 +48,11 @@ func getBoolFromAnnotation(annotations map[string]string) bool {
 	managed, newExists := parse(ManagedAnnotation, true)
 	paused, legacyExists := parse(LegacyPauseAnnoation, false)
 	if legacyExists && !newExists {
-		log.Info(fmt.Sprintf("Using legacy annotation %s. Please consider moving to %s", LegacyPauseAnnoation, ManagedAnnotation))
+		log.V(-1).Info(fmt.Sprintf("Using legacy annotation %s. Please consider moving to %s", LegacyPauseAnnoation, ManagedAnnotation), "namespace", meta.Namespace, "name", meta.Name)
 		return paused
 	}
 	if legacyExists && newExists {
-			log.Info(fmt.Sprintf("Both %s and %s are set, preferring %s", LegacyPauseAnnoation, ManagedAnnotation, ManagedAnnotation))
+			log.V(-1).Info(fmt.Sprintf("Both %s and %s are set, preferring %s", LegacyPauseAnnoation, ManagedAnnotation, ManagedAnnotation), "namespace", meta.Namespace, "name", meta.Name)
 	}
 	return !managed
 }
