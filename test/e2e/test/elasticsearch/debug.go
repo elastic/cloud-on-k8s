4e4bb8c3065d4bf42f92ec93237925bf992b92c4
Peter Brachwitz <peter.brachwitz@elastic.co>
2020-04-09 15:56:11 +0200

    consistent print messages

--- test/e2e/test/elasticsearch/debug.go
+++ test/e2e/test/elasticsearch/debug.go
@@ -17,7 +17,7 @@ func printShards(clientFactory func() (client.Client, error)) func() {
 	return func() {
 		esClient, err := clientFactory()
 		if err != nil {
-			fmt.Printf("while creating es client %v", err)
+			fmt.Printf("error while creating es client: %v", err)
 			return
 		}
 		catShards, err := http.NewRequest(
@@ -26,18 +26,18 @@ func printShards(clientFactory func() (client.Client, error)) func() {
 			nil,
 		)
 		if err != nil {
-			fmt.Printf("err while creating request: %v \n", err)
+			fmt.Printf("error while creating request: %v \n", err)
 			return
 		}
 		shards, err := esClient.Request(context.Background(), catShards)
 		if err != nil {
-			fmt.Printf("err while fetching shards: %v \n", err)
+			fmt.Printf("error while fetching shards: %v \n", err)
 			return
 		}
 		defer shards.Body.Close()
 		bytes, err := ioutil.ReadAll(shards.Body)
 		if err != nil {
-			fmt.Printf("err while reading response body: %v \n", err)
+			fmt.Printf("error while reading response body: %v \n", err)
 			return
 		}
 		fmt.Println("GET _cat/shards")
