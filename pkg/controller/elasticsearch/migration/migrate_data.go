bd1a958e5b85ee169effbb3a4dfb4d3863327c28
Michael Morello <michael.morello@elastic.co>
2020-04-15 16:19:06 +0200

    Do not use annotations to cache Elasticsearch API calls (#2883)
    * Clarify data migration function and unit tests (#2845)
    * Do not use annotations to cache Elasticsearch API calls (#2880)
    
    Co-authored-by: Sebastien Guilloux <contact.sebgl@gmail.com>

--- pkg/controller/elasticsearch/migration/migrate_data.go
+++ pkg/controller/elasticsearch/migration/migrate_data.go
@@ -8,25 +8,15 @@ import (
 	"context"
 	"strings"
 
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-
 	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
 	esclient "github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/client"
-	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var log = logf.Log.WithName("migrate-data")
 
-const (
-	// AllocationExcludeAnnotationName is the name of the annotation that stores the last
-	// cluster.routing.allocation._name setting applied to the Elasticsearch cluster.
-	AllocationExcludeAnnotationName = "elasticsearch.k8s.elastic.co/allocation-exclude"
-)
-
-// IsMigratingData looks only at the presence of shards on a given node
-// and checks if there is at least one other copy of the shard in the cluster
-// that is started and not relocating.
-func IsMigratingData(ctx context.Context, shardLister esclient.ShardLister, podName string) (bool, error) {
+// NodeHasShard returns true if the given ES Pod is holding at least one shard (primary or replica).
+func NodeHasShard(ctx context.Context, shardLister esclient.ShardLister, podName string) (bool, error) {
 	shards, err := shardLister.GetShards(ctx)
 	if err != nil {
 		return false, err
@@ -40,26 +30,9 @@ func IsMigratingData(ctx context.Context, shardLister esclient.ShardLister, podN
 	return false, nil
 }
 
-// allocationExcludeFromAnnotation returns the allocation exclude value stored in an annotation.
-// May be empty if not set.
-func allocationExcludeFromAnnotation(es esv1.Elasticsearch) string {
-	return es.Annotations[AllocationExcludeAnnotationName]
-}
-
-// updateAllocationExcludeAnnotation sets an annotation in ES with the given cluster routing allocation exclude value.
-// This is to avoid making the same ES API call over and over again.
-func updateAllocationExcludeAnnotation(c k8s.Client, es esv1.Elasticsearch, value string) error {
-	if es.Annotations == nil {
-		es.Annotations = map[string]string{}
-	}
-	es.Annotations[AllocationExcludeAnnotationName] = value
-	return c.Update(&es)
-}
-
 // MigrateData sets allocation filters for the given nodes.
 func MigrateData(
 	ctx context.Context,
-	c k8s.Client,
 	es esv1.Elasticsearch,
 	allocationSetter esclient.AllocationSetter,
 	leavingNodes []string,
@@ -69,16 +42,6 @@ func MigrateData(
 	if len(leavingNodes) > 0 {
 		exclusions = strings.Join(leavingNodes, ",")
 	}
-	// compare with what was set previously
-	// Note the user may have changed it behind our back through the ES API. It is considered their responsibility.
-	// Manually removing the annotation to force a refresh of the allocations exclude setting is a valid use case.
-	if exclusions == allocationExcludeFromAnnotation(es) {
-		return nil
-	}
 	log.Info("Setting routing allocation excludes", "namespace", es.Namespace, "es_name", es.Name, "value", exclusions)
-	if err := allocationSetter.ExcludeFromShardAllocation(ctx, exclusions); err != nil {
-		return err
-	}
-	// store updated value in an annotation so we don't make the same call over and over again
-	return updateAllocationExcludeAnnotation(c, es, exclusions)
+	return allocationSetter.ExcludeFromShardAllocation(ctx, exclusions)
 }
--- pkg/controller/elasticsearch/migration/migrate_data_test.go
+++ pkg/controller/elasticsearch/migration/migrate_data_test.go
@@ -9,17 +9,14 @@ import (
 	"fmt"
 	"testing"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/client"
-	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestIsMigratingData(t *testing.T) {
+func TestNodeHasShard(t *testing.T) {
 	type args struct {
 		shardLister client.ShardLister
 		podName     string
@@ -42,72 +39,38 @@ func TestIsMigratingData(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "Node needs to be completely evacuated",
+			name: "Node has one shard",
 			args: args{
 				podName: "A",
 				shardLister: NewFakeShardLister([]client.Shard{
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "A"},
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "B"},
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "C"},
+					{Index: "index-1", Shard: "0", NodeName: "A"},
+					{Index: "index-1", Shard: "0", NodeName: "B"},
+					{Index: "index-1", Shard: "0", NodeName: "C"},
 				}),
 			},
 			want: true,
 		},
 		{
-			name: "Nothing to migrate",
+			name: "No shard on the node",
 			args: args{
 				podName: "A",
 				shardLister: NewFakeShardLister([]client.Shard{
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "B"},
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "C"},
+					{Index: "index-1", Shard: "0", NodeName: "B"},
+					{Index: "index-1", Shard: "0", NodeName: "C"},
 				}),
 			},
 			want: false,
 		},
-		{
-			name: "Only copy needs migration",
-			args: args{
-				podName: "A",
-				shardLister: NewFakeShardLister([]client.Shard{
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "A"},
-					{Index: "index-1", Shard: "1", State: client.STARTED, NodeName: "B"},
-					{Index: "index-1", Shard: "2", State: client.STARTED, NodeName: "C"},
-				}),
-			},
-			want: true,
-		},
-		{
-			name: "Relocation is migration",
-			args: args{
-				podName: "A",
-				shardLister: NewFakeShardLister([]client.Shard{
-					{Index: "index-1", Shard: "0", State: client.RELOCATING, NodeName: "A"},
-					{Index: "index-1", Shard: "0", State: client.INITIALIZING, NodeName: "B"},
-				}),
-			},
-			want: true,
-		},
-		{
-			name: "Copy is initializing",
-			args: args{
-				podName: "A",
-				shardLister: NewFakeShardLister([]client.Shard{
-					{Index: "index-1", Shard: "0", State: client.STARTED, NodeName: "A"},
-					{Index: "index-1", Shard: "0", State: client.INITIALIZING, NodeName: "B"},
-				}),
-			},
-			want: true,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := IsMigratingData(context.Background(), tt.args.shardLister, tt.args.podName)
+			got, err := NodeHasShard(context.Background(), tt.args.shardLister, tt.args.podName)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("IsMigratingData() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("NodeHasShard() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("IsMigratingData() = %v, want %v", got, tt.want)
+				t.Errorf("NodeHasShard() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -116,130 +79,32 @@ func TestIsMigratingData(t *testing.T) {
 func TestMigrateData(t *testing.T) {
 	tests := []struct {
 		name         string
-		es           esv1.Elasticsearch
 		leavingNodes []string
 		want         string
-		wantEs       esv1.Elasticsearch
 	}{
 		{
-			name:         "no nodes to migrate, no annotation on ES",
-			es:           esv1.Elasticsearch{},
+			name:         "no nodes to migrate, allocation setting should be set to none_excluded",
 			leavingNodes: []string{},
 			want:         "none_excluded",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
 		},
 		{
-			name: "no nodes to migrate, annotation already set on ES",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
-			leavingNodes: []string{},
-			want:         "",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
+			name:         "a node to migrate",
+			leavingNodes: []string{"test-node1"},
+			want:         "test-node1",
 		},
 		{
-			name: "no nodes to migrate, annotation set with some exclusions on ES",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node2"},
-			}},
-			leavingNodes: []string{},
-			want:         "none_excluded",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
-		},
-		{
-			name:         "one node to migrate, no annotation set on ES",
-			es:           esv1.Elasticsearch{},
-			leavingNodes: []string{"test-node"},
-			want:         "test-node",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node"},
-			}},
-		},
-		{
-			name: "one node to migrate, no exclusions in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
-			leavingNodes: []string{"test-node"},
-			want:         "test-node",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node"},
-			}},
-		},
-		{
-			name: "one node to migrate, different exclusions in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node2"},
-			}},
-			leavingNodes: []string{"test-node"},
-			want:         "test-node",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node"},
-			}},
-		},
-		{
-			name: "one node to migrate, already present in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node"},
-			}},
-			leavingNodes: []string{"test-node"},
-			want:         "",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node"},
-			}},
-		},
-		{
-			name: "multiple node to migrate, no exclusions in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "none_excluded"},
-			}},
+			name:         "multiple nodes to migrate",
 			leavingNodes: []string{"test-node1", "test-node2"},
 			want:         "test-node1,test-node2",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node2"},
-			}},
-		},
-		{
-			name: "multiple node to migrate, different exclusions in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node3"},
-			}},
-			leavingNodes: []string{"test-node1", "test-node2"},
-			want:         "test-node1,test-node2",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node2"},
-			}},
-		},
-		{
-			name: "multiple node to migrate, already present in ES annotation",
-			es: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node2"},
-			}},
-			leavingNodes: []string{"test-node1", "test-node2"},
-			want:         "",
-			wantEs: esv1.Elasticsearch{ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{AllocationExcludeAnnotationName: "test-node1,test-node2"},
-			}},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			allocationSetter := fakeAllocationSetter{}
-			c := k8s.WrappedFakeClient(&tt.es)
-			err := MigrateData(context.Background(), c, tt.es, &allocationSetter, tt.leavingNodes)
+			err := MigrateData(context.Background(), esv1.Elasticsearch{}, &allocationSetter, tt.leavingNodes)
 			require.NoError(t, err)
-			assert.Contains(t, allocationSetter.value, tt.want)
-			var retrievedES esv1.Elasticsearch
-			err = c.Get(k8s.ExtractNamespacedName(&tt.es), &retrievedES)
-			require.NoError(t, err)
-			require.Equal(t, tt.wantEs.Annotations, retrievedES.Annotations)
+			assert.Equal(t, tt.want, allocationSetter.value)
 		})
 	}
 }
