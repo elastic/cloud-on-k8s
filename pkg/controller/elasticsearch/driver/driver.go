a8049cf7e3442ea4403d943d1cbd0fb386610547
Michael Morello <michael.morello@elastic.co>
2020-04-16 16:02:43 +0200

    Use annotation to track created remote clusters (#2891) (#2892)
    * Use annotation to track created remote clusters

--- pkg/controller/elasticsearch/driver/driver.go
+++ pkg/controller/elasticsearch/driver/driver.go
@@ -221,12 +221,15 @@ func (d *defaultDriver) Reconcile(ctx context.Context) *reconciler.Results {
 	)
 
 	if esReachable {
-		err = remotecluster.UpdateSettings(ctx, d.Client, esClient, d.Recorder(), d.LicenseChecker, d.ES)
+		requeue, err := remotecluster.UpdateSettings(ctx, d.Client, esClient, d.Recorder(), d.LicenseChecker, d.ES)
 		if err != nil {
 			msg := "Could not update remote clusters in Elasticsearch settings"
 			d.ReconcileState.AddEvent(corev1.EventTypeWarning, events.EventReasonUnexpected, msg)
 			log.Error(err, msg, "namespace", d.ES.Namespace, "es_name", d.ES.Name)
 			results.WithResult(defaultRequeue)
+		}
+		if requeue {
+			results = results.WithResult(defaultRequeue)
 		}
 	}
 
