ecc2a9dde70814fed141f1145398e227d1ebec4b
Anya Sabo <anya@sabolicio.us>
2020-04-28 14:18:56 -0500

    Remove docs link for CRD comment

--- pkg/apis/apm/v1beta1/apmserver_types.go
+++ pkg/apis/apm/v1beta1/apmserver_types.go
@@ -37,7 +37,6 @@ type ApmServerSpec struct {
 	PodTemplate corev1.PodTemplateSpec `json:"podTemplate,omitempty"`
 
 	// SecureSettings is a list of references to Kubernetes secrets containing sensitive configuration options for APM Server.
-	// See: https://www.elastic.co/guide/en/cloud-on-k8s/current/k8s-apm-server.html#k8s-apm-secure-settings
 	SecureSettings []commonv1beta1.SecretSource `json:"secureSettings,omitempty"`
 }
 
