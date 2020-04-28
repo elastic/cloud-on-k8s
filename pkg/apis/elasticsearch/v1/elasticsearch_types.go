e6fcf3680aebdce6a31abd083e6c2fac139af6ed
Anya Sabo <anya.sabo@elastic.co>
2020-04-28 15:14:43 -0500

    Remove docs link for CRD comment (#2984)

--- pkg/apis/elasticsearch/v1/elasticsearch_types.go
+++ pkg/apis/elasticsearch/v1/elasticsearch_types.go
@@ -51,7 +51,6 @@ type ElasticsearchSpec struct {
 	Auth Auth `json:"auth,omitempty"`
 
 	// SecureSettings is a list of references to Kubernetes secrets containing sensitive configuration options for Elasticsearch.
-	// See: https://www.elastic.co/guide/en/cloud-on-k8s/current/k8s-es-secure-settings.html
 	// +kubebuilder:validation:Optional
 	SecureSettings []commonv1.SecretSource `json:"secureSettings,omitempty"`
 
