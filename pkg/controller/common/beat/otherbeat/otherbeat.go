897bbfbce94e89ba26c6cb254c32f9c3924cd0ad
David Kowalski <david.kowalski@elastic.co>
2020-05-24 17:48:21 +0200

    Set default replica count for unknown beats

--- pkg/controller/common/beat/otherbeat/otherbeat.go
+++ pkg/controller/common/beat/otherbeat/otherbeat.go
@@ -6,6 +6,7 @@ package otherbeat
 
 import (
 	commonbeat "github.com/elastic/cloud-on-k8s/pkg/controller/common/beat"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/pointer"
 )
 
 const (
@@ -22,9 +23,10 @@ func NewDriver(params commonbeat.DriverParams) commonbeat.Driver {
 }
 
 func (d *Driver) Reconcile() commonbeat.DriverResults {
-
 	if d.DaemonSet == nil && d.Deployment == nil {
-		d.Deployment = &commonbeat.DeploymentSpec{}
+		d.Deployment = &commonbeat.DeploymentSpec{
+			Replicas: pointer.Int32(1),
+		}
 	}
 
 	return commonbeat.Reconcile(
