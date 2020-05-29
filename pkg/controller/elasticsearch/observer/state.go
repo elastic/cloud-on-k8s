2ecaa509d5600447ed6bd34dd9dc8a8d1c482bc6
sebgl <contact.sebgl@gmail.com>
2020-05-29 14:44:36 +0200

    Don't retrieve the license in the observer regular poll

--- pkg/controller/elasticsearch/observer/state.go
+++ pkg/controller/elasticsearch/observer/state.go
@@ -16,40 +16,14 @@ type State struct {
 	// TODO: verify usages of the below never assume they are set (check for nil)
 	// ClusterHealth is the current traffic light health as reported by Elasticsearch.
 	ClusterHealth *esclient.Health
-	// TODO should probably be a separate observer
-	// ClusterLicense is the current license applied to this cluster
-	ClusterLicense *esclient.License
 }
 
 // RetrieveState returns the current Elasticsearch cluster state
 func RetrieveState(ctx context.Context, cluster types.NamespacedName, esClient esclient.Client) State {
-	// retrieve cluster health and license in parallel
-	healthChan := make(chan *esclient.Health)
-	licenseChan := make(chan *esclient.License)
-
-	go func() {
-		health, err := esClient.GetClusterHealth(ctx)
-		if err != nil {
-			log.V(1).Info("Unable to retrieve cluster health", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
-			healthChan <- nil
-			return
-		}
-		healthChan <- &health
-	}()
-
-	go func() {
-		license, err := esClient.GetLicense(ctx)
-		if err != nil {
-			log.V(1).Info("Unable to retrieve cluster license", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
-			licenseChan <- nil
-			return
-		}
-		licenseChan <- &license
-	}()
-
-	// return the state when ready, may contain nil values
-	return State{
-		ClusterHealth:  <-healthChan,
-		ClusterLicense: <-licenseChan,
+	health, err := esClient.GetClusterHealth(ctx)
+	if err != nil {
+		log.V(1).Info("Unable to retrieve cluster health", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
+		return State{ClusterHealth: nil}
 	}
+	return State{ClusterHealth: &health}
 }
