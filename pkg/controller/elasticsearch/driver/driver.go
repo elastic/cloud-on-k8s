elastic/cloud-on-k8s

commit 57fdf9013e829c2a2c7bf69271bceb453a60e989

Do not stop running trial even when ECK not in trial mode (#3150)
Trials on Elasticsearch level can only be started once.
The intention behind this change is to avoid a bad user experience.
We encourage users to start trials via ECK but the Kibana UI also
advertises trials in various places when users try to access commercial features.
We don't want to change the Kibana behaviour to keep this easy way to try out a feature.
This change therefore tolerates a cluster running in trial mode even if ECK's license level
would require us to downgrade this cluster to a Basic license.

pkg/controller/elasticsearch/license/apply.go
@@ -48,6 +48,13 @@ func applyLinkedLicense(
 	)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
+			if isTrial(current) {
+				// this is the case if the user has started a trial on the ES cluster level
+				// e.g. from Kibana when trying to access a commercial feature.
+				// While this is not a supported use case we tolerate it to avoid a bad user
+				// experience because trials can only be started once on the ES cluster level
+				return nil
+			}
 			// no license linked to this cluster. Revert to basic.
 			return startBasic(ctx, updater)
 		}

pkg/controller/elasticsearch/license/apply_test.go
@@ -7,6 +7,7 @@ package license
 import (
 	"context"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -76,6 +77,22 @@ func Test_updateLicense(t *testing.T) {
 			},
 		},
 		{
+			name: "start a trial",
+			args: args{
+				current: nil,
+				desired: esclient.License{
+					Type: string(esclient.ElasticsearchLicenseTypeTrial),
+				},
+			},
+			reqFn: func(req *http.Request) *http.Response {
+				if strings.Contains(req.URL.Path, "start_trial") {
+					return esclient.NewMockResponse(200, req, `{"acknowledged": true, "trial_started": true}`)
+				}
+				panic("should only call start_trial")
+			},
+			wantErr: false,
+		},
+		{
 			name: "short-circuit: already up to date",
 			args: args{
 				current: &esclient.License{
@@ -109,6 +126,7 @@ func Test_applyLinkedLicense(t *testing.T) {
 	tests := []struct {
 		name             string
 		initialObjs      []runtime.Object
+		currentLicense   *esclient.License
 		errors           map[client.ObjectKey]error
 		wantErr          bool
 		clientAssertions func(updater fakeLicenseUpdater)
@@ -127,12 +145,23 @@ func Test_applyLinkedLicense(t *testing.T) {
 					},
 				},
 			},
+			clientAssertions: func(updater fakeLicenseUpdater) {
+				require.True(t, updater.updateLicenseCalled, "should update license")
+			},
 		},
 		{
 			name:    "no error: no license found",
 			wantErr: false,
 			clientAssertions: func(updater fakeLicenseUpdater) {
 				require.True(t, updater.startBasicCalled, "should call start_basic")
+			},
+		},
+		{
+			name:           "no error: no license found but tolerate a cluster level trial",
+			wantErr:        false,
+			currentLicense: &esclient.License{Type: string(esclient.ElasticsearchLicenseTypeTrial)},
+			clientAssertions: func(updater fakeLicenseUpdater) {
+				require.False(t, updater.startBasicCalled, "should not call start_basic")
 			},
 		},
 		{
@@ -184,7 +213,7 @@ func Test_applyLinkedLicense(t *testing.T) {
 				context.Background(),
 				c,
 				clusterName,
-				nil,
+				tt.currentLicense,
 				&updater,
 			); (err != nil) != tt.wantErr {
 				t.Errorf("applyLinkedLicense() error = %v, wantErr %v", err, tt.wantErr)
@@ -197,8 +226,9 @@ func Test_applyLinkedLicense(t *testing.T) {
 }
 
 type fakeLicenseUpdater struct {
-	license          esclient.License
-	startBasicCalled bool
+	license             esclient.License
+	startBasicCalled    bool
+	updateLicenseCalled bool
 }
 
 func (f *fakeLicenseUpdater) StartTrial(ctx context.Context) (esclient.StartTrialResponse, error) {
@@ -213,6 +243,7 @@ func (f *fakeLicenseUpdater) GetLicense(ctx context.Context) (esclient.License, 
 }
 
 func (f *fakeLicenseUpdater) UpdateLicense(ctx context.Context, licenses esclient.LicenseUpdateRequest) (esclient.LicenseUpdateResponse, error) {
+	f.updateLicenseCalled = true
 	return esclient.LicenseUpdateResponse{
 		Acknowledged:  true,
 		LicenseStatus: "valid",

commit 13b62dcbcbac45296e9c0fda803dfa3b139d9e01

Replace the license observer by a sync call in the reconciliation (#3165)
Fixes #3163.
Fixes a bug introduced in #3150.

We used to rely on the license fetched by the observers on a regular
basis, but this can lead to a race condition as described in
#3163 (comment).

Instead, let's keep things simple, and do a synchronous request to
get the license as part of the normal reconciliation, to then decide
whether or not it should be updated.

In case of error (eg. Elasticsearch req timeout), enqueue a new
reconciliation, but don't abort the current reconciliation.

This PR also adds some minor refactoring of the code, and avoids
a call to start_basic if the current license is already basic.

pkg/controller/elasticsearch/driver/driver.go
@@ -193,34 +193,20 @@ func (d *defaultDriver) Reconcile(ctx context.Context) *reconciler.Results {
 		return results.WithError(err)
 	}
 
-	results.Apply(
-		"reconcile-cluster-license",
-		func(ctx context.Context) (controller.Result, error) {
-			if !esReachable {
-				return defaultRequeue, nil
-			}
-
-			err := license.Reconcile(
-				ctx,
-				d.Client,
-				d.ES,
-				esClient,
-				observedState.ClusterLicense,
-			)
-
-			if err != nil {
-				d.ReconcileState.AddEvent(
-					corev1.EventTypeWarning,
-					events.EventReasonUnexpected,
-					fmt.Sprintf("Could not update cluster license: %s", err.Error()),
-				)
-				return defaultRequeue, err
-			}
-			return controller.Result{}, err
-		},
-	)
-
 	if esReachable {
+		// reconcile the license
+		if err := license.Reconcile(ctx, d.Client, d.ES, esClient); err != nil {
+			d.ReconcileState.AddEvent(
+				corev1.EventTypeWarning,
+				events.EventReasonUnexpected,
+				fmt.Sprintf("Could not update cluster license: %s", err.Error()),
+			)
+			log.Error(err, "Could not update cluster license", "namespace", d.ES.Namespace, "es_name", d.ES.Name)
+			// don't error out the entire reconciliation, move on with next steps and retry later
+			results.WithResult(defaultRequeue)
+		}
+
+		// reconcile remote clusters
 		requeue, err := remotecluster.UpdateSettings(ctx, d.Client, esClient, d.Recorder(), d.LicenseChecker, d.ES)
 		if err != nil {
 			msg := "Could not update remote clusters in Elasticsearch settings"
@@ -229,7 +215,7 @@ func (d *defaultDriver) Reconcile(ctx context.Context) *reconciler.Results {
 			results.WithResult(defaultRequeue)
 		}
 		if requeue {
-			results = results.WithResult(defaultRequeue)
+			results.WithResult(defaultRequeue)
 		}
 	}
 

pkg/controller/elasticsearch/license/apply.go
@@ -23,42 +23,65 @@ import (
 var log = logf.Log.WithName("elasticsearch-controller")
 
 // isTrial returns true if an Elasticsearch license is of the trial type
-func isTrial(l *esclient.License) bool {
-	return l != nil && l.Type == string(esclient.ElasticsearchLicenseTypeTrial)
+func isTrial(l esclient.License) bool {
+	return l.Type == string(esclient.ElasticsearchLicenseTypeTrial)
+}
+
+// isBasic returns true if an Elasticsearch license is of the basic type
+func isBasic(l esclient.License) bool {
+	return l.Type == string(esclient.ElasticsearchLicenseTypeBasic)
 }
 
 func applyLinkedLicense(
 	ctx context.Context,
 	c k8s.Client,
 	esCluster types.NamespacedName,
-	current *esclient.License,
 	updater esclient.LicenseClient,
 ) error {
-	var license corev1.Secret
+	// get the current license
+	ctx, cancel := context.WithTimeout(ctx, esclient.DefaultReqTimeout)
+	defer cancel()
+	current, err := updater.GetLicense(ctx)
+	if err != nil {
+		return err
+	}
+
+	// get the expected license
 	// the underlying assumption here is that either a user or a
 	// license controller has created a cluster license in the
 	// namespace of this cluster following the cluster-license naming
 	// convention
-	err := c.Get(
+	var license corev1.Secret
+	err = c.Get(
 		types.NamespacedName{
 			Namespace: esCluster.Namespace,
 			Name:      esv1.LicenseSecretName(esCluster.Name),
 		},
 		&license,
 	)
-	if err != nil {
-		if apierrors.IsNotFound(err) {
-			if isTrial(current) {
-				// this is the case if the user has started a trial on the ES cluster level
-				// e.g. from Kibana when trying to access a commercial feature.
-				// While this is not a supported use case we tolerate it to avoid a bad user
-				// experience because trials can only be started once on the ES cluster level
-				return nil
-			}
-			// no license linked to this cluster. Revert to basic.
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+	if err != nil && apierrors.IsNotFound(err) {
+		// no license expected, let's look at the current cluster license
+		switch {
+		case isBasic(current):
+			// nothing to do
+			return nil
+		case isTrial(current):
+			// Elasticsearch reports a trial license, but there's no ECK enterprise trial requested.
+			// This can be the case if:
+			// - an ECK trial was started previously, then stopped (secret removed)
+			// - the user manually started a trial at the stack level (eg. by clicking a button in Kibana when
+			// trying to access a commercial feature). While this is not a supported use case,
+			// we tolerate it to avoid a bad user experience because trials can only be started once.
+			log.V(1).Info("Preserving existing stack-level trial license",
+				"namespace", esCluster.Namespace, "es_name", esCluster.Name)
+			return nil
+		default:
+			// revert the current license to basic
 			return startBasic(ctx, updater)
 		}
-		return err
 	}
 
 	bytes, err := commonlicense.FetchLicenseData(license.Data)
@@ -90,10 +113,10 @@ func updateLicense(
 	ctx context.Context,
 	esCluster types.NamespacedName,
 	updater esclient.LicenseClient,
-	current *esclient.License,
+	current esclient.License,
 	desired esclient.License,
 ) error {
-	if current != nil && (current.UID == desired.UID || (isTrial(current) && current.Type == desired.Type)) {
+	if current.UID == desired.UID || (isTrial(current) && current.Type == desired.Type) {
 		return nil // we are done already applied
 	}
 	request := esclient.LicenseUpdateRequest{
@@ -104,7 +127,7 @@ func updateLicense(
 	ctx, cancel := context.WithTimeout(ctx, esclient.DefaultReqTimeout)
 	defer cancel()
 
-	if isTrial(&desired) {
+	if isTrial(desired) {
 		return pkgerrors.Wrap(startTrial(ctx, updater, esCluster), "failed to start trial")
 	}
 

pkg/controller/elasticsearch/license/reconcile.go
@@ -18,8 +18,7 @@ func Reconcile(
 	c k8s.Client,
 	esCluster esv1.Elasticsearch,
 	clusterClient esclient.Client,
-	current *esclient.License,
 ) error {
 	clusterName := k8s.ExtractNamespacedName(&esCluster)
-	return applyLinkedLicense(ctx, c, clusterName, current, clusterClient)
+	return applyLinkedLicense(ctx, c, clusterName, clusterClient)
 }

pkg/controller/elasticsearch/observer/state.go
@@ -16,40 +16,14 @@ type State struct {
 	// TODO: verify usages of the below never assume they are set (check for nil)
 	// ClusterHealth is the current traffic light health as reported by Elasticsearch.
 	ClusterHealth *esclient.Health
-	// TODO should probably be a separate observer
-	// ClusterLicense is the current license applied to this cluster
-	ClusterLicense *esclient.License
 }
 
 // RetrieveState returns the current Elasticsearch cluster state
 func RetrieveState(ctx context.Context, cluster types.NamespacedName, esClient esclient.Client) State {
-	// retrieve cluster health and license in parallel
-	healthChan := make(chan *esclient.Health)
-	licenseChan := make(chan *esclient.License)
-
-	go func() {
-		health, err := esClient.GetClusterHealth(ctx)
-		if err != nil {
-			log.V(1).Info("Unable to retrieve cluster health", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
-			healthChan <- nil
-			return
-		}
-		healthChan <- &health
-	}()
-
-	go func() {
-		license, err := esClient.GetLicense(ctx)
-		if err != nil {
-			log.V(1).Info("Unable to retrieve cluster license", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
-			licenseChan <- nil
-			return
-		}
-		licenseChan <- &license
-	}()
-
-	// return the state when ready, may contain nil values
-	return State{
-		ClusterHealth:  <-healthChan,
-		ClusterLicense: <-licenseChan,
+	health, err := esClient.GetClusterHealth(ctx)
+	if err != nil {
+		log.V(1).Info("Unable to retrieve cluster health", "error", err, "namespace", cluster.Namespace, "es_name", cluster.Name)
+		return State{ClusterHealth: nil}
 	}
+	return State{ClusterHealth: &health}
 }

pkg/controller/elasticsearch/license/apply_test.go
@@ -26,8 +26,12 @@ import (
 )
 
 func Test_updateLicense(t *testing.T) {
+	enterpriseLicense := esclient.License{
+		UID:  "enterpise-license",
+		Type: string(esclient.ElasticsearchLicenseTypeEnterprise),
+	}
 	type args struct {
-		current *esclient.License
+		current esclient.License
 		desired esclient.License
 	}
 	tests := []struct {
@@ -40,8 +44,7 @@ func Test_updateLicense(t *testing.T) {
 			name:    "error: HTTP error",
 			wantErr: true,
 			args: args{
-				current: nil,
-				desired: esclient.License{},
+				desired: enterpriseLicense,
 			},
 			reqFn: func(req *http.Request) *http.Response {
 				return esclient.NewMockResponse(400, req, "")
@@ -51,8 +54,7 @@ func Test_updateLicense(t *testing.T) {
 			name:    "error: ES error",
 			wantErr: true,
 			args: args{
-				current: nil,
-				desired: esclient.License{},
+				desired: enterpriseLicense,
 			},
 			reqFn: func(req *http.Request) *http.Response {
 				return esclient.NewMockResponse(
@@ -65,8 +67,7 @@ func Test_updateLicense(t *testing.T) {
 		{
 			name: "happy path",
 			args: args{
-				current: nil,
-				desired: esclient.License{},
+				desired: enterpriseLicense,
 			},
 			reqFn: func(req *http.Request) *http.Response {
 				return esclient.NewMockResponse(
@@ -79,7 +80,6 @@ func Test_updateLicense(t *testing.T) {
 		{
 			name: "start a trial",
 			args: args{
-				current: nil,
 				desired: esclient.License{
 					Type: string(esclient.ElasticsearchLicenseTypeTrial),
 				},
@@ -95,7 +95,7 @@ func Test_updateLicense(t *testing.T) {
 		{
 			name: "short-circuit: already up to date",
 			args: args{
-				current: &esclient.License{
+				current: esclient.License{
 					UID: "this-is-a-uid",
 				},
 				desired: esclient.License{
@@ -109,7 +109,6 @@ func Test_updateLicense(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			c := esclient.NewMockClient(version.MustParse("6.8.0"), tt.reqFn)
 			if err := updateLicense(context.Background(), types.NamespacedName{}, c, tt.args.current, tt.args.desired); (err != nil) != tt.wantErr {
 				t.Errorf("updateLicense() error = %v, wantErr %v", err, tt.wantErr)
@@ -126,7 +125,7 @@ func Test_applyLinkedLicense(t *testing.T) {
 	tests := []struct {
 		name             string
 		initialObjs      []runtime.Object
-		currentLicense   *esclient.License
+		currentLicense   esclient.License
 		errors           map[client.ObjectKey]error
 		wantErr          bool
 		clientAssertions func(updater fakeLicenseUpdater)
@@ -150,16 +149,25 @@ func Test_applyLinkedLicense(t *testing.T) {
 			},
 		},
 		{
-			name:    "no error: no license found",
-			wantErr: false,
+			name:           "no error: no license found but stack has an enterprise license",
+			wantErr:        false,
+			currentLicense: esclient.License{Type: string(esclient.ElasticsearchLicenseTypeEnterprise)},
 			clientAssertions: func(updater fakeLicenseUpdater) {
-				require.True(t, updater.startBasicCalled, "should call start_basic")
+				require.True(t, updater.startBasicCalled, "should call start_basic to remove the license")
+			},
+		},
+		{
+			name:           "no error: no license found, stack already in basic license",
+			wantErr:        false,
+			currentLicense: esclient.License{Type: string(esclient.ElasticsearchLicenseTypeBasic)},
+			clientAssertions: func(updater fakeLicenseUpdater) {
+				require.False(t, updater.startBasicCalled, "should not call start_basic if already basic")
 			},
 		},
 		{
 			name:           "no error: no license found but tolerate a cluster level trial",
 			wantErr:        false,
-			currentLicense: &esclient.License{Type: string(esclient.ElasticsearchLicenseTypeTrial)},
+			currentLicense: esclient.License{Type: string(esclient.ElasticsearchLicenseTypeTrial)},
 			clientAssertions: func(updater fakeLicenseUpdater) {
 				require.False(t, updater.startBasicCalled, "should not call start_basic")
 			},
@@ -208,12 +216,11 @@ func Test_applyLinkedLicense(t *testing.T) {
 				Client: k8s.WrappedFakeClient(tt.initialObjs...),
 				errors: tt.errors,
 			}
-			updater := fakeLicenseUpdater{}
+			updater := fakeLicenseUpdater{license: tt.currentLicense}
 			if err := applyLinkedLicense(
 				context.Background(),
 				c,
 				clusterName,
-				tt.currentLicense,
 				&updater,
 			); (err != nil) != tt.wantErr {
 				t.Errorf("applyLinkedLicense() error = %v, wantErr %v", err, tt.wantErr)

pkg/controller/elasticsearch/observer/state_test.go
@@ -20,7 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-func fakeEsClient(healthRespErr, licenseRespErr bool) client.Client {
+func fakeEsClient(healthRespErr bool) client.Client {
 	return client.NewMockClient(version.MustParse("6.8.0"), func(req *http.Request) *http.Response {
 		statusCode := 200
 		var respBody io.ReadCloser
@@ -30,14 +30,6 @@ func fakeEsClient(healthRespErr, licenseRespErr bool) client.Client {
 			if healthRespErr {
 				statusCode = 500
 			}
-		}
-
-		if strings.Contains(req.URL.RequestURI(), "license") {
-			respBody = ioutil.NopCloser(bytes.NewBufferString(fixtures.LicenseGetSample))
-			if licenseRespErr {
-				statusCode = 500
-			}
-
 		}
 
 		return &http.Response{
@@ -51,54 +43,27 @@ func fakeEsClient(healthRespErr, licenseRespErr bool) client.Client {
 
 func TestRetrieveState(t *testing.T) {
 	tests := []struct {
-		name        string
-		wantHealth  bool
-		wantLicense bool
+		name       string
+		wantHealth bool
 	}{
 		{
-			name:        "health, license and keystore ok",
-			wantHealth:  true,
-			wantLicense: true,
+			name:       "health ok",
+			wantHealth: true,
 		},
 		{
-			name:        "info error",
-			wantHealth:  true,
-			wantLicense: true,
-		},
-		{
-			name:        "health error",
-			wantHealth:  false,
-			wantLicense: true,
-		},
-		{
-			name:        "license error",
-			wantHealth:  false,
-			wantLicense: true,
-		},
-		{
-			name:        "info and state error",
-			wantHealth:  false,
-			wantLicense: true,
-		},
-		{
-			name:        "keystore error",
-			wantHealth:  true,
-			wantLicense: true,
+			name:       "health error",
+			wantHealth: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cluster := types.NamespacedName{Namespace: "ns1", Name: "es1"}
-			esClient := fakeEsClient(!tt.wantHealth, !tt.wantLicense)
+			esClient := fakeEsClient(!tt.wantHealth)
 			state := RetrieveState(context.Background(), cluster, esClient)
 			if tt.wantHealth {
 				require.NotNil(t, state.ClusterHealth)
 				require.Equal(t, 3, state.ClusterHealth.NumberOfNodes)
-			}
-			if tt.wantLicense {
-				require.NotNil(t, state.ClusterLicense)
-				require.Equal(t, "893361dc-9749-4997-93cb-802e3d7fa4xx", state.ClusterLicense.UID)
 			}
 		})
 	}