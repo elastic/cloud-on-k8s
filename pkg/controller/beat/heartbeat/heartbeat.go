e321480de1e546a1937fe8f8ce397106829809e3
David Kowalski <david.kowalski@elastic.co>
2020-06-16 14:25:24 +0200

    Add heartbeat driver

---
+++ pkg/controller/beat/heartbeat/heartbeat.go
@@ -0,0 +1,28 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package heartbeat
+
+import (
+	beatcommon "github.com/elastic/cloud-on-k8s/pkg/controller/beat/common"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/container"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/reconciler"
+)
+
+const (
+	Type beatcommon.Type = "heartbeat"
+)
+
+type Driver struct {
+	beatcommon.DriverParams
+	beatcommon.Driver
+}
+
+func NewDriver(params beatcommon.DriverParams) beatcommon.Driver {
+	return &Driver{DriverParams: params}
+}
+
+func (d *Driver) Reconcile() *reconciler.Results {
+	return beatcommon.Reconcile(d.DriverParams, nil, container.HeartbeatImage)
+}
