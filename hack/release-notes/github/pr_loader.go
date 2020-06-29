8d2c77a8a907e3f5ce1759affe2896cb006549ea
Charith Ellawala <52399125+charith-elastic@users.noreply.github.com>
2020-06-29 08:06:57 +0100

    Switch release notes tool to GitHub API v4 (#3323)
    * Switch release notes tool to GitHub API v4
    
    * Rename GH_TOKEN to GITHUB_TOKEN

---
+++ hack/release-notes/github/pr_loader.go
@@ -0,0 +1,202 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package github
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+const (
+	graphqlEndpoint = "https://api.github.com/graphql"
+	contentType     = "application/json; charset=utf-8"
+	maxPayloadSize  = 1024 * 1024 * 10 // 10 MiB
+
+	queryBody = `
+query ($q: String!, $per_page: Int = 50, $after: String) {
+  search(query: $q, type: ISSUE, first: $per_page, after: $after) {
+    nodes {
+      ... on PullRequest {
+        number
+        title
+        merged
+        labels(first: 10) {
+          edges {
+            node {
+             name
+            }
+          }
+        }
+		bodyHTML
+      }
+    }
+
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`
+)
+
+type PullRequest struct {
+	Number int
+	Title  string
+	Labels map[string]struct{}
+	Issues []int
+}
+
+func LoadPullRequests(repoName, version string, ignoredLabels map[string]struct{}) ([]PullRequest, error) {
+	client := mkClient()
+	loader := &prLoader{
+		apiEndpoint: graphqlEndpoint,
+		repoName:    repoName,
+		version:     version,
+		prp:         newPRProcessor(repoName, ignoredLabels),
+	}
+
+	return loader.loadPullRequests(client)
+}
+
+func mkClient() *http.Client {
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	if token := os.Getenv("GITHUB_TOKEN"); token != "" {
+		tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+		ctx := context.WithValue(context.Background(), oauth2.HTTPClient, client)
+
+		return oauth2.NewClient(ctx, tokenSource)
+	}
+
+	return client
+}
+
+type prLoader struct {
+	apiEndpoint string
+	repoName    string
+	version     string
+	prp         *prProcessor
+}
+
+func (loader *prLoader) loadPullRequests(client *http.Client) ([]PullRequest, error) {
+	var pullRequests []PullRequest
+	var cursor *string
+
+	for {
+		apiResp, err := loader.mkRequest(client, cursor)
+		if err != nil {
+			return pullRequests, err
+		}
+
+		batch := loader.prp.extractPullRequests(apiResp)
+		pullRequests = append(pullRequests, batch...)
+
+		if !apiResp.Data.Search.PageInfo.HasNextPage {
+			return pullRequests, nil
+		}
+
+		cursor = &apiResp.Data.Search.PageInfo.EndCursor
+	}
+}
+
+func (loader *prLoader) mkRequest(client *http.Client, cursor *string) (*apiResponse, error) {
+	req, err := loader.buildRequest(cursor)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			_, _ = io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received status %s from the API", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(io.LimitReader(resp.Body, maxPayloadSize))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var apiResp apiResponse
+	if err := json.Unmarshal(body, &apiResp); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &apiResp, nil
+}
+
+type apiResponse struct {
+	Data struct {
+		Search struct {
+			Nodes []struct {
+				Number   int    `json:"number"`
+				Title    string `json:"title"`
+				Merged   bool   `json:"merged"`
+				BodyHTML string `json:"bodyHTML"`
+				Labels   *struct {
+					Edges []struct {
+						Node struct {
+							Name string `json:"name"`
+						} `json:"node"`
+					} `json:"edges"`
+				} `json:"labels"`
+			} `json:"nodes"`
+			PageInfo struct {
+				HasNextPage bool   `json:"hasNextPage"`
+				EndCursor   string `json:"endCursor"`
+			} `json:"pageInfo"`
+		} `json:"search"`
+	} `json:"data"`
+}
+
+func (loader *prLoader) buildRequest(cursor *string) (*http.Request, error) {
+	variables := map[string]interface{}{
+		"q":     fmt.Sprintf("repo:%s is:pr is:closed label:v%s", loader.repoName, loader.version),
+		"after": cursor,
+	}
+
+	body := struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Variables: variables,
+		Query:     queryBody,
+	}
+
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal request body: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, loader.apiEndpoint, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-type", contentType)
+
+	return req, nil
+}
---
+++ hack/release-notes/github/pr_processor.go
@@ -0,0 +1,127 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package github
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type prProcessor struct {
+	issueRegexp   *regexp.Regexp
+	ignoredLabels map[string]struct{}
+}
+
+func newPRProcessor(repoName string, ignoredLabels map[string]struct{}) *prProcessor {
+	return &prProcessor{
+		issueRegexp:   regexp.MustCompile(fmt.Sprintf(`https://github.com/%s/issues/(\d+)`, repoName)),
+		ignoredLabels: ignoredLabels,
+	}
+}
+
+func (prp *prProcessor) extractPullRequests(resp *apiResponse) []PullRequest {
+	var prs []PullRequest
+
+MAIN_LOOP:
+	for _, r := range resp.Data.Search.Nodes {
+		if !r.Merged {
+			continue
+		}
+
+		pr := PullRequest{
+			Title:  r.Title,
+			Number: r.Number,
+		}
+
+		if r.Labels != nil {
+			pr.Labels = make(map[string]struct{}, len(r.Labels.Edges))
+
+			for _, lbl := range r.Labels.Edges {
+				lblName := lbl.Node.Name
+
+				if _, ok := prp.ignoredLabels[lblName]; ok {
+					continue MAIN_LOOP
+				}
+
+				pr.Labels[lblName] = struct{}{}
+			}
+		}
+
+		pr.Issues = prp.extractIssues(r.BodyHTML)
+
+		prs = append(prs, pr)
+	}
+
+	return prs
+}
+
+func (prp *prProcessor) extractIssues(body string) []int {
+	node, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		log.Fatalf("Failed to parse HTML: %v\n%s", err, body)
+	}
+
+	linkedIssues := prp.findLinkedIssues(node)
+	if len(linkedIssues) == 0 {
+		return nil
+	}
+
+	issueList := make([]int, 0, len(linkedIssues))
+	for i := range linkedIssues {
+		issueList = append(issueList, i)
+	}
+
+	sort.Ints(issueList)
+
+	return issueList
+}
+
+func (prp *prProcessor) findLinkedIssues(node *html.Node) map[int]struct{} {
+	linkedIssues := map[int]struct{}{}
+
+	if node.Type == html.ElementNode && node.Data == "a" {
+		if issue := prp.extractIssueNumber(node); issue > 0 {
+			linkedIssues[issue] = struct{}{}
+		}
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		issues := prp.findLinkedIssues(c)
+		for i := range issues {
+			linkedIssues[i] = struct{}{}
+		}
+	}
+
+	return linkedIssues
+}
+
+func (prp *prProcessor) extractIssueNumber(node *html.Node) int {
+	attrs := make(map[string]string, len(node.Attr))
+	for _, attr := range node.Attr {
+		attrs[attr.Key] = attr.Val
+	}
+
+	if class, ok := attrs["class"]; ok && strings.Contains(class, "issue-link") {
+		href := attrs["href"]
+		matches := prp.issueRegexp.FindStringSubmatch(href)
+
+		if len(matches) == 2 {
+			issueNum, err := strconv.Atoi(matches[1])
+			if err != nil {
+				log.Fatalf("Failed to convert %s to number: %v", matches[1], err)
+			}
+
+			return issueNum
+		}
+	}
+
+	return -1
+}
