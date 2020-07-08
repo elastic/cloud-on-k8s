076be691322228ebe07c351e7fbfe202f95bc361
David Kowalski <david.kowalski@elastic.co>
2020-07-08 10:01:12 +0200

    Add Beat config library

--- test/e2e/test/run.go
+++ test/e2e/test/run.go
@@ -40,3 +40,40 @@ func Sequence(before StepsFunc, f StepsFunc, builders ...Builder) StepList {
 
 	return steps
 }
+
+// BeforeAfterSequence returns a list of steps corresponding to a workflow that allows defining list of step to execute
+// before and after builder workflow (before steps, init, create, checks, deletes, after steps)
+func BeforeAfterSequence(before StepsFunc, after StepsFunc, builders ...Builder) StepList {
+	steps := StepList{}
+	for _, b := range builders {
+		// ignore the test if some builders cannot be tested
+		if b.SkipTest() {
+			return steps
+		}
+	}
+
+	k := NewK8sClientOrFatal()
+
+	if before != nil {
+		steps = steps.WithSteps(before(k))
+	}
+
+	for _, b := range builders {
+		steps = steps.WithSteps(b.InitTestSteps(k))
+	}
+	for _, b := range builders {
+		steps = steps.WithSteps(b.CreationTestSteps(k))
+	}
+	for _, b := range builders {
+		steps = steps.WithSteps(CheckTestSteps(b, k))
+	}
+	for _, b := range builders {
+		steps = steps.WithSteps(b.DeletionTestSteps(k))
+	}
+
+	if after != nil {
+		steps = steps.WithSteps(after(k))
+	}
+
+	return steps
+}
