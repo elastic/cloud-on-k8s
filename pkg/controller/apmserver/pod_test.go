bf6201fdbfef064bd4d8819a67ada0d7c7e6a425
Anya Sabo <anya.sabo@elastic.co>
2020-07-13 12:39:01 -0500

    Add node shard allocation awareness by default (#3380)

--- pkg/controller/apmserver/pod_test.go
+++ pkg/controller/apmserver/pod_test.go
@@ -5,7 +5,6 @@
 package apmserver
 
 import (
-	"reflect"
 	"testing"
 
 	apmv1 "github.com/elastic/cloud-on-k8s/pkg/apis/apm/v1"
@@ -13,6 +12,7 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/container"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/volume"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/settings"
+	"github.com/go-test/deep"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -75,9 +75,15 @@ func TestNewPodSpec(t *testing.T) {
 									},
 								},
 								{
-									Name: "POD_NAME",
+									Name: settings.EnvPodName,
 									ValueFrom: &corev1.EnvVarSource{
 										FieldRef: &corev1.ObjectFieldSelector{APIVersion: "v1", FieldPath: "metadata.name"},
+									},
+								},
+								{
+									Name: settings.EnvNodeName,
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{APIVersion: "v1", FieldPath: "spec.nodeName"},
 									},
 								},
 								{
@@ -105,9 +111,9 @@ func TestNewPodSpec(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newPodSpec(&tt.as, tt.p); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewPodSpec() = %v, want %v", got, tt.want)
-			}
+			got := newPodSpec(&tt.as, tt.p)
+			diff := deep.Equal(tt.want, got)
+			assert.Empty(t, diff)
 		})
 	}
 }
