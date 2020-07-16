3e31d7f81acf63d7b716d47d451edd15e8aef4db
Anya Sabo <anya.sabo@elastic.co>
2020-07-16 16:40:26 -0500

    Wrap more errors

--- pkg/controller/common/reconciler/reconciler.go
+++ pkg/controller/common/reconciler/reconciler.go
@@ -111,7 +111,7 @@ func ReconcileResource(params Params) error {
 		// Create the object, which modifies params.Reconciled in-place
 		err = params.Client.Create(params.Reconciled)
 		if err != nil {
-			return err
+			return errors.WithStack(err)
 		}
 		return nil
 	}
@@ -167,7 +167,7 @@ func ReconcileResource(params Params) error {
 		reconciledMeta.SetResourceVersion(resourceVersion)
 		err = params.Client.Update(params.Reconciled)
 		if err != nil {
-			return err
+			return errors.WithStack(err)
 		}
 		if params.PostUpdate != nil {
 			params.PostUpdate()
