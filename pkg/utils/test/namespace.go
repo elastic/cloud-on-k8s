35efa35baca323d07f2673035184ed3d1838f2b4
Peter Brachwitz <peter.brachwitz@elastic.co>
2020-07-20 16:48:53 +0200

    Fix flaky namespace creation in integration tests

---
+++ pkg/utils/test/namespace.go
@@ -0,0 +1,27 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package test
+
+import (
+	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func EnsureNamespace(c k8s.Client, ns string) error {
+	expected := corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ns,
+		},
+	}
+	existing := corev1.Namespace{}
+	err := c.Get(types.NamespacedName{Name: ns}, &existing)
+	if errors.IsNotFound(err) {
+		return c.Create(&expected)
+	}
+	return nil
+}
