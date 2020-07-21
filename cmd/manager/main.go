b1671e150c650bbc3828ec2d365e1f74dfaa583e
Walid Mellouli <mellouli.walid@gmail.com>
2020-07-21 09:54:29 +0200

    Add WebhookConfigurationName Flag (#3424) (#3425)
    Add a new optional flag webhook-configuration-name (env WEBHOOK_CONFIGURATION_NAME) to allow users to change the default WebhookConfigurationName value elastic-webhook.k8s.elastic.co when needed.

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -62,9 +62,9 @@ import (
 )
 
 const (
-	DefaultMetricPort        = 0 // disabled
-	WebhookConfigurationName = "elastic-webhook.k8s.elastic.co"
-	WebhookPort              = 9443
+	DefaultMetricPort               = 0 // disabled
+	DefaultWebhookConfigurationName = "elastic-webhook.k8s.elastic.co"
+	WebhookPort                     = 9443
 )
 
 var (
@@ -168,6 +168,11 @@ func init() {
 		operator.WebhookSecretFlag,
 		"",
 		fmt.Sprintf("K8s secret mounted into the path designated by %s to be used for webhook certificates", operator.WebhookCertDirFlag),
+	)
+	Cmd.Flags().String(
+		operator.WebhookConfigurationNameFlag,
+		DefaultWebhookConfigurationName,
+		fmt.Sprintf("Name of the Kubernetes ValidatingWebhookConfiguration resource (defaults to %s). Only used when enable-webhook is true.", DefaultWebhookConfigurationName),
 	)
 
 	// enable using dashed notation in flags and underscores in env
@@ -447,7 +452,7 @@ func setupWebhook(mgr manager.Manager, certRotation certificates.RotationParams,
 		webhookParams := webhook.Params{
 			Namespace:                viper.GetString(operator.OperatorNamespaceFlag),
 			SecretName:               viper.GetString(operator.WebhookSecretFlag),
-			WebhookConfigurationName: WebhookConfigurationName,
+			WebhookConfigurationName: viper.GetString(operator.WebhookConfigurationNameFlag),
 			Rotation:                 certRotation,
 		}
 
