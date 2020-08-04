a2563d2a690ee9ddcefbe87e85ff6b80494f6424
Michael Morello <michael.morello@elastic.co>
2020-08-04 10:40:41 +0200

    Add support for Kibana 7.9.0 keystore (#3566) (#3580)

---
+++ pkg/controller/kibana/keystore.go
@@ -0,0 +1,47 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package kibana
+
+import (
+	kbv1 "github.com/elastic/cloud-on-k8s/pkg/apis/kibana/v1"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/keystore"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/common/version"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+// keystoreInConfigDirVersion is the version in which the keystore is no longer stored in the data directory but in the config one.
+var keystoreInConfigDirVersion = version.From(7, 9, 0)
+
+// newInitContainersParameters is used to generate the init container that will load the secure settings into a keystore
+func newInitContainersParameters(kb *kbv1.Kibana) (keystore.InitContainerParameters, error) {
+	parameters := keystore.InitContainerParameters{
+		KeystoreCreateCommand:         "/usr/share/kibana/bin/kibana-keystore create",
+		KeystoreAddCommand:            `/usr/share/kibana/bin/kibana-keystore add "$key" --stdin < "$filename"`,
+		SecureSettingsVolumeMountPath: keystore.SecureSettingsVolumeMountPath,
+		KeystoreVolumePath:            DataVolumeMountPath,
+		Resources: corev1.ResourceRequirements{
+			Requests: map[corev1.ResourceName]resource.Quantity{
+				corev1.ResourceMemory: resource.MustParse("128Mi"),
+				corev1.ResourceCPU:    resource.MustParse("100m"),
+			},
+			Limits: map[corev1.ResourceName]resource.Quantity{
+				corev1.ResourceMemory: resource.MustParse("128Mi"),
+				corev1.ResourceCPU:    resource.MustParse("100m"),
+			},
+		},
+	}
+
+	kbVersion, err := version.Parse(kb.Spec.Version)
+	if err != nil {
+		return parameters, err
+	}
+
+	if kbVersion.IsSameOrAfter(keystoreInConfigDirVersion) {
+		parameters.KeystoreVolumePath = ConfigSharedVolume.ContainerMountPath
+	}
+
+	return parameters, nil
+}
