7b7510d43ab4c2597cd016947603fd1ab8e34242
Thibault Richard <thb.krkr@gmail.com>
2020-09-18 21:26:54 +0200

    Make gcp auth reusable

---
+++ hack/deployer/runner/gcloud.go
@@ -0,0 +1,70 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package runner
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const (
+	GCPDir                 = ".gcp"
+	ServiceAccountFilename = "osServiceAccount.json"
+)
+
+// authToGCP authenticates the deployer to the Google Cloud Platform as a service account or as a user.
+func authToGCP(
+	vaultInfo VaultInfo, vaultPath string, serviceAccountVaultFieldName string,
+	asServiceAccount bool, useNonDefaultCloudSDKPath bool, gCloudProject interface{},
+) error {
+	if asServiceAccount {
+		log.Println("Authenticating as service account...")
+
+		client, err := NewClient(vaultInfo)
+		if err != nil {
+			return err
+		}
+
+		gcpDir := filepath.Join(os.Getenv("HOME"), GCPDir)
+		keyFileName := filepath.Join(gcpDir, ServiceAccountFilename)
+		_ = os.MkdirAll(gcpDir, os.ModePerm)
+
+		if err := client.ReadIntoFile(keyFileName, vaultPath, serviceAccountVaultFieldName); err != nil {
+			return err
+		}
+
+		if useNonDefaultCloudSDKPath {
+			// ensure gcloud & gsutil rely on credentials stored in gcpDir instead of using the default
+			// directory (~/.config/gcloud), to not tamper any default gcloud auth already set on the system
+			log.Printf("Setting CLOUDSDK_CONFIG=%s", gcpDir)
+			if err := os.Setenv("CLOUDSDK_CONFIG", gcpDir); err != nil {
+				return err
+			}
+		}
+		// now that we're set on the cloud sdk directory, we can run any gcloud command that will rely on it
+		if err := NewCommand(fmt.Sprintf("gcloud config set project %s", gCloudProject)).Run(); err != nil {
+			return err
+		}
+
+		return NewCommand("gcloud auth activate-service-account --key-file=" + keyFileName).Run()
+	}
+
+	log.Println("Authenticating as user...")
+	accounts, err := NewCommand(`gcloud auth list "--format=value(account)"`).StdoutOnly().WithoutStreaming().Output()
+	if err != nil {
+		return err
+	}
+	if len(accounts) > 0 {
+		return nil
+	}
+
+	if err := NewCommand(fmt.Sprintf("gcloud config set project %s", gCloudProject)).Run(); err != nil {
+		return err
+	}
+
+	return NewCommand("gcloud auth login").Run()
+}
