b9cf9eae17b06c7fc1e9adf2584b731053c0ebec
Thibault Richard <thb.krkr@gmail.com>
2020-09-21 14:49:43 +0200

    UseNonDefaultCloudSDKPath on GKE

--- hack/deployer/runner/gke.go
+++ hack/deployer/runner/gke.go
@@ -80,7 +80,7 @@ func (gdf *GkeDriverFactory) Create(plan Plan) (Driver, error) {
 func (d *GkeDriver) Execute() error {
 	if err := authToGCP(
 		*d.plan.VaultInfo, GkeVaultPath, GkeServiceAccountVaultFieldName,
-		d.plan.ServiceAccount, d.plan.Ocp.UseNonDefaultCloudSDKPath, false, d.ctx["GCloudProject"],
+		d.plan.ServiceAccount, true, false, d.ctx["GCloudProject"],
 	); err != nil {
 		return err
 	}
