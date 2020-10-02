4ff8d60c2bfeb3e8f8d8086b876960d23e5a1758
sebgl <contact.sebgl@gmail.com>
2020-10-02 10:53:50 +0200

    Unit test the Elasticsearch webhook

--- pkg/controller/elasticsearch/validation/webhook_test.go
+++ pkg/controller/elasticsearch/validation/webhook_test.go
@@ -5,47 +5,133 @@
 package validation
 
 import (
+	"context"
+	"encoding/json"
 	"testing"
 
-	commonv1 "github.com/elastic/cloud-on-k8s/pkg/apis/common/v1"
 	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
+	"github.com/stretchr/testify/require"
+	"k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-func TestValidateElasticsearch(t *testing.T) {
+func asJSON(obj interface{}) []byte {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func Test_validatingWebhook_Handle(t *testing.T) {
+	decoder, _ := admission.NewDecoder(k8s.Scheme())
+	type fields struct {
+		client               k8s.Client
+		validateStorageClass bool
+	}
+	type args struct {
+		req admission.Request
+	}
 	tests := []struct {
-		name    string
-		es      esv1.Elasticsearch
-		wantErr bool
+		name   string
+		fields fields
+		args   args
+		want   admission.Response
 	}{
 		{
-			name: "has nodeset with invalid version",
-			es: esv1.Elasticsearch{
-				Spec: esv1.ElasticsearchSpec{
-					Version: "x.y",
-					NodeSets: []esv1.NodeSet{
-						{
-							Config: &commonv1.Config{
-								Data: map[string]interface{}{
-									esv1.NodeMaster: "true",
-									esv1.NodeData:   "true",
-									esv1.NodeIngest: "false",
-									esv1.NodeML:     "false",
-								},
-							},
-							Count: 1,
-						},
-					},
+			name: "accept valid creation",
+			args: args{
+				req: admission.Request{AdmissionRequest: v1beta1.AdmissionRequest{
+					Operation: v1beta1.Create,
+					Object: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "name"},
+							Spec:       esv1.ElasticsearchSpec{Version: "7.9.0", NodeSets: []esv1.NodeSet{{Name: "set1", Count: 3}}},
+						}),
+					}},
 				},
 			},
-			wantErr: true,
+			want: admission.Allowed(""),
+		},
+		{
+			name: "reject invalid creation (no version provided)",
+			args: args{
+				req: admission.Request{AdmissionRequest: v1beta1.AdmissionRequest{
+					Operation: v1beta1.Create,
+					Object: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "es"},
+							Spec:       esv1.ElasticsearchSpec{NodeSets: []esv1.NodeSet{{Name: "set1", Count: 3}}},
+						}),
+					}},
+				},
+			},
+			want: admission.Denied(parseVersionErrMsg),
+		},
+		{
+			name: "accept valid update (count++)",
+			fields: fields{
+				client: k8s.WrappedFakeClient(),
+			},
+			args: args{
+				req: admission.Request{AdmissionRequest: v1beta1.AdmissionRequest{
+					Operation: v1beta1.Update,
+					OldObject: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "name"},
+							Spec:       esv1.ElasticsearchSpec{Version: "7.9.0", NodeSets: []esv1.NodeSet{{Name: "set1", Count: 3}}},
+						}),
+					},
+					Object: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "name"},
+							Spec:       esv1.ElasticsearchSpec{Version: "7.9.0", NodeSets: []esv1.NodeSet{{Name: "set1", Count: 4}}},
+						}),
+					},
+				}},
+			},
+			want: admission.Allowed(""),
+		},
+		{
+			name: "reject invalid update (version downgrade))",
+			fields: fields{
+				client: k8s.WrappedFakeClient(),
+			},
+			args: args{
+				req: admission.Request{AdmissionRequest: v1beta1.AdmissionRequest{
+					Operation: v1beta1.Update,
+					OldObject: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "name"},
+							Spec:       esv1.ElasticsearchSpec{Version: "7.9.1", NodeSets: []esv1.NodeSet{{Name: "set1", Count: 3}}},
+						}),
+					},
+					Object: runtime.RawExtension{
+						Raw: asJSON(&esv1.Elasticsearch{
+							ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "name"},
+							Spec:       esv1.ElasticsearchSpec{Version: "7.9.0", NodeSets: []esv1.NodeSet{{Name: "set1", Count: 3}}},
+						}),
+					},
+				}},
+			},
+			want: admission.Denied(noDowngradesMsg),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := ValidateElasticsearch(tt.es); (err != nil) != tt.wantErr {
-				t.Errorf("Elasticsearch.ValidateCreate() error = %v, wantErr %v", err, tt.wantErr)
+			wh := &validatingWebhook{
+				client:               tt.fields.client,
+				decoder:              decoder,
+				validateStorageClass: tt.fields.validateStorageClass,
+			}
+			got := wh.Handle(context.Background(), tt.args.req)
+			require.Equal(t, tt.want.Allowed, got.Allowed)
+			if !got.Allowed {
+				require.Contains(t, got.Result.Reason, tt.want.Result.Reason)
 			}
 		})
 	}
 }
-
