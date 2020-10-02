6d77dfa16c2da76b85edc8c172c622e3efe1ced3
Sebastien Guilloux <contact.sebgl@gmail.com>
2020-10-02 13:47:34 +0200

    Add an E2E test to cover volume resize (#3769)
    * Add an E2E test to cover volume resize
    
    The test relies on one of the available storage class that supports
    volume expansion, or is skipped if there's none.
    
    * Fix typos

--- test/e2e/es/volume_test.go
+++ test/e2e/es/volume_test.go
@@ -5,10 +5,20 @@
 package es
 
 import (
+	"fmt"
 	"testing"
 
+	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
 	"github.com/elastic/cloud-on-k8s/test/e2e/test"
 	"github.com/elastic/cloud-on-k8s/test/e2e/test/elasticsearch"
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/pointer"
 )
 
 // TestVolumeEmptyDir tests a manual override of the default persistent storage with emptyDir.
@@ -20,4 +30,116 @@ func TestVolumeEmptyDir(t *testing.T) {
 	// volume type will be checked in creation steps
 	test.Sequence(nil, test.EmptySteps, b).
 		RunSequential(t)
+}
+
+// TestVolumeExpansion resizes an existing pvc and ensures Elasticsearch
+// correctly reports the resized filesystem.
+func TestVolumeExpansion(t *testing.T) {
+	// Is there a storage class we can use that supports volume expansion?
+	// Otherwise skip this test.
+	storageClass, err := getResizeableStorageClass(test.NewK8sClientOrFatal().Client)
+	require.NoError(t, err)
+	if storageClass == "" {
+		t.Skip("No storage class allowing volume expansion found. Skipping the test.")
+	}
+
+	b := elasticsearch.NewBuilder("test-volume-expansion").
+		WithESMasterNodes(1, elasticsearch.DefaultResources).
+		WithESDataNodes(2, elasticsearch.DefaultResources)
+	t.Log(fmt.Sprintf("Using storage class %s to test volume expansion", storageClass))
+	patchStorageClasses(&b.Elasticsearch, storageClass)
+
+	masterSset := esv1.StatefulSet(b.Elasticsearch.Name, b.Elasticsearch.Spec.NodeSets[0].Name)
+	dataSset := esv1.StatefulSet(b.Elasticsearch.Name, b.Elasticsearch.Spec.NodeSets[1].Name)
+	pvcNames := []string{
+		fmt.Sprintf("elasticsearch-data-%s-0", masterSset),
+		fmt.Sprintf("elasticsearch-data-%s-0", dataSset),
+		fmt.Sprintf("elasticsearch-data-%s-1", dataSset),
+	}
+
+	// resize the volume with an additional 1Gi after the cluster is up
+	initialStorageSize := b.Elasticsearch.Spec.NodeSets[0].VolumeClaimTemplates[0].Spec.Resources.Requests.Storage()
+	resizedStorage := initialStorageSize.DeepCopy()
+	resizedStorage.Add(resource.MustParse("1Gi"))
+
+	test.Sequence(nil, func(k *test.K8sClient) test.StepList {
+		return test.StepList{
+			{
+				Name: "Update the Elasticsearch spec with increased storage requests",
+				Test: test.Eventually(func() error {
+					var es esv1.Elasticsearch
+					if err := k.Client.Get(k8s.ExtractNamespacedName(&b.Elasticsearch), &es); err != nil {
+						return err
+					}
+					patchStorageSize(&es, resizedStorage)
+					return k.Client.Update(&es)
+				}),
+			},
+			{
+				Name: "PVCs should eventually be resized",
+				Test: test.Eventually(func() error {
+					for _, pvcName := range pvcNames {
+						var pvc corev1.PersistentVolumeClaim
+						if err := k.Client.Get(types.NamespacedName{Namespace: b.Elasticsearch.Namespace, Name: pvcName}, &pvc); err != nil {
+							return err
+						}
+						reportedStorage := pvc.Status.Capacity.Storage()
+						if reportedStorage == nil {
+							return fmt.Errorf("no storage size reported in %s status", pvcName)
+						}
+						if !reportedStorage.Equal(resizedStorage) {
+							return fmt.Errorf("expected resized capacity %s but got %s", resizedStorage.String(), reportedStorage.String())
+						}
+					}
+					return nil
+				}),
+			},
+			{
+				Name: "StatefulSets should eventually be recreated with the right storage size",
+				Test: test.Eventually(func() error {
+					for _, ssetName := range []string{masterSset, dataSset} {
+						var sset appsv1.StatefulSet
+						if err := k.Client.Get(types.NamespacedName{Namespace: b.Elasticsearch.Namespace, Name: ssetName}, &sset); err != nil {
+							return err
+						}
+						if !sset.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests.Storage().Equal(resizedStorage) {
+							return fmt.Errorf("StatefulSet %s has not been recreated with storage size %s", ssetName, resizedStorage.String())
+						}
+					}
+					return nil
+				}),
+			},
+			// re-run all the regular checks
+		}.WithSteps(test.CheckTestSteps(b, k))
+	}, b).RunSequential(t)
+}
+
+func getResizeableStorageClass(k8sClient k8s.Client) (string, error) {
+	var scs storagev1.StorageClassList
+	if err := k8sClient.List(&scs); err != nil {
+		return "", err
+	}
+	for _, sc := range scs.Items {
+		if sc.AllowVolumeExpansion != nil && *sc.AllowVolumeExpansion {
+			return sc.Name, nil
+		}
+	}
+	// not found
+	return "", nil
+}
+
+func patchStorageClasses(es *esv1.Elasticsearch, storageClassName string) {
+	for nodeSetIndex := range es.Spec.NodeSets {
+		for claimIndex := range es.Spec.NodeSets[nodeSetIndex].VolumeClaimTemplates {
+			es.Spec.NodeSets[nodeSetIndex].VolumeClaimTemplates[claimIndex].Spec.StorageClassName = pointer.StringPtr(storageClassName)
+		}
+	}
+}
+
+func patchStorageSize(es *esv1.Elasticsearch, size resource.Quantity) {
+	for nodeSetIndex := range es.Spec.NodeSets {
+		for claimIndex := range es.Spec.NodeSets[nodeSetIndex].VolumeClaimTemplates {
+			es.Spec.NodeSets[nodeSetIndex].VolumeClaimTemplates[claimIndex].Spec.Resources.Requests[corev1.ResourceStorage] = size
+		}
+	}
 }
