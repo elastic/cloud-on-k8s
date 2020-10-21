d0b60af59ac25381021f1dcd22c416144215198f
Anya Sabo <anya.sabo@elastic.co>
2020-10-21 16:26:12 -0500

    Facepalm emoji

--- test/e2e/test/apmserver/checks_apm.go
+++ test/e2e/test/apmserver/checks_apm.go
@@ -299,7 +299,7 @@ func (c *apmClusterChecks) CheckAgentConfiguration(apm apmv1.ApmServer, k *test.
 				if !apmVersion.IsSameOrAfter(version.MustParse("7.7.0")) {
 					uri += "/new"
 				}
-				_, err = kibana.DoRequest(k, kb, password, "PUT", uri, []byte(sampleDefaultAgentConfiguration))
+				_, err = kibana.DoRequest(k, kb, password, http.MethodPut, uri, []byte(sampleDefaultAgentConfiguration))
 				return err
 			}),
 		},
