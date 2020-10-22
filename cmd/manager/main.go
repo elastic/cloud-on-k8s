b7df6f4b4ea8a26041c8ac7820466d6a946ed156
Charith Ellawala <52399125+charith-elastic@users.noreply.github.com>
2020-10-22 14:51:14 +0100

    Use cluster-scoped cache only when required (#3868) (#3871)

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -459,9 +459,15 @@ func startOperator(stopChan <-chan struct{}) error {
 		opts.Namespace = managedNamespaces[0]
 	default:
 		log.Info("Operator configured to manage multiple namespaces", "namespaces", managedNamespaces, "operator_namespace", operatorNamespace)
-		// the manager cache should always include the operator namespace so that we can work with operator-internal resources,
-		// and the empty namespace to watch cluster-scoped resources (e.g. storage classes).
-		opts.NewCache = cache.MultiNamespacedCacheBuilder(append(managedNamespaces, operatorNamespace, ""))
+		// The managed cache should always include the operator namespace so that we can work with operator-internal resources.
+		managedNamespaces = append(managedNamespaces, operatorNamespace)
+
+		// Add the empty namespace to allow watching cluster-scoped resources if storage class validation is enabled.
+		if viper.GetBool(operator.ValidateStorageClassFlag) {
+			managedNamespaces = append(managedNamespaces, "")
+		}
+
+		opts.NewCache = cache.MultiNamespacedCacheBuilder(managedNamespaces)
 	}
 
 	// only expose prometheus metrics if provided a non-zero port
