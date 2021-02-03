e1a358d6f9a87064f9732ff92d399d03aec45ea7
Peter Brachwitz <peter.brachwitz@elastic.co>
2021-02-03 20:06:39 +0100

    Update sources with Elastic 2.0 license headers

--- pkg/apis/beat/v1beta1/doc.go
+++ pkg/apis/beat/v1beta1/doc.go
@@ -1,6 +1,6 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
 
 // Package v1beta1 contains API Schema definitions for the beat v1beta1 API group
 // +k8s:openapi-gen=true
--- pkg/controller/elasticsearch/certificates/transport/doc.go
+++ pkg/controller/elasticsearch/certificates/transport/doc.go
@@ -1,9 +1,5 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
 
-// Transport certificates
-//
-// For each Elasticsearch pod, we issue one certificate from the cluster CA.
-// The certificate and associated private key are passed to the pod through a secret volume mount.
 package transport
