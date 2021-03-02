a21f29a01c0ce5f2496b1bc19956ff0de189cf30
Thibault Richard <thb.krkr@gmail.com>
2021-03-02 10:32:25 +0100

    Updating ent webhook kubebuilder annotation

--- pkg/apis/enterprisesearch/v1/webhook.go
+++ pkg/apis/enterprisesearch/v1/webhook.go
@@ -33,7 +33,7 @@ var (
 	}
 )
 
-// +kubebuilder:webhook:path=/validate-enterprisesearch-k8s-elastic-co-v1beta1-enterprisesearch,mutating=false,failurePolicy=ignore,groups=enterprisesearch.k8s.elastic.co,resources=enterprisesearches,verbs=create;update,versions=v1beta1,name=elastic-ent-validation-v1beta1.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1;v1beta1
+// +kubebuilder:webhook:path=/validate-enterprisesearch-k8s-elastic-co-v1-enterprisesearch,mutating=false,failurePolicy=ignore,groups=enterprisesearch.k8s.elastic.co,resources=enterprisesearches,verbs=create;update,versions=v1,name=elastic-ent-validation-v1.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1;v1beta1
 
 var _ webhook.Validator = &EnterpriseSearch{}
 
