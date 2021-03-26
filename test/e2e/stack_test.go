fb80d989e9e4c8dfcb64016ef78314382d45ec7b
Thibault Richard <thb.krkr@gmail.com>
2021-03-26 14:53:35 +0100

    Bump initialVersion from 7.10.0 to 7.11.0 in TestVersionUpgradeOrdering

--- test/e2e/stack_test.go
+++ test/e2e/stack_test.go
@@ -33,7 +33,7 @@ import (
 // TestVersionUpgradeOrdering deploys the entire stack, with resources associated together.
 // Then, it updates their version, and ensures a strict ordering is respected during the version upgrade.
 func TestVersionUpgradeOrdering(t *testing.T) {
-	initialVersion := "7.10.0"
+	initialVersion := "7.11.0"
 	updatedVersion := "7.12.0"
 
 	// upgrading the entire stack can take some time, since we need to account for (in order):
