b135f1e21f32a578378fee0af48b6a2a82fc3a74
Thibault Richard <thb.krkr@gmail.com>
2021-03-26 10:35:14 +0100

    7.11.2 -> 7.12.0

--- test/e2e/test/version.go
+++ test/e2e/test/version.go
@@ -16,7 +16,7 @@ const (
 	// Minimum version for 6.8.x tested with the operator
 	MinVersion68x = "6.8.10"
 	// Current latest version for 7.x
-	LatestVersion7x = "7.11.2" // version to synchronize with the latest release of the Elastic Stack
+	LatestVersion7x = "7.12.0" // version to synchronize with the latest release of the Elastic Stack
 )
 
 // SkipInvalidUpgrade skips a test that would do an invalid upgrade.
