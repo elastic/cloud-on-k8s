42038918f3fa27eac498635a3f230b09ff6c3332
Peter Brachwitz <peter.brachwitz@elastic.co>
2021-05-27 08:37:51 +0200

    E2E enable Kibana, APM Server, Enterprise Search, Beats and Agent tests on ARM (#4523)

--- test/e2e/ems/ems_test.go
+++ test/e2e/ems/ems_test.go
@@ -2,6 +2,8 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
+// +build ems e2e
+
 package ems
 
 import (
