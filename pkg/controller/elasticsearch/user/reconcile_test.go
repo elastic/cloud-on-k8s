ff98c869ce97cf59b0d169141b884c29e55be4c8
Thibault Richard <thb.krkr@gmail.com>
2021-06-10 20:48:10 +0200

    Update Test_aggregateRoles

--- pkg/controller/elasticsearch/user/reconcile_test.go
+++ pkg/controller/elasticsearch/user/reconcile_test.go
@@ -75,6 +75,6 @@ func Test_aggregateRoles(t *testing.T) {
 	c := k8s.NewFakeClient(sampleUserProvidedRolesSecret...)
 	roles, err := aggregateRoles(c, sampleEsWithAuth, initDynamicWatches(), record.NewFakeRecorder(10))
 	require.NoError(t, err)
-	require.Len(t, roles, 49)
+	require.Len(t, roles, 50)
 	require.Contains(t, roles, ProbeUserRole, "role1", "role2")
 }
