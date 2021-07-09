b58a63c43cabc990f486843ff35df7063b95a353
David Kowalski <david.kowalski@elastic.co>
2021-07-09 08:55:40 +0200

    Make linter happy

--- pkg/controller/agent/config_test.go
+++ pkg/controller/agent/config_test.go
@@ -1,16 +1,20 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
 package agent
 
 import (
 	"path"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	agentv1alpha1 "github.com/elastic/cloud-on-k8s/pkg/apis/agent/v1alpha1"
 	commonv1 "github.com/elastic/cloud-on-k8s/pkg/apis/common/v1"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/certificates"
 	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
-	"github.com/stretchr/testify/require"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestBuildFleetSetupKibanaConfig(t *testing.T) {
@@ -271,7 +275,6 @@ func TestBuildFleetSetupFleetConfig(t *testing.T) {
 }
 
 func TestBuildFleetSetupFleetServerConfig(t *testing.T) {
-
 	agent := agentv1alpha1.Agent{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "agent",
@@ -362,7 +365,6 @@ func TestBuildFleetSetupFleetServerConfig(t *testing.T) {
 }
 
 func TestExtractConnectionSettings(t *testing.T) {
-
 	assoc := agentv1alpha1.AgentKibanaAssociation{
 		Agent: &agentv1alpha1.Agent{
 			ObjectMeta: metav1.ObjectMeta{
