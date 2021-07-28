ab0cda00842cb27b666d147eef4da75915bdda6f
Thibault Richard <thb.krkr@gmail.com>
2021-07-28 15:39:06 +0200

    Bump 6.8.x version to 6.8.17

--- test/e2e/test/version.go
+++ test/e2e/test/version.go
@@ -14,7 +14,7 @@ import (
 // Elastic Stack versions used in the E2E tests
 const (
 	// Minimum version for 6.8.x tested with the operator
-	MinVersion68x = "6.8.16"
+	MinVersion68x = "6.8.17"
 	// Current latest version for 7.x
 	LatestVersion7x = "7.13.1" // version to synchronize with the latest release of the Elastic Stack
 )
