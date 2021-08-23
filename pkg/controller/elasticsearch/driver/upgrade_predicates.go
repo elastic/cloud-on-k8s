11458a84d37e5ceaaf5265ea1d86f9191d2e8192
CamiloSierraH <camilo.sierra@elastic.co>
2021-08-22 20:13:11 -0700

    allow to upgrade a single node cluster with yellow status

--- pkg/controller/elasticsearch/driver/upgrade_predicates.go
+++ pkg/controller/elasticsearch/driver/upgrade_predicates.go
@@ -222,6 +222,14 @@ var predicates = [...]Predicate{
 			if err != nil {
 				return false, err
 			}
+			_, healthyNode := context.healthyPods[candidate.Name]
+			// Get the number of expected masters
+			expectedMasters := len(context.masterNodesNames)
+			healthyPods := len(context.healthyPods)
+			if healthyNode && healthyPods == 1 && expectedMasters == 1 && len(deletedPods) == 0 {
+				// If the cluster is a single node cluster, replicas can not be started, skip the predicate
+				return true, nil
+			}
 			// We maintain two data structures to record:
 			// * The total number of replicas for a shard
 			// * How many of them are STARTED
