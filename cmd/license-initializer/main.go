d7abcfb21d90d93936cd94ac71a0e1b3e674a71a
Thibault Richard <thb.krkr@gmail.com>
2021-09-27 17:52:19 +0200

    Update license header

--- cmd/license-initializer/main.go
+++ cmd/license-initializer/main.go
@@ -1,6 +1,6 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
 
 package main
 
@@ -72,8 +72,8 @@ func generateSrc(pubkeyFile string, out io.Writer) {
 		ShouldBreak func(int) bool
 	}
 	var tmpl = `// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
 
 package license 
 
--- pkg/apis/enterprisesearch/v1/doc.go
+++ pkg/apis/enterprisesearch/v1/doc.go
@@ -1,6 +1,6 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
 
 // Package v1beta1 contains API schema definitions for managing Enterprise Search resources.
 // +kubebuilder:object:generate=true
