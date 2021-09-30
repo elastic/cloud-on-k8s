887fba308501e2434a16c1c4301eb8b936f320a3
Thibault Richard <thb.krkr@gmail.com>
2021-09-30 17:37:35 +0200

    Remove APMDefaultSecurityContext()

--- test/e2e/test/default.go
+++ test/e2e/test/default.go
@@ -13,35 +13,18 @@ import (
 // Values should be inherited and checked against a PSP, but we provide some
 // default values if pods are started outside E2E tests, by a developer for example.
 func DefaultSecurityContext() *corev1.PodSecurityContext {
-	dscc := &corev1.PodSecurityContext{
-		RunAsNonRoot: BoolPtr(true),
-	}
-
-	if !Ctx().OcpCluster {
-		defaultUserID := int64(12345) // arbitrary user ID
-		// Stack images expected to run with the user ID 1000 or 0 before 7.9.0 for APM and Beats (https://github.com/elastic/beats/issues/18871)
-		// and 7.11.0 for Enterprise Search (https://github.com/elastic/enterprise-search-team/issues/285)
-		stackVersion := version.MustParse(Ctx().ElasticStackVersion)
-		if stackVersion.LT(version.MustParse("7.11.0")) {
-			defaultUserID = int64(1000)
+	// OpenShift sets the security context automatically
+	if Ctx().OcpCluster {
+		return &corev1.PodSecurityContext{
+			RunAsNonRoot: BoolPtr(true),
 		}
-		dscc.RunAsUser = &defaultUserID
-		dscc.FSGroup = &defaultUserID
 	}
 
-	return dscc
-}
-
-// It's currently not possible to run APM using OpenShift's
-// restricted SCC. Therefore, we are forcing the required UID
-// and fsGroup for APM's security context. A dedicated ServiceAccount
-// with special permissions is created by APM test's builder
-// so that this can work.
-func APMDefaultSecurityContext() *corev1.PodSecurityContext {
-	defaultUserID := int64(12345) // arbitrary user ID
-	// APM image expected to run with the user ID 1000 or 0 before 7.9.0 (https://github.com/elastic/beats/issues/18871)
-	stackVersion := version.MustParse(Ctx().ElasticStackVersion)
-	if stackVersion.LT(version.MustParse("7.9.0")) {
+	// if not OpenShift set an arbitrary user ID
+	defaultUserID := int64(12345)
+	// use 1000 before 7.11.0 as it was the only accepted non 0 user ID accepted by Enterprise Search (https://github.com/elastic/enterprise-search-team/issues/285),
+	// APM and Beats images have been fixed in 7.9.0 (https://github.com/elastic/beats/issues/18871)
+	if version.MustParse(Ctx().ElasticStackVersion).LT(version.MustParse("7.11.0")) {
 		defaultUserID = int64(1000)
 	}
 	return &corev1.PodSecurityContext{
