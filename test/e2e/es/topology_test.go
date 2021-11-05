d398117a6e017e88d7456fe58b91bc25acc88b9b
Thibault Richard <thb.krkr@gmail.com>
2021-11-05 18:12:04 +0100

    Add TestResourcesRequirements E2E test

--- test/e2e/es/topology_test.go
+++ test/e2e/es/topology_test.go
@@ -9,15 +9,56 @@ package es
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
 	"github.com/elastic/cloud-on-k8s/test/e2e/test"
 	"github.com/elastic/cloud-on-k8s/test/e2e/test/elasticsearch"
 )
 
-//TestCoordinatingNodes tests a cluster with coordinating nodes.
+// TestCoordinatingNodes tests a cluster with coordinating nodes.
 func TestCoordinatingNodes(t *testing.T) {
 	b := elasticsearch.NewBuilder("test-es-coord").
 		WithESMasterDataNodes(1, elasticsearch.DefaultResources).
 		WithESCoordinatingNodes(1, elasticsearch.DefaultResources)
+
+	test.Sequence(nil, test.EmptySteps, b).RunSequential(t)
+}
+
+// TestResourcesRequirements tests a cluster with resources (cpu/memory/storage) requirements.
+func TestResourcesRequirements(t *testing.T) {
+	sc, err := getResizeableStorageClass(test.NewK8sClientOrFatal().Client)
+	assert.NoError(t, err)
+	b := elasticsearch.NewBuilder("test-es-resreq").
+		WithNodeSet(esv1.NodeSet{
+			Name:  "masterdata",
+			Count: int32(1),
+			PodTemplate: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					SecurityContext: test.DefaultSecurityContext(),
+					Containers: []corev1.Container{
+						{
+							Name: esv1.ElasticsearchContainerName,
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("4Gi"),
+									corev1.ResourceCPU:    resource.MustParse("1000m"),
+								},
+								Limits: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("4Gi"),
+									corev1.ResourceCPU:    resource.MustParse("1600m"),
+								},
+							},
+						},
+					},
+				},
+			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+				newPVC("2Gi", sc),
+			},
+		})
 
 	test.Sequence(nil, test.EmptySteps, b).RunSequential(t)
 }
