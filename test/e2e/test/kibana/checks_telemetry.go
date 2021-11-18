2f59c76ebbacce4261101c904a9afc716220652c
Peter Brachwitz <peter.brachwitz@elastic.co>
2021-11-18 15:34:42 +0100

    import ordering

--- test/e2e/test/kibana/checks_telemetry.go
+++ test/e2e/test/kibana/checks_telemetry.go
@@ -8,10 +8,10 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/elastic/cloud-on-k8s/pkg/telemetry"
 	"github.com/pkg/errors"
 
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/version"
+	"github.com/elastic/cloud-on-k8s/pkg/telemetry"
 	"github.com/elastic/cloud-on-k8s/test/e2e/test"
 )
 
