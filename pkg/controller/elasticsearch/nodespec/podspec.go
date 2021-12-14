d6430aa6c7f8ae23518f26fc701d0f199fbb1c37
Thibault Richard <thb.krkr@gmail.com>
2021-12-14 09:41:55 +0100

    Group constant declarations

--- pkg/controller/elasticsearch/nodespec/podspec.go
+++ pkg/controller/elasticsearch/nodespec/podspec.go
@@ -29,6 +29,11 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/utils/pointer"
 )
 
+const (
+	defaultFsGroup                    = 1000
+	log4j2FormatMsgNoLookupsParamName = "-Dlog4j2.formatMsgNoLookups"
+)
+
 // Starting 8.0.0, the Elasticsearch container does not run with the root user anymore. As a result,
 // we cannot chown the mounted volumes to the right user (id 1000) in an init container.
 // Instead, we can rely on Kubernetes `securityContext.fsGroup` feature: by setting it to 1000
@@ -37,8 +42,6 @@ import (
 // is forbidden: the user can either set `--set-default-security-context=false`, or override the
 // podTemplate securityContext to an empty value.
 var minDefaultSecurityContextVersion = version.MustParse("8.0.0")
-
-const defaultFsGroup = 1000
 
 // BuildPodTemplateSpec builds a new PodTemplateSpec for an Elasticsearch node.
 func BuildPodTemplateSpec(
@@ -171,10 +174,6 @@ func buildLabels(
 
 	return podLabels, nil
 }
-
-const (
-	log4j2FormatMsgNoLookupsParamName = "-Dlog4j2.formatMsgNoLookups"
-)
 
 // enableLog4JFormatMsgNoLookups prepends the JVM parameter `-Dlog4j2.formatMsgNoLookups=true` to the environment variable `ES_JAVA_OPTS`
 // in order to mitigate the Log4Shell vulnerability CVE-2021-44228, if it is not yet defined by the user, for
