514ce63279d187354dc6ca386a4d82d8ed2dcb1c
Peter Brachwitz <peter.brachwitz@elastic.co>
2022-01-04 23:33:42 +0100

    this is not the time to update all the tags

--- test/e2e/ems/ems_test.go
+++ test/e2e/ems/ems_test.go
@@ -2,7 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
-//go:build ems || e2e
 // +build ems e2e
 
 package ems
