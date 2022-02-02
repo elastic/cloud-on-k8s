b02717befd1c672a1a2a861e899da829ea03d190
Thibault Richard <thbkrkr@users.noreply.github.com>
2022-02-02 12:01:52 +0100

    Fix Stack Monitoring with custom certificate without CA (#5310)
    Stack Monitoring incorrectly assumed that if the monitored Elastic resource has TLS enabled, there is a CA to configure in the input section of the Metricbeat config. This overlooked that you can have a certificate issued by a well-known CA, so you don't always provide a CA when TLS is enabled. This is fixed by differentiating between isSSL/isTLS and HasCA.

--- pkg/controller/common/stackmon/config_test.go
+++ pkg/controller/common/stackmon/config_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/elastic/cloud-on-k8s/pkg/controller/common/name"
@@ -18,53 +19,86 @@ import (
 
 func TestBuildMetricbeatBaseConfig(t *testing.T) {
 	tests := []struct {
-		name       string
-		isTLS      bool
-		baseConfig string
+		name        string
+		isTLS       bool
+		certsSecret *corev1.Secret
+		hasCA       bool
+		baseConfig  string
 	}{
 		{
-			name:  "with tls",
+			name:  "with TLS and a CA",
 			isTLS: true,
+			certsSecret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{Name: "name-es-http-certs-public", Namespace: "namespace"},
+				Data: map[string][]byte{
+					"tls.crt": []byte("1234567890"),
+					"ca.crt":  []byte("1234567890"),
+				},
+			},
 			baseConfig: `
 				hosts: ["scheme://localhost:1234"]
 				username: elastic-internal-monitoring
 				password: 1234567890
-				ssl.certificate_authorities: ["/mnt/elastic-internal/xx-monitoring/namespace/name/certs/ca.crt"]
+				ssl.enabled: true
+				ssl.verification_mode: "certificate"
+				ssl.certificate_authorities: ["/mnt/elastic-internal/xx-monitoring/namespace/name/certs/ca.crt"]`,
+		},
+		{
+			name:  "with TLS and no CA",
+			isTLS: true,
+			certsSecret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{Name: "name-es-http-certs-public", Namespace: "namespace"},
+				Data: map[string][]byte{
+					"tls.crt": []byte("1234567890"),
+				},
+			},
+			baseConfig: `
+				hosts: ["scheme://localhost:1234"]
+				username: elastic-internal-monitoring
+				password: 1234567890
+				ssl.enabled: true
 				ssl.verification_mode: "certificate"`,
 		},
 		{
-			name:  "without tls",
+			name:  "without TLS",
 			isTLS: false,
 			baseConfig: `
 				hosts: ["scheme://localhost:1234"]
 				username: elastic-internal-monitoring
-				password: 1234567890`,
+				password: 1234567890
+				ssl.enabled: false
+				ssl.verification_mode: "certificate"`,
 		},
 	}
-
 	baseConfigTemplate := `
 				hosts: ["{{ .URL }}"]
 				username: {{ .Username }}
 				password: {{ .Password }}
-				{{- if .IsSSL }}
-				ssl.certificate_authorities: ["{{ .SSLPath }}"]
-				ssl.verification_mode: "{{ .SSLMode }}"
+				ssl.enabled: {{ .IsSSL }}
+				ssl.verification_mode: "certificate"
+				{{- if .HasCA }}
+				ssl.certificate_authorities: ["{{ .CAPath }}"]
 				{{- end }}`
-	sampleURL := "scheme://localhost:1234"
 
-	fakeClient := k8s.NewFakeClient(&corev1.Secret{
+	sampleURL := "scheme://localhost:1234"
+	internalUsersSecret := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{Name: "name-es-internal-users", Namespace: "namespace"},
 		Data:       map[string][]byte{"elastic-internal-monitoring": []byte("1234567890")},
-	})
+	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			initObjects := []runtime.Object{internalUsersSecret}
+			if tc.certsSecret != nil {
+				initObjects = append(initObjects, tc.certsSecret)
+			}
+			fakeClient := k8s.NewFakeClient(initObjects...)
 			baseConfig, _, err := buildMetricbeatBaseConfig(
 				fakeClient,
 				"xx-monitoring",
 				types.NamespacedName{Namespace: "namespace", Name: "name"},
 				types.NamespacedName{Namespace: "namespace", Name: "name"},
-				name.NewNamer("xx"),
+				name.NewNamer("es"),
 				sampleURL,
 				tc.isTLS,
 				baseConfigTemplate,
