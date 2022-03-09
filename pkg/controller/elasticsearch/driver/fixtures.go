4413234dd964d50a3084c350aa32772e4e744e83
Peter Brachwitz <peter.brachwitz@elastic.co>
2022-03-09 18:28:22 +0100

    make linter happy

--- pkg/controller/elasticsearch/driver/fixtures.go
+++ pkg/controller/elasticsearch/driver/fixtures.go
@@ -181,20 +181,9 @@ func (u upgradeTestPods) toRuntimeObjects(version string, maxUnavailable int, f 
 }
 
 func (u upgradeTestPods) toCurrentPods() []corev1.Pod {
-	var result []corev1.Pod
+	result := make([]corev1.Pod, 0, len(u))
 	for _, testPod := range u {
 		result = append(result, testPod.toPod())
-	}
-	return result
-}
-
-func (u upgradeTestPods) toMasterPods() []corev1.Pod {
-	var result []corev1.Pod
-	for _, testPod := range u {
-		pod := testPod.toPod()
-		if label.IsMasterNode(pod) {
-			result = append(result, pod)
-		}
 	}
 	return result
 }
