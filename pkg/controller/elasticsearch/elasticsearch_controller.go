30d3e226737a720fbd1149cc4ec078040fabf231
Peter Brachwitz <peter.brachwitz@elastic.co>
2022-03-23 15:27:05 +0100

    Reset phase on each reconciliation in Elasticsearch status

--- pkg/controller/elasticsearch/elasticsearch_controller.go
+++ pkg/controller/elasticsearch/elasticsearch_controller.go
@@ -200,13 +200,9 @@ func (r *ReconcileElasticsearch) Reconcile(ctx context.Context, request reconcil
 	}
 
 	if isReconciled, message := results.IsReconciled(); !isReconciled {
-		// Do not overwrite other "non-ready" phases like MigratingData
-		if state.IsElasticsearchPhase(esv1.ElasticsearchReadyPhase) {
-			state.UpdateWithPhase(esv1.ElasticsearchApplyingChangesPhase)
-		}
+		state.UpdateWithPhase(esv1.ElasticsearchApplyingChangesPhase)
 		state.ReportCondition(esv1.ReconciliationComplete, corev1.ConditionFalse, message)
-	} else if !state.IsElasticsearchPhase(esv1.ElasticsearchResourceInvalid) {
-		// Do not overwrite invalid phase
+	} else {
 		state.UpdateWithPhase(esv1.ElasticsearchReadyPhase)
 	}
 
