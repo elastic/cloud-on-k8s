e2c22e94df56331e8fb0e3ad209024c16c848d4f
Thibault Richard <thb.krkr@gmail.com>
2022-03-28 18:01:10 +0200

    Trigger full PR job with parameters extracted from the PR comment

--- .ci/common/tests.groovy
+++ .ci/common/tests.groovy
@@ -30,4 +30,21 @@ def generateSlackMessage(baseMsg, URL, failedTests) {
     return sb.toString()
 }
 
+// extractValueByKey extracts the value corresponding to a key in a string following the pattern: '.*key1=value1 key2=value2.*'.
+// If there is no key/value pair matching the key, the provided default value is returned.
+def extractValueByKey(str, key, defaultValue) {
+    if (str != null) {
+        def items = str.split(" ")
+        for (int i = 0; i < items.length; i++) {
+            def kv = items[i].split("=")
+            if (kv.length == 2) {
+                if (kv[0] == key) {
+                    return kv[1]
+                }
+            }
+        }
+    }
+    return defaultValue
+}
+
 return this
