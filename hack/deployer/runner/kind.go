a07e9f3f74f217fd51f49e53e8851478c0319088
Michael Morello <michael.morello@elastic.co>
2022-06-22 19:11:16 +0200

    linter (again)

--- hack/deployer/runner/kind.go
+++ hack/deployer/runner/kind.go
@@ -16,6 +16,7 @@ import (
 	"text/template"
 
 	"github.com/blang/semver/v4"
+
 	"github.com/elastic/cloud-on-k8s/hack/deployer/exec"
 	"github.com/elastic/cloud-on-k8s/hack/deployer/runner/env"
 	"github.com/elastic/cloud-on-k8s/hack/deployer/runner/gatekeeper"
