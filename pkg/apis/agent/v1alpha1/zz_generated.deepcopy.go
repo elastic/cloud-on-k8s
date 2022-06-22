db25a20f6a808f2cd5caae860ec9f892d0f198f2
Thibault Richard <thb.krkr@gmail.com>
2022-06-22 18:09:36 +0200

    Regenerate CRDs and docs

--- pkg/apis/agent/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/agent/v1alpha1/zz_generated.deepcopy.go
@@ -248,13 +248,13 @@ func (in *DaemonSetSpec) DeepCopy() *DaemonSetSpec {
 func (in *DeploymentSpec) DeepCopyInto(out *DeploymentSpec) {
 	*out = *in
 	in.PodTemplate.DeepCopyInto(&out.PodTemplate)
-	if in.Replicas != nil {
-		in, out := &in.Replicas, &out.Replicas
+	if in.RevisionHistoryLimit != nil {
+		in, out := &in.RevisionHistoryLimit, &out.RevisionHistoryLimit
 		*out = new(int32)
 		**out = **in
 	}
-	if in.RevisionHistoryLimit != nil {
-		in, out := &in.RevisionHistoryLimit, &out.RevisionHistoryLimit
+	if in.Replicas != nil {
+		in, out := &in.Replicas, &out.Replicas
 		*out = new(int32)
 		**out = **in
 	}
