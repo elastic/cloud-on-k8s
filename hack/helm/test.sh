44a2373dc347e20800342a369e8502f1dfcd40c3
Michael Montgomery <mmontg1@gmail.com>
2022-06-23 13:54:59 -0500

    Update the documentation. Run the dependencies prior to linting.

--- hack/helm/test.sh
+++ hack/helm/test.sh
@@ -14,11 +14,11 @@ check() {
     local TEST_DIR="$1"
     cd "${TEST_DIR}"
 
-    echo "Running 'helm lint' on $(basename ${TEST_DIR}) chart."
-    helm lint --strict .
-
     echo "Ensuring dependencies are updated for $(basename ${TEST_DIR}) chart."
     helm dependency update . 1>/dev/null
+    
+    echo "Running 'helm lint' on $(basename ${TEST_DIR}) chart."
+    helm lint --strict .
 
     docker run -ti --rm -v "$(pwd)":/apps quintush/helm-unittest:latest -3 -f 'templates/tests/*.yaml' .
     cd -
