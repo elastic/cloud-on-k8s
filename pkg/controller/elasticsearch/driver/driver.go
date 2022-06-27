a5aadbdcd8fe3101a27f845d2e0b4ea5c2079a41
Michael Morello <michael.morello@elastic.co>
2022-06-27 10:01:41 +0200

    Do not use service accounts until Elasticsearch nodes have been upgraded (#5830) (#5831)
    * Detect if all nodes are running with service accounts
    
    * Do not use service accounts until Elasticsearch supports them

--- pkg/controller/elasticsearch/driver/driver.go
+++ pkg/controller/elasticsearch/driver/driver.go
@@ -14,6 +14,7 @@ import (
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/utils/pointer"
 	controller "sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	esv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
@@ -33,6 +34,7 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/cleanup"
 	esclient "github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/client"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/configmap"
+	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/hints"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/initcontainer"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/label"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/license"
@@ -44,6 +46,8 @@ import (
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/stackmon"
 	"github.com/elastic/cloud-on-k8s/pkg/controller/elasticsearch/user"
 	"github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/optional"
+	"github.com/elastic/cloud-on-k8s/pkg/utils/set"
 )
 
 var (
@@ -257,6 +261,10 @@ func (d *defaultDriver) Reconcile(ctx context.Context) *reconciler.Results {
 		}
 	}
 
+	// Update the service account orchestration hint. This is done early in the reconciliation loop to unblock association
+	// controllers that may be waiting for the orchestration hint.
+	results.WithError(d.maybeSetServiceAccountsOrchestrationHint(ctx, esReachable, esClient, resourcesState))
+
 	// reconcile the Elasticsearch license
 	if esReachable {
 		err = license.Reconcile(ctx, d.Client, d.ES, esClient, currentLicense)
@@ -352,6 +360,112 @@ func (d *defaultDriver) newElasticsearchClient(
 		caCerts,
 		esclient.Timeout(d.ES),
 	)
+}
+
+// maybeSetServiceAccountsOrchestrationHint attempts to update an orchestration hint to let the association controllers
+// know whether all the nodes in the cluster are ready to authenticate service accounts.
+func (d *defaultDriver) maybeSetServiceAccountsOrchestrationHint(
+	ctx context.Context,
+	esReachable bool,
+	securityClient esclient.SecurityClient,
+	resourcesState *reconcile.ResourcesState,
+) error {
+	if d.ReconcileState.OrchestrationHints().ServiceAccounts.IsTrue() {
+		// Orchestration hint is already set to true, there is no point going back to false.
+		return nil
+	}
+
+	// Case 1: New cluster, we can immediately set the orchestration hint.
+	if !bootstrap.AnnotatedForBootstrap(d.ES) {
+		allNodesRunningServiceAccounts, err := esv1.AreServiceAccountsSupported(d.ES.Spec.Version)
+		if err != nil {
+			return err
+		}
+		d.ReconcileState.UpdateOrchestrationHints(
+			d.ReconcileState.OrchestrationHints().Merge(hints.OrchestrationsHints{ServiceAccounts: optional.NewBool(allNodesRunningServiceAccounts)}),
+		)
+		return nil
+	}
+
+	// Case 2: This is an existing cluster, but actual cluster version does not support service accounts.
+	if d.ES.Status.Version == "" {
+		return nil
+	}
+	supportServiceAccounts, err := esv1.AreServiceAccountsSupported(d.ES.Status.Version)
+	if err != nil {
+		return err
+	}
+	if !supportServiceAccounts {
+		d.ReconcileState.UpdateOrchestrationHints(
+			d.ReconcileState.OrchestrationHints().Merge(hints.OrchestrationsHints{ServiceAccounts: optional.NewBool(false)}),
+		)
+		return nil
+	}
+
+	// Case 3: cluster is already running with a version that does support service account and tokens have already been created.
+	// We don't however know if all nodes have been migrated and are running with the service_tokens file mounted from the configuration Secret.
+	// Let's try to detect that situation by comparing the existing nodes and the ones returned by the /_security/service API.
+	// Note that starting with release 2.3 the association controller does not create the service account token until Elasticsearch is annotated
+	// as compatible with service accounts. This is mostly to unblock situation described in https://github.com/elastic/cloud-on-k8s/issues/5684
+	if !esReachable {
+		// This requires the Elasticsearch API to be available
+		return nil
+	}
+	allPods := names(resourcesState.AllPods)
+	// Detect if some service tokens are expected
+	saTokens, err := user.GetServiceAccountTokens(d.Client, d.ES)
+	if err != nil {
+		log.Info("Could not detect if service accounts are expected", "err", err, "namespace", d.ES.Namespace, "es_name", d.ES.Name)
+		return err
+	}
+
+	allNodesRunningServiceAccounts, err := allNodesRunningServiceAccounts(ctx, saTokens, set.Make(allPods...), securityClient)
+	if err != nil {
+		log.Info("Could not detect if all nodes are ready for using service accounts", "err", err, "namespace", d.ES.Namespace, "es_name", d.ES.Name)
+		return err
+	}
+	if allNodesRunningServiceAccounts != nil {
+		d.ReconcileState.UpdateOrchestrationHints(
+			d.ReconcileState.OrchestrationHints().Merge(hints.OrchestrationsHints{ServiceAccounts: optional.NewBool(*allNodesRunningServiceAccounts)}),
+		)
+	}
+
+	return nil
+}
+
+// allNodesRunningServiceAccounts attempts to detect if all the nodes in the clusters have loaded the service_tokens file.
+// It returns nil if no decision can be made, for example when there is no tokens are expected to be found.
+func allNodesRunningServiceAccounts(
+	ctx context.Context,
+	saTokens user.ServiceAccountTokens,
+	allPods set.StringSet,
+	securityClient esclient.SecurityClient,
+) (*bool, error) {
+	if len(allPods) == 0 {
+		return nil, nil
+	}
+	if len(saTokens) == 0 {
+		// No tokens are expected: we cannot call the Elasticsearch API to detect which nodes are
+		// running with the conf/service_tokens file.
+		return nil, nil
+	}
+
+	// Get the namespaced service name to call the /_security/service/<namespace>/<service>/credential API
+	namespacedServices := saTokens.NamespacedServices()
+
+	// Get the nodes which have loaded tokens from the conf/service_tokens file.
+	for namespacedService := range namespacedServices {
+		credentials, err := securityClient.GetServiceAccountCredentials(ctx, namespacedService)
+		if err != nil {
+			return nil, err
+		}
+		diff := allPods.Diff(credentials.Nodes())
+		if len(diff) == 0 {
+			return pointer.Bool(true), nil
+		}
+	}
+	// Some nodes are running but did not show up in the security API.
+	return pointer.Bool(false), nil
 }
 
 // warnUnsupportedDistro sends an event of type warning if the Elasticsearch Docker image is not a supported
