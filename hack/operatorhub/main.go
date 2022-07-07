0ec7ae63b14dc85c946bed24c203d80027978bd7
Michael Morello <michael.morello@elastic.co>
2022-07-07 13:24:56 +0200

    Small refactoring

--- hack/operatorhub/main.go
+++ hack/operatorhub/main.go
@@ -8,6 +8,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -38,8 +39,8 @@ const (
 	operatorManifestURL = "https://download.elastic.co/downloads/eck/%s/operator.yaml"
 	operatorName        = "elastic-operator"
 
-	RedHatAPITokenFlag = "redhat-api-token"
-	RedHatProjectId    = "redhat-project-id"
+	RedHatAPIKeyFlag    = "redhat-api-key"
+	RedHatProjectIdFLag = "redhat-project-id"
 
 	csvTemplateFile     = "csv.tpl"
 	packageTemplateFile = "package.tpl"
@@ -49,14 +50,15 @@ const (
 	packageFileSuffix = "package.yaml"
 
 	yamlSeparator = "---\n"
+
+	ImagesEndpoint = "https://catalog.redhat.com/api/containers/v1/projects/certification/id/%s/images"
 )
 
 type cmdArgs struct {
-	confPath              string
-	manifestPaths         []string
-	templatesDir          string
-	redhatAPIToken        string
-	skipCertifiedOperator bool
+	confPath      string
+	manifestPaths []string
+	templatesDir  string
+	redhatAPIKey  string
 }
 
 var args = cmdArgs{}
@@ -76,8 +78,8 @@ func main() {
 
 	cmd.Flags().StringSliceVar(&args.manifestPaths, "yaml-manifest", nil, "Path to installation manifests")
 	cmd.Flags().StringVar(&args.templatesDir, "templates", "./templates", "Path to the templates directory")
-	cmd.Flags().String(RedHatAPITokenFlag, "", "RedHat API Token")
-	cmd.Flags().String(RedHatProjectId, "", "RedHat project id")
+	cmd.Flags().String(RedHatAPIKeyFlag, "", "RedHat API key")
+	cmd.Flags().String(RedHatProjectIdFLag, "", "RedHat project id")
 
 	// enable using dashed notation in flags and underscores in env
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
@@ -100,17 +102,17 @@ func doRun(_ *cobra.Command, _ []string) error {
 	}
 
 	// Sanity check, if RedHat API key and project ID are not provided then the certified bundle cannot be generated.
-	redhatAPITokenFlag := viper.GetString(RedHatAPITokenFlag)
-	redhatProjectId := viper.GetString(RedHatProjectId)
+	redhatAPIKey := viper.GetString(RedHatAPIKeyFlag)
+	redhatProjectId := viper.GetString(RedHatProjectIdFLag)
 	imageDigest := ""
 	if conf.HasDigestPinning() {
-		if len(redhatAPITokenFlag) == 0 {
+		if len(redhatAPIKey) == 0 {
 			return errors.New("RedHat API key is required to get image digest")
 		}
 		if len(redhatProjectId) == 0 {
 			return errors.New("RedHat project ID is required to get image digest")
 		}
-		imageDigest, err = getImageDigest(redhatAPITokenFlag, redhatProjectId, conf.NewVersion)
+		imageDigest, err = getImageDigest(redhatAPIKey, redhatProjectId, conf.NewVersion)
 		if err != nil {
 			return err
 		}
@@ -571,4 +573,63 @@ func writeCRD(out io.WriteCloser, data []byte) error {
 	_, err := io.Copy(out, bytes.NewReader(data))
 
 	return err
+}
+
+type Images struct {
+	Data []struct {
+		DockerImageDigest string `json:"docker_image_digest"`
+		Id                string `json:"_id"`
+		CreationDate      string `json:"creation_date"`
+	} `json:"data"`
+}
+
+// getImageDigest connects to the RedHat catalog API to get the certified operator image digest as it is exposed
+// by the RedHat registry.
+func getImageDigest(apiKey, projectId, version string) (string, error) {
+	requestURL := fmt.Sprintf(ImagesEndpoint, projectId)
+
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("X-API-KEY", apiKey)
+
+	q := req.URL.Query()
+	q.Add("filter", fmt.Sprintf("repositories.tags.name==%s;deleted==false", version))
+	req.URL.RawQuery = q.Encode()
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("request error %s: %s", requestURL, res.Status)
+	}
+
+	var images Images
+	if err := json.NewDecoder(res.Body).Decode(&images); err != nil {
+		return "", err
+	}
+	if len(images.Data) > 1 {
+		fmt.Fprintf(os.Stderr, "\nid                       creation_date                    docker_image_digest\n")
+		for _, image := range images.Data {
+			fmt.Fprintf(os.Stderr, "%s %s %s\n", image.Id, image.CreationDate, image.DockerImageDigest)
+		}
+		return "", fmt.Errorf("found %d images with tag %s in RedHat catalog while only one is expected", len(images.Data), version)
+	}
+	if len(images.Data) == 0 {
+		return "", fmt.Errorf("no image with tag %s in RedHat catalog", version)
+	}
+	imageDigest := images.Data[0].DockerImageDigest
+	if imageDigest == "" {
+		return "", fmt.Errorf("image digest for %s is empty", version)
+	}
+	return imageDigest, nil
 }
