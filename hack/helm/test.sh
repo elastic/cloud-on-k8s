44bab713900d3f1d4fb5e6c60e43c38a53a8dff6
Michael Montgomery <mmontg1@gmail.com>
2022-07-25 12:21:39 -0500

    Use sed to force local changes during helm testing, and not charts in helm.elastic.co.

--- hack/helm/test.sh
+++ hack/helm/test.sh
@@ -6,13 +6,21 @@
 
 # Script to test helm charts
 
-set -euo pipefail
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 check() {
     local TEST_DIR="$1"
     cd "${TEST_DIR}"
+
+    local SED="sed_gnu"
+    if [[ "$OSTYPE" =~ "darwin" ]]; then
+        SED="sed_bsd"
+    fi
+
+    # Ensure official helm repository is commented out in Chart.yaml
+    "$SED" -E 's|[[:space:]]+repository: "https://helm\.elastic\.co"|    # repository: "https://helm.elastic.co"|g' Chart.yaml
+    # Uncomment file:// repository stanzas to ensure local changes to repositories are used instead
+    "$SED" -E 's|.*repository: "file://\.\./(eck-[a-z-]+)"$|    repository: "file://../\1"|' Chart.yaml
 
     echo "Ensuring dependencies are updated for $(basename "${TEST_DIR}") chart."
     helm dependency update . 1>/dev/null
@@ -21,7 +29,19 @@ check() {
     helm lint --strict .
 
     helm unittest -3 -f 'templates/tests/*.yaml' .
+
+    # restore changes to Chart.yaml
+    git checkout Chart.yaml
+
     cd -
+}
+
+sed_gnu() {
+    sed -i "$@"
+}
+
+sed_bsd() {
+    sed -i '' "$@"
 }
 
 for i in "${SCRIPT_DIR}"/../../deploy/[a-zA-Z0-9]*; do
