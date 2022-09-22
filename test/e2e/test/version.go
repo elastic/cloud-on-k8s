696d1e3ea338012f949421d8ad779874315f10f5
Thibault Richard <thb.krkr@gmail.com>
2022-09-22 11:20:44 +0200

    8.4.1 -> 8.4.2

--- test/e2e/test/version.go
+++ test/e2e/test/version.go
@@ -18,7 +18,7 @@ const (
 	// LatestReleasedVersion7x is the latest released version for 7.x
 	LatestReleasedVersion7x = "7.17.6"
 	// LatestReleasedVersion8x is the latest release version for 8.x
-	LatestReleasedVersion8x = "8.4.1"
+	LatestReleasedVersion8x = "8.4.2"
 )
 
 // SkipInvalidUpgrade skips a test that would do an invalid upgrade.
