c736740cd5a852115aad198d93ef7d02fb86c1a1
Thibault Richard <thb.krkr@gmail.com>
2022-11-08 12:55:35 +0100

    yaml for rbac, webhook, api-docs/operatorhub config

--- pkg/apis/stackconfigpolicy/v1alpha1/webhook.go
+++ pkg/apis/stackconfigpolicy/v1alpha1/webhook.go
@@ -30,7 +30,7 @@ var (
 	}
 )
 
-// +kubebuilder:webhook:path=/validate-scp-k8s-elastic-co-v1alpha1-stackconfigpolicies,mutating=false,failurePolicy=ignore,groups=stackconfigpolicy.k8s.elastic.co,resources=stackconfigpolicies,verbs=create;update,versions=v1alpha1,name=elastic-scp-validation-v1alpha1.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1alpha1,matchPolicy=Exact
+// +kubebuilder:webhook:path=/validate-scp-k8s-elastic-co-v1alpha1-stackconfigpolicies,mutating=false,failurePolicy=ignore,groups=stackconfigpolicy.k8s.elastic.co,resources=stackconfigpolicies,verbs=create;update,versions=v1alpha1,name=elastic-scp-validation-v1alpha1.k8s.elastic.co,sideEffects=None,admissionReviewVersions=v1;v1beta1,matchPolicy=Exact
 
 var _ webhook.Validator = &StackConfigPolicy{}
 
