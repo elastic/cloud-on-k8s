828a939d990ee40b0fedb5ca9ac14d0956c78072
Thibault Richard <thb.krkr@gmail.com>
2022-11-14 22:20:50 +0100

    e2e test

--- test/e2e/es/stackconfigpolicy_test.go
+++ test/e2e/es/stackconfigpolicy_test.go
@@ -20,7 +20,7 @@ import (
 	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 	policyv1alpha1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/stackconfigpolicy/v1alpha1"
 	"github.com/elastic/cloud-on-k8s/v2/pkg/controller/common/version"
-	esClient "github.com/elastic/cloud-on-k8s/v2/pkg/controller/elasticsearch/client"
+	"github.com/elastic/cloud-on-k8s/v2/pkg/controller/elasticsearch/client"
 	"github.com/elastic/cloud-on-k8s/v2/test/e2e/test"
 	"github.com/elastic/cloud-on-k8s/v2/test/e2e/test/elasticsearch"
 	"github.com/stretchr/testify/assert"
@@ -29,18 +29,16 @@ import (
 	"k8s.io/apimachinery/pkg/util/rand"
 )
 
-// TestStackConfigPolicy ...
+// TestStackConfigPolicy tests the StackConfigPolicy feature.
 func TestStackConfigPolicy(t *testing.T) {
 	// only execute this test if we have a test license to work with
 	if test.Ctx().TestLicense == "" {
-		fmt.Println("no license")
 		t.SkipNow()
 	}
 
 	// StackConfigPolicy is supported since 8.6.0
 	stackVersion := version.MustParse(test.Ctx().ElasticStackVersion)
 	if !stackVersion.GTE(version.MustParse("8.6.0-SNAPSHOT")) {
-		fmt.Println("invalid version")
 		t.SkipNow()
 	}
 
@@ -48,22 +46,22 @@ func TestStackConfigPolicy(t *testing.T) {
 		WithESMasterDataNodes(1, elasticsearch.DefaultResources).
 		WithLabel("label", "test-scp")
 
-	policyNamespace := test.Ctx().ManagedNamespace(0)
-	policyName := fmt.Sprintf("test-scp-%s", rand.String(4))
+	namespace := test.Ctx().ManagedNamespace(0)
+	secureSettingsSecretName := fmt.Sprintf("test-scp-secure-settings-%s", rand.String(4))
 	repoName := "repo-test"
 	expectedMaxBytesPerSec := "42mb"
 
 	policy := policyv1alpha1.StackConfigPolicy{
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: policyNamespace,
-			Name:      policyName,
+			Namespace: namespace,
+			Name:      fmt.Sprintf("test-scp-%s", rand.String(4)),
 		},
 		Spec: policyv1alpha1.StackConfigPolicySpec{
 			ResourceSelector: metav1.LabelSelector{
 				MatchLabels: map[string]string{"label": "test-scp"},
 			},
 			SecureSettings: []commonv1.SecretSource{
-				{SecretName: "secure-settings-1"},
+				{SecretName: secureSettingsSecretName},
 			},
 			Elasticsearch: policyv1alpha1.ElasticsearchConfigPolicySpec{
 				ClusterSettings: &commonv1.Config{Data: map[string]interface{}{
@@ -81,18 +79,19 @@ func TestStackConfigPolicy(t *testing.T) {
 		},
 	}
 
+	secureServiceAccountSettingKey := "gcs.client.secondary.credentials_file"
 	secureSettingsSecret := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("test-scp-secure-settings-%s", rand.String(4)),
-			Namespace: test.Ctx().ManagedNamespace(1),
+			Name:      secureSettingsSecretName,
+			Namespace: namespace,
 		},
 		Data: map[string][]byte{
-			"gcs.client.secondary.credentials_file": []byte(`{
+			secureServiceAccountSettingKey: []byte(`{
 				"type": "service_account",
-				"project_id": "PROJECT_ID",
-				"private_key_id": "KEY_ID",
-				"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2y8v2zfDTwdIa\n-----END PRIVATE KEY-----\n",
-				"client_email": "SERVICE_ACCOUNT_EMAIL",
+				"project_id": "FAKE_PROJECT_ID",
+				"private_key_id": "FAKE_KEY_ID",
+				"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDXgs36nwry0roc\nvJ+yNInGkapcGEiyYVE9hsaZBlHLe9erUsCALtfheMqskcs334tWF8tLqFMG1I2t\nMBfzUEudHqNQ0BP9Z+2ilMXJBhZpNU/i4i7H8LCEjKt34B5K50lJRZ+Lt2Z2zr1c\nZZ9PniPYOaqRLAjwXGwJ6rU1Xt+fJB0EHDni6dS/g6wn498yh81coFW0rSMTdArm\nfvqraf8mL6X7/ElI/RWGsvcjx31wNZv5re2R1sgAZzALac1lYdGhRQFZ73swz4bZ\nktjsZJ0v/38+YPnyELXykqpBYfKTC2n2QO2nFYR8tkfSMhMaSIrFc1YnkVsY8LmX\nZdKafnD5AgMBAAECggEAAjrCMsOOc3CmqEFzTX6ppjo/jvBZYC8Njhtk1pRwKDDB\nzG3wu+LALP746cwgVBWl9WANpFy7byinxpDmzoeYIKn+eomMi2SV2sa7PRcpCDGa\n//fjEAJ3cQebhoP1DEVURsPHoMRm9PeykdAjU8mJCWWfVB0mgoYSQBADi+fNXHIY\nVpZ+GmAg8sVmAMKws2Afa2FFXxAvNnR8wdeqBGhPhYH9yrJAc7c5BuKT/9axVg6u\n622epYd7tmfrQCnyTu1Vzwi0mylDRcC2r+ynl9Hkpr4w4fx3rlrjN/1UVOIcCQli\nHXHr8+n8GPMdDW/mUwZzO0NR2MevBsfZiKO9oCtC2QKBgQDrzuvk112cp3t4YA1I\nYjLHP7aGi+oq14UWrtNGDyS/e6bkbNZW2EkQy0TPcjA0AFe51vj0SE9iv3No1y0R\nsRwvbsg5qydY8SfSKY8Zu1CcfB3f1W1775jnkqx1LCj6LnBC1OIKaCbnAM6d2xxn\nUscNpaAW8+uQMDXtAY3BKkm4CwKBgQDp9vOoilPWVpRew8z5txnSMcwS/2U5sRjJ\nDHem/ZAJXO+4/iQAzPSumlov19fJiAZLD5/NDdJxYM7npQ8+xcZ9DgukPY6T5Qp3\nR0urYNjsEra4Q3A4OWFP7mr+oYQjwnc5slkS4hIafPVBi335Njjlce4DgZtLHIFJ\nwerY1dFpiwKBgFf+t3iGBaDXvvOEpHBGdLx1wh8jRxcFpdx5EM4sCIKMGhNTqghu\nXZWuxNbEvcgp+JKY7f36neUznFWbNm5LsUDiDkW24NAH7dw3NfdcNxCuIFfOxTRi\njKSdz01KVWBGxA2sc01+4EWDv5aYlVjZQv6Mt9jY3SbJVtZCpitXJHtRAoGBANnz\nJjqcWcsyrla1Oe5qRpCLuRr9ddPPiVJI3fHfBd3jCKIhhXKFe25n9ZnaDXf80jf8\nXxYLST47O6OJHPGSFfyLKAchHP/i/uPss637szf/mt1+XTzTHzbx2BRKbClPz/cc\nkGPJ26l3PJWJl5mfjFMZ1erIQt0uubX3AopqbQFPAoGAFsAMpU7OE/VG4/BLEufp\n7XEYhU5UiA4qsFKxobZpek7wQMAw3e3qrsyh3mD0D3qTl6Jq+YXW9c62kEny67mG\nW6v/s3KPbYdWDSf3R3t0Wx1Ym9QMT+oxUEjO9fZ79Gfa17XC9xH6Uqkn7FakzzPX\nTQI222EehboxE6Cys4/usKg=\n-----END PRIVATE KEY-----\n",
+				"client_email": "FAKE_SERVICE_ACCOUNT_EMAIL",
 				"client_id": "CLIENT_ID",
 				"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 				"token_uri": "https://accounts.google.com/o/oauth2/token",
@@ -106,7 +105,7 @@ func TestStackConfigPolicy(t *testing.T) {
 		Type: "gcs",
 		Settings: SnapshotRepositorySettings{
 			Bucket:   "bucket-test",
-			BasePath: fmt.Sprintf("snapshots/%s-%s", policyNamespace, policyName)},
+			BasePath: fmt.Sprintf("snapshots/%s-%s", es.Namespace(), es.Name())},
 	}
 	steps := func(k *test.K8sClient) test.StepList {
 		return test.StepList{
@@ -154,12 +153,27 @@ func TestStackConfigPolicy(t *testing.T) {
 						return err
 					}
 
-					if !reflect.DeepEqual(repos[repoName], expectedRepo) {
-						return errors.New("snapshot repository not configured")
+					actualRepo, ok := repos[repoName]
+					if !ok {
+						return fmt.Errorf("snapshot repository '%s' not found", repoName)
+					}
+					if !reflect.DeepEqual(actualRepo, expectedRepo) {
+						act, err := json.Marshal(actualRepo)
+						if err != nil {
+							return err
+						}
+						exp, err := json.Marshal(expectedRepo)
+						if err != nil {
+							return err
+						}
+						return fmt.Errorf("snapshot repository not configured: expected: %s, actual: %s", act, exp)
 					}
 					return nil
 				}),
 			},
+			elasticsearch.CheckESKeystoreEntries(k, es, []string{
+				secureServiceAccountSettingKey,
+			}),
 			test.Step{
 				Name: "Deleting the StackConfigPolicy should return no error",
 				Test: test.Eventually(func() error {
@@ -231,7 +245,7 @@ type SnapshotRepositorySettings struct {
 }
 
 // Request is a utility function to call a specific Elasticsearch API not implemented in the Elasticsearch client.
-func Request(esClient esClient.Client, method string, url string, body io.Reader, response interface{}) error {
+func Request(esClient client.Client, method string, url string, body io.Reader, response interface{}) error {
 	req, err := http.NewRequest(method, url, body) //nolint:noctx
 	if err != nil {
 		return err
