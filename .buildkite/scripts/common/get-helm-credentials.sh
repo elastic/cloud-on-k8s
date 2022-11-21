7157f8348c06ed91a51c08f98d4e915aaecd02d0
Michael Montgomery <mmontg1@gmail.com>
2022-11-21 10:03:43 -0600

    Fail and give feedback when vault read command fails

--- .buildkite/scripts/common/get-helm-credentials.sh
+++ .buildkite/scripts/common/get-helm-credentials.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-vault read -field="data" -format=json "${VAULT_ROOT_PATH}/helm-charts-publisher" > .ci/credentials.json
+if vault read -field="data" -format=json "${VAULT_ROOT_PATH}/helm-charts-publisher" > .ci/credentials.json ; then
+    echo "Vault read of helm credentials succeeded"
+else
+    echo "Vault read of helm credentials failed"
+    exit 1
+fi
