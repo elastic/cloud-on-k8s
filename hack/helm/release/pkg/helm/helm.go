77759ada589a8b87f58fbeb7f5f83530cb552796
Michael Montgomery <mmontg1@gmail.com>
2022-12-02 09:18:06 -0600

    wip

--- hack/helm/release/pkg/helm/helm.go
+++ hack/helm/release/pkg/helm/helm.go
@@ -14,6 +14,7 @@ import (
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/option"
+	"gopkg.in/yaml.v3"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/downloader"
@@ -37,19 +38,34 @@ type ReleaseConfig struct {
 func Release(conf ReleaseConfig) error {
 	// remove existing releases
 	log.Println("Removing existing releases.")
-	if err := removeExistinReleases(conf.ChartsDir); err != nil {
+	if err := removeExistingReleases(conf.ChartsDir); err != nil {
 		return err
 	}
 	if err := ensureCredentialsFile(conf.CredentialsFilePath); err != nil {
 		return err
 	}
-	// upload charts
-	if err := uploadCharts(conf.ChartsDir, conf.Bucket, conf.GCSURL, conf.UpdateDependencies); err != nil {
+	charts, err := readCharts(conf.ChartsDir)
+	if err != nil {
 		return err
 	}
-	// update index
-	if err := updateIndex(); err != nil {
+	noDeps, withDeps := process(charts)
+	// upload charts with no dependencies
+	if err := uploadCharts(noDeps, conf.ChartsDir, conf.Bucket, conf.GCSURL); err != nil {
 		return err
+	}
+	if conf.UploadIndex {
+		if err := updateIndex(); err != nil {
+			return err
+		}
+	}
+	// upload charts with dependencies
+	if err := uploadCharts(withDeps, conf.ChartsDir, conf.Bucket, conf.GCSURL); err != nil {
+		return err
+	}
+	if conf.UploadIndex {
+		if err := updateIndex(); err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -62,7 +78,7 @@ func ensureCredentialsFile(path string) error {
 	return os.Setenv(googleCredentialsEnvVar, path)
 }
 
-func removeExistinReleases(chartsDir string) error {
+func removeExistingReleases(chartsDir string) error {
 	// Cleanup existing releases
 	files, _ := filepath.Glob(chartsDir + "*/*.tgz")
 	for _, file := range files {
@@ -74,7 +90,72 @@ func removeExistinReleases(chartsDir string) error {
 	return nil
 }
 
-func uploadCharts(chartsDir, bucket, gcsURL string, updateDeps bool) error {
+func readCharts(chartsDir string) ([]chart, error) {
+	cs, err := filepath.Glob(filepath.Join(chartsDir, "*/Chart.yaml"))
+	if err != nil {
+		return nil, err
+	}
+	charts := make([]chart, len(cs))
+	for _, fullChartPath := range cs {
+		f, err := os.Open(fullChartPath)
+		if err != nil {
+			return nil, fmt.Errorf("while opening %s: %w", fullChartPath, err)
+		}
+		var fileBytes []byte
+		_, err = f.Read(fileBytes)
+		if err != nil {
+			return nil, fmt.Errorf("while reading %s: %w", fullChartPath, err)
+		}
+		var ch chart
+		if err = yaml.Unmarshal(fileBytes, &ch); err != nil {
+			return nil, fmt.Errorf("while unmarshing %s to chart: %w", fullChartPath, err)
+		}
+		charts = append(charts, ch)
+	}
+	return charts, nil
+}
+
+func process(charts []chart) (noDeps []chart, withDeps []chart) {
+	for i, ch := range charts {
+		if len(ch.dependencies) == 0 {
+			noDeps = append(noDeps, ch)
+			// remove this element in slice
+			charts[i] = charts[len(charts)-1]
+			charts = charts[:len(charts)-1]
+		}
+	}
+	for i, ch := range charts {
+		foundInDeps := false
+		for _, dep := range ch.dependencies {
+			if in(dep.name, noDeps) {
+				withDeps = append(withDeps, ch)
+				// remove this element in slice
+				charts[i] = charts[len(charts)-1]
+				charts = charts[:len(charts)-1]
+				foundInDeps = true
+				break
+			}
+		}
+		if !foundInDeps {
+			noDeps = append(noDeps, ch)
+			// remove this element in slice
+			charts[i] = charts[len(charts)-1]
+			charts = charts[:len(charts)-1]
+		}
+	}
+	return
+}
+
+func in(name string, charts []chart) bool {
+	for _, ch := range charts {
+		if ch.name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func uploadCharts(charts []chart, chartsDir, bucket, gcsURL string) error {
 	settings := cli.New()
 	log.Println("Adding https://charts.helm.sh/stable repository.")
 	// helm repo add stable https://charts.helm.sh/stable
@@ -85,48 +166,46 @@ func uploadCharts(chartsDir, bucket, gcsURL string, updateDeps bool) error {
 		return fmt.Errorf("while adding helm repository: %w", err)
 	}
 	log.Println("Done Adding https://charts.helm.sh/stable repository.")
-	charts, _ := filepath.Glob(filepath.Join(chartsDir, "*/Chart.yaml"))
+	// charts, _ := filepath.Glob(filepath.Join(chartsDir, "*/Chart.yaml"))
 	for _, chart := range charts {
-		chartName := strings.Split(chart, "/")[len(strings.Split(chart, "/"))-2]
-		chartPath := filepath.Join(chartsDir, chartName)
-		log.Printf("Attempting to add chart (%s)\n", chartName)
+		// chartName := strings.Split(chart, "/")[len(strings.Split(chart, "/"))-2]
+		chartPath := filepath.Join(chartsDir, chart.name)
+		log.Printf("Attempting to add chart (%s)\n", chart.name)
 		// helm dependency update charts_dir/chart
-		if updateDeps {
-			client := action.NewDependency()
-			cfg := action.Configuration{}
-			man := &downloader.Manager{
-				Out:              os.Stdout,
-				ChartPath:        chartPath,
-				Keyring:          client.Keyring,
-				SkipUpdate:       client.SkipRefresh,
-				Getters:          getter.All(settings),
-				RegistryClient:   cfg.RegistryClient,
-				RepositoryConfig: settings.RepositoryConfig,
-				RepositoryCache:  settings.RepositoryCache,
-				Debug:            settings.Debug,
-			}
-			if client.Verify {
-				man.Verify = downloader.VerifyAlways
-			}
-			log.Printf("Updating dependencies for chart (%s)\n", chartName)
-			if err := man.Update(); err != nil {
-				return fmt.Errorf("while running 'helm dependency update %s': %w", chart, err)
-			}
+		client := action.NewDependency()
+		cfg := action.Configuration{}
+		man := &downloader.Manager{
+			Out:              os.Stdout,
+			ChartPath:        chartPath,
+			Keyring:          client.Keyring,
+			SkipUpdate:       client.SkipRefresh,
+			Getters:          getter.All(settings),
+			RegistryClient:   cfg.RegistryClient,
+			RepositoryConfig: settings.RepositoryConfig,
+			RepositoryCache:  settings.RepositoryCache,
+			Debug:            settings.Debug,
+		}
+		if client.Verify {
+			man.Verify = downloader.VerifyAlways
+		}
+		log.Printf("Updating dependencies for chart (%s)\n", chart.name)
+		if err := man.Update(); err != nil {
+			return fmt.Errorf("while running 'helm dependency update %s': %w", chart, err)
 		}
 		// helm package "charts_dir/chart" --destination "chart"
 		packageClient := action.NewPackage()
-		log.Printf("Packaging chart (%s)\n", chartName)
+		log.Printf("Packaging chart (%s)\n", chart.name)
 		if _, err := packageClient.Run(chartPath, map[string]interface{}{}); err != nil {
 			return fmt.Errorf("while running 'helm package %s: %w", chartPath, err)
 		}
-		source := fmt.Sprintf("%s-*.tgz", chartName)
-		destination := fmt.Sprintf("%s/helm/%s/", bucket, chartName)
+		source := fmt.Sprintf("%s-*.tgz", chart.name)
+		destination := fmt.Sprintf("%s/helm/%s/", bucket, chart.name)
 
 		// gsutil cp -n source destination
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 		defer cancel()
 
-		log.Printf("Writing chart (%s) to bucket\n", chartName)
+		log.Printf("Writing chart (%s) to bucket\n", chart.name)
 		storageClient, err := getGCSClient(ctx, gcsURL)
 		if err != nil {
 			return fmt.Errorf("while creating gcs storage client: %w", err)
