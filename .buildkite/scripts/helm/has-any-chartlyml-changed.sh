051c40b796d7176dc334ddea6b49e1dfd0470a12
Michael Montgomery <mmontg1@gmail.com>
2023-01-03 08:03:45 -0600

    Fix SC2068 and SC2207

--- .buildkite/scripts/helm/has-any-chartlyml-changed.sh
+++ .buildkite/scripts/helm/has-any-chartlyml-changed.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # get the names of the files changed between HEAD and the previous commit.
-filesChanged=( $(git diff --name-only HEAD~1...HEAD) )
+# bash veersion 4.4+ only.
+# same command as filesChanged=( $(git diff --name-only HEAD~1...HEAD) ) 
+# avoids SC2207
+mapfile -t filesChanged < <(git diff --name-only HEAD~1...HEAD)
 
 re='^deploy\/eck-[a-z]+[-]*[a-z]*\/Chart\.yaml$'
 
-for i in ${filesChanged[@]}; do
+for i in "${filesChanged[@]}"; do
     if [[ ${i} =~ ${re} ]]; then
         exit 0
     fi
