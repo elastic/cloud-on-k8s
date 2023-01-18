3a3f3d7763a1d83706fc1bdf2f1771e38a6e1521
Thibault Richard <thb.krkr@gmail.com>
2023-01-18 16:05:53 +0100

    Disabling TestFleetCustomLogsIntegrationRecipe and TestFleetAPMIntegrationRecipe

--- test/e2e/agent/recipes_test.go
+++ test/e2e/agent/recipes_test.go
@@ -133,6 +133,10 @@ func TestFleetKubernetesIntegrationRecipe(t *testing.T) {
 }
 
 func TestFleetCustomLogsIntegrationRecipe(t *testing.T) {
+
+	// This test is disabled until we understand why it is failing
+	t.Skip("TestFleetCustomLogsIntegrationRecipe is disabled, see https://github.com/elastic/cloud-on-k8s/issues/6331")
+
 	notLoggingPod := beat.NewPodBuilder("test")
 	loggingPod := beat.NewPodBuilder("test")
 	loggingPod.Pod.Namespace = "default"
@@ -159,6 +163,10 @@ func TestFleetCustomLogsIntegrationRecipe(t *testing.T) {
 }
 
 func TestFleetAPMIntegrationRecipe(t *testing.T) {
+
+	// This test is disabled until we understand why it is failing
+	t.Skip("TestFleetAPMIntegrationRecipe is disabled, see https://github.com/elastic/cloud-on-k8s/issues/6331")
+
 	customize := func(builder agent.Builder) agent.Builder {
 		if !builder.Agent.Spec.FleetServerEnabled {
 			return builder
