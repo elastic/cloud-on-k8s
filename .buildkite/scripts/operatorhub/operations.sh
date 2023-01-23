7e13c9826b581890029cf1391ff7afcbbd185ebd
Michael Montgomery <mmontg1@gmail.com>
2023-01-23 10:37:41 -0600

    Move large commands to a script in buildkite.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

---
+++ .buildkite/scripts/operatorhub/operations.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Required environment variables:
+#  For 'preflight' command:
+#   OHUB_TAG
+#  For 'release' command:
+#   OHUB_PREV_VERSION
+#   OHUB_STACK_VERSION
+
+preflight() {
+    if [[ -Z ${OHUB_TAG} ]]; then
+        echo "OHUB_TAG environment variable is required"
+        exit 1
+    fi
+
+    export OHUB_API_KEY=$(vault read -field=api-key secret/ci/elastic-cloud-on-k8s/operatorhub-release-redhat)
+    export OHUB_PROJECT_ID=$(vault read -field=project-id secret/ci/elastic-cloud-on-k8s/operatorhub-release-redhat)
+
+    curl -s -G "https://catalog.redhat.com/api/containers/v1/projects/certification/id/$OHUB_PROJECT_ID/images?filter=repositories.tags.name==$OHUB_TAG" -H "X-API-KEY: $OHUB_API_KEY" > /tmp/redhat.json
+
+    # The following command will fail if no data was returned, so preflight will not run if it's already run
+    jq -e '.data[0]' /tmp/redhat.json && exit 0
+
+    # Install known working version of preflight tool.
+    curl -L -o /tmp/preflight https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/1.2.1/preflight-linux-amd64
+    chmod u+x /tmp/preflight
+
+    # Pull authentication information for quay.io from vault
+    vault read -format=json -field=data secret/ci/elastic-cloud-on-k8s/operatorhub-release-preflight > /tmp/auth.json
+
+    /tmp/preflight check container quay.io/redhat-isv-containers/$OHUB_PROJECT_ID:$OHUB_TAG --pyxis-api-token=$OHUB_API_KEY --certification-project-id=$OHUB_PROJECT_ID --submit -d /tmp/auth.json
+}
+
+release() {
+    if [[ -Z ${OHUB_PREV_VERSION} ]]; then
+        echo "OHUB_PREV_VERSION environment variable is required"
+        exit 1
+    fi
+    if [[ -Z ${OHUB_STACK_VERSION} ]]; then
+        echo "OHUB_STACK_VERSION environment variable is required"
+        exit 1
+    fi
+
+    buildkite-agent artifact download "bin/operator*" /usr/local/
+    buildkite-agent artifact download "config/*.yaml" .
+    chmod u+x /usr/local/bin/operatorhub
+    /usr/local/bin/operatorhub container publish --dry-run=false
+    cd hack/operatorhub
+    /usr/local/bin/operatorhub generate-manifests --prev-version=$OHUB_PREV_VERSION --stack-version=$OHUB_STACK_VERSION --yaml-manifest=../../config/crds.yaml --yaml-manifest=../../config/operator.yaml
+    /usr/local/bin/operatorhub bundle generate --dir=$(pwd)
+    /usr/local/bin/operatorhub bundle create-pr --dir=$(pwd)
+}
+
+usage() {
+    echo "Usage: $0 preflight|release"
+    echo "preflight: Runs the operatorhub preflight operations in buildkite"
+    echo "release: Runs the last steps of the operatorhub release steps in buildkite"
+    exit 2
+}
+
+if [[ "$#" -ne 1 ]]; then
+    usage
+fi
+
+case "$1" in
+    preflight)
+        list_all
+        ;;
+    release)
+        release
+        ;;
+    help)
+        usage
+        ;;
+    *)
+        echo "Unknown action '$1'"
+        usage
+        ;;
+esac
