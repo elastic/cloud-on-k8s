c5fdd441486e035fef709898dc74e4eee1b29483
Peter Brachwitz <peter.brachwitz@elastic.co>
2023-01-25 18:01:24 +0100

    Revert "Reenable fleet tests and temporarily test against 8.6.1-SNAPSHOT"
    This reverts commit ec43ee156a499644ba4a3f9ad2fbb7c2a2cd38cc.

--- test/e2e/agent/recipes_test.go
+++ test/e2e/agent/recipes_test.go
@@ -88,6 +88,8 @@ func TestMultiOutputRecipe(t *testing.T) {
 }
 
 func TestFleetKubernetesIntegrationRecipe(t *testing.T) {
+	// This test is disabled until we understand why it is failing
+	t.Skip("TestFleetKubernetesIntegrationRecipe is disabled, see https://github.com/elastic/cloud-on-k8s/issues/6331")
 
 	customize := func(builder agent.Builder) agent.Builder {
 		if !builder.Agent.Spec.FleetServerEnabled {
@@ -132,6 +134,9 @@ func TestFleetKubernetesIntegrationRecipe(t *testing.T) {
 
 func TestFleetCustomLogsIntegrationRecipe(t *testing.T) {
 
+	// This test is disabled until we understand why it is failing
+	t.Skip("TestFleetCustomLogsIntegrationRecipe is disabled, see https://github.com/elastic/cloud-on-k8s/issues/6331")
+
 	notLoggingPod := beat.NewPodBuilder("test")
 	loggingPod := beat.NewPodBuilder("test")
 	loggingPod.Pod.Namespace = "default"
@@ -158,6 +163,9 @@ func TestFleetCustomLogsIntegrationRecipe(t *testing.T) {
 }
 
 func TestFleetAPMIntegrationRecipe(t *testing.T) {
+
+	// This test is disabled until we understand why it is failing
+	t.Skip("TestFleetAPMIntegrationRecipe is disabled, see https://github.com/elastic/cloud-on-k8s/issues/6331")
 
 	customize := func(builder agent.Builder) agent.Builder {
 		if !builder.Agent.Spec.FleetServerEnabled {
