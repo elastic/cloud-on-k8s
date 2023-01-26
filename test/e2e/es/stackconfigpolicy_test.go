d6d534cf2d73b07d006b24ffc4db0dc1b0618e77
Thibault Richard <thbkrkr@users.noreply.github.com>
2023-01-26 15:46:05 +0100

    Activate enterprise license in TestStackConfigPolicy (#6361)
    This commit updates TestStackConfigPolicy so that an Enterprise license is created/removed
    at the start/end of the test, which is the license level required to use the StackConfigPolicy feature.

--- test/e2e/es/stackconfigpolicy_test.go
+++ test/e2e/es/stackconfigpolicy_test.go
@@ -113,6 +113,8 @@ func TestStackConfigPolicy(t *testing.T) {
 		},
 	}
 
+	esWithlicense := test.LicenseTestBuilder(es)
+
 	var noEntries []string
 	steps := func(k *test.K8sClient) test.StepList {
 		return test.StepList{
@@ -228,7 +230,7 @@ func TestStackConfigPolicy(t *testing.T) {
 		}
 	}
 
-	test.Sequence(nil, steps, es).RunSequential(t)
+	test.Sequence(nil, steps, esWithlicense).RunSequential(t)
 }
 
 func checkAPIStatusCode(esClient client.Client, url string, expectedStatusCode int) error {
