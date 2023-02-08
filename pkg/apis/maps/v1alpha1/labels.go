7be95e6e409ca71f9fc2449f1e14625f66eac1cc
Michael Montgomery <mmontg1@gmail.com>
2023-02-08 14:02:11 -0600

    Fix comment for Elastic Maps.
    Co-authored-by: Thibault Richard <thbkrkr@users.noreply.github.com>

--- pkg/apis/maps/v1alpha1/labels.go
+++ pkg/apis/maps/v1alpha1/labels.go
@@ -8,7 +8,7 @@ import (
 	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 )
 
-// GetElasticLabels will return the common Elastic assigned labels for EnterprisMapsServer.
+// GetIdentityLabels will return the common Elastic assigned labels for the Elastic Maps Server.
 func (m *ElasticMapsServer) GetIdentityLabels() map[string]string {
 	return map[string]string{
 		commonv1.TypeLabelName:     "maps",
