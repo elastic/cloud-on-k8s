f0f5c9874679b939a67ff8d1b78acbc9ecff8423
Michael Montgomery <mmontg1@gmail.com>
2023-02-10 08:45:11 -0600

    Add script to run main e2e tests, which allows onFailure gsutil run.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

---
+++ .buildkite/scripts/common/run-main-e2e-tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# On any command failures, make sure that diagnostics are copied from bucket to local Buildkite environment
+# to be uploaded as artifacts.
+onError() {
+    # On Copy any artifacts from the test run locally to be picked up s buildkite artifacts
+    if gsutil ls "gs://eck-e2e-buildkite-artifacts/jobs/$$BUILDKITE_PIPELINE_NAME/$$BUILDKITE_BUILD_NUMBER/eck-diagnostic*.zip" ; then
+        gsutil cp "gs://eck-e2e-buildkite-artifacts/jobs/$$BUILDKITE_PIPELINE_NAME/$$BUILDKITE_BUILD_NUMBER/eck-diagnostic*.zip"
+    fi
+}
+
+main() {
+    make run-deployer
+    # Read gcp bucket credentials to allow eck-diagnostics output writing to eck-e2e-buildkite-artifacts bucket.
+    vault read -field=service-account secret/ci/elastic-cloud-on-k8s/ci-gcp-k8s-operator > /tmp/auth.json
+    make e2e-run
+}
+
+trap 'onError' ERR
+
+main
+
