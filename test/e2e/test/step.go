0f5135bfb08bee484c4a5ecd4afe7d3a6d7f80f7
Michael Montgomery <mmontg1@gmail.com>
2023-02-13 09:56:35 -0600

    Name the eck-diagnostics output file. Remove returning an error from runECKDiagnostics as it's unused.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- test/e2e/test/step.go
+++ test/e2e/test/step.go
@@ -57,7 +57,7 @@ func (l StepList) RunSequential(t *testing.T) {
 			log.Info("Skipping test", "name", ts.Name)
 			continue
 		}
-		if !t.Run(ts.Name, ts.Test) { //nolint:nestif
+		if !t.Run(ts.Name, ts.Test) { 
 			logf.Log.Error(errors.New("test failure"), "continuing with additional tests")
 			if ts.OnFailure != nil {
 				ts.OnFailure()
@@ -74,11 +74,7 @@ func (l StepList) RunSequential(t *testing.T) {
 			if ctx.JobName != "" {
 				once.Do(initGSUtil)
 				logf.Log.Info("running eck-diagnostics job")
-				err := runECKDiagnostics(ctx, ts)
-				if err != nil {
-					logf.Log.Error(err, "while running eck diagnostics")
-					continue
-				}
+				runECKDiagnostics(ctx, ts)
 				uploadDiagnosticsArtifacts()
 			}
 		}
@@ -95,16 +91,15 @@ func initGSUtil() {
 	}
 }
 
-func runECKDiagnostics(ctx Context, step Step) error {
+func runECKDiagnostics(ctx Context, step Step) {
 	otherNS := append([]string{ctx.E2ENamespace}, ctx.Operator.ManagedNamespaces...)
-	cmd := exec.Command("eck-diagnostics", "--output-directory", "/tmp", "-o", ctx.Operator.Namespace, "-r", strings.Join(otherNS, ","), "--run-agent-diagnostics") //nolint:gosec
+	cmd := exec.Command("eck-diagnostics", "--output-directory", "/tmp", "-n", fmt.Sprintf("eck-diagnostics-%s.zip", step.Name), "-o", ctx.Operator.Namespace, "-r", strings.Join(otherNS, ","), "--run-agent-diagnostics") //nolint:gosec
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = ensureTmpHomeEnv(cmd.Environ())
 	if err := cmd.Run(); err != nil {
 		log.Error(err, fmt.Sprintf("Failed to run eck-diagnostics: %s", err))
 	}
-	return nil
 }
 
 func uploadDiagnosticsArtifacts() {
