0ec7127c85a996fe7afb344162611a1a6785fde1
Thibault Richard <thb.krkr@gmail.com>
2023-03-16 12:06:53 +0100

    Unit test build/setenv.sh

---
+++ .buildkite/scripts/build/setenv_test.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+
+set -u
+
+ROOT="$(cd "$(dirname "$0")"; pwd)/../../.."
+
+current_version=$(cat VERSION)
+current_sha1=$(git rev-parse --short=8 --verify HEAD)
+
+setenv=$ROOT/.buildkite/scripts/build/setenv.sh 
+
+TOTAL=0
+OK=0
+KO=0
+assert_image() {
+    TOTAL=$((TOTAL+1))
+    expected=$1
+    got=$(make print-operator-image)
+    grep -q "$expected$" <<< "$got"
+    if [[ $? != 0 ]]; then
+        KO=$((KO+1))
+        echo " 🔴 expected: $expected"
+        echo " 🔴 got:      $got"
+    else
+        OK=$((OK+1))
+        echo " 🟢  $got"
+    fi
+    echo "--"
+}
+
+export BUILDKITE_BUILD_NUMBER=999999999999999
+
+echo "test trigger_pr"; BUILDKITE_PULL_REQUEST="4242" $setenv > /dev/null
+assert_image "docker.elastic.co/eck-ci/eck-operator-pr:4242-${current_sha1}"
+
+echo "test trigger_nightly_main"; BUILDKITE_BRANCH="main" BUILDKITE_SOURCE="schedule" $setenv > /dev/null
+assert_image "docker.elastic.co/eck-ci/eck-operator-nightly:${current_version}-${current_sha1}"
+
+echo "test trigger_nightly_main-dev"; BUILDKITE_BRANCH="main" BUILDKITE_SOURCE="schedule" BUILD_LICENSE_PUBKEY=dev $setenv > /dev/null
+assert_image "docker.elastic.co/eck-ci/eck-operator-nightly:${current_version}-${current_sha1}-dev"
+
+echo "test trigger_merge_main"; BUILDKITE_BRANCH="main" $setenv > /dev/null
+assert_image "docker.elastic.co/eck-snapshots/eck-operator:${current_version}-${current_sha1}"
+
+echo "test trigger_tag"; BUILDKITE_TAG="v1.2.3" $setenv build > /dev/null
+assert_image "docker.elastic.co/eck/eck-operator:v1.2.3"
+
+echo "test trigger_vtag"; BUILDKITE_TAG="v1.2.3" $setenv build > /dev/null
+assert_image "docker.elastic.co/eck/eck-operator:v1.2.3"
+
+echo "test trigger_vtagbc"; BUILDKITE_TAG="v1.2.3-bc1" $setenv build > /dev/null
+assert_image "docker.elastic.co/eck/eck-operator:v1.2.3-bc1"
+
+echo "test trigger_branch"; BUILDKITE_BRANCH="4.2" BUILDKITE_PULL_REQUEST="false" $setenv > /dev/null
+assert_image "docker.elastic.co/eck-ci/eck-operator-branch:${current_version}-${current_sha1}"
+
+echo "Passed: $OK/$TOTAL"
+if [[ "$KO" -gt 0 ]]; then
+    exit 1
+fi
