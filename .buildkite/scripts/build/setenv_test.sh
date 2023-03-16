fed9c35aa97ac9f0e6db54826f78c21c3dcd11a8
Thibault Richard <thb.krkr@gmail.com>
2023-03-16 13:06:30 +0100

    shellcheck

--- .buildkite/scripts/build/setenv_test.sh
+++ .buildkite/scripts/build/setenv_test.sh
@@ -4,9 +4,10 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 
-set -u
+set -eu
 
-ROOT="$(cd "$(dirname "$0")"; pwd)/../../.."
+WD="$(cd "$(dirname "$0")"; pwd)"
+ROOT="$WD/../../.."
 
 current_version=$(cat VERSION)
 current_sha1=$(git rev-parse --short=8 --verify HEAD)
@@ -20,8 +21,7 @@ assert_image() {
     TOTAL=$((TOTAL+1))
     expected=$1
     got=$(make print-operator-image)
-    grep -q "$expected$" <<< "$got"
-    if [[ $? != 0 ]]; then
+    if ! grep -q "$expected$" <<< "$got"; then
         KO=$((KO+1))
         echo " 🔴 expected: $expected"
         echo " 🔴 got:      $got"
