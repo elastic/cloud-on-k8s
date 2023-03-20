aa436d1534e685b781ced41fadfcd3d19af67551
Michael Montgomery <mmontg1@gmail.com>
2023-03-20 08:33:43 -0500

    Stop using jobname/buildnumber and use buildnumber/clustername which is unique.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- .buildkite/scripts/common/run-main-e2e-tests.sh
+++ .buildkite/scripts/common/run-main-e2e-tests.sh
@@ -6,9 +6,9 @@ set -euo pipefail
 # to be uploaded as artifacts.
 onError() {
     # On error, copy any artifacts from the test run locally to be picked up as buildkite artifacts
-    if gsutil ls "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_PIPELINE_NAME/$BUILDKITE_BUILD_NUMBER/eck-diagnostic*.zip" ; then
-        gsutil cp "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_PIPELINE_NAME/$BUILDKITE_BUILD_NUMBER/eck-diagnostic*.zip" .
-        gsutil rm "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_PIPELINE_NAME/$BUILDKITE_BUILD_NUMBER/eck-diagnostic*.zip" .
+    if gsutil ls "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_BUILD_NUMBER/$CLUSTER_NAME/eck-diagnostic*.zip" ; then
+        gsutil cp "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_BUILD_NUMBER/$CLUSTER_NAME/eck-diagnostic*.zip" .
+        gsutil rm "gs://eck-e2e-buildkite-artifacts/jobs/$BUILDKITE_BUILD_NUMBER/$CLUSTER_NAME/eck-diagnostic*.zip" .
     fi
 }
 
