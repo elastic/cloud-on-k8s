0b2bacd564b214322017e1801ae9c8ecd2b7fc67
Michael Montgomery <mmontg1@gmail.com>
2023-03-22 21:07:08 -0500

    Ignore stderr on ls command.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- .buildkite/scripts/common/run-e2e-tests.sh
+++ .buildkite/scripts/common/run-e2e-tests.sh
@@ -12,7 +12,7 @@ source "$ROOT/.env"
 # to be uploaded as artifacts.
 onError() {
     # On error, copy any artifacts from the test run locally to be picked up as buildkite artifacts
-    if gsutil ls "gs://eck-e2e-buildkite-artifacts/jobs/$CLUSTER_NAME/eck-diagnostic*.zip" ; then
+    if gsutil ls "gs://eck-e2e-buildkite-artifacts/jobs/$CLUSTER_NAME/eck-diagnostic*.zip" 2> /dev/null ; then
         gsutil cp "gs://eck-e2e-buildkite-artifacts/jobs/$CLUSTER_NAME/eck-diagnostic*.zip" .
         gsutil rm "gs://eck-e2e-buildkite-artifacts/jobs/$CLUSTER_NAME/eck-diagnostic*.zip" .
     fi
