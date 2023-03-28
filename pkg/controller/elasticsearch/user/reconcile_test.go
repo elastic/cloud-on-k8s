461235b5263b76f5c392ea2144003f3c329dc0b9
Thibault Richard <thbkrkr@users.noreply.github.com>
2023-03-28 15:57:57 +0200

    Add a new role for APM Server 8.7.0+ (#6605) (#6608)
    Add a new role for APM Server 8.7.0+, adding read privileges for the system indices
    `.apm-agent-configuration` and `.apm-source-map`.
    This change is essentially to allow features that previously required a kibanaRef to work
    without a connection to Kibana.
    
    Co-authored-by: Andrew Wilkins <axwalk@gmail.com>

--- pkg/controller/elasticsearch/user/reconcile_test.go
+++ pkg/controller/elasticsearch/user/reconcile_test.go
@@ -96,6 +96,6 @@ func Test_aggregateRoles(t *testing.T) {
 	c := k8s.NewFakeClient(sampleUserProvidedRolesSecret...)
 	roles, err := aggregateRoles(context.Background(), c, sampleEsWithAuth, initDynamicWatches(), record.NewFakeRecorder(10))
 	require.NoError(t, err)
-	require.Len(t, roles, 52)
+	require.Len(t, roles, 53)
 	require.Contains(t, roles, ProbeUserRole, "role1", "role2")
 }
