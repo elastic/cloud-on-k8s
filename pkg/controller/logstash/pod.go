107704481bac50445ec6fddb7bcb18f559e66ac1
Kaise Cheng <kaise.cheng@elastic.co>
2023-04-05 16:06:20 +0100

    lint

--- pkg/controller/logstash/pod.go
+++ pkg/controller/logstash/pod.go
@@ -7,7 +7,6 @@ package logstash
 import (
 	"fmt"
 	"hash"
-	"path"
 
 	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 	commonassociation "github.com/elastic/cloud-on-k8s/v2/pkg/controller/common/association"
