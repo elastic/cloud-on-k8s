eab2a7e186b4c9d4edf48a9ce93f4200e4843272
Thibault Richard <thb.krkr@gmail.com>
2023-04-06 15:47:25 +0200

    Update pipeline definitions

--- .buildkite/scripts/release/redhat-preflight.sh
+++ .buildkite/scripts/release/redhat-preflight.sh
@@ -7,28 +7,28 @@
 # Script to run the preflight CLI to verify that containers meet minimum requirements for Red Hat Software Certification.
 # See https://github.com/redhat-openshift-ecosystem/openshift-preflight.
 
-set -euox pipefail
+set -euo pipefail
 
 VAULT_ROOT_PATH=${VAULT_ROOT_PATH:-secret/ci/elastic-cloud-on-k8s}
 DRY_RUN=${DRY_RUN:-true}
 
 tmpDir=$(mktemp -d)
-trap "rm -rf $tmpDir" 0
+trap 'rm -rf "$tmpDir"' 0
 
-preflight_already_submitted() {
-    curl -H "X-API-KEY: $API_KEY" -sG "https://catalog.redhat.com/api/containers/v1/projects/certification/id/$PROJECT_ID/images?filter=repositories.tags.name==$tag" \
-        jq -e '.data[0]' >/dev/null
+container_already_verified() {
+    curl -H "X-API-KEY: $API_KEY" -s "https://catalog.redhat.com/api/containers/v1/projects/certification/id/$PROJECT_ID/images?filter=repositories.tags.name==$tag" | \
+        jq --exit-status '.data[0]' >/dev/null
 }
 
 main() {
-    local tag=$1
+    local tag="${1#v}"
 
     API_KEY=$(vault read -field=api-key "$VAULT_ROOT_PATH/operatorhub-release-redhat")
     export API_KEY
     PROJECT_ID=$(vault read -field=project-id "$VAULT_ROOT_PATH/operatorhub-release-redhat")
     export PROJECT_ID
 
-    if preflight_already_submitted; then
+    if container_already_verified; then
         echo "Preflight has already been submitted ✅"
         exit 0
     fi
@@ -38,9 +38,9 @@ main() {
         chmod u+x "$tmpDir/preflight"
     fi
 
-    echo vault read -format=json -field=data "$VAULT_ROOT_PATH/operatorhub-release-preflight" > "$tmpDir/auth.json"
+    vault read -format=json -field=data "$VAULT_ROOT_PATH/operatorhub-release-preflight" > "$tmpDir/auth.json"
     
-    echo preflight check container "quay.io/redhat-isv-containers/$PROJECT_ID:$tag" --pyxis-api-token="$API_KEY" --certification-project-id="$PROJECT_ID" --submit -d "$tmpDir/auth.json"
+    preflight check container "quay.io/redhat-isv-containers/$PROJECT_ID:$tag" --pyxis-api-token="$API_KEY" --certification-project-id="$PROJECT_ID" --submit -d "$tmpDir/auth.json"
     
     echo "Preflight submitted ✅"
 }
