797f1c93e6ae4a8de9a85617dc5fd20b8bdd23b1
Rob Bavey <rob.bavey@elastic.co>
2023-04-11 11:06:21 -0400

    Logstash controller unit and integration tests (#6575)
    This commit adds extra unit tests for the Logstash Controller, including tests for config, controller, pods and services

--- pkg/controller/logstash/pod.go
+++ pkg/controller/logstash/pod.go
@@ -91,8 +91,9 @@ func buildPodTemplate(params Params, configHash hash.Hash32) corev1.PodTemplateS
 		WithDockerImage(spec.Image, container.ImageRepository(container.LogstashImage, spec.Version)).
 		WithAutomountServiceAccountToken().
 		WithPorts(ports).
-		WithReadinessProbe(readinessProbe(false)).
-		WithVolumeLikes(vols...)
+		WithReadinessProbe(readinessProbe(params.Logstash)).
+		WithVolumeLikes(vols...).
+		WithInitContainerDefaults()
 
 	builder, err := stackmon.WithMonitoring(params.Context, params.Client, builder, params.Logstash)
 	if err != nil {
@@ -117,12 +118,15 @@ func getDefaultContainerPorts() []corev1.ContainerPort {
 }
 
 // readinessProbe is the readiness probe for the Logstash container
-func readinessProbe(useTLS bool) corev1.Probe {
-	scheme := corev1.URISchemeHTTP
-	if useTLS {
-		scheme = corev1.URISchemeHTTPS
+func readinessProbe(logstash logstashv1alpha1.Logstash) corev1.Probe {
+	var scheme = corev1.URISchemeHTTP
+	var port = network.HTTPPort
+	for _, service := range logstash.Spec.Services {
+		if service.Name == LogstashAPIServiceName && len(service.Service.Spec.Ports) > 0 {
+			port = int(service.Service.Spec.Ports[0].Port)
+		}
 	}
-	return corev1.Probe{
+	probe := corev1.Probe{
 		FailureThreshold:    3,
 		InitialDelaySeconds: 30,
 		PeriodSeconds:       10,
@@ -130,10 +134,11 @@ func readinessProbe(useTLS bool) corev1.Probe {
 		TimeoutSeconds:      5,
 		ProbeHandler: corev1.ProbeHandler{
 			HTTPGet: &corev1.HTTPGetAction{
-				Port:   intstr.FromInt(network.HTTPPort),
+				Port:   intstr.FromInt(port),
 				Path:   "/",
 				Scheme: scheme,
 			},
 		},
 	}
+	return probe
 }
