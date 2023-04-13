d09f7febaf40e9141f5590a1eefb0ffe9a0d34e3
Rob Bavey <rob.bavey@elastic.co>
2023-04-13 09:02:15 -0400

    Mark pods as updated to speed up discovery and application of pipeline changes

--- pkg/controller/logstash/labels.go
+++ pkg/controller/logstash/labels.go
@@ -5,6 +5,8 @@
 package logstash
 
 import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 	logstashv1alpha1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/logstash/v1alpha1"
 )
@@ -26,4 +28,9 @@ func NewLabels(logstash logstashv1alpha1.Logstash) map[string]string {
 		commonv1.TypeLabelName: TypeLabelValue,
 		NameLabelName:          logstash.Name,
 	}
+}
+
+// NewLabelSelectorForLogstash returns a labels.Selector that matches the labels as constructed by NewLabels
+func NewLabelSelectorForLogstash(ls logstashv1alpha1.Logstash) client.MatchingLabels {
+	return client.MatchingLabels(map[string]string{commonv1.TypeLabelName: TypeLabelValue, NameLabelName: ls.Name})
 }
