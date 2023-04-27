bf0f18940a1c2985f61b7e17b2f8ee8b163d6a50
Michael Morello <michael.morello@elastic.co>
2023-04-27 08:12:58 +0200

    Assume that events are recorded synchronously

--- pkg/utils/test/events.go
+++ pkg/utils/test/events.go
@@ -6,37 +6,27 @@ package test
 
 import (
 	"testing"
-	"time"
 
 	"k8s.io/client-go/tools/record"
 )
 
-const (
-	// EventsTimeout is na arbitrary timeout for events to be collected in a unit test.
-	EventsTimeout = time.Second * 5
-)
-
 // ReadAtMostEvents attempts to read at most minEventCount from a FakeRecorder.
+// This functions assumes that all the events are available in recorder.
 func ReadAtMostEvents(t *testing.T, minEventCount int, recorder *record.FakeRecorder) []string {
 	t.Helper()
 	if minEventCount == 0 {
 		return nil
 	}
-	c := time.After(EventsTimeout)
 	gotEvents := make([]string, 0, minEventCount)
 	gotEventCount := 0
-	for {
+	for i := 0; i < minEventCount; i++ {
 		select {
 		case e := <-recorder.Events:
 			gotEvents = append(gotEvents, e)
 			gotEventCount++
-			if gotEventCount == minEventCount {
-				// We have read the min. expected number of events
-				return gotEvents
-			}
-		case <-c:
-			t.Errorf("Timeout: expected at least %d events, got %d", minEventCount, gotEventCount)
-			return gotEvents
+		default:
+			t.Errorf("expected at least %d events, got %d", minEventCount, gotEventCount)
 		}
 	}
+	return gotEvents
 }
