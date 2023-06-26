fdde6f9dbab5def390339c4c41116181aa72a7e1
Michael Montgomery <mmontg1@gmail.com>
2023-06-26 14:19:15 -0500

    Handle Azure Clusters
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- .buildkite/scripts/test/cleanup-e2e-clusters.sh
+++ .buildkite/scripts/test/cleanup-e2e-clusters.sh
@@ -15,6 +15,8 @@ ROOT="$WD/../../.."
 # Get the date 3 days in the past.
 DATE=$(date --date='3 days ago' --iso-8601=seconds)
 
+## Google Clusters
+
 # Activate the Google service account
 vault read -field=service-account "$VAULT_ROOT_PATH/ci-gcp-k8s-operator" > /tmp/ci-gcp-k8s-operator.json
 gcloud auth activate-service-account --key-file=/tmp/ci-gcp-k8s-operator.json
@@ -29,4 +31,21 @@ for i in ${CLUSTERS} ; do
     cd "$ROOT"
     E2E_PROVIDER=gke CLUSTER_NAME=$i DEPLOYER_OPERATION=delete .buildkite/scripts/test/set-deployer-config.sh
     make run-deployer
+done
+
+## Azure Clusters
+
+# Handle logging into Azure using cli
+vault read -field=data "$VAULT_ROOT_PATH/ci-azr-k8s-operator" > /tmp/ci-azr-k8s-operator.json
+CLIENT_ID=$(jq .appId /tmp/ci-azr-k8s-operator.json -r)
+CLIENT_SECRET=$(jq .password /tmp/ci-azr-k8s-operator.json -r)
+TENANT_ID=$(jq .tenant /tmp/ci-azr-k8s-operator.json -r)
+az login --service-principal -u "${CLIENT_ID}" -p "${CLIENT_SECRET}" --tenant "${TENANT_ID}"
+
+# Get a list of cluslter names with a `createdTime` < 3 days ago.
+AZURE_CLUSTERS=$(az resource list -l westeurope -g cloudonk8s-dev --resource-type "Microsoft.ContainerService/managedClusters" --query "[?tags.project == 'eck-ci']" | jq -r --arg d $DATE 'map(select(.createdTime | . <= $d))|.[].name')
+
+for i in ${AZURE_CLUSTERS}; do
+    echo "Deleting azure cluster $i"
+    az aks delete -n $i -g cloudonk8s-dev
 done
