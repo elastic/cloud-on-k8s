elastic/cloud-on-k8s

commit e242ca5045fc63bde358c5cb383153bea31861e9

Multi-arch images build via drivah and serverless-docker-builder (#6959)
This change revisits the build of ECK's container images by making them drivah-able and use the `serverless-docker-builder`.

The main thing is:
```yaml
  - group: build operator
    key: "operator-image-build"
    steps:
      - label: ":buildkite:"
        agents:
          image: "docker.elastic.co/ci-agent-images/serverless-docker-builder:0.0.5"
        commands: make -C /agent generate-docker-images
        env:
          DOCKER_REGISTRY_VAULT_PATH: secret/ci/elastic-cloud-on-k8s/docker-registry-elastic
          PRE_BUILD_SCRIPT: ".buildkite/scripts/build/pre-build-operator.sh"
          DRIVAH_BUILD_PATH: "build"
          RECURSIVE: true

  - group: build e2e-tests
    key: "e2e-tests-image-build"
    steps:
      - label: ":buildkite:"
        agents:
          image: "docker.elastic.co/ci-agent-images/serverless-docker-builder:0.0.5"
        commands: make -C /agent generate-docker-images
        env:
          DOCKER_REGISTRY_VAULT_PATH: secret/ci/elastic-cloud-on-k8s/docker-registry-elastic
          DRIVAH_BUILD_PATH: "test/e2e"
```

The `serverless-docker-builder` agent step generates 3 steps to build the arm and the amd images and push the manifest. It executes `drivah build $DRIVAH_BUILD_PATH --push` which discovers all `Dockerfile` and `drivah.toml*` in the given path and build the associated container images using the buildah engine, in a `k8s` agent for amd and a `vm` agent for arm.
This means that we don't have `make` or `go` when `drivah build`.

In order to be able to use drivah to build eck, we now fetch the license public key first and then we `go-generate` in Docker during the build.

I made that we can still invoke the build of images from the `Makefile` to dev purposes.

```sh
# to install drivah (requires python 3.9+, git, buildah or docker)
pip install git+ssh://git@github.com/elastic/drivah@0.24.0
```

### e2e-tests image

build/Dockerfile
@@ -1,28 +1,33 @@
 # Build the operator binary
-FROM --platform=$TARGETPLATFORM docker.io/library/golang:1.20.5 as builder
+FROM docker.io/library/golang:1.20.5 as builder
 
-ARG TARGETPLATFORM
-ARG BUILDPLATFORM
-ARG GO_LDFLAGS
+ARG VERSION
+ARG SHA1
+ARG SNAPSHOT
 ARG GO_TAGS
+ARG LICENSE_PUBKEY_PATH
+
+ENV LICENSE_PUBKEY=/license.key
+
 WORKDIR /go/src/github.com/elastic/cloud-on-k8s
 
-# cache deps before building and copying source so that we don't need to re-download as much
-# and so that source changes don't invalidate our downloaded layer
-COPY ["go.mod", "go.sum", "./"]
+# Cache go dependencies
+COPY Makefile go.mod go.sum ./
 RUN --mount=type=cache,mode=0755,target=/go/pkg/mod go mod download
 
-# Copy the go source
-COPY pkg/    pkg/
-COPY cmd/    cmd/
+# Copy default eck config
+COPY config/eck.yaml .
+
+# Copy the sources
+COPY pkg/ pkg/
+COPY cmd/ cmd/
+COPY $LICENSE_PUBKEY_PATH /license.key
 
 # Build
-RUN --mount=type=cache,mode=0755,target=/go/pkg/mod CGO_ENABLED=0 GOOS=linux \
-      go build \
-      -mod readonly \
-      -ldflags "$GO_LDFLAGS" -tags="$GO_TAGS" -a \
-      -o elastic-operator github.com/elastic/cloud-on-k8s/v2/cmd
+RUN --mount=type=cache,mode=0755,target=/go/pkg/mod \
+      CGO_ENABLED=0 GOOS=linux LICENSE_PUBKEY=/license.key make go-build
 
+# ---------------------------------------------
 # Copy the operator binary into a lighter image
 FROM gcr.io/distroless/static:nonroot
 
@@ -43,7 +48,7 @@ LABEL io.k8s.description="Elastic Cloud on Kubernetes automates the deployment, 
       org.opencontainers.image.url="https://github.com/elastic/cloud-on-k8s/"
 
 COPY --from=builder /go/src/github.com/elastic/cloud-on-k8s/elastic-operator /elastic-operator
-COPY config/eck.yaml /conf/eck.yaml
+COPY --from=builder /go/src/github.com/elastic/cloud-on-k8s/eck.yaml         /conf/eck.yaml
 
 # Copy NOTICE.txt and LICENSE.txt into the image
 COPY *.txt /licenses/

build/Dockerfile-ubi
@@ -1,28 +1,33 @@
 # Build the operator binary
-FROM --platform=$TARGETPLATFORM docker.io/library/golang:1.20.5 as builder
+FROM docker.io/library/golang:1.20.5 as builder
 
-ARG TARGETPLATFORM
-ARG BUILDPLATFORM
-ARG GO_LDFLAGS
+ARG VERSION
+ARG SHA1
+ARG SNAPSHOT
 ARG GO_TAGS
+ARG LICENSE_PUBKEY_PATH
+
+ENV LICENSE_PUBKEY=/license.key
+
 WORKDIR /go/src/github.com/elastic/cloud-on-k8s
 
-# cache deps before building and copying source so that we don't need to re-download as much
-# and so that source changes don't invalidate our downloaded layer
-COPY ["go.mod", "go.sum", "./"]
-RUN go mod download
+# Cache go dependencies
+COPY Makefile go.mod go.sum ./
+RUN --mount=type=cache,mode=0755,target=/go/pkg/mod go mod download
 
-# Copy the go source
-COPY pkg/    pkg/
-COPY cmd/    cmd/
+# Copy default eck config
+COPY config/eck.yaml .
+
+# Copy the sources
+COPY $LICENSE_PUBKEY_PATH /license.key
+COPY pkg/ pkg/
+COPY cmd/ cmd/
 
 # Build
-RUN CGO_ENABLED=0 GOOS=linux \
-      go build \
-      -mod readonly \
-      -ldflags "$GO_LDFLAGS" -tags="$GO_TAGS" -a \
-      -o elastic-operator github.com/elastic/cloud-on-k8s/v2/cmd
+RUN --mount=type=cache,mode=0755,target=/go/pkg/mod \
+      CGO_ENABLED=0 GOOS=linux make go-build
 
+# ---------------------------------------------
 # Copy the operator binary into a lighter image
 FROM registry.access.redhat.com/ubi8/ubi-minimal:8.8-1014
 
@@ -43,7 +48,7 @@ LABEL name="Elastic Cloud on Kubernetes" \
       io.k8s.description="Elastic Cloud on Kubernetes automates the deployment, provisioning, management, and orchestration of Elasticsearch, Kibana, APM Server, Beats, and Enterprise Search on Kubernetes"
 
 COPY --from=builder /go/src/github.com/elastic/cloud-on-k8s/elastic-operator .
-COPY config/eck.yaml /conf/eck.yaml
+COPY --from=builder /go/src/github.com/elastic/cloud-on-k8s/eck.yaml         /conf/eck.yaml
 
 # Copy NOTICE.txt and LICENSE.txt into the image
 COPY *.txt /licenses/

build/gen-drivah.toml.sh
@@ -1,0 +1,110 @@
+#!/bin/bash
+
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+
+# Prepare Dockerfile(s) and drivah.toml(s) to build ECK operator container image(s).
+
+set -euo pipefail
+
+HERE="$(cd "$(dirname "$0")"; pwd)"
+ROOT="$HERE/.."
+
+source "$ROOT/.buildkite/scripts/common/lib.sh"
+
+retry() { "$ROOT/hack/retry.sh" 5 "$@"; }
+
+: "$IMAGE_NAME"
+: "$IMAGE_TAG"
+: "$BUILD_FLAVORS"
+
+ARCH=$(common::arch)
+SHA1=$(common::sha1)
+VERSION=$(common::version)
+
+SNAPSHOT=true
+GO_TAGS=${GO_TAGS-release}
+LICENSE_PUBKEY=license.key
+
+generate_drivah_config() {
+    local name=$1
+    local tag=$2
+    local go_tags=$3
+    local license_pub_key=$4
+cat <<END
+[container.image]
+names = ["${name}"]
+tags = ["${tag}-${ARCH}"]
+build_context = "../../"
+
+[container.image.build_args]
+VERSION = "${VERSION}"
+SHA1 = "${SHA1}"
+GO_TAGS = "${go_tags}"
+SNAPSHOT = "${SNAPSHOT}"
+LICENSE_PUBKEY_PATH = "build/$license_pub_key"
+END
+}
+
+main() {
+    echo "# -- gen-drivah-config BUILD_FLAVORS=$BUILD_FLAVORS"
+
+    # disable SNAPSHOT for tags
+    tag_pattern="^[0-9]+\.[0-9]+\.[0-9]+"
+    if [[ "$IMAGE_TAG"  =~ $tag_pattern ]]; then
+        SNAPSHOT=false
+    fi
+
+    # delete only dirs
+    find "$HERE" -maxdepth 1 -mindepth 1 -type d -exec rm -rf '{}' \;
+
+    IFS=","; for flavor in $BUILD_FLAVORS; do
+
+        # default vars reset at each iteration
+        container_file_path=$HERE/Dockerfile
+        go_tags=$GO_TAGS
+        license_pubkey=$LICENSE_PUBKEY
+
+        name="$IMAGE_NAME"
+        tag="$IMAGE_TAG"
+
+         # dev build without public license key
+        if [[ "$flavor" == "dev" ]]; then
+            go_tags=
+            echo "fake empty license" > "$HERE/$LICENSE_PUBKEY"
+        fi
+        # DEV license public key build
+        if [[ "$flavor" =~ -dev ]]; then
+                tag="$tag-dev"
+                license_pubkey=dev-license.key
+                BUILD_LICENSE_PUBKEY=dev
+        fi
+        # UBI8 build
+        if [[ "$flavor" =~ -ubi8 ]]; then
+                name="$name-ubi8"
+                container_file_path=$HERE/Dockerfile-ubi
+        fi
+        # FIPS build
+        if [[ "$flavor" =~ -fips ]]; then
+                name="$name-fips"
+                go_tags="$go_tags,goexperiment.boringcrypto"
+        fi
+
+        # fetch public license key
+        if [[ ! -f "$HERE/$license_pubkey" ]]; then
+            prefix="${BUILD_LICENSE_PUBKEY:+$BUILD_LICENSE_PUBKEY-}" # add "-" suffix
+            retry vault read -field="${prefix}pubkey" secret/ci/elastic-cloud-on-k8s/license \
+                | base64 --decode > "$HERE/$license_pubkey"
+        fi
+
+        # generate drivah.toml and copy Dockerfile
+        echo "# -- build: $name:$tag"
+        mkdir -p "$HERE/$flavor"
+        generate_drivah_config "$name" "$tag" "$go_tags" "$license_pubkey" > "$HERE/$flavor/drivah.toml"
+        cp -f "$container_file_path" "$HERE/$flavor/Dockerfile"
+
+    done
+}
+
+main

test/e2e/Dockerfile
@@ -1,13 +1,11 @@
 # Docker image for the E2E tests runner
-FROM --platform=$TARGETPLATFORM docker.io/library/golang:1.20.5
+FROM docker.io/library/golang:1.20.5
 
-ARG TARGETPLATFORM
-ARG BUILDPLATFORM
-
-ENV ECK_DIAG_VERSION=1.4.0
+ARG GO_TAGS
 
 WORKDIR /go/src/github.com/elastic/cloud-on-k8s
 
+ENV ECK_DIAG_VERSION=1.4.0
 RUN curl -fsSLO https://github.com/elastic/eck-diagnostics/releases/download/${ECK_DIAG_VERSION}/eck-diagnostics_${ECK_DIAG_VERSION}_Linux_x86_64.tar.gz && \
     tar xzf eck-diagnostics_${ECK_DIAG_VERSION}_Linux_x86_64.tar.gz && \
     mv eck-diagnostics /usr/local/bin/eck-diagnostics
@@ -15,17 +13,22 @@ RUN curl -fsSLO https://github.com/elastic/eck-diagnostics/releases/download/${E
 # create the go test cache directory
 RUN mkdir -p /.cache && chmod 777 /.cache
 
-# non-root user to support restricted security policies
-RUN chown -R 1001 /go/src/github.com/elastic/cloud-on-k8s
-USER 1001
-
-# download go dependencies
-COPY ["go.mod", "go.sum","./"]
+# cache go dependencies
+COPY Makefile go.mod go.sum ./
 RUN go mod download
 
 # copy sources
 COPY pkg/ pkg/
+COPY cmd/ cmd/
 COPY config/ config/
 COPY test/ test/
+
+# non-root user to support restricted security policies
+RUN chown -R 1001 /go/src/github.com/elastic/cloud-on-k8s
+USER 1001
+
+# go generate pub key
+COPY test/e2e/license.key /license.key
+RUN LICENSE_PUBKEY=/license.key go generate -tags="${GO_TAGS}" ./pkg/... ./cmd/...
 
 ENTRYPOINT ["test/e2e/run.sh"]

.buildkite/e2e/pipeline-gen/main.go
@@ -356,11 +356,11 @@ func commonTestEnv(name string, slugName string, stackVersion string) (map[strin
 
 	operatorImage, err := getMetadata("operator-image")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to get operator-image metadata: %w", err)
 	}
 	e2eImage, err := getMetadata("e2e-image")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to get e2e-image metadata: %w", err)
 	}
 
 	env := map[string]string{

.buildkite/e2e/pipeline-gen/pipeline.tpl.yaml
@@ -8,6 +8,7 @@ steps:
 
   {{- if $test.RemoteKubeconfig }}
 
+  # create k8s cluster
   - label: ":k8s: {{ $test.Name }}"
     key: "e2e-cluster-{{ $test.SlugName }}"
 
@@ -36,17 +37,14 @@ steps:
 
   {{- end }}
 
+  # create k8s cluster and run e2e tests, or just run e2e tests (if RemoteKubeconfig)
   {{- if $test.RemoteKubeconfig }}
   - label: ":go: {{ $test.Name }}"
   {{- else }}
   - label: ":k8s: :go: {{ $test.Name }}"
   {{- end }}
     key: "e2e-{{ $test.SlugName }}"
-
-    {{- if $test.RemoteKubeconfig }}
-    depends_on:
-      - "e2e-cluster-{{ $test.SlugName }}"
-    {{- end }}
+    # let's be optimistic and run the tests without dependency on the images build
 
     env:
       {{- range $key, $val := $test.Env }}
@@ -98,6 +96,7 @@ steps:
 {{- range $test := .Tests }}
     {{- if $test.Cleanup }}
 
+      # delete k8s cluster
       - label: ":broom: {{ $test.Name }}"
         depends_on:
           - step: "e2e-{{ $test.SlugName }}"
@@ -123,14 +122,15 @@ steps:
           automatic:
             - limit: 5
 
-
     {{- end }}
 {{- end }}
 
-  - wait: ~
-    continue_on_failure: true
-
   - label: "aggregate tests-results"
+    depends_on:
+      {{- range $test := .Tests }}
+      - step: "e2e-{{ $test.SlugName }}"
+        allow_failure: true
+      {{- end }}
     command: .buildkite/scripts/test/report-tests-results.sh
     artifact_paths:
       - ".buildkite/e2e/reporter/*.md"

.buildkite/pipeline-build.yml
@@ -1,69 +1,0 @@
-steps:
-  - group: build
-    steps:
-
-      # multiarch dockerx build for !PR branch
-      - label: ":docker: multiarch operator image"
-        key: "operator-image-build"
-        commands:
-          - .buildkite/scripts/build/setenv.sh
-          - make -C .buildkite TARGET="generate-manifests publish-operator-multiarch-image" ci
-          # save image for e2e-tests
-          - buildkite-agent meta-data set operator-image $$(make print-operator-image)
-        agents:
-          provider: "gcp"
-          image: "family/elastic-buildkite-agent-ubuntu-2004-lts"
-          machineType: "n1-standard-16"
-        artifact_paths:
-          - config/*.yaml
-
-      - label: ":docker: e2e-tests image"
-        key: "e2e-tests-image-build"
-        commands:
-          - .buildkite/scripts/build/setenv.sh
-          - make -C .buildkite TARGET="publish-e2e-multiarch-image" ci
-          # save image for e2e-tests
-          - buildkite-agent meta-data set e2e-image $$(make print-e2e-image)
-        agents:
-          provider: "gcp"
-          image: "family/elastic-buildkite-agent-ubuntu-2004-lts"
-          machineType: "n1-standard-16"
-
-      # dev-license build for main nightly builds and tags (used in release build candidates)
-      - label: ":docker: dev-license operator image"
-        if: |
-          build.tag != null
-          || ( build.branch == "main" && build.source == "schedule" )
-          || build.message =~ /^buildkite test .*dev-build.*/
-          || build.env("GITHUB_PR_TRIGGER_COMMENT") =~ /s=[0-9\.]*-SNAPSHOT/
-        key: "dev-operator-image-build"
-        env:
-          BUILD_LICENSE_PUBKEY: "dev"
-          OPERATOR_VERSION_SUFFIX: "dev"
-        commands:
-          - .buildkite/scripts/build/setenv.sh
-          - make -C .buildkite TARGET="publish-operator-multiarch-image" ci
-        agents:
-          provider: "gcp"
-          image: "family/elastic-buildkite-agent-ubuntu-2004-lts"
-          machineType: "n1-standard-16"
-
-      # fips build for tag builds
-      - label: ":docker: fips operator image"
-        if: |
-          build.tag != null 
-          || build.message =~ /^buildkite test .*release.*/
-        env:
-          ENABLE_FIPS: "true"
-        commands:
-          - .buildkite/scripts/build/setenv.sh
-          - make -C .buildkite TARGET="publish-operator-multiarch-image" ci
-        agents:
-          provider: "gcp"
-          image: "family/elastic-buildkite-agent-ubuntu-2004-lts"
-          machineType: "n1-standard-16"
-
-      - label: ":go: helm releaser tool"
-        commands:
-          - cd hack/helm/release
-          - make build

.buildkite/pipeline-e2e-tests.yml
@@ -3,45 +3,12 @@ steps:
   - group: "e2e-tests"
     steps:
 
-      # for commits in all branches (PR, release) except main
-      - label: ":buildkite:"
-        depends_on:
-          - "operator-image-build"
-          - "e2e-tests-image-build"
-        if: |
-            ( build.branch != "main" )
-            && build.env("GITHUB_PR_TRIGGER_COMMENT") !~ /^buildkite test this -[fm]/
-            && ( build.message !~ /^buildkite test .*e2e.*/ || build.message !~ /^buildkite test .*release.*/ )
-        command: |
-          cd .buildkite/e2e/pipeline-gen && go build -o pipeline-gen
-          cat <<DEF | ./pipeline-gen | buildkite-agent pipeline upload
-          - label: "kind/TestSmoke"
-            fixed:
-              E2E_PROVIDER: kind
-              TESTS_MATCH: TestSmoke
-              E2E_TAGS: "e2e,es,kb"
-          DEF
-
-      # for PR comment
-      - label: ":buildkite:"
-        depends_on:
-          - "operator-image-build"
-          - "e2e-tests-image-build"
-        if: build.env("GITHUB_PR_TRIGGER_COMMENT") =~ /^buildkite test this -[fm]/
-        command: |
-          cd .buildkite/e2e/pipeline-gen && go build -o pipeline-gen
-          $$(echo ./pipeline-gen $$GITHUB_PR_COMMENT_VAR_ARGS) \
-            | buildkite-agent pipeline upload
-
       # for commits in main
       - label: ":buildkite: e2e"
-        depends_on:
-          - "operator-image-build"
-          - "e2e-tests-image-build"
         if: |
-          ( build.branch == "main" && build.source == "webhook" )
-          || build.message =~ /^buildkite test .*e2e\/main.*/
+          build.branch == "main" && build.source == "webhook"
         command: |
+          .buildkite/scripts/build/set-images.sh
           cd .buildkite/e2e/pipeline-gen && go build -o pipeline-gen
           cat <<DEF | ./pipeline-gen | buildkite-agent pipeline upload
           - label: gke/all
@@ -52,13 +19,11 @@ steps:
       # for all tags
       # for nightly builds from main
       - label: ":buildkite: e2e"
-        depends_on:
-          - "operator-image-build"
-          - "e2e-tests-image-build"
         if: |
           build.tag != null
           || ( build.branch == "main" && build.source == "schedule" )
           || build.message =~ /^buildkite test .*e2e\/all.*/
         command: |
+          .buildkite/scripts/build/set-images.sh
           cd .buildkite/e2e/pipeline-gen && go build -o pipeline-gen
           cat ../nightly-main-matrix.yaml | ./pipeline-gen | buildkite-agent pipeline upload

commit 68ae19c25481c3295bb20ef8578857278d882405

Make 'RUN go generate' specific between prod and dev builds (#6980)
This fixes a regression introduced in #6959, where the `eck` flavor build uses the layer that generates the public key of the `eck-dev` flavor build, using the specific name of the public license key in the `RUN go-generate` instruction.

build/Dockerfile
@@ -5,9 +5,7 @@ ARG VERSION
 ARG SHA1
 ARG SNAPSHOT
 ARG GO_TAGS
-ARG LICENSE_PUBKEY_PATH
-
-ENV LICENSE_PUBKEY=/license.key
+ARG LICENSE_PUBKEY
 
 WORKDIR /go/src/github.com/elastic/cloud-on-k8s
 
@@ -21,11 +19,11 @@ COPY config/eck.yaml .
 # Copy the sources
 COPY pkg/ pkg/
 COPY cmd/ cmd/
-COPY $LICENSE_PUBKEY_PATH /license.key
+COPY build/$LICENSE_PUBKEY /$LICENSE_PUBKEY
 
 # Build
 RUN --mount=type=cache,mode=0755,target=/go/pkg/mod \
-      CGO_ENABLED=0 GOOS=linux LICENSE_PUBKEY=/license.key make go-build
+      CGO_ENABLED=0 GOOS=linux LICENSE_PUBKEY=/$LICENSE_PUBKEY make go-build
 
 # ---------------------------------------------
 # Copy the operator binary into a lighter image

build/Dockerfile-ubi
@@ -5,9 +5,7 @@ ARG VERSION
 ARG SHA1
 ARG SNAPSHOT
 ARG GO_TAGS
-ARG LICENSE_PUBKEY_PATH
-
-ENV LICENSE_PUBKEY=/license.key
+ARG LICENSE_PUBKEY
 
 WORKDIR /go/src/github.com/elastic/cloud-on-k8s
 
@@ -19,13 +17,13 @@ RUN --mount=type=cache,mode=0755,target=/go/pkg/mod go mod download
 COPY config/eck.yaml .
 
 # Copy the sources
-COPY $LICENSE_PUBKEY_PATH /license.key
 COPY pkg/ pkg/
 COPY cmd/ cmd/
+COPY build/$LICENSE_PUBKEY /$LICENSE_PUBKEY
 
 # Build
 RUN --mount=type=cache,mode=0755,target=/go/pkg/mod \
-      CGO_ENABLED=0 GOOS=linux make go-build
+      CGO_ENABLED=0 GOOS=linux LICENSE_PUBKEY=/$LICENSE_PUBKEY make go-build
 
 # ---------------------------------------------
 # Copy the operator binary into a lighter image

build/gen-drivah.toml.sh
@@ -31,7 +31,7 @@ generate_drivah_config() {
     local name=$1
     local tag=$2
     local go_tags=$3
-    local license_pub_key=$4
+    local license_pubkey=$4
 cat <<END
 [container.image]
 names = ["${name}"]
@@ -43,7 +43,7 @@ VERSION = "${VERSION}"
 SHA1 = "${SHA1}"
 GO_TAGS = "${go_tags}"
 SNAPSHOT = "${SNAPSHOT}"
-LICENSE_PUBKEY_PATH = "build/$license_pub_key"
+LICENSE_PUBKEY = "$license_pubkey"
 END
 }
 