32b52c6c7538e1c0cb358577fcfe87bb177a0f3d
Kaise Cheng <kaise.cheng@elastic.co>
2023-07-14 18:14:34 +0100

    add comment

--- pkg/controller/logstash/keystore.go
+++ pkg/controller/logstash/keystore.go
@@ -53,7 +53,7 @@ func reconcileKeystore(params Params, configHash hash.Hash) (*keystore.Resources
 		return nil, err
 	} else if keystoreResources != nil {
 		_, _ = configHash.Write([]byte(keystoreResources.Version))
-		// Logstash requires keystore password in environment variable
+		// Logstash requires keystore password in environment variable to bypass the prompt
 		keystoreResources.InitContainer.Env = append(keystoreResources.InitContainer.Env, getKeystorePass(params.Logstash))
 		return keystoreResources, nil
 	}
