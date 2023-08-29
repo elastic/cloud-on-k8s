87af2e65539a2e1f4f8f7c120d3bbb8cba5d4aba
Thibault Richard <thb.krkr@gmail.com>
2023-08-29 15:15:35 +0200

    Refactoring metrics options

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -35,10 +35,12 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	crwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
@@ -574,7 +576,9 @@ func startOperator(ctx context.Context) error {
 	if metricsPort != 0 {
 		log.Info("Exposing Prometheus metrics on /metrics", "port", metricsPort)
 	}
-	opts.MetricsBindAddress = fmt.Sprintf(":%d", metricsPort) // 0 to disable
+	opts.Metrics = metricsserver.Options{
+		BindAddress: fmt.Sprintf(":%d", metricsPort), // 0 to disable
+	}
 
 	webhookPort := viper.GetInt(operator.WebhookPortFlag)
 	webhookCertDir := viper.GetString(operator.WebhookCertDirFlag)
