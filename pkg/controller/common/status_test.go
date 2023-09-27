eed31fb515591b5f3e740f419b76fc94e950a934
Michael Morello <michael.morello@elastic.co>
2023-09-27 09:19:21 +0200

    lint

--- pkg/controller/common/status_test.go
+++ pkg/controller/common/status_test.go
@@ -9,10 +9,11 @@ import (
 	"reflect"
 	"testing"
 
-	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 )
 
 func TestLowestVersionFromPods(t *testing.T) {
