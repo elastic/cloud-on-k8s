dfb6f7d36f1a96158a20513767e18edef24560b5
Michael Montgomery <mmontg1@gmail.com>
2023-11-02 09:19:51 -0500

    Also skip 8.11-snapshot on failing test.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- test/e2e/kb/version_upgrade_test.go
+++ test/e2e/kb/version_upgrade_test.go
@@ -178,7 +178,7 @@ func TestVersionUpgradeAndRespecToLatest8x(t *testing.T) {
 	// Skip for 8.10.0-SNAPSHOT because after a few seconds, ES is yellow because of unassigned
 	// fleet-files-agent-000001 and fleet-file-data-agent-000001 shards.
 	// TODO: remove once https://github.com/elastic/cloud-on-k8s/issues/7013 is resolved
-	if test.Ctx().ElasticStackVersion == "8.10.0-SNAPSHOT" {
+	if test.Ctx().ElasticStackVersion == "8.10.0-SNAPSHOT" || test.Ctx().ElasticStackVersion == "8.11.0-SNAPSHOT" {
 		t.SkipNow()
 	}
 
