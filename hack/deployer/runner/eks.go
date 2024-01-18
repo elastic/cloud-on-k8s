de539273aaaf68f55c28a4f49fbdc95f92253df4
Michael Montgomery <mmontg1@gmail.com>
2024-01-18 09:03:12 -0600

    Add --disable-nodegroup-eviction to fix eks cluster deletions failing. (#7481)
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- hack/deployer/runner/eks.go
+++ hack/deployer/runner/eks.go
@@ -283,7 +283,8 @@ func (e *EKSDriver) Cleanup(prefix string, olderThan time.Duration) error {
 func (e *EKSDriver) delete() error {
 	log.Printf("Deleting cluster %s", e.ctx["ClusterName"])
 	// --wait to surface failures to delete all resources in the Cloud formation
-	return e.newCmd("eksctl delete cluster -v 1 --name {{.ClusterName}} --region {{.Region}} --wait").Run()
+	// --disable-nodegroup-eviction to avoid pod disruption budgets causing nodegroup draining to fail.
+	return e.newCmd("eksctl delete cluster -v 1 --name {{.ClusterName}} --region {{.Region}} --wait --disable-nodegroup-eviction").Run()
 }
 
 var _ Driver = &EKSDriver{}
