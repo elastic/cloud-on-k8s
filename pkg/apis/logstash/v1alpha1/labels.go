86d7722b1954a0c12a4b1e5bf511331900ff01d8
Rob Bavey <rob.bavey@elastic.co>
2024-02-02 15:03:06 -0500

    Code review: Use getIdentityLabels to populate GetPodIdentityLabels

--- pkg/apis/logstash/v1alpha1/labels.go
+++ pkg/apis/logstash/v1alpha1/labels.go
@@ -6,6 +6,7 @@ package v1alpha1
 
 import (
 	commonv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
+	"github.com/elastic/cloud-on-k8s/v2/pkg/utils/maps"
 )
 
 // GetIdentityLabels will return the common Elastic assigned labels for Logstash
@@ -18,9 +19,7 @@ func (logstash *Logstash) GetIdentityLabels() map[string]string {
 
 // GetPodIdentityLabels will return the common Elastic assigned labels for a Logstash Pod
 func (logstash *Logstash) GetPodIdentityLabels() map[string]string {
-	return map[string]string{
-		commonv1.TypeLabelName:                     "logstash",
-		"logstash.k8s.elastic.co/name":             logstash.Name,
+	return maps.Merge(logstash.GetIdentityLabels(), map[string]string{
 		"logstash.k8s.elastic.co/statefulset-name": Name(logstash.Name),
-	}
+	})
 }
