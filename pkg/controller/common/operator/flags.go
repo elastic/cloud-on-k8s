81df44189ef54bc3e5afc77144fc25b862d1d274
Michael Montgomery <mmontg1@gmail.com>
2024-03-07 08:44:16 -0600

    Add RBAC authentication option for the ECK metrics endpoint. (#7567)
    * Add RBAC+TLS options to the ECK metrics endpoint.
    * Add documentation for RBAC/TLS ECK metrics features.
    
    ---------
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- pkg/controller/common/operator/flags.go
+++ pkg/controller/common/operator/flags.go
@@ -33,6 +33,7 @@ const (
 	ManageWebhookCertsFlag               = "manage-webhook-certs"
 	MaxConcurrentReconcilesFlag          = "max-concurrent-reconciles"
 	MetricsPortFlag                      = "metrics-port"
+	MetricsHostFlag                      = "metrics-host"
 	NamespacesFlag                       = "namespaces"
 	OperatorNamespaceFlag                = "operator-namespace"
 	SetDefaultSecurityContextFlag        = "set-default-security-context"
