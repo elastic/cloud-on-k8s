b271388450b7f88abcadccac3979f1d3e6aa99ab
Karthikeyan Valliyurnatt <karthikeyan.valliyurnatt@elastic.co>
2024-05-09 13:20:27 -0400

    increase default memory for agent

--- pkg/controller/agent/pod.go
+++ pkg/controller/agent/pod.go
@@ -92,11 +92,11 @@ const (
 var (
 	defaultResources = corev1.ResourceRequirements{
 		Limits: map[corev1.ResourceName]resource.Quantity{
-			corev1.ResourceMemory: resource.MustParse("350Mi"),
+			corev1.ResourceMemory: resource.MustParse("400Mi"),
 			corev1.ResourceCPU:    resource.MustParse("200m"),
 		},
 		Requests: map[corev1.ResourceName]resource.Quantity{
-			corev1.ResourceMemory: resource.MustParse("350Mi"),
+			corev1.ResourceMemory: resource.MustParse("400Mi"),
 			corev1.ResourceCPU:    resource.MustParse("200m"),
 		},
 	}
