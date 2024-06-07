f038740b53396b492691050e6a5a8e452a3aa316
Karthikeyan Valliyurnatt <karthikeyan.valliyurnatt@elastic.co>
2024-06-07 15:51:11 -0400

    reverting LatestReleasedVersion8x in e2e test to 8.13.2

--- test/e2e/test/version.go
+++ test/e2e/test/version.go
@@ -16,9 +16,9 @@ const (
 	// LatestReleasedVersion6x is the latest released version for 6.x
 	LatestReleasedVersion6x = "6.8.23"
 	// LatestReleasedVersion7x is the latest released version for 7.x
-	LatestReleasedVersion7x = "7.17.8"
+	LatestReleasedVersion7x = "7.17.21"
 	// LatestReleasedVersion8x is the latest release version for 8.x
-	LatestReleasedVersion8x = "8.14.0"
+	LatestReleasedVersion8x = "8.13.2"
 )
 
 // SkipInvalidUpgrade skips a test that would do an invalid upgrade.
