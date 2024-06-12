9e2205892f01e1e9d31f551de9020bac802555d6
dbxbbm <bob.van.bokkem@kvk.nl>
2024-06-12 08:14:53 +0200

    fix: add working version with alot of debug info

--- pkg/controller/elasticsearch/nodespec/lifecycle_hook.go
+++ pkg/controller/elasticsearch/nodespec/lifecycle_hook.go
@@ -29,7 +29,8 @@ const PreStopHookScriptConfigKey = "pre-stop-hook-script.sh"
 var preStopHookScriptTemplate = template.Must(template.New("pre-stop").Parse(`#!/usr/bin/env bash
 # shellcheck disable=SC1083  # remove errors from golang templating
 
-set -uo pipefail
+#set -uo pipefail
+set -o pipefail
 
 # This script will wait for up to $PRE_STOP_ADDITIONAL_WAIT_SECONDS before allowing termination of the Pod
 # This slows down the process shutdown and allows to make changes to the pool gracefully, without blackholing traffic when DNS
@@ -51,7 +52,7 @@ shutdown_type=${PRE_STOP_SHUTDOWN_TYPE:=restart}
 # capture response bodies in a temp file for better error messages and to extract necessary information for subsequent requests
 resp_body=$(mktemp)
 # shellcheck disable=SC2064
-trap "rm -f $resp_body" EXIT
+# trap "rm -f $resp_body" EXIT
 
 script_start=$(date +%s)
 
@@ -70,8 +71,12 @@ global_dns_error_cnt=0
 
 function request() {
   local status exit
-  status=$(curl -k -sS -o "$resp_body" -w "%{http_code}" "$@")
+  echo "$*" >> /tmp/curl.log
+  status=$(curl -k -sS -o "${resp_body}" -w "%{http_code}" "$@")
   exit=$?
+  echo "status: ${exit}"
+  curl -k -sS "$@" >> /tmp/curl.log
+  echo "" >> /tmp/curl.log
   if [ "$exit" -ne 0 ] || [ "$status" -lt 200 ] || [ "$status" -gt 299 ]; then
     # track curl DNS errors separately
     if [ "$exit" -eq 6 ]; then ((global_dns_error_cnt++)); fi
@@ -169,30 +174,43 @@ fi
 
 ES_URL={{.ServiceURL}}
 
-log "retrieving node ID"
-if ! retry 8 request -X GET "${ES_URL}/_cat/nodes?full_id=true&h=id,name" ${BASIC_AUTH}
+log "retrieving nodes"
+if ! retry 7 request -X GET "${ES_URL}/_cat/nodes?full_id=true&h=id,name" "${BASIC_AUTH[@]}"
 then
   error_exit "failed to retrieve nodes"
 fi
 
-# check if the pod is present
-if ! NODE_ID=$(grep -oP "\K\w+(?= ${POD_NAME})" "$resp_body")
-  error_exit "failed to retrieve node ID"
+if [ ! -s "${resp_body}" ]; then
+  error_exit "response is empty"
 fi
 
-# check if there is an ongoing shutdown request
-if request -X GET "${ES_URL}/_nodes/${NODE_ID}/shutdown" ${BASIC_AUTH}
+log "retrieving node id"
+log "resp_body: ${resp_body}"
+NODE_ID="$(grep -oP "\K\w+(?= ${POD_NAME})" "${resp_body}")"
+return_code=$?
+if [ "${return_code}" -gt 0 ] && [[ -z "${NODE_ID}" ]]
+then
+  log "failed to retrieve node id ($return_code)"
+  error_exit "failed to retrieve node id"
+else
+  log "NODE_ID: ${NODE_ID}"
+  log "success to retrieve node id"
+fi
+
+log "retrieving shutdown request"
+if ! request -X GET "${ES_URL}/_nodes/${NODE_ID}/shutdown" "${BASIC_AUTH[@]}"
 then
   error_exit "failed to retrieve shutdown status"
 fi
 
+log "check shutdown response"
 if grep -q -v '"nodes":\[\]' "$resp_body"; then
   log "shutdown managed by ECK operator"
   delayed_exit
 fi
 
 log "initiating node shutdown"
-if retry 8 request -X PUT "${ES_URL}/_nodes/${NODE_ID}/shutdown" ${BASIC_AUTH} -H 'Content-Type: application/json' -d"
+if ! retry 7 request -X PUT "${ES_URL}/_nodes/${NODE_ID}/shutdown" "${BASIC_AUTH[@]}" -H 'Content-Type: application/json' -d"
 {
   \"type\": \"${shutdown_type}\",
   \"reason\": \"pre-stop hook\"
@@ -204,7 +222,7 @@ fi
 while :
 do
   log "waiting for node shutdown to complete"
-  if request -X GET "${ES_URL}/_nodes/${NODE_ID}/shutdown" ${BASIC_AUTH} &&
+  if request -X GET "${ES_URL}/_nodes/${NODE_ID}/shutdown" "${BASIC_AUTH[@]}" &&
     grep -q -v 'IN_PROGRESS\|STALLED' "$resp_body"
   then
     break
