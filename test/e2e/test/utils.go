5dee506e474d4927f549293dd1ba0dc36422906d
Michael Morello <michael.morello@elastic.co>
2025-01-06 08:26:40 +0100

    Add comment

--- test/e2e/test/utils.go
+++ test/e2e/test/utils.go
@@ -214,6 +214,7 @@ func deleteTestResources(ctx context.Context) error {
 		}
 	}
 
+	// Dynamic client must be built from the configuration, not by reusing the existing REST client using clntset.RESTClient()
 	dynamicClient, err := dynamic.NewForConfig(cfg)
 	if err != nil {
 		return fmt.Errorf("while creating dynamic client to delete resources: %v", err)
