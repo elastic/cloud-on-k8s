6d8c39081d4700b3aaa992337a0ab7804e39b813
Michael Montgomery <mmontg1@gmail.com>
2025-01-09 10:30:00 -0600

    Adjust namers test to include all namers
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- pkg/apis/kibana/v1/name_test.go
+++ pkg/apis/kibana/v1/name_test.go
@@ -10,49 +10,51 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestHTTPService(t *testing.T) {
-	type args struct {
-		kbName string
-	}
+func TestNamers(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-		want string
+		name  string
+		namer any
+		arg   any
+		want  string
 	}{
 		{
-			name: "sample",
-			args: args{kbName: "sample"},
-			want: "sample-kb-http",
+			name:  "test httpService namer",
+			namer: HTTPService,
+			arg:   "sample",
+			want:  "sample-kb-http",
+		},
+		{
+			name:  "test deployment namer",
+			namer: Deployment,
+			arg:   "sample",
+			want:  "sample-kb",
+		},
+		{
+			name:  "test scripts configmap namer",
+			namer: ScriptsConfigMap,
+			arg:   "sample",
+			want:  "sample-kb-scripts",
+		},
+		{
+			name:  "test ConfigSecret namer",
+			namer: ConfigSecret,
+			arg:   Kibana{ObjectMeta: metav1.ObjectMeta{Name: "sample"}},
+			want:  "sample-kb-config",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := HTTPService(tt.args.kbName); got != tt.want {
-				t.Errorf("HTTPService() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestConfigSecret(t *testing.T) {
-	type args struct {
-		kb Kibana
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "sample",
-			args: args{kb: Kibana{ObjectMeta: metav1.ObjectMeta{Name: "sample"}}},
-			want: "sample-kb-config",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ConfigSecret(tt.args.kb); got != tt.want {
-				t.Errorf("ConfigSecret() = %v, want %v", got, tt.want)
+			switch f := tt.namer.(type) {
+			case func(string) string:
+				arg := tt.arg.(string)
+				if got := f(arg); got != tt.want {
+					t.Errorf("%s = %v, want %v", tt.name, got, tt.want)
+				}
+			case func(Kibana) string:
+				arg := tt.arg.(Kibana)
+				if got := f(arg); got != tt.want {
+					t.Errorf("%s = %v, want %v", tt.name, got, tt.want)
+				}
 			}
 		})
 	}
