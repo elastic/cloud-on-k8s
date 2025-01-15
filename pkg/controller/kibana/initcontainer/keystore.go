3ee82dfc40455b099c44d72dcfa2af9d85a37c01
Michael Montgomery <mmontg1@gmail.com>
2025-01-15 14:44:50 -0600

    Init container updates for persisting kibana plugins. (#8389) (#8411)
    * Init container for persisting kibana plugins.
    ---------
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    (cherry picked from commit 0efeb943cd04bfaa252f2950a7fff410d40085c4)

--- pkg/controller/kibana/initcontainer/keystore.go
+++ pkg/controller/kibana/initcontainer/keystore.go
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
-package kibana
+package initcontainer
 
 import (
 	corev1 "k8s.io/api/core/v1"
@@ -11,18 +11,19 @@ import (
 	kbv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/kibana/v1"
 	"github.com/elastic/cloud-on-k8s/v2/pkg/controller/common/keystore"
 	"github.com/elastic/cloud-on-k8s/v2/pkg/controller/common/version"
+	"github.com/elastic/cloud-on-k8s/v2/pkg/controller/kibana/volume"
 )
 
 // keystoreInConfigDirVersion is the version in which the keystore is no longer stored in the data directory but in the config one.
 var keystoreInConfigDirVersion = version.From(7, 9, 0)
 
-// newInitContainersParameters is used to generate the init container that will load the secure settings into a keystore
-func newInitContainersParameters(kb *kbv1.Kibana) (keystore.InitContainerParameters, error) {
+// NewInitContainersParameters is used to generate the init container that will load the secure settings into a keystore
+func NewInitContainersParameters(kb *kbv1.Kibana) (keystore.InitContainerParameters, error) {
 	parameters := keystore.InitContainerParameters{
 		KeystoreCreateCommand:         "/usr/share/kibana/bin/kibana-keystore create",
 		KeystoreAddCommand:            `/usr/share/kibana/bin/kibana-keystore add "$key" --stdin < "$filename"`,
 		SecureSettingsVolumeMountPath: keystore.SecureSettingsVolumeMountPath,
-		KeystoreVolumePath:            DataVolumeMountPath,
+		KeystoreVolumePath:            volume.DataVolumeMountPath,
 		Resources: corev1.ResourceRequirements{
 			Requests: map[corev1.ResourceName]resource.Quantity{
 				corev1.ResourceMemory: resource.MustParse("128Mi"),
---
+++ pkg/controller/kibana/volume/volume.go
@@ -0,0 +1,42 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package volume
+
+const (
+	// DataVolumeName is the name of the volume that holds the Kibana data
+	DataVolumeName = "kibana-data"
+	// DataVolumeMountPath is the path where the Kibana data is mounted in the Kibana container
+	DataVolumeMountPath = "/usr/share/kibana/data"
+	// PluginsVolumeName is the name of the volume that holds the Kibana plugins
+	PluginsVolumeName = "kibana-plugins"
+	// PluginsVolumeMountPath is the path where the Kibana plugins are mounted in the Kibana container
+	PluginsVolumeMountPath = "/usr/share/kibana/plugins"
+	// PluginsVolumeInternalMountPath is the path where the Kibana plugins are mounted in the init container
+	PluginsVolumeInternalMountPath = "/mnt/elastic-internal/kibana-plugins-local"
+	// LogsVolumeName is the name of the volume that holds the Kibana logs
+	LogsVolumeName = "kibana-logs"
+	// LogsVolumeMountPath is the path where the Kibana logs are mounted in the Kibana container
+	LogsVolumeMountPath = "/usr/share/kibana/logs"
+	// TempVolumeName is the name of the volume that holds the temporary files
+	TempVolumeName = "temp-volume"
+	// TempVolumeMountPath is the path where the temporary files are mounted in the Kibana container
+	TempVolumeMountPath = "/tmp"
+	// ScriptsVolumeName is the name of the volume that holds the Kibana scripts for the init container
+	ScriptsVolumeName = "kibana-scripts"
+	// ScriptsVolumeMountPath is the path where the Kibana scripts are mounted in the init container
+	ScriptsVolumeMountPath = "/mnt/elastic-internal/scripts"
+	// InitConfigContainerName is the name of the container that initializes the configuration
+	InitContainerName = "elastic-internal-init"
+	// ConfigVolumeName is the name of the volume that holds the Kibana configuration
+	ConfigVolumeName = "elastic-internal-kibana-config-local"
+	// ConfigVolumeMountPath is the path where the Kibana configuration is mounted in the Kibana container
+	ConfigVolumeMountPath = "/usr/share/kibana/config"
+	// InitContainerConfigVolumeMountPath is the path where the Kibana configuration is mounted in the init container
+	InitContainerConfigVolumeMountPath = "/mnt/elastic-internal/kibana-config-local"
+	// InternalConfigVolumeName is a volume which contains the generated configuration.
+	InternalConfigVolumeName = "elastic-internal-kibana-config"
+	// InternalConfigVolumeMountPath is the path where the generated configuration is mounted in the Kibana init container
+	InternalConfigVolumeMountPath = "/mnt/elastic-internal/kibana-config"
+)
