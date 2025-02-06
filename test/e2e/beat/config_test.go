0885e921ec44b8cd04654524ecff0740562aa7ce
Peter Brachwitz <peter.brachwitz@elastic.co>
2025-02-06 13:24:32 +0100

    Remove remaining references to deprecated container input in Beats (#8456)
    Follow up to #8455
    
    Replaces all the references to the container input type with the new syntax based on filestream taken from elastic/beats#36950
    
    I will follow up on the e2e monitoring config in a separate PR.

--- test/e2e/beat/config_test.go
+++ test/e2e/beat/config_test.go
@@ -211,7 +211,15 @@ filebeat:
         default_config:
           paths:
           - /var/log/containers/*${data.kubernetes.container.id}.log
-          type: container
+          type: filestream
+          id: kubernetes-container-logs-${data.kubernetes.pod.name}-${data.kubernetes.container.id}
+          parsers:
+          - container: ~
+          prospector:
+            scanner:
+              fingerprint.enabled: true
+              symlinks: true
+          file_identity.fingerprint: ~
         enabled: true
       host: ${NODE_NAME}
       type: kubernetes
