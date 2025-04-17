5fe139bbda3fc5222739c9e1011346af3b2180bd
Karthikeyan Valliyurnatt <karthikeyan.valliyurnatt@elastic.co>
2025-04-17 12:36:30 -0400

    add label mappings

--- hack/release-notes/main.go
+++ hack/release-notes/main.go
@@ -21,8 +21,9 @@ const (
 	releaseNotesTemplate = `## Release Notes for {{.Version}} 
 
 {{range $group := .GroupOrder -}}
-{{with (index $.Groups $group)}}
-# {{index $.GroupLabels $group}}
+{{$grouplbl := index $.LabelMapping $group}}
+{{with (index $.Groups $grouplbl)}}
+# {{index $.GroupLabels $grouplbl}}
 {{range .}}
 - {{.Title}} ([#{{.Number}}](https://github.com/{{$.Repo}}/pull/{{.Number}})){{with .Issues -}}
 {{$length := len .}} (issue{{if gt $length 1}}s{{end}}: {{range $idx, $el := .}}{{if $idx}}, {{end}}[#{{$el}}](https://github.com/{{$.Repo}}/issues/{{$el}}){{end}})
@@ -34,13 +35,12 @@ const (
 
 var (
 	groupLabels = map[string]string{
-		">breaking":    "Breaking changes",
-		">deprecation": "Deprecations",
-		">feature":     "Features and enhancements",
-		">enhancement": "Features and enhancements",
-		">bug":         "Fixes",
-		">docs":        "Documentation improvements",
-		noGroup:        "Miscellaneous",
+		"breaking":    "Breaking changes",
+		"deprecation": "Deprecations",
+		"feature":     "Features and enhancements",
+		"bug":         "Fixes",
+		"docs":        "Documentation improvements",
+		noGroup:       "Miscellaneous",
 	}
 
 	groupOrder = []string{
@@ -50,6 +50,16 @@ var (
 		">bug",
 		">docs",
 		noGroup,
+	}
+
+	labelMapping = map[string]string{
+		">bug":         "bug",
+		">docs":        "docs",
+		">feature":     "feature",
+		">enhancement": "feature",
+		">deprecation": "deprecation",
+		">breaking":    "breaking",
+		noGroup:        noGroup,
 	}
 
 	ignoredLabels = map[string]struct{}{
@@ -93,23 +103,10 @@ func groupPullRequests(prs []github.PullRequest) map[string][]github.PullRequest
 
 PR_LOOP:
 	for _, pr := range prs {
-		for _, lbl := range groupOrder {
-			// Consolidate >enhancement into >feature
-			if lbl == ">enhancement" {
-				continue // Skip >enhancement in groupOrder to avoid duplicate processing
-			}
-
-			if _, ok := pr.Labels[lbl]; ok {
-				groups[lbl] = append(groups[lbl], pr)
+		for prLabel, mappedLabel := range labelMapping {
+			if _, ok := pr.Labels[prLabel]; ok {
+				groups[mappedLabel] = append(groups[mappedLabel], pr)
 				continue PR_LOOP
-			}
-
-			// Special handling for >enhancement - treat as >feature
-			if lbl == ">feature" {
-				if _, ok := pr.Labels[">enhancement"]; ok {
-					groups[lbl] = append(groups[lbl], pr)
-					continue PR_LOOP
-				}
 			}
 		}
 
@@ -121,17 +118,19 @@ PR_LOOP:
 
 func render(version string, groups map[string][]github.PullRequest) error {
 	params := struct {
-		Version     string
-		Repo        string
-		Groups      map[string][]github.PullRequest
-		GroupLabels map[string]string
-		GroupOrder  []string
+		Version      string
+		Repo         string
+		Groups       map[string][]github.PullRequest
+		GroupLabels  map[string]string
+		GroupOrder   []string
+		LabelMapping map[string]string
 	}{
-		Version:     version,
-		Repo:        repoName,
-		Groups:      groups,
-		GroupLabels: groupLabels,
-		GroupOrder:  groupOrder,
+		Version:      version,
+		Repo:         repoName,
+		Groups:       groups,
+		GroupLabels:  groupLabels,
+		GroupOrder:   groupOrder,
+		LabelMapping: labelMapping,
 	}
 
 	funcs := template.FuncMap{
