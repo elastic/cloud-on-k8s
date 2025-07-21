3df2c9755e6897e397b961a856352d10564397cd
Michael Morello <michael.morello@elastic.co>
2025-07-21 10:18:17 +0200

    Update hack/licence-detector/generate-notice.sh

--- hack/licence-detector/generate-notice.sh
+++ hack/licence-detector/generate-notice.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR=${SCRIPT_DIR}/../..
 TEMP_DIR=$(mktemp -d)
-LICENCE_DETECTOR="go.elastic.co/go-licence-detector@v0.3.0"
+LICENCE_DETECTOR="go.elastic.co/go-licence-detector@template-variables"
 
 trap '[[ $TEMP_DIR ]] && rm -rf "$TEMP_DIR"' EXIT
 
@@ -19,13 +19,30 @@ get_licence_detector() {
     GOBIN="$TEMP_DIR" go install "$LICENCE_DETECTOR"
 }
 
+is_version() {
+  [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+}
+
+get_current_version() {
+  version="$(<"${PROJECT_DIR}/VERSION")"
+  if is_version "${version}"; then
+    echo "${version}"
+  else
+    echo "main"
+  fi
+}
+
 generate_notice() {
     (
+        version="$(get_current_version)"
+        # Remove dots from the version string for compatibility with the doc web site.
+        outFile="${version//./_}.md"
         cd "$PROJECT_DIR"
         go mod download
         go list -m -json all | "${TEMP_DIR}"/go-licence-detector \
             -depsTemplate="${SCRIPT_DIR}"/templates/dependencies.md.tmpl \
-            -depsOut="${PROJECT_DIR}"/docs/reference/third-party-dependencies.md \
+            -template-value=eckVersion="${version}" \
+            -depsOut="${PROJECT_DIR}"/docs/reference/third-party-dependencies/"${outFile}" \
             -noticeTemplate="${SCRIPT_DIR}"/templates/NOTICE.txt.tmpl \
             -noticeOut="${PROJECT_DIR}"/NOTICE.txt \
             -overrides="${SCRIPT_DIR}"/overrides/overrides.json \
