013339e943216ee79ed36a3deee3009007feb7d1
Nick Reisinger <nickreisinger@csitx.com>
2025-07-26 14:54:07 +0000

    Add tests

--- pkg/controller/elasticsearch/securitycontext/securitycontext.go
+++ pkg/controller/elasticsearch/securitycontext/securitycontext.go
@@ -28,7 +28,7 @@ func For(ver version.Version, enableReadOnlyRootFilesystem bool) corev1.Security
 		Privileged:               ptr.To[bool](false),
 		ReadOnlyRootFilesystem:   ptr.To[bool](enableReadOnlyRootFilesystem),
 		AllowPrivilegeEscalation: ptr.To[bool](false),
-		SeccompProfile: &corev1.SeccompProfile{
+		SeccompProfile:           &corev1.SeccompProfile{
 			Type: corev1.SeccompProfileTypeRuntimeDefault,
 		},
 	}
@@ -53,6 +53,9 @@ func DefaultBeatSecurityContext(ver version.Version) *corev1.SecurityContext {
 		Privileged:               ptr.To[bool](false),
 		ReadOnlyRootFilesystem:   ptr.To[bool](true),
 		AllowPrivilegeEscalation: ptr.To[bool](false),
+		SeccompProfile:           &corev1.SeccompProfile{
+			Type: corev1.SeccompProfileTypeRuntimeDefault,
+		},
 	}
 	if ver.LT(RunAsNonRootMinStackVersion) {
 		return sc
