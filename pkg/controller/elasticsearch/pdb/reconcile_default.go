8466261dac50daac6221f2a58523095d54f291b7
Michael Montgomery <mmontg1@gmail.com>
2025-08-08 07:28:40 -0500

    expired license comment.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- pkg/controller/elasticsearch/pdb/reconcile_default.go
+++ pkg/controller/elasticsearch/pdb/reconcile_default.go
@@ -30,10 +30,11 @@ import (
 )
 
 // Reconcile ensures that PodDisruptionBudget(s) exists for this cluster, inheriting the spec content.
-//  1. For non-enterprise users: The default PDB we setup dynamically adapts MinAvailable to the number of nodes in the cluster.
-//  2. For enterprise users: We optimize the PDBs that we setup to speed up Kubernetes cluster operations such as upgrades as much
+//  1. Without an enterprise license: The default PDB we setup dynamically adapts MinAvailable to the number of nodes in the cluster.
+//  2. With an enterprise license: We optimize the PDBs that we setup to speed up Kubernetes cluster operations such as upgrades as much
 //     as safely possible by grouping statefulSets by associated Elasticsearch node roles into the same PDB, and then dynamically setting
 //     maxUnavailable according to whatever cluster health is optimal for the set of roles.
+//  3. In the case of an expired enterprise license, the PDBs will revert back to a single PDB that covers the whole cluster.
 //
 // If the spec has disabled the default PDB, it will ensure none exist.
 func Reconcile(ctx context.Context, k8sClient k8s.Client, es esv1.Elasticsearch, statefulSets sset.StatefulSetList, resources nodespec.ResourcesList, meta metadata.Metadata) error {
