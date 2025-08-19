c41d672591cb40b6855a71114558a33dd935313f
Peter Brachwitz <peter.brachwitz@elastic.co>
2025-08-19 19:17:15 +0200

    Missing comment

--- test/e2e/cmd/run/run.go
+++ test/e2e/cmd/run/run.go
@@ -832,6 +832,7 @@ func (h *helper) dumpEventLog() {
 
 func (h *helper) runECKDiagnostics() {
 	operatorNS := h.testContext.Operator.Namespace
+	// include the default namespace to have diagnostics on the local disk provisioner used in some environments
 	otherNS := append([]string{h.testContext.E2ENamespace, "default"}, h.testContext.Operator.ManagedNamespaces...)
 	cmd := exec.Command("eck-diagnostics", "-o", operatorNS, "-r", strings.Join(otherNS, ","), "--run-agent-diagnostics")
 	cmd.Stdout = os.Stdout
