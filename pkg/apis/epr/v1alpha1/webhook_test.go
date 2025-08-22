88678018d85cb75573b80cb489ff8437a6489ce5
tehbooom <ajcarp29@gmail.com>
2025-08-22 12:00:36 -0400

    fix: Add e2e tests for epr and kb

--- pkg/apis/epr/v1alpha1/webhook_test.go
+++ pkg/apis/epr/v1alpha1/webhook_test.go
@@ -15,8 +15,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
-	commonv1 "github.com/elastic/cloud-on-k8s/v3/pkg/apis/common/v1"
-	emsv1alpha1 "github.com/elastic/cloud-on-k8s/v3/pkg/apis/maps/v1alpha1"
+	eprv1alpha1 "github.com/elastic/cloud-on-k8s/v3/pkg/apis/epr/v1alpha1"
 	"github.com/elastic/cloud-on-k8s/v3/pkg/utils/test"
 )
 
@@ -27,8 +26,8 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookSucceeded,
 		},
@@ -37,11 +36,11 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.SetAnnotations(map[string]string{
-					corev1.LastAppliedConfigAnnotation: `{"metadata":{"name": "ekesn", "namespace": "default", "uid": "e7a18cfb-b017-475c-8da2-1ec941b1f285", "creationTimestamp":"2020-03-24T13:43:20Z" },"spec":{"version":"7.6.1", "unknown": "UNKNOWN"}}`,
+				epr := mkEPR(uid)
+				epr.SetAnnotations(map[string]string{
+					corev1.LastAppliedConfigAnnotation: `{"metadata":{"name": "test-epr", "namespace": "default", "uid": "e7a18cfb-b017-475c-8da2-1ec941b1f285", "creationTimestamp":"2020-03-24T13:43:20Z" },"spec":{"version":"8.15.0", "unknown": "UNKNOWN"}}`,
 				})
-				return serialize(t, m)
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookFailed(
 				`"unknown": unknown field found in the kubectl.kubernetes.io/last-applied-configuration annotation is unknown`,
@@ -52,9 +51,9 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.SetName(strings.Repeat("x", 100))
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				epr.SetName(strings.Repeat("x", 100))
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookFailed(
 				`metadata.name: Too long: may not be more than 36 bytes`,
@@ -65,9 +64,9 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "7.x"
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				epr.Spec.Version = "7.x"
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookFailed(
 				`spec.version: Invalid value: "7.x": Invalid version: No Major.Minor.Patch elements found`,
@@ -78,9 +77,9 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "7.14.0"
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				epr.Spec.Version = "7.14.0"
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookSucceededWithWarnings(
 				`Version 7.14.0 is EOL and support for it will be removed in a future release of the ECK operator`,
@@ -91,9 +90,9 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "3.1.2"
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				epr.Spec.Version = "3.1.2"
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookFailed(
 				`spec.version: Invalid value: "3.1.2": Unsupported version: version 3.1.2 is lower than the lowest supported version`,
@@ -104,86 +103,34 @@ func TestWebhook(t *testing.T) {
 			Operation: admissionv1beta1.Create,
 			Object: func(t *testing.T, uid string) []byte {
 				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "300.1.2"
-				return serialize(t, m)
+				epr := mkEPR(uid)
+				epr.Spec.Version = "300.1.2"
+				return serialize(t, epr)
 			},
 			Check: test.ValidationWebhookFailed(
 				`spec.version: Invalid value: "300.1.2": Unsupported version: version 300.1.2 is higher than the highest supported version`,
 			),
 		},
-		{
-			Name:      "named-es-ref",
-			Operation: admissionv1beta1.Create,
-			Object: func(t *testing.T, uid string) []byte {
-				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "8.12.0"
-				m.Spec.ElasticsearchRef = commonv1.ObjectSelector{Name: "esname", Namespace: "esns", ServiceName: "essvc"}
-				return serialize(t, m)
-			},
-			Check: test.ValidationWebhookSucceeded,
-		},
-		{
-			Name:      "secret-es-ref",
-			Operation: admissionv1beta1.Create,
-			Object: func(t *testing.T, uid string) []byte {
-				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "8.12.0"
-				m.Spec.ElasticsearchRef = commonv1.ObjectSelector{SecretName: "esname"}
-				return serialize(t, m)
-			},
-			Check: test.ValidationWebhookSucceeded,
-		},
-		{
-			Name:      "invalid-secret-es-ref-name",
-			Operation: admissionv1beta1.Create,
-			Object: func(t *testing.T, uid string) []byte {
-				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "8.12.0"
-				m.Spec.ElasticsearchRef = commonv1.ObjectSelector{SecretName: "esname", Name: "esname"}
-				return serialize(t, m)
-			},
-			Check: test.ValidationWebhookFailed(
-				`spec.elasticsearchRef: Forbidden: Invalid association reference: specify name or secretName, not both`,
-			),
-		},
-		{
-			Name:      "invalid-secret-es-ref-namespace",
-			Operation: admissionv1beta1.Create,
-			Object: func(t *testing.T, uid string) []byte {
-				t.Helper()
-				m := mkMaps(uid)
-				m.Spec.Version = "8.12.0"
-				m.Spec.ElasticsearchRef = commonv1.ObjectSelector{SecretName: "esname", Namespace: "esname"}
-				return serialize(t, m)
-			},
-			Check: test.ValidationWebhookFailed(
-				`spec.elasticsearchRef: Forbidden: Invalid association reference: serviceName or namespace can only be used in combination with name, not with secretName`,
-			),
-		},
 	}
 
-	validator := &emsv1alpha1.ElasticMapsServer{}
-	gvk := metav1.GroupVersionKind{Group: emsv1alpha1.GroupVersion.Group, Version: emsv1alpha1.GroupVersion.Version, Kind: emsv1alpha1.Kind}
+	validator := &eprv1alpha1.ElasticPackageRegistry{}
+	gvk := metav1.GroupVersionKind{Group: eprv1alpha1.GroupVersion.Group, Version: eprv1alpha1.GroupVersion.Version, Kind: eprv1alpha1.Kind}
 	test.RunValidationWebhookTests(t, gvk, validator, testCases...)
 }
 
-func mkMaps(uid string) *emsv1alpha1.ElasticMapsServer {
-	return &emsv1alpha1.ElasticMapsServer{
+func mkEPR(uid string) *eprv1alpha1.ElasticPackageRegistry {
+	return &eprv1alpha1.ElasticPackageRegistry{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "webhook-test",
 			UID:  types.UID(uid),
 		},
-		Spec: emsv1alpha1.MapsSpec{
-			Version: "7.17.0",
+		Spec: eprv1alpha1.PackageRegistrySpec{
+			Version: "8.15.0",
 		},
 	}
 }
 
-func serialize(t *testing.T, k *emsv1alpha1.ElasticMapsServer) []byte {
+func serialize(t *testing.T, k *eprv1alpha1.ElasticPackageRegistry) []byte {
 	t.Helper()
 
 	objBytes, err := json.Marshal(k)
---
+++ test/e2e/test/epr/checks.go
@@ -0,0 +1,133 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package epr
+
+import (
+	"context"
+	"fmt"
+
+	commonv1 "github.com/elastic/cloud-on-k8s/v3/pkg/apis/common/v1"
+	"github.com/elastic/cloud-on-k8s/v3/pkg/apis/epr/v1alpha1"
+	"github.com/elastic/cloud-on-k8s/v3/pkg/utils/k8s"
+	"github.com/elastic/cloud-on-k8s/v3/test/e2e/test"
+)
+
+// CheckSecrets checks that expected secrets have been created.
+func CheckSecrets(b Builder, k *test.K8sClient) test.Step {
+	return test.CheckSecretsContent(k, b.EPR.Namespace, func() []test.ExpectedSecret {
+		eprName := b.EPR.Name
+		// hardcode all secret names and keys to catch any breaking change
+		expected := []test.ExpectedSecret{
+			{
+				Name: eprName + "-epr-config",
+				Keys: []string{"config.yml"},
+				Labels: map[string]string{
+					"eck.k8s.elastic.co/credentials":       "true",
+					"packageregistry.k8s.elastic.co/name": eprName,
+				},
+			},
+		}
+		if b.EPR.Spec.HTTP.TLS.Enabled() {
+			expected = append(expected,
+				test.ExpectedSecret{
+					Name: eprName + "-epr-http-ca-internal",
+					Keys: []string{"tls.crt", "tls.key"},
+					Labels: map[string]string{
+						"packageregistry.k8s.elastic.co/name": eprName,
+						"common.k8s.elastic.co/type":          "packageregistry",
+					},
+				},
+				test.ExpectedSecret{
+					Name: eprName + "-epr-http-certs-internal",
+					Keys: []string{"tls.crt", "tls.key", "ca.crt"},
+					Labels: map[string]string{
+						"packageregistry.k8s.elastic.co/name": eprName,
+						"common.k8s.elastic.co/type":          "packageregistry",
+					},
+				},
+				test.ExpectedSecret{
+					Name: eprName + "-epr-http-certs-public",
+					Keys: []string{"ca.crt", "tls.crt"},
+					Labels: map[string]string{
+						"packageregistry.k8s.elastic.co/name": eprName,
+						"common.k8s.elastic.co/type":          "packageregistry",
+					},
+				},
+			)
+		}
+		return expected
+	})
+}
+
+func CheckStatus(b Builder, k *test.K8sClient) test.Step {
+	return test.Step{
+		Name: "Elastic Package Registry status should be updated",
+		Test: test.Eventually(func() error {
+			var epr v1alpha1.ElasticPackageRegistry
+			if err := k.Client.Get(context.Background(), k8s.ExtractNamespacedName(&b.EPR), &epr); err != nil {
+				return err
+			}
+
+			// Selector is a string built from a map, it is validated with a dedicated function.
+			// The expected value is hardcoded on purpose to ensure there is no regression in the way the set of labels
+			// is created.
+			if err := test.CheckSelector(
+				epr.Status.Selector,
+				map[string]string{
+					"packageregistry.k8s.elastic.co/name": epr.Name,
+					"common.k8s.elastic.co/type":          "packageregistry",
+				}); err != nil {
+				return err
+			}
+			epr.Status.Selector = ""
+			epr.Status.ObservedGeneration = 0
+
+			// don't check the association status that may vary across tests
+			expected := v1alpha1.PackageRegistryStatus{
+				DeploymentStatus: commonv1.DeploymentStatus{
+					Count:          b.EPR.Spec.Count,
+					AvailableNodes: b.EPR.Spec.Count,
+					Version:        b.EPR.Spec.Version,
+					Health:         "green",
+				},
+			}
+			if epr.Status.DeploymentStatus != expected.DeploymentStatus {
+				return fmt.Errorf("expected status %+v but got %+v", expected.DeploymentStatus, epr.Status.DeploymentStatus)
+			}
+			return nil
+		}),
+	}
+}
+
+type eprStatus struct {
+	Version string `json:"version"`
+	Overall struct {
+		State string `json:"state"`
+	} `json:"overall"`
+}
+
+func (b Builder) CheckStackTestSteps(k *test.K8sClient) test.StepList {
+	return test.StepList{
+		{
+			Name: "Elastic Package Registry should respond to requests",
+			Test: test.Eventually(func() error {
+				client, err := NewEPRClient(b.EPR, k)
+				if err != nil {
+					return err
+				}
+				bytes, err := DoRequest(client, b.EPR, "GET", "/search")
+				if err != nil {
+					return err
+				}
+				// For EPR, just check that we get a valid response
+				// EPR search endpoint returns package information
+				if len(bytes) == 0 {
+					return fmt.Errorf("expected non-empty response from EPR")
+				}
+				return nil
+			}),
+		},
+	}
+}
