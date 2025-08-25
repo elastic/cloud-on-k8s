63f76e07f275f5ac08d77b1458eaf5e2e1af5d07
Michael Montgomery <mmontg1@gmail.com>
2025-08-25 14:55:02 -0500

    Remove webhook admissionVersion v1beta1
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -558,8 +558,6 @@ func startOperator(ctx context.Context) error {
 	// Setup Scheme for all resources
 	log.Info("Setting up scheme")
 	controllerscheme.SetupScheme()
-	// also set up the v1beta1 scheme, used by the v1beta1 webhook
-	controllerscheme.SetupV1beta1Scheme()
 
 	// Create a new Cmd to provide shared dependencies and start components
 	opts := ctrl.Options{
