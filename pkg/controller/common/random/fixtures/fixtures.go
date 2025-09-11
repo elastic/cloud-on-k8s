164c1347343cd6ea552de9e877c077617696ab81
Michael Montgomery <mmontg1@gmail.com>
2025-09-11 16:12:16 -0500

    add missing package.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

---
+++ pkg/controller/common/random/fixtures/fixtures.go
@@ -0,0 +1,20 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package fixtures
+
+import (
+	"github.com/elastic/cloud-on-k8s/v3/pkg/controller/common/random"
+	"github.com/sethvargo/go-password/password"
+)
+
+func DefaultByteGeneratorParams() random.ByteGeneratorParams {
+	return random.ByteGeneratorParams{
+		LowerLetters: password.LowerLetters,
+		UpperLetters: password.UpperLetters,
+		Symbols:      password.Symbols,
+		Digits:       password.Digits,
+		Length:       24,
+	}
+}
