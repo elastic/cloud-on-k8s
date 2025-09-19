0cd267bc7fccad03156dd83d885f0276598b9a4b
Michael Montgomery <mmontg1@gmail.com>
2025-09-19 15:26:56 -0500

    move validation to it's own file.
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -22,7 +22,6 @@ import (
 	"github.com/spf13/viper"
 	"go.elastic.co/apm/v2"
 	"go.uber.org/automaxprocs/maxprocs"
-	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -857,19 +856,6 @@ func asyncTasks(
 	tracing.EndContextTransaction(gcCtx)
 }
 
-func chooseAndValidateIPFamily(ipFamilyStr string, ipFamilyDefault corev1.IPFamily) (corev1.IPFamily, error) {
-	switch strings.ToLower(ipFamilyStr) {
-	case "":
-		return ipFamilyDefault, nil
-	case "ipv4":
-		return corev1.IPv4Protocol, nil
-	case "ipv6":
-		return corev1.IPv6Protocol, nil
-	default:
-		return ipFamilyDefault, fmt.Errorf("IP family can be one of: IPv4, IPv6 or \"\" to auto-detect, but was %s", ipFamilyStr)
-	}
-}
-
 // determineSetDefaultSecurityContext determines what settings we need to use for security context by using the following rules:
 //  1. If the setDefaultSecurityContext is explicitly set to either true, or false, use this value.
 //  2. use OpenShift detection to determine whether or not we are running within an OpenShift cluster.
@@ -975,38 +961,6 @@ func registerControllers(mgr manager.Manager, params operator.Parameters, access
 	}
 
 	return nil
-}
-
-func validatePasswordFlags(passwordAllowedCharactersFlag string, passwordLengthFlag string) (operator.PasswordGeneratorParams, error) {
-	allowedCharacters := viper.GetString(passwordAllowedCharactersFlag)
-	generatorParams, other := categorizeAllowedCharacters(allowedCharacters)
-	if len(other) > 0 {
-		return operator.PasswordGeneratorParams{}, fmt.Errorf("invalid characters in passwords allowed characters: %s", string(other))
-	}
-
-	generatorParams.Length = viper.GetInt(passwordLengthFlag)
-	// Elasticsearch requires at least 6 characters for passwords
-	// https://www.elastic.co/guide/en/elasticsearch/reference/7.5/security-api-put-user.html
-	if generatorParams.Length < 6 || generatorParams.Length > 72 {
-		return operator.PasswordGeneratorParams{}, fmt.Errorf("password length must be at least 6 and at most 72")
-	}
-
-	if len(generatorParams.LowerLetters)+len(generatorParams.UpperLetters)+len(generatorParams.Digits)+len(generatorParams.Symbols) < 10 {
-		return operator.PasswordGeneratorParams{}, fmt.Errorf("allowedCharacters for password generation needs to be at least 10 for randomness")
-	}
-
-	return generatorParams, nil
-}
-
-func validateCertExpirationFlags(validityFlag string, rotateBeforeFlag string) (time.Duration, time.Duration, error) {
-	certValidity := viper.GetDuration(validityFlag)
-	certRotateBefore := viper.GetDuration(rotateBeforeFlag)
-
-	if certRotateBefore > certValidity {
-		return certValidity, certRotateBefore, fmt.Errorf("%s must be larger than %s", validityFlag, rotateBeforeFlag)
-	}
-
-	return certValidity, certRotateBefore, nil
 }
 
 func garbageCollectUsers(ctx context.Context, cfg *rest.Config, managedNamespaces []string) error {
---
+++ cmd/manager/validation.go
@@ -0,0 +1,60 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package manager
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/elastic/cloud-on-k8s/v3/pkg/controller/common/operator"
+	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func chooseAndValidateIPFamily(ipFamilyStr string, ipFamilyDefault corev1.IPFamily) (corev1.IPFamily, error) {
+	switch strings.ToLower(ipFamilyStr) {
+	case "":
+		return ipFamilyDefault, nil
+	case "ipv4":
+		return corev1.IPv4Protocol, nil
+	case "ipv6":
+		return corev1.IPv6Protocol, nil
+	default:
+		return ipFamilyDefault, fmt.Errorf("IP family can be one of: IPv4, IPv6 or \"\" to auto-detect, but was %s", ipFamilyStr)
+	}
+}
+
+func validatePasswordFlags(passwordAllowedCharactersFlag string, passwordLengthFlag string) (operator.PasswordGeneratorParams, error) {
+	allowedCharacters := viper.GetString(passwordAllowedCharactersFlag)
+	generatorParams, other := categorizeAllowedCharacters(allowedCharacters)
+	if len(other) > 0 {
+		return operator.PasswordGeneratorParams{}, fmt.Errorf("invalid characters in passwords allowed characters: %s", string(other))
+	}
+
+	generatorParams.Length = viper.GetInt(passwordLengthFlag)
+	// Elasticsearch requires at least 6 characters for passwords
+	// https://www.elastic.co/guide/en/elasticsearch/reference/7.5/security-api-put-user.html
+	if generatorParams.Length < 6 || generatorParams.Length > 72 {
+		return operator.PasswordGeneratorParams{}, fmt.Errorf("password length must be at least 6 and at most 72")
+	}
+
+	if len(generatorParams.LowerLetters)+len(generatorParams.UpperLetters)+len(generatorParams.Digits)+len(generatorParams.Symbols) < 10 {
+		return operator.PasswordGeneratorParams{}, fmt.Errorf("allowedCharacters for password generation needs to be at least 10 for randomness")
+	}
+
+	return generatorParams, nil
+}
+
+func validateCertExpirationFlags(validityFlag string, rotateBeforeFlag string) (time.Duration, time.Duration, error) {
+	certValidity := viper.GetDuration(validityFlag)
+	certRotateBefore := viper.GetDuration(rotateBeforeFlag)
+
+	if certRotateBefore > certValidity {
+		return certValidity, certRotateBefore, fmt.Errorf("%s must be larger than %s", validityFlag, rotateBeforeFlag)
+	}
+
+	return certValidity, certRotateBefore, nil
+}
