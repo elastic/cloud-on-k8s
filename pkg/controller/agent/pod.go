63bff027f984d8dd1487b573d6231918777b9c55
Michael Montgomery <mmontg1@gmail.com>
2025-10-21 02:56:55 -0500

    Consistently mount CONFIG_PATH and STATE_PATH to same directory in Fleet mode (#8856)
    * Consistently mount CONFIG_PATH and STATE_PATH to same directory in Fleet
    mode.
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Add comment
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Maybe add init container.
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Fix to be deployment specific
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * remove unneeded call
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Add some logging
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Fix bug
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Add check for existing init container
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Add volume mount
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Remove test code to add init container.
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    
    * Adjust test expecations to new command
    
    ---------
    
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>
    Co-authored-by: Peter Brachwitz <peter.brachwitz@elastic.co>

--- pkg/controller/agent/pod.go
+++ pkg/controller/agent/pod.go
@@ -148,6 +148,8 @@ func buildPodTemplate(params Params, fleetCerts *certificates.CertificatesSecret
 		if builder, err = amendBuilderForFleetMode(params, fleetCerts, fleetToken, builder, configHash); err != nil {
 			return corev1.PodTemplateSpec{}, err
 		}
+		// Point agent to static config file mounted from a secret to /etc/agent/elastic-agent.yml
+		builder = builder.WithArgs("-e", "-c", path.Join(ConfigMountPath, ConfigFileName))
 	} else if spec.StandaloneModeEnabled() {
 		// cleanup secret used in Fleet mode
 		if err := cleanupEnvVarsSecret(params); err != nil {
@@ -156,6 +158,8 @@ func buildPodTemplate(params Params, fleetCerts *certificates.CertificatesSecret
 
 		builder = builder.
 			WithResources(defaultResources).
+			WithEnv(corev1.EnvVar{Name: "STATE_PATH", Value: DataMountPath}).
+			// Point agent to static config file mounted from a secret to /etc/agent/elastic-agent.yml
 			WithArgs("-e", "-c", path.Join(ConfigMountPath, ConfigFileName))
 	}
 
@@ -191,15 +195,6 @@ func buildPodTemplate(params Params, fleetCerts *certificates.CertificatesSecret
 		)
 
 	return builder.PodTemplate, nil
-}
-
-func fleetConfigPath(v version.Version) string {
-	if v.LT(agentv1alpha1.FleetAdvancedConfigMinVersion) {
-		// default to the in-container config directory for older versions of Elastic Agent
-		// that still try to rewrite the config file in Fleet mode during enrollment.
-		return "/usr/share/elastic-agent"
-	}
-	return ConfigMountPath
 }
 
 func amendBuilderForFleetMode(params Params, fleetCerts *certificates.CertificatesSecret, fleetToken EnrollmentAPIKey, builder *defaults.PodTemplateBuilder, configHash hash.Hash) (*defaults.PodTemplateBuilder, error) {
@@ -241,7 +236,10 @@ func amendBuilderForFleetMode(params Params, fleetCerts *certificates.Certificat
 
 	builder = builder.
 		WithResources(defaultFleetResources).
-		WithEnv(corev1.EnvVar{Name: "CONFIG_PATH", Value: fleetConfigPath(params.AgentVersion)})
+		WithEnv(
+			corev1.EnvVar{Name: "STATE_PATH", Value: DataMountPath},
+			corev1.EnvVar{Name: "CONFIG_PATH", Value: DataMountPath},
+		)
 
 	return builder, nil
 }
@@ -452,7 +450,7 @@ if [[ -f %[1]s ]]; then
     %[5]s
   fi
 fi
-/usr/bin/tini -- /usr/local/bin/docker-entrypoint -e
+/usr/bin/tini -- /usr/local/bin/docker-entrypoint -e "$@"
 `, caPath, ubiSharedCAPath, ubiUpdateCmd, debianSharedCAPath, debianUpdateCmd)
 }
 
