890d7304ecf9af55de8eba20c2ecc5dd410322e0
Michael Montgomery <mmontg1@gmail.com>
2025-12-17 20:33:14 -0600

    actually add accessreviewer
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- pkg/controller/autoops/controller.go
+++ pkg/controller/autoops/controller.go
@@ -59,6 +59,7 @@ func newReconciler(mgr manager.Manager, accessReviewer rbac.AccessReviewer, para
 	k8sClient := mgr.GetClient()
 	return &AgentPolicyReconciler{
 		Client:           k8sClient,
+		accessReviewer:   accessReviewer,
 		recorder:         mgr.GetEventRecorderFor(controllerName),
 		licenseChecker:   license.NewLicenseChecker(k8sClient, params.OperatorNamespace),
 		params:           params,
