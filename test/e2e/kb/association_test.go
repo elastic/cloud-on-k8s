2dc4548172b4e4e7f1ec252ce4d388dce0503f68
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2025-12-22 11:45:37 +0200

    fix: re-introduce build tags for kibana association e2e tests

--- test/e2e/kb/association_test.go
+++ test/e2e/kb/association_test.go
@@ -2,6 +2,8 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
+//go:build kb || e2e
+
 package kb
 
 import (
