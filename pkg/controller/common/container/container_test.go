b0d7a7cb3184dee01e6e090c28753243f7f20988
Michael Montgomery <mmontg1@gmail.com>
2026-01-07 15:13:09 -0600

    Fix test name
    Signed-off-by: Michael Montgomery <mmontg1@gmail.com>

--- pkg/controller/common/container/container_test.go
+++ pkg/controller/common/container/container_test.go
@@ -183,7 +183,7 @@ func TestImageRepository(t *testing.T) {
 			want:    testRegistry + "/package-registry/distribution:lite-8.15.1",
 		},
 		{
-			name:    "Package registry image from 8.15.1 onwards useslite prefix",
+			name:    "Package registry image from 8.15.1 onwards uses lite prefix",
 			image:   PackageRegistryImage,
 			version: "8.16.0",
 			want:    testRegistry + "/package-registry/distribution:lite-8.16.0",
