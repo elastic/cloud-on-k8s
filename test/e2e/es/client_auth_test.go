eb5646000f938aec53ca615ed9409d1d05f1cb1a
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2026-03-21 14:19:27 +0200

    remove TestClientAuthRequired e2e test as it is covered by TestClientAuthRequiredTransition

--- test/e2e/es/client_auth_test.go
+++ test/e2e/es/client_auth_test.go
@@ -24,102 +24,9 @@ import (
 	"github.com/elastic/cloud-on-k8s/v3/test/e2e/test/elasticsearch"
 )
 
-// TestClientAuthRequired tests that when client authentication is enabled via the spec field,
-// ECK manages self-signed client certificates and the cluster is healthy.
-func TestClientAuthRequired(t *testing.T) {
-	if test.Ctx().TestLicense == "" {
-		t.Skip("Skipping client authentication test: no enterprise test license configured")
-	}
-
-	esName := "test-mtls-required"
-	esNamespace := test.Ctx().ManagedNamespace(0)
-
-	esBuilder := elasticsearch.NewBuilder(esName).
-		WithESMasterDataNodes(1, elasticsearch.DefaultResources).
-		WithClientAuthenticationRequired()
-
-	esWithLicense := test.LicenseTestBuilder(esBuilder)
-
-	k := test.NewK8sClientOrFatal()
-
-	test.StepList{}.
-		WithSteps(esWithLicense.InitTestSteps(k)).
-		WithSteps(esWithLicense.CreationTestSteps(k)).
-		WithSteps(test.CheckTestSteps(esWithLicense, k)).
-		WithSteps(test.StepList{
-			{
-				Name: "Verify client-certificate-required annotation is set",
-				Test: test.Eventually(func() error {
-					var es esv1.Elasticsearch
-					if err := k.Client.Get(context.Background(), types.NamespacedName{
-						Namespace: esNamespace,
-						Name:      esBuilder.Elasticsearch.Name,
-					}, &es); err != nil {
-						return err
-					}
-					if !annotation.HasClientAuthenticationRequired(&es) {
-						return fmt.Errorf("annotation %s not found on Elasticsearch resource", annotation.ClientAuthenticationRequiredAnnotation)
-					}
-					return nil
-				}),
-			},
-			{
-				Name: "Verify operator client certificate secret exists",
-				Test: test.Eventually(func() error {
-					secretName := certificates.OperatorClientCertSecretName(esv1.ESNamer, esBuilder.Elasticsearch.Name)
-					var secret corev1.Secret
-					return k.Client.Get(context.Background(), types.NamespacedName{
-						Namespace: esNamespace,
-						Name:      secretName,
-					}, &secret)
-				}),
-			},
-			{
-				Name: "Verify trust bundle secret exists",
-				Test: test.Eventually(func() error {
-					secretName := certificates.ClientCertTrustBundleSecretName(esv1.ESNamer, esBuilder.Elasticsearch.Name)
-					var secret corev1.Secret
-					return k.Client.Get(context.Background(), types.NamespacedName{
-						Namespace: esNamespace,
-						Name:      secretName,
-					}, &secret)
-				}),
-			},
-			{
-				Name: "Verify operator client cert is self-signed",
-				Test: test.Eventually(func() error {
-					secretName := certificates.OperatorClientCertSecretName(esv1.ESNamer, esBuilder.Elasticsearch.Name)
-					var secret corev1.Secret
-					if err := k.Client.Get(context.Background(), types.NamespacedName{
-						Namespace: esNamespace,
-						Name:      secretName,
-					}, &secret); err != nil {
-						return err
-					}
-
-					certs, err := certificates.ParsePEMCerts(secret.Data[certificates.CertFileName])
-					if err != nil {
-						return err
-					}
-					if len(certs) == 0 {
-						return fmt.Errorf("no certificates found in client cert secret")
-					}
-					cert := certs[0]
-					require.Contains(t, cert.ExtKeyUsage, x509.ExtKeyUsageClientAuth)
-					require.Equal(t, cert.Subject.CommonName, cert.Issuer.CommonName)
-
-					// No ca.crt should be present
-					require.Empty(t, secret.Data[certificates.CAFileName])
-					return nil
-				}),
-			},
-		}).
-		WithSteps(esWithLicense.DeletionTestSteps(k)).
-		RunSequential(t)
-}
-
 // TestClientAuthRequiredTransition tests transitioning from no client authentication to required and back to disabled.
-// Verifies the cluster remains healthy and client cert secrets are created and cleaned up across transitions.
+// Verifies the cluster remains healthy, client cert secrets are created (and are valid self-signed certs),
+// and cleaned up across transitions.
 func TestClientAuthRequiredTransition(t *testing.T) {
 	if test.Ctx().TestLicense == "" {
 		t.Skip("Skipping client authentication test: no enterprise test license configured")
@@ -148,18 +55,43 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 
 	// Use the builder's actual ES name (includes random suffix from NewBuilder).
 	actualESName := initialBuilder.Elasticsearch.Name
-	clientCertSecretName := certificates.OperatorClientCertSecretName(esv1.ESNamer, actualESName)
-	trustBundleSecretName := certificates.ClientCertTrustBundleSecretName(esv1.ESNamer, actualESName)
 
-	// Helper steps to assert client cert resources exist
-	verifyClientCertResourcesExist := test.StepList{
+	test.StepList{}.
+		// Create with client auth disabled (with license)
+		WithSteps(initialWithLicense.InitTestSteps(k)).
+		WithSteps(initialWithLicense.CreationTestSteps(k)).
+		WithSteps(test.CheckTestSteps(initialWithLicense, k)).
+		WithSteps(verifyClientCertResourcesRemoved(k, esNamespace, actualESName)).
+		// Phase 1: enable client authentication
+		// Annotate pods before mutation so CheckExpectedPodsEventuallyReady can verify all pods are rolled.
+		WithSteps(elasticsearch.AnnotatePodsWithBuilderHash(initialBuilder, k)).
+		WithSteps(enabledBuilder.UpgradeTestSteps(k)).
+		WithSteps(test.CheckTestSteps(*enabledBuilder, k)).
+		WithSteps(verifyClientCertResourcesExist(t, k, esNamespace, actualESName)).
+		// Phase 2: disable client authentication
+		// Annotate pods before mutation so CheckExpectedPodsEventuallyReady can verify all pods are rolled.
+		WithSteps(elasticsearch.AnnotatePodsWithBuilderHash(*enabledBuilder, k)).
+		WithSteps(disabledBuilder.UpgradeTestSteps(k)).
+		WithSteps(test.CheckTestSteps(*disabledBuilder, k)).
+		WithSteps(verifyClientCertResourcesRemoved(k, esNamespace, actualESName)).
+		WithSteps(initialWithLicense.DeletionTestSteps(k)).
+		RunSequential(t)
+}
+
+// verifyClientCertResourcesExist returns steps that verify the client authentication annotation is set,
+// the operator client certificate and trust bundle secrets exist, and the client cert is a valid self-signed certificate.
+func verifyClientCertResourcesExist(t *testing.T, k *test.K8sClient, namespace, esName string) test.StepList {
+	clientCertSecretName := certificates.OperatorClientCertSecretName(esv1.ESNamer, esName)
+	trustBundleSecretName := certificates.ClientCertTrustBundleSecretName(esv1.ESNamer, esName)
+
+	return test.StepList{
 		{
 			Name: "Verify client-certificate-required annotation is set",
 			Test: test.Eventually(func() error {
 				var es esv1.Elasticsearch
 				if err := k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
-					Name:      actualESName,
+					Namespace: namespace,
+					Name:      esName,
 				}, &es); err != nil {
 					return err
 				}
@@ -174,7 +106,7 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 			Test: test.Eventually(func() error {
 				var secret corev1.Secret
 				return k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
+					Namespace: namespace,
 					Name:      clientCertSecretName,
 				}, &secret)
 			}),
@@ -184,22 +116,55 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 			Test: test.Eventually(func() error {
 				var secret corev1.Secret
 				return k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
+					Namespace: namespace,
 					Name:      trustBundleSecretName,
 				}, &secret)
 			}),
 		},
-	}
+		{
+			Name: "Verify operator client cert is self-signed",
+			Test: test.Eventually(func() error {
+				var secret corev1.Secret
+				if err := k.Client.Get(context.Background(), types.NamespacedName{
+					Namespace: namespace,
+					Name:      clientCertSecretName,
+				}, &secret); err != nil {
+					return err
+				}
 
-	// Helper steps to assert client cert resources are removed
-	verifyClientCertResourcesRemoved := test.StepList{
+				certs, err := certificates.ParsePEMCerts(secret.Data[certificates.CertFileName])
+				if err != nil {
+					return err
+				}
+				if len(certs) == 0 {
+					return fmt.Errorf("no certificates found in client cert secret")
+				}
+				cert := certs[0]
+				require.Contains(t, cert.ExtKeyUsage, x509.ExtKeyUsageClientAuth)
+				require.Equal(t, cert.Subject.CommonName, cert.Issuer.CommonName)
+
+				// No ca.crt should be present
+				require.Empty(t, secret.Data[certificates.CAFileName])
+				return nil
+			}),
+		},
+	}
+}
+
+// verifyClientCertResourcesRemoved returns steps that verify the client authentication annotation is removed
+// and the operator client certificate and trust bundle secrets are deleted.
+func verifyClientCertResourcesRemoved(k *test.K8sClient, namespace, esName string) test.StepList {
+	clientCertSecretName := certificates.OperatorClientCertSecretName(esv1.ESNamer, esName)
+	trustBundleSecretName := certificates.ClientCertTrustBundleSecretName(esv1.ESNamer, esName)
+
+	return test.StepList{
 		{
 			Name: "Verify annotation is removed",
 			Test: test.Eventually(func() error {
 				var es esv1.Elasticsearch
 				if err := k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
-					Name:      actualESName,
+					Namespace: namespace,
+					Name:      esName,
 				}, &es); err != nil {
 					return err
 				}
@@ -214,7 +179,7 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 			Test: test.Eventually(func() error {
 				var secret corev1.Secret
 				err := k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
+					Namespace: namespace,
 					Name:      clientCertSecretName,
 				}, &secret)
 				if apierrors.IsNotFound(err) {
@@ -231,7 +196,7 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 			Test: test.Eventually(func() error {
 				var secret corev1.Secret
 				err := k.Client.Get(context.Background(), types.NamespacedName{
-					Namespace: esNamespace,
+					Namespace: namespace,
 					Name:      trustBundleSecretName,
 				}, &secret)
 				if apierrors.IsNotFound(err) {
@@ -244,25 +209,4 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 			}),
 		},
 	}
-
-	test.StepList{}.
-		// Create with client auth disabled (with license)
-		WithSteps(initialWithLicense.InitTestSteps(k)).
-		WithSteps(initialWithLicense.CreationTestSteps(k)).
-		WithSteps(test.CheckTestSteps(initialWithLicense, k)).
-		WithSteps(verifyClientCertResourcesRemoved).
-		// Phase 1: enable client authentication
-		// Annotate pods before mutation so CheckExpectedPodsEventuallyReady can verify all pods are rolled.
-		WithSteps(elasticsearch.AnnotatePodsWithBuilderHash(initialBuilder, k)).
-		WithSteps(enabledBuilder.UpgradeTestSteps(k)).
-		WithSteps(test.CheckTestSteps(*enabledBuilder, k)).
-		WithSteps(verifyClientCertResourcesExist).
-		// Phase 2: disable client authentication
-		// Annotate pods before mutation so CheckExpectedPodsEventuallyReady can verify all pods are rolled.
-		WithSteps(elasticsearch.AnnotatePodsWithBuilderHash(*enabledBuilder, k)).
-		WithSteps(disabledBuilder.UpgradeTestSteps(k)).
-		WithSteps(test.CheckTestSteps(*disabledBuilder, k)).
-		WithSteps(verifyClientCertResourcesRemoved).
-		WithSteps(initialWithLicense.DeletionTestSteps(k)).
-		RunSequential(t)
 }
