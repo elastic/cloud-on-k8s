084be00d91a83bdc1a81fdd87b929f9931b73455
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2026-03-21 14:26:00 +0200

    make TestHasClientAuthenticationRequired, TestSetClientAuthenticationRequiredAnnotation, and TestRemoveClientAuthenticationRequiredAnnotation table-driven

--- pkg/controller/common/annotation/client_cert_test.go
+++ pkg/controller/common/annotation/client_cert_test.go
@@ -17,106 +17,109 @@ import (
 )
 
 func TestHasClientAuthenticationRequired(t *testing.T) {
-	t.Run("returns true when annotation is set to true", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
-		}}
-		require.True(t, HasClientAuthenticationRequired(obj))
-	})
+	tests := []struct {
+		name        string
+		annotations map[string]string
+		want        bool
+	}{
+		{
+			name:        "returns true when annotation is set to true",
+			annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
+			want:        true,
+		},
+		{
+			name: "returns false when annotation is absent",
+			want: false,
+		},
+		{
+			name:        "returns false when annotation has unexpected value",
+			annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "false"},
+			want:        false,
+		},
+		{
+			name:        "returns false when annotation is empty string",
+			annotations: map[string]string{ClientAuthenticationRequiredAnnotation: ""},
+			want:        false,
+		},
+	}
 
-	t.Run("returns false when annotation is absent", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{}}
-		require.False(t, HasClientAuthenticationRequired(obj))
-	})
-
-	t.Run("returns false when annotation has unexpected value", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "false"},
-		}}
-		require.False(t, HasClientAuthenticationRequired(obj))
-	})
-
-	t.Run("returns false when annotation is empty string", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: ""},
-		}}
-		require.False(t, HasClientAuthenticationRequired(obj))
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Annotations: tt.annotations}}
+			require.Equal(t, tt.want, HasClientAuthenticationRequired(obj))
+		})
+	}
 }
 
 func TestSetClientAuthenticationRequiredAnnotation(t *testing.T) {
-	ctx := context.Background()
-	nsn := types.NamespacedName{Namespace: "ns", Name: "test"}
+	tests := []struct {
+		name        string
+		annotations map[string]string
+	}{
+		{
+			name: "sets annotation on object with no annotations",
+		},
+		{
+			name:        "no-op when annotation already present",
+			annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
+		},
+		{
+			name:        "overwrites incorrect annotation value",
+			annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "false"},
+		},
+	}
 
-	t.Run("sets annotation on object with no annotations", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "test"}}
-		client := k8s.NewFakeClient(obj)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
+				Namespace:   "ns",
+				Name:        "test",
+				Annotations: tt.annotations,
+			}}
+			c := k8s.NewFakeClient(obj)
 
-		require.NoError(t, SetClientAuthenticationRequiredAnnotation(ctx, client, obj))
+			require.NoError(t, SetClientAuthenticationRequiredAnnotation(ctx, c, obj))
 
-		var updated corev1.Secret
-		require.NoError(t, client.Get(ctx, nsn, &updated))
-		require.Equal(t, "true", updated.Annotations[ClientAuthenticationRequiredAnnotation])
-	})
-
-	t.Run("no-op when annotation already present", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Namespace:   "ns",
-			Name:        "test",
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
-		}}
-		client := k8s.NewFakeClient(obj)
-
-		require.NoError(t, SetClientAuthenticationRequiredAnnotation(ctx, client, obj))
-
-		var updated corev1.Secret
-		require.NoError(t, client.Get(ctx, nsn, &updated))
-		require.Equal(t, "true", updated.Annotations[ClientAuthenticationRequiredAnnotation])
-	})
-
-	t.Run("overwrites incorrect annotation value", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Namespace:   "ns",
-			Name:        "test",
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "false"},
-		}}
-		client := k8s.NewFakeClient(obj)
-
-		require.NoError(t, SetClientAuthenticationRequiredAnnotation(ctx, client, obj))
-
-		var updated corev1.Secret
-		require.NoError(t, client.Get(ctx, nsn, &updated))
-		require.Equal(t, "true", updated.Annotations[ClientAuthenticationRequiredAnnotation])
-	})
+			var updated corev1.Secret
+			require.NoError(t, c.Get(ctx, types.NamespacedName{Namespace: "ns", Name: "test"}, &updated))
+			require.Equal(t, "true", updated.Annotations[ClientAuthenticationRequiredAnnotation])
+		})
+	}
 }
 
 func TestRemoveClientAuthenticationRequiredAnnotation(t *testing.T) {
-	ctx := context.Background()
-	nsn := types.NamespacedName{Namespace: "ns", Name: "test"}
+	tests := []struct {
+		name                 string
+		annotations          map[string]string
+		wantAnnotationAbsent bool
+	}{
+		{
+			name:                 "removes annotation when present",
+			annotations:          map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
+			wantAnnotationAbsent: true,
+		},
+		{
+			name:                 "no-op when annotation already absent",
+			wantAnnotationAbsent: true,
+		},
+	}
 
-	t.Run("removes annotation when present", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
-			Namespace:   "ns",
-			Name:        "test",
-			Annotations: map[string]string{ClientAuthenticationRequiredAnnotation: "true"},
-		}}
-		client := k8s.NewFakeClient(obj)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{
+				Namespace:   "ns",
+				Name:        "test",
+				Annotations: tt.annotations,
+			}}
+			c := k8s.NewFakeClient(obj)
 
-		require.NoError(t, RemoveClientAuthenticationRequiredAnnotation(ctx, client, obj))
+			require.NoError(t, RemoveClientAuthenticationRequiredAnnotation(ctx, c, obj))
 
-		var updated corev1.Secret
-		require.NoError(t, client.Get(ctx, nsn, &updated))
-		require.False(t, HasClientAuthenticationRequired(&updated))
-	})
-
-	t.Run("no-op when annotation already absent", func(t *testing.T) {
-		obj := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Namespace: "ns", Name: "test"}}
-		client := k8s.NewFakeClient(obj)
-
-		require.NoError(t, RemoveClientAuthenticationRequiredAnnotation(ctx, client, obj))
-
-		var updated corev1.Secret
-		require.NoError(t, client.Get(ctx, nsn, &updated))
-		require.Empty(t, updated.Annotations)
-	})
+			var updated corev1.Secret
+			require.NoError(t, c.Get(ctx, types.NamespacedName{Namespace: "ns", Name: "test"}, &updated))
+			require.False(t, HasClientAuthenticationRequired(&updated))
+		})
+	}
 }
