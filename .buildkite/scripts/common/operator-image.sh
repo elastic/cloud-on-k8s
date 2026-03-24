e718d68cb0e3a3a7cb9d76fff67a9a1bdb7cffe9
Kostas Stamatakis <kostas.stamatakis@elastic.co>
2026-03-24 15:51:12 +0200

    restore images produced on pr

--- .buildkite/scripts/common/operator-image.sh
+++ .buildkite/scripts/common/operator-image.sh
@@ -63,7 +63,7 @@ operator::set_build_flavors_var() {
             merge-xyz)       BUILD_FLAVORS="eck,eck-dev,eck-fips,eck-ubi,eck-ubi-fips" ;;
             merge-main)      BUILD_FLAVORS="eck,eck-dev" ;;
             *-test-snapshot) BUILD_FLAVORS="eck,eck-dev" ;;
-            pr-*)            BUILD_FLAVORS="eck,eck-dev,eck-fips,eck-ubi,eck-ubi-fips" ;;
+            pr-*)            BUILD_FLAVORS="eck" ;;
             dev)             BUILD_FLAVORS="dev" ;;
             *)               echo "error: trigger '$trigger' not supported"; exit ;;
         esac
