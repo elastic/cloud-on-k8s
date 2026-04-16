fc69f50c83d0049ff81a433d5d1f2207d8e4db66
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2026-04-16 10:31:12 +0300

    tolerate brief red health during client auth mutation tests

--- test/e2e/apm/client_auth_test.go
+++ test/e2e/apm/client_auth_test.go
@@ -35,7 +35,8 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 
 	esBuilder := elasticsearch.NewBuilder(name).
 		WithESMasterDataNodes(3, elasticsearch.DefaultResources).
-		WithClientAuthenticationRequired()
+		WithClientAuthenticationRequired().
+		TolerateMutationChecksFailures()
 
 	apmBuilder := apmserver.NewBuilder(name).
 		WithElasticsearchRef(esBuilder.Ref()).
@@ -100,7 +101,8 @@ func TestClientAuthRequiredCustomCertificate(t *testing.T) {
 
 	esBuilder := elasticsearch.NewBuilder(name).
 		WithESMasterDataNodes(3, elasticsearch.DefaultResources).
-		WithClientAuthenticationRequired()
+		WithClientAuthenticationRequired().
+		TolerateMutationChecksFailures()
 
 	apmBuilder := apmserver.NewBuilder(name).
 		WithElasticsearchRef(commonv1.ObjectSelector{
