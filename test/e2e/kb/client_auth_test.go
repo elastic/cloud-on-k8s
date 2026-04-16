0c5a7f122a82988593b101922467d95be41d84fa
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2026-04-16 12:08:49 +0300

    tolerate brief red health during client auth mutation tests (#9355)

--- test/e2e/kb/client_auth_test.go
+++ test/e2e/kb/client_auth_test.go
@@ -40,7 +40,8 @@ func TestClientAuthRequiredTransition(t *testing.T) {
 
 	esBuilder := elasticsearch.NewBuilder(name).
 		WithESMasterDataNodes(3, elasticsearch.DefaultResources).
-		WithClientAuthenticationRequired()
+		WithClientAuthenticationRequired().
+		TolerateMutationChecksFailures()
 
 	kbBuilder := kibana.NewBuilder(name).
 		WithElasticsearchRef(esBuilder.Ref()).
